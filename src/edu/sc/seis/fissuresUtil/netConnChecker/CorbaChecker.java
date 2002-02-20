072a6e714511499118cc1853257b9b0ebfa3caaa
Srinivasa Telukutla <telukutl@seis.sc.edu>
2002-02-20 13:42:53 -0500

    [svn r1226] *** empty log message ***

--- src/edu/sc/seis/fissuresUtil/netConnChecker/CorbaChecker.java
+++ src/edu/sc/seis/fissuresUtil/netConnChecker/CorbaChecker.java
@@ -56,27 +56,31 @@ public class CorbaChecker implements ConnChecker  {
             if((conncheckerobject.getrootObj())._non_existent() == true){
 	       logger.warn("No connection to Corba.");
                conncheckerobject.setSuccessful(false);
+	       conncheckerobject.checker.fireStatusChanged(conncheckerobject.getName(), ConnStatus.FAILED);
 	    }else {
                conncheckerobject.setSuccessful(true);
+	       conncheckerobject.checker.fireStatusChanged(conncheckerobject.getName(), ConnStatus.SUCCESSFUL);
+	       
 	    }
             long endtime = System.currentTimeMillis();
             long duration = endtime-begintime;
             conncheckerobject.setTime(duration);
             //logger.debug
 		System.out.println("Corba connection: "+ conncheckerobject.getName()+" "+conncheckerobject.getTime()+" milliseconds"); 
+		return;
          } catch(COMM_FAILURE cf){
 	     //logger.warn
+	     conncheckerobject.setSuccessful(false);
 	     System.out.println("!!!!! CORBA CONNECTION FAILURE.");
 	       conncheckerobject.checker.fireStatusChanged(conncheckerobject.getName(), ConnStatus.FAILED);
-               conncheckerobject.setSuccessful(false);
+             
 	 } catch(Exception e) {
-
+	     conncheckerobject.setSuccessful(false);
 	     e.printStackTrace();
+	       conncheckerobject.checker.fireStatusChanged(conncheckerobject.getName(), ConnStatus.FAILED);
 	 }
        conncheckerobject.setFinished(true);
-       conncheckerobject.checker.fireStatusChanged(conncheckerobject.getName(), ConnStatus.SUCCESSFUL);
-  
-     }// close run
+   }// close run
 
     CORBAConfig conncheckerobject;
     Checker checker;
