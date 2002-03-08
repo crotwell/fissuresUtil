806a5fb51d7acc00460b4b563a3dfc5869382b9b
Philip Crotwell <crotwell@seis.sc.edu>
2002-03-07 21:40:46 -0500

    [svn r1331] initial

---
+++ src/edu/sc/seis/fissuresUtil/cache/SeismogramLoadedListener.java
@@ -0,0 +1,25 @@
+package edu.sc.seis.fissuresUtil.cache;
+
+import edu.iris.Fissures.*;
+import edu.iris.Fissures.IfSeismogramDC.*;
+
+/**
+ * SeismogramLoadedListener.java
+ * Allows a backgroung seismogram loader to notify interested parties that the
+ * attributes and preferred origin have been added to the cache for the seismogram.
+ *
+ * Created: Fri Jan 25 12:22:38 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface SeismogramLoadedListener extends java.util.EventListener {
+
+    public void seismogramLoaded(RequestFilter request, 
+				 LocalSeismogram[] seismogram);
+
+    public void seismogramError(RequestFilter request, 
+				 FissuresException e);
+	    
+}// SeismogramLoadedListener
