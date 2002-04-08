50d4c3db3aa270ff8ce2fa4e08c10d33a8fab935
telukutl <telukutl@localhost>
2002-04-08 14:11:24 -0400

    [svn r1403] *** empty log message ***

---
+++ src/edu/sc/seis/fissuresUtil/cache/AbstractQueue.java
@@ -0,0 +1,10 @@
+package edu.sc.seis.fissuresUtil.cache;
+
+public abstract class AbstractQueue {
+	//data structure to hold the values.???
+	public AbstractQueue() {
+
+	}
+	public abstract void push(java.lang.Object obj);
+	public abstract java.lang.Object pop(); 
+};
