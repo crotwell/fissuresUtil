da09013e2a351db92fa67ef20741458ebb85a216
telukutl <telukutl@localhost>
2002-04-15 10:19:48 -0400

    [svn r1461] after having multiple WaveFormArmThreads

--- src/edu/sc/seis/fissuresUtil/cache/EventQueue.java
+++ src/edu/sc/seis/fissuresUtil/cache/EventQueue.java
@@ -8,43 +8,58 @@ import java.util.*;
 public class EventQueue extends AbstractQueue {
 
 
-	public EventQueue() {
-		super();
+    public EventQueue() {
+	super();
 		
-	}
+    }
 
-	public synchronized void push(java.lang.Object obj) {
+    public synchronized void push(java.lang.Object obj) {
 		
-	    while(list.size() == 4) {
-		try {
-		    wait();
-		} catch(InterruptedException ie) { }
-	    }
-	    list.add(0, obj);
-	    notifyAll();
+	while(list.size() == 24) {
+	    try {
+		System.out.println("Watiting in PUSH");
+		wait();
+	    } catch(InterruptedException ie) { }
 	}
+	list.add(0, obj);
+	notifyAll();
+    }
 
-	public synchronized java.lang.Object pop() {
-	    
-	    while(list.size() == 0) {
-		try {
-		    wait();
-		} catch(InterruptedException ie) { }
-
-	    }
-	    java.lang.Object obj = list.get(list.size()-1);
-	    list.remove(list.size() - 1);
-	    notifyAll();
-	    return obj;
+    public synchronized java.lang.Object pop() {
+	
+	while(list.size() == 0 && sourceAlive == true) {
+	    try {
+		System.out.println("Waiting in POP");
+		wait();
+	    } catch(InterruptedException ie) { }
 
 	}
+	if(list.size() == 0) return null;
+	java.lang.Object obj = list.get(list.size()-1);
+	list.remove(list.size() - 1);
+	notifyAll();
+	return obj;
 
-	public synchronized int getLength() {
+    }
+
+    public synchronized int getLength() {
 	
-	    return list.size();
+	return list.size();
 	
-	}
+    }
+
+    public synchronized void setSourceAlive(boolean value) {
+
+	this.sourceAlive = value;
+	notifyAll();
+    }
+    
+    public synchronized boolean getSourceAlive() {
+	return this.sourceAlive;
+    }
 	
+    private boolean sourceAlive = true;
+
     private List list = new LinkedList();
 	
 }
