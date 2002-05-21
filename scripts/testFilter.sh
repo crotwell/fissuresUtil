6662878449b0c7ae3eac0c538e69b50c2d669dc9
crotwell <crotwell@localhost>
2002-05-21 17:07:41 -0400

    [svn r1657] test script for filter

---
+++ scripts/testFilter.sh
@@ -0,0 +1,1 @@
+java -cp ../dist/lib/fissuresUtil.jar:/seis/data0/crotwell/fissures/dist/lib/FissuresIDL.jar:/seis/data0/crotwell/fissures/dist/lib/FissuresImpl.jar edu/sc/seis/fissuresUtil/freq/FilterTest $*
