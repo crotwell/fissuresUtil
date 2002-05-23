26b6380a33d660d560c5e083ddf970376dad58c2
crotwell <crotwell@localhost>
2002-05-23 17:08:58 -0400

    [svn r1679] initial

---
+++ src/edu/sc/seis/fissuresUtil/xml/XMLProperty.java
@@ -0,0 +1,45 @@
+package edu.sc.seis.fissuresUtil.xml;
+
+import edu.iris.Fissures.*;
+import edu.iris.Fissures.IfSeismogramDC.*;
+import edu.iris.Fissures.seismogramDC.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import org.apache.xpath.*;
+import org.apache.xpath.objects.*;
+import java.io.*;
+import java.net.*;
+
+public class XMLProperty {
+
+    public static Property parse(Element element) {
+	try {
+	    String name = xpath.eval(element, "name/text()").str();
+	    String value = xpath.eval(element, "value/text()").str();
+	    return new Property(name, value);
+	} catch (Exception e) {
+	    return null;
+	} // end of try-catch
+	
+    }
+
+    public static Element createElement(Document doc, 
+					Property prop, 
+					String tagName) {
+	Element element = doc.createElement(tagName);
+
+	Text textNode = doc.createTextNode(prop.name);
+	Element tempElement = doc.createElement("name");
+	tempElement.appendChild(textNode);
+	element.appendChild(tempElement);
+
+	textNode = doc.createTextNode(prop.value);
+	tempElement = doc.createElement("value");
+	tempElement.appendChild(textNode);
+	element.appendChild(tempElement);
+
+	return element;
+    }
+
+    private static CachedXPathAPI xpath = new CachedXPathAPI();
+}
