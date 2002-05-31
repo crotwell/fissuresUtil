209179b6ac26a614b3482e2f2ec95795fc768d74
Philip Crotwell <crotwell@seis.sc.edu>
2002-05-31 13:34:30 -0400

    [svn r1762] refactor methods to superclass

--- src/edu/sc/seis/fissuresUtil/display/NetInfoDisplay.java
+++ src/edu/sc/seis/fissuresUtil/display/NetInfoDisplay.java
@@ -22,19 +22,10 @@ import edu.iris.Fissures.display.*;
  * @version
  */
 
-public class NetInfoDisplay extends JPanel {
+public class NetInfoDisplay extends TextInfoDisplay {
     
     public NetInfoDisplay() {
-	setLayout(new BorderLayout());
-	//Create a text pane.
-        textPane = new JTextPane();
-        paneScrollPane = new JScrollPane(textPane);
-        paneScrollPane.setVerticalScrollBarPolicy(
-                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        //paneScrollPane.setPreferredSize(new Dimension(250, 155));
-        paneScrollPane.setMinimumSize(new Dimension(10, 10));
-	add(paneScrollPane, BorderLayout.CENTER);
-	initStylesForTextPane(textPane);
+	super();
     }
         
     public void displayNetwork(NetworkAttr attr) {
@@ -165,85 +156,10 @@ public class NetInfoDisplay extends JPanel {
         } 
     }
 
-    public void displayProblem(String problem) {
-	Document doc = textPane.getDocument();
-        try {
-	    doc.remove(0, doc.getLength());
-	    doc.insertString(doc.getLength(), 
-			     "Problem: ",
-			     textPane.getStyle("label"));
-	    doc.insertString(doc.getLength(), 
-			     problem+"\n",
-			     textPane.getStyle("problem"));
-            toTop();
-        } catch (BadLocationException ble) {
-            System.err.println("Couldn't insert message.");
-        }
-    }
 
 
-    protected void appendLabelValue(Document doc, String label, String value)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(), 
-			     label+": ",
-			     textPane.getStyle("label"));
-	    doc.insertString(doc.getLength(), 
-			     value+"\n",
-			     textPane.getStyle("value"));
-    }
-
-    protected void appendLine(Document doc, String value)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(), 
-			     value+"\n",
-			     textPane.getStyle("value"));
-    }
 
 
-    protected void appendHeader(Document doc, String value)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(), 
-			     value+"\n",
-			     textPane.getStyle("header"));
-    }
-
-    protected void toTop() {
-        int min = paneScrollPane.getVerticalScrollBar().getMinimum();
-        paneScrollPane.getVerticalScrollBar().setValue(min);
-        repaint();
-    }
-
-    protected void initStylesForTextPane(JTextPane textPane) {
-        //Initialize some styles.
-        Style def = StyleContext.getDefaultStyleContext().
-                                        getStyle(StyleContext.DEFAULT_STYLE);
-
-        Style header = textPane.addStyle("header", def);
-        StyleConstants.setFontFamily(header, "SansSerif");
-        StyleConstants.setFontSize(header, 18);
-        StyleConstants.setForeground(header, Color.green);
-
-        Style regular = textPane.addStyle("regular", def);
-        StyleConstants.setFontFamily(regular, "Serif");
-
-        Style s = textPane.addStyle("label", regular);
-
-        s = textPane.addStyle("value", regular);
-        StyleConstants.setBold(s, true);
-
-        s = textPane.addStyle("problem", regular);
-        StyleConstants.setBold(s, true);
-        StyleConstants.setForeground(s, Color.red);
-
-        Style mono = StyleContext.getDefaultStyleContext().
-            getStyle(StyleContext.DEFAULT_STYLE);
-        StyleConstants.setFontFamily(mono, "MonoSpaced");
-        mono = textPane.addStyle("mono", mono);
-
-    }
-
-    JTextPane textPane;
-    JScrollPane paneScrollPane;
     NetworkExplorer netExplorer;
     NetworkFinder netFinder;
 } // NetInfoDisplay
