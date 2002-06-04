579f691708303fba0c6094f15d56b3a86395b20c
telukutl <telukutl@localhost>
2002-06-04 16:28:36 -0400

    [svn r1789] after the initial test

--- src/edu/sc/seis/fissuresUtil/display/RTTimeRangeConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/RTTimeRangeConfig.java
@@ -6,6 +6,9 @@ import edu.iris.Fissures.IfSeismogramDC.LocalSeismogram;
 import edu.iris.Fissures.seismogramDC.*;
 
 import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
 /**
  * RTTimeRangeConfig.java
  *
@@ -18,8 +21,10 @@ import java.util.*;
 
  public class RTTimeRangeConfig extends AbstractTimeRangeConfig{
     public RTTimeRangeConfig (){
-	
-    }
+	super();
+	setDisplayInterval(width);
+	beginTime = new MicroSecondDate(0);
+   }
 
     /**
      * Takes the information from the passed seismogram and uses it along with the information already taken from other seismograms 
@@ -29,8 +34,13 @@ import java.util.*;
      * @return the time it will be displayed
      */
      public MicroSecondTimeRange getTimeRange(LocalSeismogram seis) {
-
-	 return null;
+	 /* MicroSecondDate curr = (MicroSecondDate)(seismos.get(seis));
+	 MicroSecondDate tempTime = curr.add(displayInterval);
+	 MicroSecondDate endTime = tempTime.add(displayInterval);
+	 beginTime = tempTime;*/
+	 MicroSecondDate endTime = beginTime.add(displayInterval);
+	 MicroSecondDate tempTime = beginTime;
+	 return new MicroSecondTimeRange(beginTime, endTime);
      }
 
     /**
@@ -40,7 +50,12 @@ import java.util.*;
      */
      public MicroSecondTimeRange getTimeRange() {
 
-	 return null;
+	 MicroSecondDate endTime = beginTime.add(displayInterval);
+	 MicroSecondDate tempTime = beginTime;
+	 //this.beginTime = endTime;
+	 System.out.println("The function getTimeRange is Called "+tempTime+" end Time is "+beginTime);
+ 
+	 return new MicroSecondTimeRange(tempTime, endTime);
      }
 
     
@@ -52,4 +67,37 @@ import java.util.*;
      public  void fireTimeRangeEvent(TimeSyncEvent e) {
 
      }
+
+     public void startTimer() {
+	 
+	 if (timer == null) {
+	     timer = 
+		new javax.swing.Timer(1000,
+			  new ActionListener() {
+			      public void actionPerformed(ActionEvent e) {
+				  width = new TimeInterval(speed, UnitImpl.SECOND);
+				  setBeginTime(beginTime.add(width));
+				  // updateTimeSyncListeners();
+			      }
+			  });//20000
+	     timer.setCoalesce(true); 
+	     timer.start();
+	 }
+     }
+
+     public void setSpeed(float speed) {
+
+	 this.speed = speed;
+     }
+
+     private float speed = 1;
+
+     /** Timers are used for realTime update of the Seismograms **/
+     protected javax.swing.Timer timer;
+
+     protected javax.swing.Timer reloadTimer;
+
+     protected TimeInterval width = new TimeInterval(1, UnitImpl.SECOND);
+
+           
 }// RTTimeRangeConfig
