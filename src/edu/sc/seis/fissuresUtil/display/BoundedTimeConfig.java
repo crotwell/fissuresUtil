a695b81ddf463f00c5dd6fb232268542dc405809
Charles Groves <groves@seis.sc.edu>
2002-06-05 11:22:10 -0400

    [svn r1798] using time interval now

--- src/edu/sc/seis/fissuresUtil/display/BoundedTimeConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/BoundedTimeConfig.java
@@ -7,7 +7,7 @@ import edu.iris.Fissures.seismogramDC.*;
 
 import java.util.*;
 /**
- * BoundedTimeConfig is a MicroSecondTimeRangeConfig implementation that allows absolute time ranges to be set for the display.  It only displays
+ * BoundedTimeConfig is a TimeRangeConfig implementation that allows absolute time ranges to be set for the display.  It only displays
  * whatever is inside of a user defined time range.  If none is specified, it displays the entire time range of the first seismogram
  * it receives.
  *
@@ -23,41 +23,32 @@ public class BoundedTimeConfig extends AbstractTimeRangeConfig{
     /** Merely returns the time range that has been set
      */
     public MicroSecondTimeRange getTimeRange(LocalSeismogram seis){
-	return new MicroSecondTimeRange(this.beginTime, this.endTime);
+	return new MicroSecondTimeRange(this.beginTime, this.beginTime.add(displayInterval));
     }
 
     public MicroSecondTimeRange getTimeRange(){
-	return new MicroSecondTimeRange(this.beginTime, this.endTime);
+	return new MicroSecondTimeRange(this.beginTime, this.beginTime.add(displayInterval));
     }
 
     public void addSeismogram(LocalSeismogram seis){
 	if(beginTime == null)
 	    this.beginTime = ((LocalSeismogramImpl)seis).getBeginTime();
-	if(endTime == null)
-	    this.endTime = ((LocalSeismogramImpl)seis).getEndTime();
+	if(displayInterval == null)
+	    this.displayInterval = new TimeInterval(((LocalSeismogramImpl)seis).getBeginTime(), ((LocalSeismogramImpl)seis).getEndTime());
 	seismos.put(seis, ((LocalSeismogramImpl)seis).getBeginTime());
 	this.updateTimeSyncListeners();
     }	
     
-    /** This method allows the time range for display to be set by the user
-     *
-     *
-     */
-    public MicroSecondTimeRange setTimeRange(MicroSecondDate newBeginTime, MicroSecondDate newEndTime){
-	this.beginTime = newBeginTime;
-	this.endTime = newEndTime;
-	return new MicroSecondTimeRange(beginTime, endTime);
-    }
-
     /**  When BoundedTimeConfig receives a TimeSyncEvent, it merely changes the time range by the percentages contained in the 
      *   TimeSyncEvent
      */
     public void fireTimeRangeEvent(TimeSyncEvent e){
 	double begin = e.getBegin();
 	double end = e.getEnd();
-	long intv = endTime.getMicroSecondTime() - beginTime.getMicroSecondTime();
-	endTime = new MicroSecondDate((long)(endTime.getMicroSecondTime() + intv*end));
+	double intv = displayInterval.getValue();
+	MicroSecondDate endTime = new MicroSecondDate((long)(beginTime.add(displayInterval).getMicroSecondTime() + intv*end));
 	beginTime = new MicroSecondDate((long)(beginTime.getMicroSecondTime() + intv*begin));
+	displayInterval = new TimeInterval(beginTime, endTime);
 	this.updateTimeSyncListeners();
     }
     
