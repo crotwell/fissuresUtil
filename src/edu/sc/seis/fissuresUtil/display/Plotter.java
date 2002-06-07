edac934b8b851fd78050e7f34e1d86d4a59a477f
groves <groves@localhost>
2002-06-07 13:06:41 -0400

    [svn r1826] forgot the interface.....

---
+++ src/edu/sc/seis/fissuresUtil/display/Plotter.java
@@ -0,0 +1,24 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import java.awt.Shape;
+import java.awt.Dimension;
+
+/**
+ * Plotters are objects to be put in the main display of a SeismogramDisplay.  
+ * 
+ * Created: Fri Jun  7 10:27:49 2002
+ *
+ * @author <a href="mailto:">Charlie Groves</a>
+ * @version 0.1
+ * @see BasicPlotter, SeismogramPlotter
+ */
+
+public interface Plotter {
+    /**
+     * This method takes the object to be drawn and returns a shape represetnting
+     * it
+     *
+     */
+    public Shape draw(Dimension size);
+    
+}// Plotter
