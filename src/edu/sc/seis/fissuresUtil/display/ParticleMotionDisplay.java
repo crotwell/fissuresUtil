26c065d83e0e154623773a14518200a1bfa07a46
telukutl <telukutl@localhost>
2002-06-20 12:57:33 -0400

    [svn r1921] initial version

---
+++ src/edu/sc/seis/fissuresUtil/display/ParticleMotionDisplay.java
@@ -0,0 +1,234 @@
+package edu.sc.seis.fissuresUtil.display;
+
+
+import edu.iris.Fissures.IfSeismogramDC.*;
+import edu.iris.Fissures.seismogramDC.*;
+import edu.iris.Fissures.model.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+/**
+ * ParticleMotionDisplay.java
+ *
+ *
+ * Created: Tue Jun 11 15:22:30 2002
+ *
+ * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public class ParticleMotionDisplay extends JLayeredPane implements AmpSyncListener {
+    /**
+     * Creates a new <code>ParticleMotionDisplay</code> instance.
+     *
+     * @param hSeis a <code>LocalSeismogramImpl</code> value
+     * @param vSeis a <code>LocalSeismogramImpl</code> value
+     * @param hAmpRangeConfig an <code>AmpRangeConfig</code> value
+     * @param vAmpRangeConfig an <code>AmpRangeConfig</code> value
+     */
+    public ParticleMotionDisplay (LocalSeismogramImpl hSeis,
+				  LocalSeismogramImpl vSeis,
+				  AmpRangeConfig hAmpRangeConfig,
+				  AmpRangeConfig vAmpRangeConfig){
+	
+	this.hAmpRangeConfig = hAmpRangeConfig;
+	this.vAmpRangeConfig = vAmpRangeConfig;
+	
+	this.setLayout(new OverlayLayout(this));
+	view = new ParticleMotionView(hSeis, vSeis, hAmpRangeConfig, vAmpRangeConfig, this);
+	add(view, PARTICLE_MOTION_LAYER);
+        hAmpScaleMap = new AmpScaleMapper(50,
+                                          4,
+					  hAmpRangeConfig.getAmpRange(hSeis));
+        vAmpScaleMap = new AmpScaleMapper(50,
+                                          4,
+					  vAmpRangeConfig.getAmpRange(vSeis));
+	scaleBorder = new ScaleBorder();
+	scaleBorder.setBottomScaleMapper(hAmpScaleMap);
+	scaleBorder.setLeftScaleMapper(vAmpScaleMap);        
+        hTitleBorder = 
+            new CenterTitleBorder(((LocalSeismogramImpl)hSeis).getName());
+        vTitleBorder = 
+            new CenterTitleBorder(((LocalSeismogramImpl)vSeis).getName());
+	setBorder(BorderFactory.createCompoundBorder(
+						     //	     BorderFactory.createCompoundBorder(
+			 BorderFactory.createRaisedBevelBorder(),
+			 //hTitleBorder),
+			 //BorderFactory.createCompoundBorder(hTitleBorder,
+			 //				    vTitleBorder)),
+	    		     BorderFactory.createCompoundBorder(
+								scaleBorder,
+								BorderFactory.createLoweredBevelBorder()))
+	  );
+
+	this.addComponentListener(new ComponentAdapter() {
+		public void componentResized(ComponentEvent e) {
+		    resolveParticleMotion();
+		    resize();
+		}
+		public void componentShown(ComponentEvent e) {
+		    resize();
+		}
+	    });
+   
+    }
+
+
+    public void resolveParticleMotion() {
+
+    }
+
+    public void updateAmpRange() {
+	this.hAmpScaleMap.setUnitRange(hAmpRangeConfig.getAmpRange());
+    }
+    
+    public void updateVerticalAmpRange() {
+	this.vAmpScaleMap.setUnitRange(vAmpRangeConfig.getAmpRange());
+    }
+    public void updateHorizontalAmpScale(UnitRangeImpl r) {
+	//System.out.println("The amplitudeRange is being updated ");
+	//System.out.println("The minimum value of the updated value is "+r.getMinValue());
+	//System.out.println("The maximum value of the updated vlue is "+r.getMaxValue());
+	this.hAmpScaleMap.setUnitRange(r);
+	resize();
+    }
+
+    public void updateVerticalAmpScale(UnitRangeImpl r) {
+
+	this.vAmpScaleMap.setUnitRange(r);
+	resize();
+    }
+
+    /**
+     * Describe <code>resize</code> method here.
+     *
+     */
+    public void resize() {
+	Dimension dim = view.getSize();
+	Insets insets =	view.getInsets();
+	hAmpScaleMap.setTotalPixels(dim.width  - insets.left - insets.right);
+        vAmpScaleMap.setTotalPixels(dim.height  - insets.top - insets.bottom);
+        repaint();
+    }
+    
+
+
+    public void addParticleMotionDisplay(LocalSeismogramImpl hseis,
+					 LocalSeismogramImpl vseis,
+					 AmpRangeConfig hAmpRangeConfig,
+					 AmpRangeConfig vAmpRangeConfig) {
+	view.addParticleMotionDisplay(hseis,
+				      vseis,
+				      hAmpRangeConfig,
+				      vAmpRangeConfig);
+    }
+    
+    
+
+    /**
+     * sets the AmplitudeRange of the ParticleMotionDisplay.
+     *
+     * @param amplitudeRange an <code>AmpRangeConfig</code> value
+     */
+    public void setAmplitudeRange(AmpRangeConfig amplitudeRange) {
+	
+	this.hAmpRangeConfig = amplitudeRange;
+	this.vAmpRangeConfig = amplitudeRange;
+    }
+
+    public void addAzimuthLine(double degrees) {
+
+	view.addAzimuthLine(degrees);
+    }
+
+    public void addSector(double degreeone, double degreetwo) {
+
+	view.addSector(degreeone, degreetwo);
+    }
+
+    /**
+     * Describe <code>main</code> method here.
+     *
+     * @param args a <code>String[]</code> value
+     */
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Test Particle Motion View");
+	JPanel displayPanel = new JPanel();
+        //        Seismogram hSeis = SeisPlotUtil.createTestData();
+        //        Seismogram vSeis = SeisPlotUtil.createTestData();
+        LocalSeismogramImpl hSeis = 
+            (LocalSeismogramImpl)SeisPlotUtil.createSineWave(0, .4, 200, -1000);
+        LocalSeismogramImpl vSeis = 
+            (LocalSeismogramImpl)SeisPlotUtil.createSineWave(Math.PI/2, .8, 200, 1000);	
+	LocalSeismogramImpl hSeisex = 
+            (LocalSeismogramImpl)SeisPlotUtil.createSineWave(30, .6, 100, -400);
+        LocalSeismogramImpl vSeisex = 
+            (LocalSeismogramImpl)SeisPlotUtil.createSineWave(-265, .1, 100, 400);
+	
+	RMeanAmpConfig vAmpRangeConfig = new RMeanAmpConfig();
+       
+        ParticleMotionDisplay sv = new ParticleMotionDisplay(hSeis, vSeis,
+							  vAmpRangeConfig, 
+							  vAmpRangeConfig);
+        java.awt.Dimension size = new java.awt.Dimension(400, 400);
+        sv.setPreferredSize(size);
+
+	/*ParticleMotionDisplay svex = new ParticleMotionDisplay(hSeisex, vSeisex,
+							  vAmpRangeConfig, 
+							  vAmpRangeConfig);*/
+	sv.addParticleMotionDisplay(hSeisex, vSeisex,
+				    vAmpRangeConfig,
+				    vAmpRangeConfig);
+	//System.out.println("The min amp of second before "+
+	//	vAmpRangeConfig.getAmpRange(vSeisex).getMinValue());
+	//System.out.println("The max amp of second before "
+	///+vAmpRangeConfig.getAmpRange(vSeisex).getMaxValue());
+	//sv.addAzimuthLine(30);
+	//sv.addAzimuthLine(60);
+	//sv.addAzimuthLine(90);
+	sv.addAzimuthLine(15);
+	//sv.addAzimuthLine(0);
+	sv.addSector(10, 20);
+	//	sv.addSector(-30, -60);
+	//sv.addAzimuthLine(-40);
+	//	sv.addAzimuthLine(80);
+	displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(sv, java.awt.BorderLayout.CENTER);
+
+	displayPanel.setSize(size);
+	jf.getContentPane().add(displayPanel);
+        jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jf.setSize(size);
+        jf.setVisible(true);
+        jf.addWindowListener(new java.awt.event.WindowAdapter() {
+                public void windowClosed(java.awt.event.WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+
+    }
+    
+
+    /**
+     * Describe constant <code>PARTICLE_MOTION_LAYER</code> here.
+     *
+     */
+    public static final Integer PARTICLE_MOTION_LAYER = new Integer(2);
+
+    protected AmpScaleMapper hAmpScaleMap, vAmpScaleMap;
+
+    protected ScaleBorder scaleBorder;
+
+    protected CenterTitleBorder hTitleBorder, vTitleBorder;
+    
+    protected ParticleMotionView view;
+
+
+    private AmpRangeConfig hAmpRangeConfig, vAmpRangeConfig;
+    
+    
+}// ParticleMotionDisplay
---
+++ src/edu/sc/seis/fissuresUtil/display/ParticleMotionView.java
@@ -0,0 +1,560 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import edu.iris.Fissures.IfSeismogramDC.*;
+import edu.iris.Fissures.seismogramDC.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+
+import java.awt.geom.*;
+import java.awt.event.*;
+import java.util.*;
+import edu.iris.Fissures.model.*;
+import edu.iris.Fissures.display.*;
+
+/**
+ * ParticleMotionView.java
+ *
+ *
+ * Created: Tue Jun 11 15:14:17 2002
+ *
+ * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public class ParticleMotionView extends JComponent{
+    public ParticleMotionView (final LocalSeismogramImpl hseis, 
+			       LocalSeismogramImpl vseis,
+			       final AmpRangeConfig hAmpRangeConfig,
+			       AmpRangeConfig vAmpRangeConfig, ParticleMotionDisplay particleMotionDisplay){
+	
+	this.particleMotionDisplay = particleMotionDisplay;
+
+	ParticleMotion particleMotion = new ParticleMotion(hseis,
+							   vseis,
+							   hAmpRangeConfig,
+							   vAmpRangeConfig);
+	displays.add(particleMotion);
+	    
+	vunitRangeImpl = vAmpRangeConfig.getAmpRange(vseis);
+	hunitRangeImpl = hAmpRangeConfig.getAmpRange(hseis);
+	
+	this.addMouseListener(new MouseAdapter() {
+
+		public void mouseClicked(MouseEvent me) {
+		    //UnitRangeImpl unitRangeImpl = hAmpRangeConfig.getAmpRange(hseis);
+		    //double min = unitRangeImpl.getMinValue();
+		    //double max = unitRangeImpl.getMaxValue();
+		    ///Insets insets = getInsets();
+		    //double  fmin = getSize().getWidth() - insets.left - insets.right;
+		    //double fmax = getSize().getHeight() - insets.top - insets.bottom;
+		    //int newx = (int)(((max - min) / fmin * me.getX()) - max);
+		    //(int)(max - (((max - min) * (fmin - me.getX()) )/ (fmax - me.getY())));
+		    //int newy =  (int)(((min - max) / fmax * me.getY()) + max);
+		    if(me.getClickCount() == 1) 
+		    zoomInParticleMotionDisplay(1, me.getX(), me.getY());
+		    //System.out.println("me x "+me.getX()+" me y "+me.getY());
+		    //findPoint(me.getClickCount(), me.getX(), me.getY());
+		    startPoint = null;
+		    endPoint = null;
+		}
+
+		public void mousePressed(MouseEvent me) {
+		    startPoint = new java.awt.geom.Point2D.Float((float)me.getX(), (float)me.getY());
+    		}
+		
+		public void mouseReleased(MouseEvent me) {
+		    // endPoint = new java.awt.geom.Point2D.Float(me.getX(), me.getY());
+		    
+		    // zoomInParticleMotionDisplay(me.getX(), me.getY());
+		  
+		    
+		}
+	
+	    });
+	
+
+	this.addMouseMotionListener(new MouseMotionAdapter() {
+		
+		public void mouseDragged(MouseEvent me) {
+		    endPoint = new java.awt.geom.Point2D.Float((float)me.getX(), (float)me.getY());
+		    repaint();
+		}
+	    });
+	
+				    
+    }
+
+    public void zoomInParticleMotionDisplay(int clickCount, int mx, int my) {
+	
+	double hmin = hunitRangeImpl.getMinValue();
+	double hmax = hunitRangeImpl.getMaxValue();
+	double vmin = vunitRangeImpl.getMinValue();
+	double vmax = vunitRangeImpl.getMaxValue();
+	if(hmin > hmax) { double temp = hmax; hmax = hmin; hmin = temp;}
+	if(vmin > vmax) { double temp = vmax; vmax = vmin; vmin = temp;}
+	Insets insets = getInsets();
+	double width = getSize().getWidth() - insets.left - insets.right;
+	double height = getSize().getHeight() - insets.top - insets.bottom;
+	
+	int centerx, centery;
+	if(clickCount == 1) {
+	    centerx = (int)((hmax - hmin) / 4);
+	    centery = (int)((vmax - vmin) / 4);
+	} else {
+	    centerx = (int)((hmax - hmin) / 4);
+	    centery = (int)((hmax - hmin) /4);
+	}
+	int xone = (int)(((hmax - hmin)/width * mx) + hmin);
+	int yone = (int)(((vmin - vmax)/height * my) + vmax);
+	System.out.println("----------------------------------------------------------");
+	System.out.println("clickCount = "+clickCount);
+	System.out.println(" hmin = "+hmin+" hmax = "+hmax);
+	System.out.println(" vmin = "+vmin+" vmax = "+vmax);
+	System.out.println(" xone = "+xone+" yone = "+yone);
+	System.out.println(" bcenterx = "+centerx+" bcentery = "+centery);	
+	if(clickCount == 1) {
+	    if(xone < 0) centerx = -centerx;
+	    if(yone < 0) centery = -centery;
+	} else {
+	    if(hmin < 0 ) centerx = -centerx;
+	    if(vmin < 0) centery = -centery;
+	}
+	int xa, xs, ya, ys;
+	if(clickCount == 1) {
+	    xa = xone - centerx;
+	    xs = xone + centerx;
+	    ya = yone - centery;
+	    ys = yone + centery;
+	} else {
+	 
+	    xa = (int)hmin - centerx;
+	    xs = (int)hmax + centerx;
+	    ya = (int)vmin - centery;
+	    ys = (int)vmax + centery;
+	    
+	}
+	if(xa > xs) { int temp = xs; xs = xa; xa = temp;}
+	if(ya > ys) {int temp = ys; ys = ya; ya = temp;}
+	System.out.println(" acenterx = "+centerx+" acentery = "+centery);
+	System.out.println(" xa = "+xa+" xs = "+xs);	
+	System.out.println(" ya = "+ya+" ys = "+ys);
+	//int xtwo = (int)(((hmax - hmin)/width * endPoint.getX()) - hmax);
+	//int ytwo = (int)(((vmin - vmax)/height * endPoint.getY()) + vmax);
+	startPoint = null;
+	endPoint = null;
+	if(xa < xs && ya < ys) {
+	    particleMotionDisplay.updateHorizontalAmpScale(new UnitRangeImpl(xa, xs, UnitImpl.COUNT));
+	    particleMotionDisplay.updateVerticalAmpScale(new UnitRangeImpl(ya, ys, UnitImpl.COUNT));
+	    vunitRangeImpl = new UnitRangeImpl(ya, ys, UnitImpl.COUNT);
+	    hunitRangeImpl = new UnitRangeImpl(xa, xs, UnitImpl.COUNT);
+	}
+    }
+  
+    public boolean findPoint(int count, int newx, int newy) {
+	
+	 
+	  boolean rtn = false;
+	  
+	  int size = displays.size();
+	  for(int counter = 0; counter < size; counter++) {
+	      ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	      Shape shape = particleMotion.getShape();
+	      //for(int i = -4; i < 4; i++) {
+	      //	  for(int j = -4; j < 4; j++) {
+		      if(shape.contains(newx, newy)){  
+			  particleMotion.setSelected(true);rtn = true;
+			  // particleMotionDisplay.updateAmpScale(particleMotion.hAmpRangeConfig.getAmpRange(particleMotion.hseis));
+		      }
+		      // }
+		      //}
+	  }
+	 
+	  //System.out.println("The return value is "+rtn);
+	  if(rtn == true) {repaint();}
+	  return rtn;
+			
+    }
+
+    
+  
+    /*public void update(Graphics g) {
+	paintComponent(g);
+	}*/
+
+    public void paintComponent(Graphics g) {
+
+	//	if(setSelected == 1) g.setColor(Color.red);
+	//else if(setSelected == 2) g.setColor(getBackground());
+	Graphics2D graphics2D = (Graphics2D)g;
+
+   
+	if(startPoint != null && endPoint != null) {
+	    graphics2D.setColor(Color.yellow);
+	    //System.out.println("Start Point "+startPoint.getX()+"  "+startPoint.getY());
+	    //System.out.println("End Point "+endPoint.getX()+"  "+endPoint.getY());
+	    java.awt.geom.Rectangle2D.Double rect = new java.awt.geom.Rectangle2D.Double(startPoint.getX(),
+											 startPoint.getY(),
+											 endPoint.getX() - startPoint.getX(),
+											 endPoint.getY() - startPoint.getY());
+	    graphics2D.fill(rect);
+	    graphics2D.draw(rect);
+	}
+	Dimension dimension = getSize();
+	int size = displays.size();
+	Shape sector = getSectorShape();
+	graphics2D.setColor(Color.blue);
+	graphics2D.fill(sector);
+	graphics2D.draw(sector);
+	graphics2D.setStroke(new BasicStroke(2.0f));
+	graphics2D.setColor(Color.green);
+	Shape azimuth = getAzimuthPath();
+	graphics2D.draw(azimuth);
+	graphics2D.setStroke(new BasicStroke(1.0f));
+	for(int counter = 0; counter < displays.size(); counter++) {
+	    ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	    LocalSeismogramImpl hseis = particleMotion.hseis;
+	    LocalSeismogramImpl vseis = particleMotion.vseis;
+	    AmpRangeConfig vAmpRangeConfig = particleMotion.vAmpRangeConfig;
+	    AmpRangeConfig hAmpRangeConfig = particleMotion.hAmpRangeConfig;
+	    
+	    Color color = particleMotion.getColor();
+	    if(color == null) {
+
+		color = COLORS[RGBCOLOR];
+		particleMotion.setColor(color);
+		RGBCOLOR++;
+		if(RGBCOLOR == COLORS.length) RGBCOLOR = 0;
+	    }
+	    graphics2D.setColor(color);
+
+					       
+	    Dimension flipDimension = new Dimension(dimension.height,
+						    dimension.width);
+	    
+	    try {
+
+		
+		int[][] hPixels = SimplePlotUtil.compressYvalues(hseis, 
+								 new MicroSecondTimeRange(new MicroSecondDate(hseis.getBeginTime()),
+											  new MicroSecondDate(hseis.getEndTime())),
+								 hunitRangeImpl,							
+								 flipDimension);
+
+
+		SimplePlotUtil.scaleYvalues(hPixels,
+					    hseis, 
+					    new MicroSecondTimeRange(new MicroSecondDate(hseis.getBeginTime()),
+								     new MicroSecondDate(hseis.getEndTime())),
+					    hunitRangeImpl,							
+					    flipDimension);
+		int[][] vPixels = SimplePlotUtil.compressYvalues(vseis, 
+								 new MicroSecondTimeRange(new MicroSecondDate(vseis.getBeginTime()),
+											  new MicroSecondDate(vseis.getEndTime())),
+								 vunitRangeImpl,			
+								 dimension);
+
+		SimplePlotUtil.scaleYvalues(vPixels,
+					    vseis, 
+					    new MicroSecondTimeRange(new MicroSecondDate(vseis.getBeginTime()),
+								     new MicroSecondDate(vseis.getEndTime())),
+					    vunitRangeImpl,			
+					    dimension);
+
+		int len = vPixels[1].length;
+		int[] x, y;
+		x = new int[hPixels[1].length];
+		y = new int[vPixels[1].length];
+		x = hPixels[1];
+		y = vPixels[1];
+		if (hPixels[1].length < len) { len = hPixels.length; }
+		
+		
+	
+		
+		Shape shape = getParticleMotionPath(hPixels[1], vPixels[1]);
+		particleMotion.setShape(shape);
+		graphics2D.draw(shape);
+		Composite c = AlphaComposite.getInstance(AlphaComposite.SRC_OUT);
+		//graphics2D.setComposite(c);
+			
+	    } catch(Exception e) {
+		e.printStackTrace();
+	    }
+	    
+	 
+	}//end of for
+	
+    }
+    
+    public Shape getParticleMotionPath(int[] x, int[] y) {
+	int len = x.length;
+	if(y.length < len) { len = y.length;}
+	GeneralPath generalPath = new GeneralPath(GeneralPath.WIND_EVEN_ODD); 
+	if(len != 0) {
+		generalPath.moveTo(x[0], y[0]);
+	}
+	for(int counter = 1; counter < len; counter++) {
+	    generalPath.lineTo(x[counter], y[counter]);
+	    
+	}
+	for(int counter = 0; counter < len; counter++) {
+	    generalPath.append(new Rectangle2D.Float(x[counter]-2, y[counter]-2, 4, 4), false);
+	}
+
+	return (Shape)generalPath;
+    }
+    
+    public Shape getAzimuthPath() {
+
+	//System.out.println("*******************************************************");
+	int size = azimuths.size();
+	ParticleMotion particleMotion = (ParticleMotion)displays.get(0);
+	AmpRangeConfig ampRangeConfig = particleMotion.vAmpRangeConfig;
+	UnitRangeImpl unitRangeImpl = vunitRangeImpl;//ampRangeConfig.getAmpRange(particleMotion.vseis);
+	double vmin = unitRangeImpl.getMinValue();
+	double vmax = unitRangeImpl.getMaxValue();
+	unitRangeImpl =  hunitRangeImpl;//particleMotion.hAmpRangeConfig.getAmpRange(particleMotion.hseis);
+	double hmin = unitRangeImpl.getMinValue();
+	double hmax = unitRangeImpl.getMaxValue();
+	
+	Insets insets = getInsets();
+       
+	double  fmin = getSize().getWidth() - insets.left - insets.right;
+	double fmax = getSize().getHeight() - insets.top - insets.bottom;
+       	int newx = (int)(((0-hmin) * fmin)/ (hmax - hmin));
+	    //(int)(((max - min) / fmin * me.getX()) - max);
+	//(int)(max - (((max - min) * (fmin - me.getX()) )/ (fmax - me.getY())));
+	int newy =  (int)(fmax - (((0-vmin)*fmax)/(vmax - vmin)));
+	    //(int)(((min - max) / fmax * me.getY()) + max);
+	//System.out.println(" min= "+hmin+" max= "+hmax+" fmin= "+fmin+" fmax= "+fmax);
+	//System.out.println("newx = "+newx+" newy = "+newy);
+	GeneralPath generalPath = new GeneralPath();
+	for(int counter = 0; counter < size; counter++) {
+	    
+	    double degrees = ((Double)azimuths.get(counter)).doubleValue();
+	    degrees = degrees;
+	    //System.out.println("The degrees are "+degrees);
+ 	    int x = (int)(fmin * Math.cos(Math.toRadians(degrees)));
+	    int y = (int)(fmax * Math.sin(Math.toRadians(degrees)));
+
+	    //System.out.println("x = "+x+" y= "+y);
+	    generalPath.moveTo(newx+x, newy-y);
+	    generalPath.lineTo(newx-x, newy+y);
+	    //System.out.println("resulttwox = "+(newx-x)+" resulttwoy = "+(newy+y));
+	    //System.out.println("resultonex = "+(newx+x)+" resultoney = "+(newy-y));
+	      
+	}
+	//System.out.println("-----------------------------------------------------------");
+	return (Shape)generalPath;
+    }
+    public Shape getSectorShape() {
+
+	ParticleMotion particleMotion = (ParticleMotion)displays.get(0);
+	AmpRangeConfig ampRangeConfig = particleMotion.vAmpRangeConfig;
+	UnitRangeImpl unitRangeImpl = vunitRangeImpl;//ampRangeConfig.getAmpRange(particleMotion.vseis);
+	double vmin = unitRangeImpl.getMinValue();
+	double vmax = unitRangeImpl.getMaxValue();
+	unitRangeImpl =  hunitRangeImpl;//particleMotion.hAmpRangeConfig.getAmpRange(particleMotion.hseis);
+	double hmin = hunitRangeImpl.getMinValue();
+	double hmax = hunitRangeImpl.getMaxValue();
+
+	Insets insets = getInsets();
+	double  fmin = getSize().getWidth() - insets.left - insets.right;
+	double fmax = getSize().getHeight() - insets.top - insets.bottom;
+	int newx = (int)(((0-hmin) * fmin)/ (hmax - hmin));//(int)((max * fmin)/ (max - min));
+	int newy = (int)(fmax - (int)(((0-vmin)*fmax)/(vmax - vmin)));
+
+	GeneralPath generalPath = new GeneralPath();
+
+	int size = sectors.size();
+				  
+	for(int counter = 0; counter < size; counter++) {
+
+	    Point2D.Double point = (Point2D.Double)sectors.get(counter);
+	    double degreeone = point.getX();
+	    double degreetwo = point.getY();
+	    
+	    int xone = (int)(fmin * Math.cos(Math.toRadians(degreeone)));
+	    int yone = (int)(fmax * Math.sin(Math.toRadians(degreeone)));
+	    generalPath.moveTo(newx+xone, newy-yone);
+	    generalPath.lineTo(newx-xone, newy+yone);
+
+	    int xtwo = (int)(fmin * Math.cos(Math.toRadians(degreetwo)));
+	    int ytwo = (int)(fmax * Math.sin(Math.toRadians(degreetwo)));
+	    generalPath.lineTo(newx-xtwo, newy+ytwo);
+	    generalPath.lineTo(newx+xtwo, newy-ytwo);
+	    generalPath.lineTo(newx+xone, newy-yone);
+	    
+	}
+	return (Shape)generalPath;
+    }
+    
+    public void addParticleMotionDisplay(LocalSeismogramImpl hseis,
+					 LocalSeismogramImpl vseis,
+					 AmpRangeConfig hAmpRangeConfig,
+					 AmpRangeConfig vAmpRangeConfig) {
+	ParticleMotion particleMotion = new ParticleMotion(hseis,
+							   vseis,
+							   hAmpRangeConfig,
+							   vAmpRangeConfig);
+	displays.add(particleMotion);
+
+	hunitRangeImpl = new UnitRangeImpl(getMinHorizontalAmplitude(),
+					   getMaxHorizontalAmplitude(),
+					   UnitImpl.COUNT);
+	vunitRangeImpl = new UnitRangeImpl(getMinVerticalAmplitude(),
+					   getMaxVerticalAmplitude(),
+					   UnitImpl.COUNT);
+	particleMotionDisplay.updateHorizontalAmpScale(hunitRangeImpl);
+	particleMotionDisplay.updateVerticalAmpScale(vunitRangeImpl);
+					   
+    }
+
+    
+    public void addSector(double degreeone, double degreetwo) {
+
+	sectors.add(new java.awt.geom.Point2D.Double(degreeone, degreetwo));
+	
+    }
+
+    public double getMinHorizontalAmplitude() {
+	
+	int size = displays.size();
+	double min = Double.POSITIVE_INFINITY;
+	for(int counter = 0; counter < size; counter++) {
+
+	    ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	    AmpRangeConfig ampRangeConfig = particleMotion.hAmpRangeConfig;
+	    UnitRangeImpl unitRangeImpl = ampRangeConfig.getAmpRange(particleMotion.hseis);
+	    if(min > unitRangeImpl.getMinValue()) { min = unitRangeImpl.getMinValue();}
+	}
+	return min;
+    }
+
+    public double getMaxHorizontalAmplitude() {
+	
+	int size = displays.size();
+	double max = Double.NEGATIVE_INFINITY;
+	for(int counter = 0; counter < size; counter++) {
+
+	    ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	    AmpRangeConfig ampRangeConfig = particleMotion.hAmpRangeConfig;
+	    UnitRangeImpl unitRangeImpl = ampRangeConfig.getAmpRange(particleMotion.hseis);
+	    if(max < unitRangeImpl.getMaxValue()) { max = unitRangeImpl.getMaxValue();}
+	}
+	return max;
+    }
+
+    public double getMinVerticalAmplitude() {
+	
+	int size = displays.size();
+	double min = Double.POSITIVE_INFINITY;
+	for(int counter = 0; counter < size; counter++) {
+	    
+	    ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	    AmpRangeConfig ampRangeConfig = particleMotion.vAmpRangeConfig;
+	    UnitRangeImpl unitRangeImpl = ampRangeConfig.getAmpRange(particleMotion.vseis);
+	    if(min > unitRangeImpl.getMinValue()) { min = unitRangeImpl.getMinValue();}
+	}
+	return min;
+    }
+    
+    public double getMaxVerticalAmplitude() {
+	
+	int size = displays.size();
+	double max = Double.NEGATIVE_INFINITY;
+	for(int counter = 0; counter < size; counter++) {
+
+	    ParticleMotion particleMotion = (ParticleMotion)displays.get(counter);
+	    AmpRangeConfig ampRangeConfig = particleMotion.vAmpRangeConfig;
+	    UnitRangeImpl unitRangeImpl = ampRangeConfig.getAmpRange(particleMotion.vseis);
+	    if(max < unitRangeImpl.getMaxValue()) { max = unitRangeImpl.getMaxValue();}
+	}
+	return max;
+    }
+    
+
+    public void addAzimuthLine(double degrees) {
+
+	azimuths.add(new Double(degrees));
+    }
+
+    /** must be square */
+    public void setSize(Dimension d) {
+        if (d.width < d.height) {
+            super.setSize(new Dimension(d.width, d.width));
+        } else {
+            super.setSize(new Dimension(d.height, d.height));
+        }
+    }
+    
+    LinkedList displays = new LinkedList();
+    LinkedList azimuths = new LinkedList();
+    LinkedList sectors = new LinkedList();
+    UnitRangeImpl hunitRangeImpl = null;
+    UnitRangeImpl vunitRangeImpl = null;
+    java.awt.geom.Point2D.Float startPoint;
+    java.awt.geom.Point2D.Float endPoint;
+
+    private static int RGBCOLOR = 0;
+    private ParticleMotionDisplay particleMotionDisplay; 
+
+    private static final Color[]  COLORS = { Color.red,
+					   Color.orange,
+					   Color.pink,
+					   Color.yellow,
+					   Color.green,
+					   Color.magenta,
+					   Color.cyan,
+					   Color.blue,
+					   Color.white,
+					   Color.black};
+
+    class ParticleMotion{
+	public ParticleMotion(final LocalSeismogramImpl hseis, 
+			       LocalSeismogramImpl vseis,
+			       final AmpRangeConfig hAmpRangeConfig,
+			       AmpRangeConfig vAmpRangeConfig) {
+
+	    this.hseis = hseis;
+	    this.vseis = vseis;
+	    this.hAmpRangeConfig = hAmpRangeConfig;
+	    this.vAmpRangeConfig = vAmpRangeConfig;
+	}
+
+	public void setShape(Shape shape) {
+	    this.shape = shape;
+	}
+
+	public Shape getShape() {
+	    return shape;  
+	} 
+	public void setColor(Color color) {
+	      this.color = color;
+	}
+
+	public Color getColor() {
+	    if(selected) return Color.cyan;
+	    return this.color;
+	}
+
+	public boolean isSelected() {
+	    return this.selected;
+	}
+
+	public void setSelected(boolean value) {
+
+	    this.selected = value;
+	}
+
+	public LocalSeismogramImpl hseis;
+	public LocalSeismogramImpl vseis;
+	public AmpRangeConfig hAmpRangeConfig;
+	public AmpRangeConfig vAmpRangeConfig;
+	private Shape shape;
+	private Color color = null;
+	private boolean selected = false;
+    }
+
+}// ParticleMotionView
