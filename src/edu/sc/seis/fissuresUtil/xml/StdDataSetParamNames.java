59794bd3b2c7483bffe616667b2014f5363da4cd
Philip Crotwell <crotwell@seis.sc.edu>
2002-07-09 16:20:19 -0400

    [svn r2090] initial

---
+++ src/edu/sc/seis/fissuresUtil/xml/StdDataSetParamNames.java
@@ -0,0 +1,21 @@
+package edu.sc.seis.fissuresUtil.xml;
+
+/**
+ * StdDataSetParamNames.java
+ *
+ *
+ * Created: Tue Jul  9 15:55:09 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface StdDataSetParamNames {
+
+    public static final String prefix = "edu.sc.seis.fissuresUtil.xml#";
+
+    public static final String EVENT = prefix+"EVENT";
+
+    public static final String CHANNEL = prefix+"CHANNEL_";
+    
+}// StdDataSetParamNames
