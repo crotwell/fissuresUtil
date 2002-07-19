177e7149ba10b56ee8d10b15228fd9f3484f189d
Srinivasa Telukutla <telukutl@seis.sc.edu>
2002-07-18 22:07:53 -0400

    [svn r2310] copied from the cee/parmo3d and made minor changes so that every thing compiles fine

---
+++ src/edu/sc/seis/fissuresUtil/parmo3d/tjoInterpolator.java
@@ -0,0 +1,31 @@
+package edu.sc.seis.fissuresUtil.parmo3d;
+
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.*;
+import java.awt.Font;
+import com.sun.j3d.utils.applet.MainFrame; 
+// import com.sun.j3d.utils.geometry.Text2D;
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.geometry.Box;
+import com.sun.j3d.utils.universe.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import java.util.Enumeration;
+
+public class tjoInterpolator extends PositionPathInterpolator {
+
+    public tjoInterpolator(Alpha alpha, TransformGroup target, Transform3D axisOfTranslation, 
+                           float[] knots, Point3f[] positions) {
+
+           super(alpha, target, axisOfTranslation, knots, positions);
+
+    }
+    public void processStimulus(Enumeration criteria) {
+           super.processStimulus(criteria);
+           this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
+    }
+
+}
