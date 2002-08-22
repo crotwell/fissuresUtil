8ea7c48caf4eb7569805941aff5021274743e01e
Philip Crotwell <crotwell@seis.sc.edu>
2002-08-22 15:39:32 -0400

    [svn r2531] reformating, no content changes

--- src/edu/sc/seis/fissuresUtil/display/TimeScaleCalc.java
+++ src/edu/sc/seis/fissuresUtil/display/TimeScaleCalc.java
@@ -22,84 +22,84 @@ public class TimeScaleCalc implements ScaleMapper, TimeSyncListener {
 
     */
     TimeScaleCalc (int totalPixels, TimeConfigRegistrar tr){
-	this.totalPixels = totalPixels;
-	this.timeRegistrar = new TimeConfigRegistrar(tr, this);
-	setTimes();
+        this.totalPixels = totalPixels;
+        this.timeRegistrar = new TimeConfigRegistrar(tr, this);
+        setTimes();
     }
 
     public void calculateTicks(){
-	int majTickNum = totalPixels/50;
-	majTickTime = timeIntv/majTickNum;
-	majTickRatio = 10;
-	if(majTickTime <= 100000){
-	    timeFormat = new SimpleDateFormat("ss.S");
-	       if(majTickTime <= 1000){
-		majTickTime = 1000;
-	    }else if(majTickTime <= 10000){
-		majTickTime = 10000;
-	    }else if(majTickTime <= 100000){
-		majTickTime = 100000;	
-	    }
-	}else if(majTickTime <= 30 * 1000000){
-	    majTickRatio = 10;
-	    timeFormat = new SimpleDateFormat("HH:mm:ss");
-	       if(majTickTime <= 1000000){
-		majTickTime = 1000000;
-	    }else if(majTickTime <= 5000000){
-		majTickTime = 5000000;
-	    }else if(majTickTime <= 1000000){
-		majTickTime = 10000000;
-	    }else
-		majTickTime = 30000000;
-	}
-	else if(majTickTime <= 180000000){
-	    majTickRatio = 6;
-	    timeFormat = new SimpleDateFormat("HH:mm:ss");
-	       if(majTickTime <= 60000000){
-		majTickTime = 60000000;
-	    }else if(majTickTime <= 120000000){
-		majTickTime = 120000000;
-	    }else
-		majTickTime = 180000000;
-	}
-	else if(majTickTime <= 1800000000){
-	    majTickRatio = 10;
-	    timeFormat = new SimpleDateFormat("HH:mm:ss");
-	       if(majTickTime <= 300000000)
-		majTickTime = 300000000;
-	    else if(majTickTime <= 600000000)
-		majTickTime = 600000000;
-	    else if(majTickTime <= 1200000000){
-		majTickTime = 1200000000;
-	    }else             
-		majTickTime = 1800000000;
-	}
-	else if(majTickTime <= 43200000000l){
-	    majTickRatio = 6;
-	    timeFormat = new SimpleDateFormat("MM/dd HH:mm");
-	    if(majTickTime <= 3600000000l){
-		majTickTime = 3600000000l;
-	    }else if(majTickTime <= 7200000000l){
-		majTickTime = 7200000000l;
-	    }else if(majTickTime <= 10800000000l){
-		majTickTime = 10800000000l;
-	    }else if(majTickTime <= 21600000000l){
-		majTickTime = 21600000000l;
-	    }else
-		majTickTime = 43200000000l;
-	}
-	else{
-	    majTickRatio = 6;
-	    timeFormat = new SimpleDateFormat("MM/dd");
-	       majTickTime = 86400000000l;
-	}
-	timeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-	double numTicksDbl = ((timeIntv/(double)majTickTime) * majTickRatio);
-    numTicks = (int)numTicksDbl;
-	firstLabelTime = (beginTime/majTickTime + 1) * majTickTime;
-	majTickOffset = (int)((firstLabelTime - beginTime)/(double)timeIntv * numTicks);
-	tickOffset = (firstLabelTime - beginTime)/(double)timeIntv/majTickRatio * totalPixels;
-	tickSpacing = totalPixels/numTicksDbl;
+        int majTickNum = totalPixels/50;
+        majTickTime = timeIntv/majTickNum;
+        majTickRatio = 10;
+        if(majTickTime <= 100000){
+            timeFormat = new SimpleDateFormat("ss.S");
+            if(majTickTime <= 1000){
+                majTickTime = 1000;
+            }else if(majTickTime <= 10000){
+                majTickTime = 10000;
+            }else if(majTickTime <= 100000){
+                majTickTime = 100000;	
+            }
+        }else if(majTickTime <= 30 * 1000000){
+            majTickRatio = 10;
+            timeFormat = new SimpleDateFormat("HH:mm:ss");
+            if(majTickTime <= 1000000){
+                majTickTime = 1000000;
+            }else if(majTickTime <= 5000000){
+                majTickTime = 5000000;
+            }else if(majTickTime <= 1000000){
+                majTickTime = 10000000;
+            }else
+                majTickTime = 30000000;
+        }
+        else if(majTickTime <= 180000000){
+            majTickRatio = 6;
+            timeFormat = new SimpleDateFormat("HH:mm:ss");
+            if(majTickTime <= 60000000){
+                majTickTime = 60000000;
+            }else if(majTickTime <= 120000000){
+                majTickTime = 120000000;
+            }else
+                majTickTime = 180000000;
+        }
+        else if(majTickTime <= 1800000000){
+            majTickRatio = 10;
+            timeFormat = new SimpleDateFormat("HH:mm:ss");
+            if(majTickTime <= 300000000)
+                majTickTime = 300000000;
+            else if(majTickTime <= 600000000)
+                majTickTime = 600000000;
+            else if(majTickTime <= 1200000000){
+                majTickTime = 1200000000;
+            }else             
+                majTickTime = 1800000000;
+        }
+        else if(majTickTime <= 43200000000l){
+            majTickRatio = 6;
+            timeFormat = new SimpleDateFormat("MM/dd HH:mm");
+            if(majTickTime <= 3600000000l){
+                majTickTime = 3600000000l;
+            }else if(majTickTime <= 7200000000l){
+                majTickTime = 7200000000l;
+            }else if(majTickTime <= 10800000000l){
+                majTickTime = 10800000000l;
+            }else if(majTickTime <= 21600000000l){
+                majTickTime = 21600000000l;
+            }else
+                majTickTime = 43200000000l;
+        }
+        else{
+            majTickRatio = 6;
+            timeFormat = new SimpleDateFormat("MM/dd");
+            majTickTime = 86400000000l;
+        }
+        timeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        double numTicksDbl = ((timeIntv/(double)majTickTime) * majTickRatio);
+        numTicks = (int)numTicksDbl;
+        firstLabelTime = (beginTime/majTickTime + 1) * majTickTime;
+        majTickOffset = (int)((firstLabelTime - beginTime)/(double)timeIntv * numTicks);
+        tickOffset = (firstLabelTime - beginTime)/(double)timeIntv/majTickRatio * totalPixels;
+        tickSpacing = totalPixels/numTicksDbl;
     }
 
     public void  setTotalPixels(int totalPixels) {
@@ -112,7 +112,7 @@ public class TimeScaleCalc implements ScaleMapper, TimeSyncListener {
         this.beginTime = beginTime.getMicroSecondTime();
         this.endTime = endTime.getMicroSecondTime();
         timeIntv = (this.endTime - this.beginTime);
-	calculateTicks();
+        calculateTicks();
     }
 
     public void setTimes(){
@@ -125,12 +125,12 @@ public class TimeScaleCalc implements ScaleMapper, TimeSyncListener {
        @param i the current tick
     */
     public  String getLabel(int i){
-	if (isLabelTick(i)) {
-	    MicroSecondDate date = new MicroSecondDate((long)(firstLabelTime + i/majTickRatio * majTickTime));
-	    calendar.setTime(date);
-	    return timeFormat.format(calendar.getTime());
+        if (isLabelTick(i)) {
+            MicroSecondDate date = new MicroSecondDate((long)(firstLabelTime + i/majTickRatio * majTickTime));
+            calendar.setTime(date);
+            return timeFormat.format(calendar.getTime());
 	    }
-	return "";
+        return "";
     }
 
     /**
@@ -155,13 +155,13 @@ public class TimeScaleCalc implements ScaleMapper, TimeSyncListener {
        @param i the current tick
     */
     public  boolean isMajorTick(int i){
-	if(i%majTickRatio - majTickOffset == 0)
-	    return true;
-	return false;
+        if(i%majTickRatio - majTickOffset == 0)
+            return true;
+        return false;
     }
 
     public void updateTimeRange(){
-	setTimes();
+        setTimes();
     }
 
     protected SimpleDateFormat timeFormat;
