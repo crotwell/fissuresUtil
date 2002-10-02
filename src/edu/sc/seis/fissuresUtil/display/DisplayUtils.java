986885a8dc688685e8b7875d1c31a955fe714ac1
Srinivasa Telukutla <telukutl@seis.sc.edu>
2002-10-01 23:14:41 -0400

    [svn r2662] added the methods getSeismogramNames, getSeismogram, and added check for endTimes also

--- src/edu/sc/seis/fissuresUtil/display/DisplayUtils.java
+++ src/edu/sc/seis/fissuresUtil/display/DisplayUtils.java
@@ -8,6 +8,7 @@ import edu.iris.Fissures.model.*;
 import edu.iris.Fissures.network.*;
 import edu.iris.Fissures.seismogramDC.*;
 
+import java.util.*;
 /**
  * DisplayUtils.java
  *
@@ -19,19 +20,53 @@ import edu.iris.Fissures.seismogramDC.*;
  */
 
 public class DisplayUtils {
-    public static String getSeismogramName(ChannelId channelId, DataSet dataset, TimeRange timeRange) {
+    public static String[] getSeismogramNames(ChannelId channelId, DataSet dataset, TimeRange timeRange) {
 	Channel channel = ((XMLDataSet)dataset).getChannel(channelId);
 	SeismogramAttr[] attrs = ((XMLDataSet)dataset).getSeismogramAttrs();
 	MicroSecondDate startDate = new MicroSecondDate(timeRange.start_time);
 	MicroSecondDate endDate = new MicroSecondDate(timeRange.end_time);
+	ArrayList arrayList = new ArrayList();
 	for(int counter = 0; counter < attrs.length; counter++) {
 	    if(ChannelIdUtil.toString(channelId).equals(ChannelIdUtil.toString(((SeismogramAttrImpl)attrs[counter]).getChannelID()))){
-		if((((SeismogramAttrImpl)attrs[counter]).getBeginTime().equals(startDate) ||
-		    ((SeismogramAttrImpl)attrs[counter]).getBeginTime().before(startDate))){
-		    return ((SeismogramAttrImpl)attrs[counter]).getName();
+		if(((((SeismogramAttrImpl)attrs[counter]).getBeginTime().equals(startDate) ||
+		    ((SeismogramAttrImpl)attrs[counter]).getBeginTime().before(startDate))) &&
+		   (((SeismogramAttrImpl)attrs[counter]).getEndTime().equals(endDate) ||
+		    ((SeismogramAttrImpl)attrs[counter]).getEndTime().after(endDate))){
+			arrayList.add(((SeismogramAttrImpl)attrs[counter]).getName());
+		    //return ((SeismogramAttrImpl)attrs[counter]).getName();
 		}
 	    }
 	}
-	return null;
+	String[] rtnValues = new String[arrayList.size()];
+	rtnValues = (String[]) arrayList.toArray(rtnValues);														
+	return rtnValues;
     }    
+	
+	public static LocalSeismogram[] getSeismogram(ChannelId channelId, DataSet dataset, TimeRange timeRange) {
+		String[] seisNames = DisplayUtils.getSeismogramNames(channelId, dataset, timeRange);
+		LocalSeismogram[] localSeismograms = new LocalSeismogram[seisNames.length];
+		for(int counter = 0 ; counter < seisNames.length; counter++) {
+				localSeismograms[counter] = ((XMLDataSet)dataset).getSeismogram(seisNames[counter]);
+		}
+		return localSeismograms;
+    }
+	
+	 public static String getSeismogramName(ChannelId channelId, DataSet dataset, TimeRange timeRange) {
+        Channel channel = ((XMLDataSet)dataset).getChannel(channelId);
+        SeismogramAttr[] attrs = ((XMLDataSet)dataset).getSeismogramAttrs();
+        MicroSecondDate startDate = new MicroSecondDate(timeRange.start_time);
+        MicroSecondDate endDate = new MicroSecondDate(timeRange.end_time);
+        for(int counter = 0; counter < attrs.length; counter++) {
+            if(ChannelIdUtil.toString(channelId).equals(ChannelIdUtil.toString(((SeismogramAttrImpl)attrs[counter]).getChannelID()))){
+                if(((((SeismogramAttrImpl)attrs[counter]).getBeginTime().equals(startDate) ||
+		    ((SeismogramAttrImpl)attrs[counter]).getBeginTime().before(startDate))) &&
+		   (((SeismogramAttrImpl)attrs[counter]).getEndTime().equals(endDate) ||
+		    ((SeismogramAttrImpl)attrs[counter]).getEndTime().after(endDate))){
+                    return ((SeismogramAttrImpl)attrs[counter]).getName();
+                }
+            }
+        }
+        return null;
+    }    
+
 }// DisplayUtils
