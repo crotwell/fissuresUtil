6a1c2d71b25697c16b078a5b5b7c29aed47650e1
Philip Crotwell <crotwell@seis.sc.edu>
2002-10-03 21:34:58 -0400

    [svn r2673] small changes

--- src/edu/sc/seis/fissuresUtil/bag/Cut.java
+++ src/edu/sc/seis/fissuresUtil/bag/Cut.java
@@ -10,7 +10,7 @@ import edu.iris.Fissures.model.*;
  * Created: Tue Oct  1 21:23:44 2002
  *
  * @author Philip Crotwell
- * @version $Id: Cut.java 2661 2002-10-02 01:41:35Z crotwell $
+ * @version $Id: Cut.java 2673 2002-10-04 01:34:58Z crotwell $
  */
 
 public class Cut {
@@ -19,10 +19,18 @@ public class Cut {
 					  MicroSecondDate begin,
 					  MicroSecondDate end) {
 	TimeInterval sampPeriod = seis.getSampling().getPeriod();
-	QuantityImpl beginShift = seis.getBeginTime().subtract(begin);
+	QuantityImpl beginShift = begin.subtract(seis.getBeginTime());
 	beginShift = beginShift.divideBy(sampPeriod);
+	int beginIndex = (int)Math.ceil(beginShift.value);
+	if (beginIndex < 0) {
+	    beginIndex = 0;
+	} // end of if (beginIndex < 0)
+	
 	QuantityImpl endShift = seis.getEndTime().subtract(end);
 	endShift = endShift.divideBy(sampPeriod);
+	int endIndex = seis.getNumPoints() - (int)Math.floor(endShift.value);
+
+
 
 	return new LocalSeismogramImpl(seis, seis.getData());
     }
