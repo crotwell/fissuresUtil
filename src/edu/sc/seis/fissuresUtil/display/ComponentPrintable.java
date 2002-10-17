05fbd6f8f93e8d8ee584af92a8f3b25af932b163
groves <groves@localhost>
2002-10-17 13:14:38 -0400

    [svn r2760] removed some old debugging stuff

--- src/edu/sc/seis/fissuresUtil/display/ComponentPrintable.java
+++ src/edu/sc/seis/fissuresUtil/display/ComponentPrintable.java
@@ -18,22 +18,15 @@ import javax.swing.JComponent;
 public class ComponentPrintable implements Printable{
     public ComponentPrintable (Component c){
 	mComponent = c;
-	System.out.println("set waiting");
-	mComponent.setSize(new Dimension(648, 468));
     }
     
     public int print(Graphics g, PageFormat pageFormat, int pageIndex){
-	System.out.println("PRIIIIINT");
 	if (pageIndex > 0) return NO_SUCH_PAGE;
 	pageFormat.setOrientation(PageFormat.LANDSCAPE);
 	Graphics2D g2 = (Graphics2D)g;
 	g2.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
 	boolean wasBuffered = disableDoubleBuffering(mComponent);
-	Dimension currentSize = mComponent.getSize();
-	System.out.println((int)pageFormat.getImageableWidth() + " " +  (int)pageFormat.getImageableHeight());
-	//mComponent.setSize(new Dimension((int)pageFormat.getImageableWidth(), (int)pageFormat.getImageableHeight()));	   
 	mComponent.paint(g2);
-	mComponent.setSize(currentSize);
 	restoreDoubleBuffering(mComponent, wasBuffered);
 	return PAGE_EXISTS;
     }
