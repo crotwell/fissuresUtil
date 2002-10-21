83581a88e6b6630701d1a6ea9e4284646b5817fd
Philip Crotwell <crotwell@seis.sc.edu>
2002-10-20 20:01:07 -0400

    [svn r2784] initial

---
+++ src/edu/sc/seis/fissuresUtil/bag/RMean.java
@@ -0,0 +1,18 @@
+package edu.sc.seis.fissuresUtil.bag;
+
+/**
+ * RMean.java
+ *
+ *
+ * Created: Sat Oct 19 21:54:26 2002
+ *
+ * @author <a href="mailto:www@seis.sc.edu">Philip Crotwell</a>
+ * @version
+ */
+
+public class RMean {
+    public RMean (){
+	
+    }
+    
+}// RMean
