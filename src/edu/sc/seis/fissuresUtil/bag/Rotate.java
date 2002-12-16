2c7d33e9037554eae234a6f443d6cdcee0d6cdf1
Philip Crotwell <crotwell@seis.sc.edu>
2002-12-16 13:48:33 -0500

    [svn r3022] compiles, but does not work

--- src/edu/sc/seis/fissuresUtil/bag/Rotate.java
+++ src/edu/sc/seis/fissuresUtil/bag/Rotate.java
@@ -1,5 +1,12 @@
 package edu.sc.seis.fissuresUtil.bag;
 
+import edu.iris.Fissures.IfSeismogramDC.LocalMotionVector;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.seismogramDC.LocalMotionVectorImpl;
+import edu.iris.Fissures.IfSeismogramDC.VectorComponent;
+import edu.iris.Fissures.IfSeismogramDC.LocalSeismogram;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+
 /**
  * Rotate.java
  *
@@ -7,7 +14,7 @@ package edu.sc.seis.fissuresUtil.bag;
  * Created: Sun Dec 15 13:43:21 2002
  *
  * @author Philip Crotwell
- * @version $Id: Rotate.java 3013 2002-12-15 18:45:49Z crotwell $
+ * @version $Id: Rotate.java 3022 2002-12-16 18:48:33Z crotwell $
  */
 public class Rotate implements LocalMotionVectorFunction {
 
@@ -15,8 +22,21 @@ public class Rotate implements LocalMotionVectorFunction {
 	
     } // Rotate constructor
     
-    public LocalMotionVector apply(LocalmotionVector vec) {
-	return vec;
+    public LocalMotionVector apply(LocalMotionVector vec) {
+	VectorComponent[] data = new VectorComponent[3];
+
+
+	return new  LocalMotionVectorImpl(vec.get_id(),
+					  vec.properties,
+					  vec.begin_time,
+					  vec.num_points,
+					  vec.sampling_info,
+					  vec.y_unit,
+					  vec.channel_group,
+					  vec.parm_ids,
+					  (TimeInterval[])vec.time_corrections,
+					  vec.sample_rate_history,
+					  data);
     }
 
 } // Rotate
