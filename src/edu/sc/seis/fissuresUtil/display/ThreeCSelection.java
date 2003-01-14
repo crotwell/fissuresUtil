d6758ec4449b0b71fef77935153d4b2b17cf3329
crotwell <crotwell@localhost>
2003-01-14 16:15:15 -0500

    [svn r3102] make Selection abstract and create two subclasses for single and three components

---
+++ src/edu/sc/seis/fissuresUtil/display/ThreeCSelection.java
@@ -0,0 +1,25 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import edu.iris.Fissures.model.MicroSecondDate;
+import java.awt.Color;
+
+/**
+ * ThreeCSelection.java
+ *
+ *
+ * Created: Tue Jan 14 12:50:57 2003
+ *
+ * @author <a href="mailto:crotwell@owl.seis.sc.edu">Philip Crotwell</a>
+ * @version 1.0
+ */
+public class ThreeCSelection extends Selection {
+     public ThreeCSelection(MicroSecondDate begin, 
+			    MicroSecondDate end, 
+			    Registrar reg, 
+			    DataSetSeismogram[] seismograms, 
+			    BasicSeismogramDisplay parent, 
+			    Color color){
+	 super(begin, end, reg, seismograms, parent, color);
+     }
+
+} // ThreeCSelection
