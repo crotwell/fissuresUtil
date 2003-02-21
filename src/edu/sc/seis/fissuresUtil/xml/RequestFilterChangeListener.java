5e96eb7aeb2775cb62090dffcba337a327816029
telukutl <telukutl@localhost>
2003-02-21 10:09:06 -0500

    [svn r3312] initial version of database in which gee stores the seismogram information

---
+++ src/edu/sc/seis/fissuresUtil/xml/RequestFilterChangeListener.java
@@ -0,0 +1,21 @@
+package edu.sc.seis.fissuresUtil.xml;
+
+/**
+ * RequestFilterChangeListener.java
+ *
+ *
+ * Created: Tue Feb 11 10:38:22 2003
+ *
+ * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public interface RequestFilterChangeListener {
+    
+    public void beginTimeChanged();
+
+    public void endTimeChanged();
+    
+   
+
+}// RequestFilterChangeListener
---
+++ src/edu/sc/seis/fissuresUtil/xml/SeisDataChangeListener.java
@@ -0,0 +1,21 @@
+package edu.sc.seis.fissuresUtil.xml;
+import edu.sc.seis.fissuresUtil.database.*;
+
+import edu.iris.Fissures.IfSeismogramDC.*;
+
+/**
+ * DSSDataListener.java
+ *
+ *
+ * Created: Tue Feb 11 10:13:15 2003
+ *
+ * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public interface SeisDataChangeListener {
+    public void pushData(SeisDataChangeEvent sdce);
+    
+    public void finished(SeisDataChangeEvent sdce);
+
+}// DSSDataListener
