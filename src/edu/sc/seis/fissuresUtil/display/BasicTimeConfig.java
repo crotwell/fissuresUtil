f492649fa1f709b5a65486f89762bd4211041beb
groves <groves@localhost>
2003-03-03 17:01:30 -0500

    [svn r3366] incremental changes to implement DataSetSeismogram change

--- src/edu/sc/seis/fissuresUtil/display/BasicTimeConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/BasicTimeConfig.java
@@ -2,12 +2,13 @@ package edu.sc.seis.fissuresUtil.display;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
+import org.apache.log4j.Category;
 /**
  * BasicTimeConfig synchronizes all the seismograms it holds around their initial times. It gets the first added 
  * seismogram's time interval and uses it to initialize the display interval of subsequently added seismograms.  Any time shifts or 
@@ -22,158 +23,170 @@ import java.util.Map;
 
 public class BasicTimeConfig implements TimeConfig{
     public BasicTimeConfig(){}
-
+	
     /**
-     * Creates a new <code>BasicTimeConfig</code> instance.  The display interval is initialized to be the same as the seismogram
-     * being passed
-     * @param seismo the initial seismogram
-     */
+	 * Creates a new <code>BasicTimeConfig</code> instance.  The display interval is initialized to be the same as the seismogram
+	 * being passed
+	 * @param seismo the initial seismogram
+	 */
     public BasicTimeConfig(DataSetSeismogram[] seismos){
-	add(seismos);
+		add(seismos);
     }
     
     /**
-     * <code>add</code> adds a seismogram to the config
-     *
-     * @param seismo the seismogram to be added
-     */
+	 * <code>add</code> adds a seismogram to the config
+	 *
+	 * @param seismo the seismogram to be added
+	 */
     public void add(DataSetSeismogram[] seismos){
-	if(time == null){
-	    time = new MicroSecondTimeRange(seismos[0].getSeismogram().getBeginTime(), getInterval(seismos[0]));
+		if(time == null){
+			try{
+				time = new MicroSecondTimeRange(seismos[0].getRequestFilter());
+			}catch(ParseException e){	
+				logger.debug("Error parsing the request filter to create a MicroSecondTimeRange");
+				time = MicroSecondTimeRange.ONE_MS;
+			}
+		}
+		for(int i = 0; i < seismos.length; i++){
+			if(!contains(seismos[i])){
+				seismoTimes.put(seismos[i], time);
+			}
+		}
+		seismograms = null;
+		fireTimeEvent();
 	}
-	for(int i = 0; i < seismos.length; i++){
-	    if(!contains(seismos[i])){
-		seismoTimes.put(seismos[i], time);
-	    }
+	
+	/**
+	 * <code>remove</code> removes a seismogram from this object
+	 *
+	 * @param seismo the seismogram to be removed
+	 */
+	public boolean remove(DataSetSeismogram[] seismos){
+		boolean allRemoved = true;
+		for(int i = 0; i < seismos.length; i++){
+			if(seismoTimes.containsKey(seismos[i])){
+				seismoTimes.remove(seismos[i]);
+			}else{
+				allRemoved = false;
+			}
+		}
+		seismograms = null;
+		//fireTimeEvent();
+		return allRemoved;
 	}
-	seismograms = null;
-	fireTimeEvent();
-    }
-
-    /**
-     * <code>remove</code> removes a seismogram from this object
-     *
-     * @param seismo the seismogram to be removed
-     */
-    public boolean remove(DataSetSeismogram[] seismos){
-	boolean allRemoved = true;
-	for(int i = 0; i < seismos.length; i++){
-	    if(seismoTimes.containsKey(seismos[i])){
-		seismoTimes.remove(seismos[i]);
-	    }else{
-		allRemoved = false;
-	    }
+	
+	/**
+	 * <code>contains</code> checks the receptacle for the presence of seismo
+	 *
+	 * @param seismo the seismogram whose presence is to be tested
+	 * @return true if the receptacle contains seismo, false otherwise
+	 */
+	public boolean contains(DataSetSeismogram seismo){
+		if(seismoTimes.containsKey(seismo)){
+			return true;
+		}
+		return false;
 	}
-	seismograms = null;
-	//fireTimeEvent();
-	return allRemoved;
-    }
-
-    /**
-     * <code>contains</code> checks the receptacle for the presence of seismo
-     *
-     * @param seismo the seismogram whose presence is to be tested
-     * @return true if the receptacle contains seismo, false otherwise
-     */
-    public boolean contains(DataSetSeismogram seismo){
-	if(seismoTimes.containsKey(seismo)){
-	    return true;
+	
+	public DataSetSeismogram[] getSeismograms(){
+		if(seismograms == null){
+			seismograms = (DataSetSeismogram[])seismoTimes.keySet().toArray(new DataSetSeismogram[seismoTimes.size()]);
+		}
+		return seismograms;
 	}
-	return false;
-    }
-
-    public DataSetSeismogram[] getSeismograms(){
-	if(seismograms == null){
-	    seismograms = (DataSetSeismogram[])seismoTimes.keySet().toArray(new DataSetSeismogram[seismoTimes.size()]);
+	
+	/**
+	 * <code>reset</code> causes the interval of the config to be set to the total time interval of one of the seismograms
+	 * and the rest of the seismograms to be set as if they were just added based on that interval
+	 */
+	public void reset(){ 
+		time = null;
+		shift = 0;
+		scale = 1;
+		reset(getSeismograms());
 	}
-	return seismograms;
-    }
-
-    /**
-     * <code>reset</code> causes the interval of the config to be set to the total time interval of one of the seismograms
-     * and the rest of the seismograms to be set as if they were just added based on that interval
-     */
-    public void reset(){ 
-	time = null;
-	shift = 0;
-	scale = 1;
-	reset(getSeismograms());
-    }
-
-    /**
-     * <code>reset</code> causes this seismogram to be set around the current shift and interval
-     *
-     * @param seismo a <code>DataSetSeismogram</code> to be reset
-     */
-    public void reset(DataSetSeismogram[] seismos){
-	remove(seismos);
-	add(seismos);
-    }
-
-    public void shaleTime(double shift, double scale){
-	shaleTime(shift, scale, getSeismograms());
-    }
-
-    public void shaleTime(double shift, double scale, DataSetSeismogram[] seismos){
-	this.shift += shift * this.scale;
-	this.scale *= scale;
-	time = time.shale(shift, scale);
-	for(int i = 0; i < seismos.length; i++){
-	    seismoTimes.put(seismos[i], time);
+	
+	/**
+	 * <code>reset</code> causes this seismogram to be set around the current shift and interval
+	 *
+	 * @param seismo a <code>DataSetSeismogram</code> to be reset
+	 */
+	public void reset(DataSetSeismogram[] seismos){
+		remove(seismos);
+		add(seismos);
 	}
-	fireTimeEvent();
-    }
-    
-    public TimeEvent fireTimeEvent(){
-	if(seismoTimes.size() == 0 && time != null){
-	    return fireTimeEvent(new EmptyTimeEvent(time));
+	
+	public void shaleTime(double shift, double scale){
+		shaleTime(shift, scale, getSeismograms());
 	}
-	DataSetSeismogram[] seismos = getSeismograms();
-	MicroSecondTimeRange[] times = new MicroSecondTimeRange[seismos.length];
-	for(int i = 0; i < seismos.length; i++){
-	    times[i] = time;
+	
+	public void shaleTime(double shift, double scale, DataSetSeismogram[] seismos){
+		this.shift += shift * this.scale;
+		this.scale *= scale;
+		time = time.shale(shift, scale);
+		for(int i = 0; i < seismos.length; i++){
+			seismoTimes.put(seismos[i], time);
+		}
+		fireTimeEvent();
 	}
-	return fireTimeEvent(new TimeEvent(seismos, times));
-    }
-
-    protected TimeEvent fireTimeEvent(TimeEvent event){
-	Iterator f = listeners.iterator();
-	while(f.hasNext()){
-	    ((TimeListener)f.next()).updateTime(event);
+	
+	public TimeEvent fireTimeEvent(){
+		if(seismoTimes.size() == 0 && time != null){
+			return fireTimeEvent(new EmptyTimeEvent(time));
+		}
+		DataSetSeismogram[] seismos = getSeismograms();
+		MicroSecondTimeRange[] times = new MicroSecondTimeRange[seismos.length];
+		for(int i = 0; i < seismos.length; i++){
+			times[i] = time;
+		}
+		return fireTimeEvent(new TimeEvent(seismos, times));
 	}
-	return event;
-    }
-
-    public void addListener(TimeListener listener){
-	listeners.add(listener);
-	fireTimeEvent();
-    }
-
-    public void removeListener(TimeListener listener){
-	listeners.remove(listener);
-    }
-    
-    private TimeInterval getInterval(DataSetSeismogram seismo){  
-	return new TimeInterval(((LocalSeismogramImpl)seismo.getSeismogram()).getBeginTime(), 
-				((LocalSeismogramImpl)seismo.getSeismogram()).getEndTime());
-    }
-
-    /**
-     * <code>listeners</code> contains all of the listeners registered with this config
-    */
-    private List listeners = new ArrayList();
-    
-    /**
-     * <code>seismoTimes</code> contains all of the seismograms held by this config with their current TimeRange
-     *
-     */
-    protected Map seismoTimes = new HashMap();
-    
-    private DataSetSeismogram[] seismograms;
-
-    protected double shift;
-
-    protected double scale = 1;
-
-    protected MicroSecondTimeRange time;
+	
+	protected TimeEvent fireTimeEvent(TimeEvent event){
+		Iterator f = listeners.iterator();
+		while(f.hasNext()){
+			((TimeListener)f.next()).updateTime(event);
+		}
+		return event;
+	}
+	
+	public void addListener(TimeListener listener){
+		listeners.add(listener);
+		fireTimeEvent();
+	}
+	
+	public void removeListener(TimeListener listener){
+		listeners.remove(listener);
+	}
+	
+	private TimeInterval getInterval(DataSetSeismogram seismo){  
+		try{
+			MicroSecondTimeRange intervalContainer = new MicroSecondTimeRange(seismo.getRequestFilter());
+			return intervalContainer.getInterval();
+		}catch(ParseException e){
+			logger.debug("caught parse exception turning RequestFilter into MicroSecondTimeRange.  Using one MS interval");
+			return MicroSecondTimeRange.ONE_MS.getInterval();
+		}
+	}
+	
+	/**
+	 * <code>listeners</code> contains all of the listeners registered with this config
+	 */
+	private List listeners = new ArrayList();
+	
+	/**
+	 * <code>seismoTimes</code> contains all of the seismograms held by this config with their current TimeRange
+	 *
+	 */
+	protected Map seismoTimes = new HashMap();
+	
+	private DataSetSeismogram[] seismograms;
+	
+	protected double shift;
+	
+	protected double scale = 1;
+	
+	protected MicroSecondTimeRange time;
+	
+	private static Category logger = Category.getInstance(BasicTimeConfig.class.getName());
 }// BasicTimeConfig
--- src/edu/sc/seis/fissuresUtil/display/ComponentSortedSeismogramDisplay.java
+++ src/edu/sc/seis/fissuresUtil/display/ComponentSortedSeismogramDisplay.java
@@ -2,6 +2,7 @@ package edu.sc.seis.fissuresUtil.display;
 
 import edu.sc.seis.TauP.Arrival;
 import java.util.List;
+import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 
 /**
  * ComponentSortedSeismogramDisplay.java
@@ -15,140 +16,140 @@ import java.util.List;
 
 public class ComponentSortedSeismogramDisplay extends VerticalSeismogramDisplay {
     /**
-     * Creates a <code>ComponentSortedSeismogramDisplay</code> without a parent
-     *
-     * @param mouseForwarder the object every contained BSD forwards its mouse events to
-     * @param motionForwarder the object every contained BSD forwards its mouse motion events to
-     */
+	 * Creates a <code>ComponentSortedSeismogramDisplay</code> without a parent
+	 *
+	 * @param mouseForwarder the object every contained BSD forwards its mouse events to
+	 * @param motionForwarder the object every contained BSD forwards its mouse motion events to
+	 */
     public ComponentSortedSeismogramDisplay(MouseForwarder mouseForwarder, MouseMotionForwarder motionForwarder){
-	this(mouseForwarder, motionForwarder, null);
+		this(mouseForwarder, motionForwarder, null);
     }
-
+	
     /**
-     * Creates a <code>ComponentSortedSeismogramDisplay</code> with the passed in parent controlling it
-     *
-     * @param mouseForwarder the object every contained BSD forwards its mouse events to
-     * @param motionForwarder the object every contained BSD forwards its mouse motion events to
-     * @param parent the VSD that controls this VSD
-     */
+	 * Creates a <code>ComponentSortedSeismogramDisplay</code> with the passed in parent controlling it
+	 *
+	 * @param mouseForwarder the object every contained BSD forwards its mouse events to
+	 * @param motionForwarder the object every contained BSD forwards its mouse motion events to
+	 * @param parent the VSD that controls this VSD
+	 */
     public ComponentSortedSeismogramDisplay(MouseForwarder mouseForwarder, MouseMotionForwarder motionForwarder, 
-				     VerticalSeismogramDisplay parent){
-	super(mouseForwarder, motionForwarder, parent);
+											VerticalSeismogramDisplay parent){
+		super(mouseForwarder, motionForwarder, parent);
     }
-
+	
     /**
-     * finds all three components of the passed seismograms and adds them to their respective component displays 
-     * with an individual RMeanAmpConfig and the global time registrar for each component display
-     *
-     * @param dss the seismograms for the new BSD
-     * @return the created BSD
-     */
+	 * finds all three components of the passed seismograms and adds them to their respective component displays 
+	 * with an individual RMeanAmpConfig and the global time registrar for each component display
+	 *
+	 * @param dss the seismograms for the new BSD
+	 * @return the created BSD
+	 */
     public BasicSeismogramDisplay addDisplay(DataSetSeismogram[] dss){
-	return addDisplay(dss, globalRegistrar, new RMeanAmpConfig(dss));
+		return addDisplay(dss, globalRegistrar, new RMeanAmpConfig(dss));
     }
-
+	
     /**
-     *  finds all three components of the passed seismograms and adds them to their respective component displays with an 
-     * individual RMeanAmpConfig and the passed in TImeConfig
-     *
-     * @param dss the seismograms for the new BSD
-     * @param tc the time config for the new BSD
-     * @return the created BSD
-     */
+	 *  finds all three components of the passed seismograms and adds them to their respective component displays with an 
+	 * individual RMeanAmpConfig and the passed in TImeConfig
+	 *
+	 * @param dss the seismograms for the new BSD
+	 * @param tc the time config for the new BSD
+	 * @return the created BSD
+	 */
     public BasicSeismogramDisplay addDisplay(DataSetSeismogram[] dss, TimeConfig tc){
-	return addDisplay(dss, tc, new RMeanAmpConfig(dss));
+		return addDisplay(dss, tc, new RMeanAmpConfig(dss));
     }
     
-     /**
-     *  finds all three components of the passed seismograms and adds them to their respective component displays 
-     * with the passed in amp config and the global TImeConfig
-     *
-     * @param dss the seismograms for the new BSD
-     * @param ac the amp config for the new BSD
-     * @return the created BSD
-     */
+	/**
+	 *  finds all three components of the passed seismograms and adds them to their respective component displays 
+	 * with the passed in amp config and the global TImeConfig
+	 *
+	 * @param dss the seismograms for the new BSD
+	 * @param ac the amp config for the new BSD
+	 * @return the created BSD
+	 */
     public BasicSeismogramDisplay addDisplay(DataSetSeismogram[] dss, AmpConfig ac){
-	return addDisplay(dss, globalRegistrar, ac);
+		return addDisplay(dss, globalRegistrar, ac);
     }
-   
+	
     /**
-     * finds all three components of the passed seismograms and adds them to their respective component display
-     *  with the passed in amp and time configs 
-     *
-     * @param dss the seismograms for the new BSD
-     * @param tc the time config for the new BSD
-     * @param ac the amp config for the new BSD
-     * @return the created BSD
-     */ 
+	 * finds all three components of the passed seismograms and adds them to their respective component display
+	 *  with the passed in amp and time configs 
+	 *
+	 * @param dss the seismograms for the new BSD
+	 * @param tc the time config for the new BSD
+	 * @param ac the amp config for the new BSD
+	 * @return the created BSD
+	 */ 
     public BasicSeismogramDisplay addDisplay(DataSetSeismogram[] dss, TimeConfig tc, AmpConfig ac){
-	if(tc == globalRegistrar && globalRegistrar == null){
-	    globalRegistrar = new Registrar(dss);
-	    tc = globalRegistrar;
-	}
-	List seismogramNames = getSeismogramNames();
-	DataSetSeismogram[][] componentSorted = DisplayUtils.getComponents(dss, suffix);
-   	addNorth(componentSorted[0], tc, new RMeanAmpConfig(componentSorted[0]));
-	addEast(componentSorted[1], tc , new RMeanAmpConfig(componentSorted[1]));
-	addZ(componentSorted[2], tc, new RMeanAmpConfig(componentSorted[2]));
-	return north;
+		if(tc == globalRegistrar && globalRegistrar == null){
+			globalRegistrar = new Registrar(dss);
+			tc = globalRegistrar;
+		}
+		List seismogramNames = getSeismogramNames();
+		DataSetSeismogram[][] componentSorted = DisplayUtils.getComponents(dss, suffix);
+		addNorth(componentSorted[0], tc, new RMeanAmpConfig(componentSorted[0]));
+		addEast(componentSorted[1], tc , new RMeanAmpConfig(componentSorted[1]));
+		addZ(componentSorted[2], tc, new RMeanAmpConfig(componentSorted[2]));
+		return north;
     }
-
+	
     public void addFlags(Arrival[] arrivals){
-	if(north != null){
-	    north.addFlags(arrivals);
-	}
-	if(east != null){
-	    east.addFlags(arrivals);
-	}
-	if(z != null){
-	    z.addFlags(arrivals);
-	}
+		if(north != null){
+			north.addFlags(arrivals);
+		}
+		if(east != null){
+			east.addFlags(arrivals);
+		}
+		if(z != null){
+			z.addFlags(arrivals);
+		}
     }
-
+	
     private void addNorth(DataSetSeismogram[] newNorth, TimeConfig tc, AmpConfig ac){
-	north = addToDisplay(north, newNorth, tc, ac);
+		north = addToDisplay(north, newNorth, tc, ac);
     }
-
+	
     private void addEast(DataSetSeismogram[] newEast, TimeConfig tc, AmpConfig ac){
-	east = addToDisplay(east, newEast, tc, ac);
+		east = addToDisplay(east, newEast, tc, ac);
     }
-
+	
     private void addZ(DataSetSeismogram[] newZ, TimeConfig tc, AmpConfig ac){
-	z = addToDisplay(z, newZ, tc, ac);
+		z = addToDisplay(z, newZ, tc, ac);
     }
     
     private BasicSeismogramDisplay addToDisplay(BasicSeismogramDisplay display, DataSetSeismogram[] seismos, 
-						TimeConfig tc, AmpConfig ac){
-        if(seismos.length > 0){
-	    if(display == null){
-		display = new BasicSeismogramDisplay(seismos, tc, ac, this);
-		initializeBSD(display, 0);
-	    }else{
-		display.add(seismos);
-	    }
-	}
-	return display;
+												TimeConfig tc, AmpConfig ac){
+		if(seismos.length > 0){
+			if(display == null){
+				display = new BasicSeismogramDisplay(seismos, tc, ac, this);
+				initializeBSD(display, 0);
+			}else{
+				display.add(seismos);
+			}
+		}
+		return display;
     }
     
     private void initializeBSD(BasicSeismogramDisplay disp, int position){
-	super.add(disp, position);
-	disp.addMouseMotionListener(motionForwarder);
-	disp.addMouseListener(mouseForwarder);
-	disp.addBottomTimeBorder();
-	disp.addTopTimeBorder();
-	basicDisplays.add(disp);
+		super.add(disp, position);
+		disp.addMouseMotionListener(motionForwarder);
+		disp.addMouseListener(mouseForwarder);
+		disp.addBottomTimeBorder();
+		disp.addTopTimeBorder();
+		basicDisplays.add(disp);
     }
-
+	
     public boolean removeDisplay(BasicSeismogramDisplay display){
-	if(display == north){
-	    north = null;
-	}else if(display == east){
-	    east = null;
-	}else{
-	    z = null;
-	}
-	return super.removeDisplay(display);
+		if(display == north){
+			north = null;
+		}else if(display == east){
+			east = null;
+		}else{
+			z = null;
+		}
+		return super.removeDisplay(display);
     }
-
+	
     private BasicSeismogramDisplay north, east, z;
 }// ComponentSortedSeismogramDisplay
