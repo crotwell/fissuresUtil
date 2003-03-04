60e6119da4a63a2551e148d413d14020c4abaee3
groves <groves@localhost>
2003-03-04 15:46:53 -0500

    [svn r3377] fixed omission of parenthesis on method name.length

--- src/edu/sc/seis/fissuresUtil/xml/ExtendedXMLDataSet.java
+++ src/edu/sc/seis/fissuresUtil/xml/ExtendedXMLDataSet.java
@@ -18,56 +18,56 @@ import java.util.*;
  */
 
 public class ExtendedXMLDataSet extends XMLDataSet{
-   
+	
     public ExtendedXMLDataSet(DocumentBuilder docBuilder, URL datasetURL) {
-	super(docBuilder, datasetURL);
+		super(docBuilder, datasetURL);
     }
-
-    public ExtendedXMLDataSet(DocumentBuilder docBuilder, 
-			      URL base, 
-			      String id, 
-			      String name,
-			      String owner) {
-	super(docBuilder,
-	      base,
-	      id,
-	      name,
-	      owner);
+	
+    public ExtendedXMLDataSet(DocumentBuilder docBuilder,
+							  URL base,
+							  String id,
+							  String name,
+							  String owner) {
+		super(docBuilder,
+			  base,
+			  id,
+			  name,
+			  owner);
     }
     
     public ExtendedXMLDataSet(DocumentBuilder docBuilder, URL base, Element config) {
-	super(docBuilder, base, config);
+		super(docBuilder, base, config);
     }
-
-    public String[] getDataSetSeismogramNames() {
-	return (String[])dssNames.toArray(new String[0]);
-    }
-
-    public void addDataSetSeismogram(DataSetSeismogram dss) {
-	String name;
-	name = dss.getName();
-	if ( name == null || name.length == 0) {
-	    name = ChannelIdUtil.toStringNoDates(dss.getRequestFilter().channel_id);
-	} // end of if ()
-
-	name = getUniqueName(getDataSetSeismogramNames(), name);
-
-	if ( ! name.equals(dss.getName()) ) {
-	    dss.setName(name);
-	} // end of if ()
 	
-	dssNames.add(name);
-	dataSetSeismograms.put(name,
-			       dss);
+    public String[] getDataSetSeismogramNames() {
+		return (String[])dssNames.toArray(new String[0]);
+    }
+	
+    public void addDataSetSeismogram(DataSetSeismogram dss) {
+		String name;
+		name = dss.getName();
+		if ( name == null || name.length() == 0) {
+			name = ChannelIdUtil.toStringNoDates(dss.getRequestFilter().channel_id);
+		} // end of if ()
+		
+		name = getUniqueName(getDataSetSeismogramNames(), name);
+		
+		if ( ! name.equals(dss.getName()) ) {
+			dss.setName(name);
+		} // end of if ()
+		
+		dssNames.add(name);
+		dataSetSeismograms.put(name,
+							   dss);
     }
     
     public DataSetSeismogram getDataSetSeismogram(String name) {
-	
-	DataSetSeismogram dss = (DataSetSeismogram)dataSetSeismograms.get(name);
-	return dss;
+		
+		DataSetSeismogram dss = (DataSetSeismogram)dataSetSeismograms.get(name);
+		return dss;
     }
-
-
+	
+	
     Map dataSetSeismograms = new HashMap();
     List dssNames = new LinkedList();
     
