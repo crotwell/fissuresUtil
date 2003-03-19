03b4d4b28a234b04f29095ba38479bba0ee713ac
Charles Groves <groves@seis.sc.edu>
2003-03-19 15:38:22 -0500

    [svn r3510] added check for data existing in shape before drawing it

--- src/edu/sc/seis/fissuresUtil/display/DrawableSeismogram.java
+++ src/edu/sc/seis/fissuresUtil/display/DrawableSeismogram.java
@@ -14,77 +14,78 @@ import javax.swing.JComponent;
 
 public class DrawableSeismogram implements NamedPlotter{
     public DrawableSeismogram(JComponent parent, DataSetSeismogram seis){
-		this(parent, seis, Color.blue);
+        this(parent, seis, Color.blue);
     }
-    
+
     public DrawableSeismogram(JComponent parent, DataSetSeismogram seis, Color color){
-		this(parent, seis, color, seis.toString());
+        this(parent, seis, color, seis.toString());
     }
-    
-    public DrawableSeismogram(JComponent parent, 
-							  DataSetSeismogram seis, 
-							  Color color, 
-							  String name){
-		this(parent, new SeismogramShape(parent, seis), color, name);
+
+    public DrawableSeismogram(JComponent parent,
+                              DataSetSeismogram seis,
+                              Color color,
+                              String name){
+        this(parent, new SeismogramShape(parent, seis), color, name);
     }
-    
+
     public DrawableSeismogram(JComponent parent, SeismogramShape shape, Color color, String name){
-		this.parent = parent;
-		this.color = color;
-		this.name = name;
-		this.shape = shape;
+        this.parent = parent;
+        this.color = color;
+        this.name = name;
+        this.shape = shape;
     }
-	
+
     public JComponent getParent() {
-		return parent;
+        return parent;
     }
-    
+
     public void setVisibility(boolean b){
-		visible = b;
+        visible = b;
     }
-    
+
     public boolean getVisiblity(){ return visible; }
-    
+
     public void toggleVisibility(){
     }
-    
+
     public void draw(Graphics2D canvas,
-					 Dimension size,
-					 TimeEvent currentTime,
-					 AmpEvent currentAmp){
-		if(visible && size.width > 0 && size.height > 0){
-			canvas.setPaint(color);
-			shape.update(currentTime.getTime(shape.getSeismogram()),
-						 currentAmp.getAmp(shape.getSeismogram()),
-						 size);
-			canvas.draw(shape);
-		}
+                     Dimension size,
+                     TimeEvent currentTime,
+                     AmpEvent currentAmp){
+        if(visible && size.width > 0 && size.height > 0){
+            canvas.setPaint(color);
+            if(shape.update(currentTime.getTime(shape.getSeismogram()),
+                            currentAmp.getAmp(shape.getSeismogram()),
+                            size)){
+                canvas.draw(shape);
+            }
+        }
     }
-    
+
     public boolean drawName(Graphics2D canvas, int xPosition, int yPosition){
-		if(visible){
-			canvas.setPaint(color);
-			canvas.drawString(name, xPosition, yPosition);
-			return true;
-		}
-		return false;
+        if(visible){
+            canvas.setPaint(color);
+            canvas.drawString(name, xPosition, yPosition);
+            return true;
+        }
+        return false;
     }
-    
+
     public String getName(){ return name; }
-    
+
     public String toString(){ return name; }
-    
+
     public DataSetSeismogram getSeismogram(){ return shape.getSeismogram(); }
-    
-    private JComponent parent; 
-	
-	private Color color;
-    
+
+    private JComponent parent;
+
+    private Color color;
+
     private String name;
-    
+
     protected SeismogramShape shape;
-    
+
     private boolean visible = true;
-    
+
 }
 
