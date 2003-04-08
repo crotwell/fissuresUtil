32727043f0322abbd50ee5792f0e8e49538c0b6c
Philip Crotwell <crotwell@seis.sc.edu>
2003-04-08 13:33:36 -0400

    [svn r3652] allow names and codes to both/either be used

--- src/edu/sc/seis/fissuresUtil/chooser/NameListCellRenderer.java
+++ src/edu/sc/seis/fissuresUtil/chooser/NameListCellRenderer.java
@@ -9,75 +9,96 @@ import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
 
 class NameListCellRenderer extends DefaultListCellRenderer {
+
     NameListCellRenderer(boolean useNames){
-	this.useNames = useNames;
+        this(useNames, ! useNames, true);
+    }
+
+    /**
+     * Creates a new <code>NameListCellRenderer</code> instance.
+     *
+     * @param useNames true if names should be used
+     * @param useCodes true if codes should be used
+     * @param codeIsFirst true if the string should be code - name, false if
+     *    it should be name - code
+     */
+    NameListCellRenderer(boolean useNames,
+                         boolean useCodes,
+                         boolean codeIsFirst){
+        this.useNames = useNames;
+        this.useCodes = useCodes;
+        this.codeIsFirst = codeIsFirst;
+    }
+
+    private String getDisplayString(String name, String code) {
+        String out = "";
+        if (useNames) {
+            out = name;
+            if ((name == null || name.length() == 0) && ! useCodes) {
+                out = code;
+            }
+            if ( useCodes && codeIsFirst) {
+                out = code +" - "+out;
+            }
+            if ( useCodes && ! codeIsFirst) {
+                out = out + " - "+ code;
+            }
+            return out;
+        }
+        return code; 
     }
 
     public Component getListCellRendererComponent(JList list,
-						  Object value,
-						  int index,
-						  boolean isSelected,
-						  boolean cellHasFocus) {
-	String name = "XXXX";
-	if (value instanceof NetworkAccess) {
-	    if (useNames) {
-		name = ((NetworkAccess)value).get_attributes().name;
-		if (name == null || name.length() == 0) {
-		    name = ((NetworkAccess)value).get_attributes().get_code();
-		    if (name.startsWith("X") || name.startsWith("Y") || name.startsWith("Z")) {
-			edu.iris.Fissures.Time start =
-			    ((NetworkAccess)value).get_attributes().get_id().begin_time;
-			name += start.date_time.substring(2,4);
-		    } // end of if (name.startsWith("X"))
+                                                  Object value,
+                                                  int index,
+                                                  boolean isSelected,
+                                                  boolean cellHasFocus) {
+        String name = "XXXX";
+        if (value instanceof NetworkAccess) {
+            String netCode =
+                ((NetworkAccess)value).get_attributes().get_code();
+            if (netCode.startsWith("X") || 
+                netCode.startsWith("Y") ||
+                netCode.startsWith("Z")) {
+                edu.iris.Fissures.Time start =
+                    ((NetworkAccess)value).get_attributes().get_id().begin_time;
+                netCode += start.date_time.substring(2,4);
+            }
+            name = 
+                getDisplayString(((NetworkAccess)value).get_attributes().name,
+                                 netCode);    
 		    
-		}
-	    } else {
-		name = ((NetworkAccess)value).get_attributes().get_code();
-	    }
-	}
-	if (value instanceof Station) {
-	    if (useNames) {
-		name = ((Station)value).name;
-		// assume name of length 1 isn't a name
-		if (name == null || name.length() <= 1) {
-		    name = ((Station)value).get_code();
-		}
-	    } else {
-		name = ((Station)value).get_code();
-	    }
-	}
-	if (value instanceof Site) {
-	    if (useNames) {
-		name = ((Site)value).get_code();
-		if (name == null || name.length() == 0) {
-		    name = ((Site)value).get_code();
-		}
-	    } else {
-		name = ((Site)value).get_code();
-	    }
-	}
-	
-	if (value instanceof Channel) {
-	    if (useNames) {
-		name = ((Channel)value).name;
-		if (name == null || name.length() == 0) {
-		    name = ((Channel)value).get_code();
-		}
-	    } else {
-		name = ((Channel)value).get_code();
-	    }
-	}
-	
-	if (value instanceof String) {
-	    name = (String)value;
-	} // end of if (value instanceof String)
-	
-	
-	return super.getListCellRendererComponent(list,
-						  name,
-						  index,
-						  isSelected,
-						  cellHasFocus);
         }
+        if (value instanceof Station) {
+            name = getDisplayString(((Station)value).name,
+                                    ((Station)value).get_code());
+        }
+        if (value instanceof Site) {
+            // sites do not have names
+            name = ((Site)value).get_code();
+        }
+	
+        if (value instanceof Channel) {
+            name = getDisplayString(((Channel)value).name,
+                                    ((Channel)value).get_code());
+        }
+	
+        if (value instanceof String) {
+            name = (String)value;
+        } // end of if (value instanceof String)
+	
+	
+        return super.getListCellRendererComponent(list,
+                                                  name,
+                                                  index,
+                                                  isSelected,
+                                                  cellHasFocus);
+    }
+
     boolean useNames;
+
+    boolean useCodes;
+
+    boolean codeIsFirst;
+
 }
