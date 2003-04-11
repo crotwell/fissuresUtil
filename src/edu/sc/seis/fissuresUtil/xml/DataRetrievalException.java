9345b76008455bfe46c587f80c271ccde6b0299e
Philip Crotwell <crotwell@seis.sc.edu>
2003-04-11 15:21:35 -0400

    [svn r3668] added javadoc

--- src/edu/sc/seis/fissuresUtil/xml/DataRetrievalException.java
+++ src/edu/sc/seis/fissuresUtil/xml/DataRetrievalException.java
@@ -3,7 +3,7 @@ package edu.sc.seis.fissuresUtil.xml;
 import edu.sc.seis.fissuresUtil.exceptionHandlerGUI.WrappedException;
 
 /**
- * DataRetrievalException.java
+ * Exception to be thrown if there is an error in retrieving data.
  *
  *
  * Created: Fri Apr 11 14:57:41 2003
