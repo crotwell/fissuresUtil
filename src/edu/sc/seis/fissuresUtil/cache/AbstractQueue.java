1caa3beddaae0def3e512c3ad3147132d2838e41
Philip Crotwell <crotwell@seis.sc.edu>
2003-04-23 14:37:35 -0400

    [svn r3737] moved the generic push and pop methods to AbstractQueue from EvenQueue for easier reuse

--- src/edu/sc/seis/fissuresUtil/cache/AbstractQueue.java
+++ src/edu/sc/seis/fissuresUtil/cache/AbstractQueue.java
@@ -1,10 +1,84 @@
 package edu.sc.seis.fissuresUtil.cache;
+import java.util.List;
+import java.util.LinkedList;
+
+
 
 public abstract class AbstractQueue {
-	//data structure to hold the values.???
-	public AbstractQueue() {
+    //data structure to hold the values.???
+    public AbstractQueue() {
 
-	}
-	public abstract void push(java.lang.Object obj);
-	public abstract java.lang.Object pop(); 
-};
+    }
+
+
+    /**
+     * pops the first element of the queue.
+     * @return a <code>java.lang.Object</code> value
+     */
+    public synchronized java.lang.Object pop() {
+
+        while(list.size() == 0 && sourceAlive == true) {
+            try {
+                System.out.println("Waiting in POP");
+                wait();
+            } catch(InterruptedException ie) { }
+
+        }
+        if(list.size() == 0) return null;
+        java.lang.Object obj = list.get(list.size()-1);
+        list.remove(list.size() - 1);
+        notifyAll();
+        return obj;
+
+    }
+
+
+    /**
+     * inserts the obj at the end of the queue.
+     *
+     * @param obj a <code>java.lang.Object</code> value to be inserted into the queue.
+     */
+    public synchronized void push(java.lang.Object obj) {
+        list.add(0, obj);
+        notifyAll();
+    }
+
+
+    /**
+     * returns the length of the queue.
+     *
+     * @return an <code>int</code> value
+     */
+    public synchronized int getLength() {
+
+        return list.size();
+
+    }
+
+    /**
+     * sets if the source i.e., the thread which pushes objects into the queue
+     * is alive
+     *
+     * @param value a <code>boolean</code> value
+     */
+    public synchronized void setSourceAlive(boolean value) {
+
+        this.sourceAlive = value;
+        notifyAll();
+    }
+
+    /**
+     * returns true if the source i.e., the thread which pushes objects into the queue
+     * is alive, else returns false.
+     *
+     * @return a <code>boolean</code> value
+     */
+    public synchronized boolean getSourceAlive() {
+        return this.sourceAlive;
+    }
+
+    private boolean sourceAlive = true;
+
+    private List list = new LinkedList();
+
+}
--- src/edu/sc/seis/fissuresUtil/cache/EventQueue.java
+++ src/edu/sc/seis/fissuresUtil/cache/EventQueue.java
@@ -6,8 +6,8 @@ import edu.iris.Fissures.*;
 import java.util.*;
 
 /**
- * This is an implementation of the Queue DataStructure. 
- * This  class is thread safe.  
+ * This is an implementation of the Queue DataStructure.
+ * This  class is thread safe.
  *
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version 1.0
@@ -20,83 +20,8 @@ public class EventQueue extends AbstractQueue {
      *
      */
     public EventQueue() {
-	super();
-		
-    }
-
-    /**
-     * inserts the obj at the end of the queue.
-     * 
-     * @param obj a <code>java.lang.Object</code> value to be inserted into the queue.
-     */
-    public synchronized void push(java.lang.Object obj) {
-		
-	while(list.size() == 24) {
-	    try {
-		System.out.println("Watiting in PUSH");
-		wait();
-	    } catch(InterruptedException ie) { }
-	}
-	list.add(0, obj);
-	notifyAll();
-    }
-
-    /**
-     * pops the first element of the queue.
-     * @return a <code>java.lang.Object</code> value
-     */
-    public synchronized java.lang.Object pop() {
-	
-	while(list.size() == 0 && sourceAlive == true) {
-	    try {
-		System.out.println("Waiting in POP");
-		wait();
-	    } catch(InterruptedException ie) { }
-
-	}
-	if(list.size() == 0) return null;
-	java.lang.Object obj = list.get(list.size()-1);
-	list.remove(list.size() - 1);
-	notifyAll();
-	return obj;
+    super();
 
     }
 
-    /**
-     * returns the length of the queue.
-     *
-     * @return an <code>int</code> value
-     */
-    public synchronized int getLength() {
-	
-	return list.size();
-	
-    }
-
-    /**
-     * sets if the source i.e., the thread which pushes objects into the queue
-     * is alive 
-     *
-     * @param value a <code>boolean</code> value
-     */
-    public synchronized void setSourceAlive(boolean value) {
-
-	this.sourceAlive = value;
-	notifyAll();
-    }
-    
-    /**
-     * returns true if the source i.e., the thread which pushes objects into the queue
-     * is alive, else returns false.
-     *
-     * @return a <code>boolean</code> value
-     */
-    public synchronized boolean getSourceAlive() {
-	return this.sourceAlive;
-    }
-	
-    private boolean sourceAlive = true;
-
-    private List list = new LinkedList();
-	
 }
