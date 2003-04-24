b0ede493691663c3053ac8ee105af5468df69ec3
Charles Groves <groves@seis.sc.edu>
2003-04-24 14:57:25 -0400

    [svn r3750] added ability to calculate over several local seismograms for every datasetseismogram

--- src/edu/sc/seis/fissuresUtil/display/RMeanAmpConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/RMeanAmpConfig.java
@@ -36,7 +36,7 @@ public class RMeanAmpConfig extends BasicAmpConfig {
                 }
             }else if(current.hasNewData()){
                 setAmpRange(current.getDSS());
-                    changed = true;
+                changed = true;
             }
         }
         if(changed || currentAmpEvent == null){
@@ -68,38 +68,58 @@ public class RMeanAmpConfig extends BasicAmpConfig {
     private boolean setAmpRange(DataSetSeismogram seismo){
         AmpConfigData data = (AmpConfigData)ampData.get(seismo);
 
-    if ( data.getSeismograms().length == 0) {
-        return data.setCleanRange(DisplayUtils.ZERO_RANGE);
-    } // end of if ()
+        if ( data.getSeismograms().length == 0) {
+            return data.setCleanRange(DisplayUtils.ZERO_RANGE);
+        } // end of if ()
 
-        LocalSeismogramImpl seis = data.getSeismograms()[0];
-        int[] seisIndex = DisplayUtils.getSeisPoints(seis, data.getTime());
-        if(seisIndex[1] < 0 || seisIndex[0] >= seis.getNumPoints()) {
-            //no data points in window, set range to 0
-            data.setCalcIndex(seisIndex);
+        LocalSeismogramImpl[] seismograms = data.getSeismograms();
+        double points = 0;
+        double meanTotal = 0;
+        double[] minMaxAll = new double[2];
+        minMaxAll[0] = Double.POSITIVE_INFINITY;
+        minMaxAll[1] = Double.NEGATIVE_INFINITY;
+        boolean dataInRange = false;
+        for(int i = 0; i < seismograms.length; i++){
+            LocalSeismogramImpl seis = seismograms[i];
+            int[] seisIndex = DisplayUtils.getSeisPoints(seis, data.getTime());
+            if(seisIndex[1] < 0 || seisIndex[0] >= seis.getNumPoints()) {
+                //no data points in window, don't calculate range
+                break;
+            }
+            dataInRange = true;
+            if(seisIndex[0] < 0){
+                seisIndex[0] = 0;
+            }
+            if(seisIndex[1] >= seis.getNumPoints()){
+                seisIndex[1] = seis.getNumPoints() -1;
+            }
+            double[] minMaxMean =
+                data.getStatistics(seis).minMaxMean(seisIndex[0], seisIndex[1]);
+            if(minMaxMean[0] < minMaxAll[0]){
+                minMaxAll[0] = minMaxMean[0];
+            }
+            if(minMaxMean[1] > minMaxAll[1]){
+                minMaxAll[1] = minMaxMean[1];
+            }
+            points += seisIndex[1] - seisIndex[0];
+            meanTotal += minMaxMean[2]*(seisIndex[1] - seisIndex[0]);
+        }
+        if(dataInRange){
+            double mean = meanTotal/points;
+            double meanDiff;
+            double maxToMeanDiff = Math.abs(mean - minMaxAll[1]);
+            double minToMeanDiff = Math.abs(mean - minMaxAll[0]);
+            if(maxToMeanDiff > minToMeanDiff){
+                meanDiff = maxToMeanDiff;
+            }else{
+                meanDiff = minToMeanDiff;
+            }
+            double min = mean - meanDiff;
+            double max = mean + meanDiff;
+            return data.setCleanRange(new UnitRangeImpl(min, max, UnitImpl.COUNT));
+        }else{
             return data.setCleanRange(DisplayUtils.ZERO_RANGE);
         }
-
-        if(seisIndex[0] < 0){
-            seisIndex[0] = 0;
-        }
-        if(seisIndex[1] >= seis.getNumPoints()){
-            seisIndex[1] = seis.getNumPoints() -1;
-        }
-        double[] minMaxMean =
-            data.getStatistics(seis).minMaxMean(seisIndex[0], seisIndex[1]);
-        double meanDiff;
-        double maxToMeanDiff = Math.abs(minMaxMean[2] - minMaxMean[1]);
-        double minToMeanDiff = Math.abs(minMaxMean[2] - minMaxMean[0]);
-        if(maxToMeanDiff > minToMeanDiff){
-            meanDiff = maxToMeanDiff;
-        }else{
-            meanDiff = minToMeanDiff;
-        }
-        data.setCalcIndex(seisIndex);
-        double min = minMaxMean[2] - meanDiff;
-        double max = minMaxMean[2] + meanDiff;
-        return data.setCleanRange(new UnitRangeImpl(min, max, UnitImpl.COUNT));
     }
 
     private UnitRangeImpl setRange(UnitRangeImpl currRange, double range){
