3fae9b34dd8a9652867bfafdec267fd8cbbdcb19
crotwell <crotwell@localhost>
2003-05-06 21:28:21 -0400

    [svn r3828] add check for array lengths

--- src/edu/sc/seis/fissuresUtil/display/AmpEvent.java
+++ src/edu/sc/seis/fissuresUtil/display/AmpEvent.java
@@ -15,46 +15,50 @@ import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 
 public class AmpEvent {
     public AmpEvent(DataSetSeismogram[] seismos, UnitRangeImpl[] amps){
-	this.seismos = seismos;
-	this.amps = amps;
+        if ( seismos.length != amps.length) {
+            throw new IllegalArgumentException("seismogram and amp arrays must have equal length "+seismos.length+" != "+amps.length);
+        } // end of if ()
+        
+        this.seismos = seismos;
+        this.amps = amps;
     }
     
     public UnitRangeImpl getAmp(DataSetSeismogram seismo){
-	return amps[indexOf(seismo)];
+        return amps[indexOf(seismo)];
     }
     
     public UnitRangeImpl getAmp(){
-	if(genericAmp == null){
-	    if(amps.length == 0){
+        if(genericAmp == null){
+            if(amps.length == 0){
                 genericAmp = new UnitRangeImpl(-1, 1, UnitImpl.COUNT);
-	    } else if(amps.length == 1){
-		genericAmp = amps[0];
-	    }else{
-		boolean equal = true;
-		for(int i = 1; i < amps.length; i++){
-		    if(!amps[i].equals(amps[i-1])){
-			equal = false;
-			i = amps.length;
-		    }
-		}
-		if(equal){
-		    genericAmp = amps[0];
-		}else{
-		    double halfRange = (amps[0].getMaxValue() - amps[0].getMinValue())/2;
-		    genericAmp = new UnitRangeImpl(-halfRange, halfRange, amps[0].getUnit());
-		}
-	    }
-	}
-	return genericAmp;
+            } else if(amps.length == 1){
+                genericAmp = amps[0];
+            }else{
+                boolean equal = true;
+                for(int i = 1; i < amps.length; i++){
+                    if(!amps[i].equals(amps[i-1])){
+                        equal = false;
+                        i = amps.length;
+                    }
+                }
+                if(equal){
+                    genericAmp = amps[0];
+                }else{
+                    double halfRange = (amps[0].getMaxValue() - amps[0].getMinValue())/2;
+                    genericAmp = new UnitRangeImpl(-halfRange, halfRange, amps[0].getUnit());
+                }
+            }
+        }
+        return genericAmp;
     }
 
     private int indexOf(DataSetSeismogram seismo){
-	for(int i = 0; i < seismos.length; i++){
-	    if(seismos[i] == seismo){
-		return i;
-	    }
-	}
-	return 0;
+        for(int i = 0; i < seismos.length; i++){
+            if(seismos[i] == seismo){
+                return i;
+            }
+        }
+        return 0;
     }
 
     private DataSetSeismogram[] seismos;
