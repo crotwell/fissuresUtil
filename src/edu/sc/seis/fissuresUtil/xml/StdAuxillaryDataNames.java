af66380f9609c4642089eb01b78b4b6c0891251d
crotwell <crotwell@localhost>
2003-05-12 16:05:29 -0400

    [svn r3857] initial

---
+++ src/edu/sc/seis/fissuresUtil/xml/StdAuxillaryDataNames.java
@@ -0,0 +1,16 @@
+/**
+ * StdAuxillaryDataNames.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.xml;
+
+public interface StdAuxillaryDataNames
+{
+    public static final String prefix = StdAuxillaryDataNames.class.getName();
+
+    public static final String RESPONSE = prefix+"/RESPONSE";
+
+}
+
