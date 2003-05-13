c90e8ad11433f3659487bb2e56b23c379c2426ac
crotwell <crotwell@localhost>
2003-05-13 13:53:16 -0400

    [svn r3865] made double version of method use the float version to avoid 2 sets of similar code

--- src/edu/sc/seis/fissuresUtil/display/SeismogramShapeIterator.java
+++ src/edu/sc/seis/fissuresUtil/display/SeismogramShapeIterator.java
@@ -20,14 +20,14 @@ public class SeismogramShapeIterator implements PathIterator {
     public SeismogramShapeIterator(SeismogramShapeIterator brother){
         this(brother.getTime(), brother.getAmp(), brother.getSize());
     }
-    
+
     public SeismogramShapeIterator(MicroSecondTimeRange time, UnitRangeImpl amp,
                                    Dimension size){
         this.time = time;
         this.amp = amp;
         this.size = size;
     }
-    
+
     public boolean isDraggedFrom(SeismogramShapeIterator otherIterator){
         if(otherIterator == null ||
            !size.equals(otherIterator.getSize()) ||
@@ -36,7 +36,7 @@ public class SeismogramShapeIterator implements PathIterator {
         }
         return true;
     }
-    
+
     //checks if amp changes between these two plot info objects are within
     //one pixel
     public boolean hasSimilarAmp(SeismogramShapeIterator otherIterator){
@@ -54,117 +54,117 @@ public class SeismogramShapeIterator implements PathIterator {
         }
         return false;
     }
-    
+
     public void copyBasicInfo(SeismogramShapeIterator iterator){
         setPointsPerPixel(iterator.getPointsPerPixel());
         setBaseSeisPoint(iterator.getBaseSeisPoint());
         setPoints(iterator.getPoints());
     }
-    
+
     public Dimension getSize(){
         return size;
     }
-    
+
     public void setTime(MicroSecondTimeRange time){
         this.time = time;
     }
-    
+
     public MicroSecondTimeRange getTime(){
         return time;
     }
-    
+
     public void setAmp(UnitRangeImpl amp){
         this.amp = amp;
     }
-    
+
     public UnitRangeImpl getAmp(){
         return amp;
     }
-    
+
     public void setPointsPerPixel(){
         setPointsPerPixel((seisPoints[1] - seisPoints[0])/(double)size.width);
     }
-    
+
     public void setPointsPerPixel(double pointsPerPixel){
         this.pointsPerPixel = pointsPerPixel;
     }
-    
+
     public double getPointsPerPixel()
     {
         return pointsPerPixel;
     }
-    
+
     public void setDrawnPixels(int[] drawnPixels){
         this.drawnPixels = drawnPixels;
         startIndex = drawnPixels[0];
         endIndex = drawnPixels[1];
         currentIndex = startIndex;
     }
-    
+
     public int[] getDrawnPixels(){
         return drawnPixels;
     }
-    
+
     public void setPoints(int[][] points){
         this.points = points;
     }
-    
+
     public int[][] getPoints(){
         return points;
     }
-    
+
     public void setSeisPoints(int[] seisPoints){
         this.seisPoints = seisPoints;
     }
-    
+
     public int[] getSeisPoints(){
         return seisPoints;
     }
-    
+
     public void setLeftoverPixels(double leftoverPixels){
         this.leftoverPixels = leftoverPixels;
     }
-    
+
     public double getLeftoverPixels(){
         return leftoverPixels;
     }
-    
+
     public void setBaseSeisPoint(){
         setBaseSeisPoint(seisPoints[0]);
     }
-    
+
     public void setBaseSeisPoint(int baseSeisPoint){
         this.baseSeisPoint = baseSeisPoint;
     }
-    
+
     public int getBaseSeisPoint(){
         return baseSeisPoint;
     }
-    
+
     public void setTotalShift(int totalShift){
         this.totalShift = totalShift;
     }
-    
+
     public int getTotalShift(){
         return totalShift;
     }
-    
+
     public void setAT(AffineTransform at){
         this.at = at;
     }
-    
+
     public AffineTransform getAt(){
         return at;
     }
-    
+
     public void next(){
         currentIndex++;
     }
-    
+
     public int getWindingRule(){
         return WIND_NON_ZERO;
     }
-    
+
     public boolean isDone(){
         if(currentIndex == endIndex){
             currentIndex = startIndex;
@@ -173,7 +173,7 @@ public class SeismogramShapeIterator implements PathIterator {
         }
         return false;
     }
-    
+
     public int currentSegment(float[] coordinates){
         int i = 0;
         if(min){
@@ -192,53 +192,42 @@ public class SeismogramShapeIterator implements PathIterator {
             return SEG_LINETO;
         }
     }
-    
+
     public int currentSegment(double[] coordinates){
-        int i = 0;
-        if(min){
-            i = 1;
-            currentIndex--;
-        }
-        min = !min;
-        coordinates[0] = currentIndex;
-        coordinates[1] = points[i][currentIndex];
-        if(at != null){
-            at.transform(coordinates, 0, coordinates, 0, 1);
-        }
-        if(currentIndex == startIndex){
-            return SEG_MOVETO;
-        }else{
-            return SEG_LINETO;
-        }
+        float[] temp = new float[2];
+        int val = currentSegment(temp);
+        coordinates[0] = temp[0];
+        coordinates[1] = temp[1];
+        return val;
     }
-    
+
     private boolean min = false;
-    
+
     private int totalShift = 0;
-    
+
     private double leftoverPixels = 0;
-    
+
     private int baseSeisPoint;
-    
+
     private int[] seisPoints;
-    
+
     private int[][] points;
-    
+
     private int[] drawnPixels;
-    
+
     private double pointsPerPixel;
-    
+
     private UnitRangeImpl amp;
-    
+
     private MicroSecondTimeRange time;
-    
+
     private Dimension size;
-    
+
     protected int startIndex, endIndex, currentIndex;
-    
-    protected AffineTransform at; 
-    
+
+    protected AffineTransform at;
+
     private static Category logger =
-        Category.getInstance(SeismogramShapeIterator.class.getName());    
+        Category.getInstance(SeismogramShapeIterator.class.getName());
 }// SeismogramShapeIterator
 
