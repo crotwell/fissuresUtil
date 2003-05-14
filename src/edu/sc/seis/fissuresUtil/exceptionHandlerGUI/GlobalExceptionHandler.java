1eef7d6365768d3be3978d7fadf2e5207c2343a7
Philip Crotwell <crotwell@seis.sc.edu>
2003-05-14 16:25:02 -0400

    [svn r3874] actually show the frame

--- src/edu/sc/seis/fissuresUtil/exceptionHandlerGUI/GlobalExceptionHandler.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandlerGUI/GlobalExceptionHandler.java
@@ -6,11 +6,17 @@
 
 package edu.sc.seis.fissuresUtil.exceptionHandlerGUI;
 
+import javax.swing.JFrame;
+
 public class GlobalExceptionHandler
 {
 
     public void handle(Throwable thrown) {
-        ExceptionHandlerGUI.getExceptionHandlerGUI("An uncaught exception has occured. Please report this to geebugs@seis.sc.edu", thrown);
+        ExceptionHandlerGUI exceptH =
+            ExceptionHandlerGUI.getExceptionHandlerGUI("An uncaught exception has occured. Please report this to geebugs@seis.sc.edu", thrown);
+        JFrame frame = exceptH.display();
+        //frame.pack();
+        frame.show();
     }
 
 }
