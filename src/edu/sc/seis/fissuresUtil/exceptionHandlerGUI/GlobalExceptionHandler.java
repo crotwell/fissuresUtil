2d4a9a7dfb523651bf2f1fe342ac96d17e9427f1
crotwell <crotwell@localhost>
2003-05-16 13:23:01 -0400

    [svn r3906] add methods to allow a String message

--- src/edu/sc/seis/fissuresUtil/exceptionHandlerGUI/GlobalExceptionHandler.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandlerGUI/GlobalExceptionHandler.java
@@ -7,11 +7,17 @@
 package edu.sc.seis.fissuresUtil.exceptionHandlerGUI;
 
 import javax.swing.JFrame;
+import org.apache.log4j.Logger;
 
 public class GlobalExceptionHandler
 {
 
     public void handle(Throwable thrown) {
+        handle("A problem has occured.", thrown);
+    }
+
+    public void handle(String message, Throwable thrown) {
+        logger.error(message, thrown);
         ExceptionHandlerGUI exceptH =
             ExceptionHandlerGUI.getExceptionHandlerGUI("An uncaught exception has occured. Please report this to geebugs@seis.sc.edu", thrown);
         JFrame frame = exceptH.display();
@@ -23,5 +29,13 @@ public class GlobalExceptionHandler
         GlobalExceptionHandler g = new GlobalExceptionHandler();
         g.handle(thrown);
     }
+
+    public static void handleStatic(String message, Throwable thrown) {
+        GlobalExceptionHandler g = new GlobalExceptionHandler();
+        g.handle(message, thrown);
+    }
+
+    Logger logger = Logger.getLogger(GlobalExceptionHandler.class);
+
 }
 
