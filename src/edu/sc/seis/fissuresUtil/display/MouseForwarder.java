91b7a38a260ca73213cccbc61d880b26d5d0bafc
groves <groves@localhost>
2003-05-26 22:48:41 -0400

    [svn r4067] removed no arg constructor

--- src/edu/sc/seis/fissuresUtil/display/MouseForwarder.java
+++ src/edu/sc/seis/fissuresUtil/display/MouseForwarder.java
@@ -15,93 +15,88 @@ import javax.swing.event.EventListenerList;
  */
 
 public class MouseForwarder implements MouseListener {
-    
-    public MouseForwarder() {
-	
-    }
-
     public void setMouseListener(MouseListener m){
-	if(current != null){
-	    listenerList.remove(MouseListener.class, current);
-	}
-	current = m;
-	listenerList.add(MouseListener.class, m);
+        if(current != null){
+            listenerList.remove(MouseListener.class, current);
+        }
+        current = m;
+        listenerList.add(MouseListener.class, m);
     }
 
     public void addMouseListener(MouseListener m) {
-	if(current != null)
-	    listenerList.remove(MouseListener.class, current);
-	current = m;
-	listenerList.add(MouseListener.class, m);
+        if(current != null)
+            listenerList.remove(MouseListener.class, current);
+        current = m;
+        listenerList.add(MouseListener.class, m);
     }
 
     public void removeMouseListener(MouseListener m) {
-	current = null;
-	listenerList.remove(MouseListener.class, m);
+        current = null;
+        listenerList.remove(MouseListener.class, m);
     }
 
     public void removeMouseListener(){
-	if(current != null){
-	    listenerList.remove(MouseListener.class, current);
-	    current = null;
-	}
+        if(current != null){
+            listenerList.remove(MouseListener.class, current);
+            current = null;
+        }
     }
 
     public void mouseClicked(MouseEvent e) {
-	Object[] listeners = listenerList.getListenerList();
-	// Process the listeners last to first, notifying
-	// those that are interested in this event
-	for (int i = listeners.length-2; i>=0; i-=2) {
-	    if (listeners[i]==MouseListener.class) {
-		((MouseListener)listeners[i+1]).mouseClicked(e);
-	    }              
-	}
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==MouseListener.class) {
+                ((MouseListener)listeners[i+1]).mouseClicked(e);
+            }
+        }
     }
 
     public void mousePressed(MouseEvent e) {
-	Object[] listeners = listenerList.getListenerList();
-	// Process the listeners last to first, notifying
-	// those that are interested in this event
-	for (int i = listeners.length-2; i>=0; i-=2) {
-	    if (listeners[i]==MouseListener.class) {
-		((MouseListener)listeners[i+1]).mousePressed(e);
-	    }              
-	}
-     }
-    
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==MouseListener.class) {
+                ((MouseListener)listeners[i+1]).mousePressed(e);
+            }
+        }
+    }
+
     public void mouseReleased(MouseEvent e) {
-	Object[] listeners = listenerList.getListenerList();
-	// Process the listeners last to first, notifying
-	// those that are interested in this event
-	for (int i = listeners.length-2; i>=0; i-=2) {
-	    if (listeners[i]==MouseListener.class) {
-		((MouseListener)listeners[i+1]).mouseReleased(e);
-	    }              
-	}
-     }
-    
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==MouseListener.class) {
+                ((MouseListener)listeners[i+1]).mouseReleased(e);
+            }
+        }
+    }
+
     public void mouseEntered(MouseEvent e) {
-	Object[] listeners = listenerList.getListenerList();
-	// Process the listeners last to first, notifying
-	// those that are interested in this event
-	for (int i = listeners.length-2; i>=0; i-=2) {
-	    if (listeners[i]==MouseListener.class) {
-		((MouseListener)listeners[i+1]).mouseEntered(e);
-	    }              
-	}
-     }
-    
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==MouseListener.class) {
+                ((MouseListener)listeners[i+1]).mouseEntered(e);
+            }
+        }
+    }
+
     public void mouseExited(MouseEvent e) {
-	Object[] listeners = listenerList.getListenerList();
-	// Process the listeners last to first, notifying
-	// those that are interested in this event
-	for (int i = listeners.length-2; i>=0; i-=2) {
-	    if (listeners[i]==MouseListener.class) {
-		((MouseListener)listeners[i+1]).mouseExited(e);
-	    }              
-	}
-     }
-    
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==MouseListener.class) {
+                ((MouseListener)listeners[i+1]).mouseExited(e);
+            }
+        }
+    }
+
     protected EventListenerList listenerList = new EventListenerList();
 
     protected MouseListener current;
