b7f78ba1c1b61cfe5944ef9e1d4eb85754a31f10
Charles Groves <groves@seis.sc.edu>
2003-05-26 22:44:12 -0400

    [svn r4062] initial commit of RecordSection things

---
+++ src/edu/sc/seis/fissuresUtil/display/RecordSectionDisplay.java
@@ -0,0 +1,266 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import edu.sc.seis.fissuresUtil.display.registrar.*;
+
+import edu.sc.seis.fissuresUtil.display.drawable.DrawableSeismogram;
+import edu.sc.seis.fissuresUtil.freq.ColoredFilter;
+import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.swing.BorderFactory;
+
+public class RecordSectionDisplay extends SeismogramDisplay implements ConfigListener, LayoutListener{
+
+    public RecordSectionDisplay(){
+        addMouseMotionListener(SeismogramDisplay.getMouseMotionForwarder());
+        addMouseListener(SeismogramDisplay.getMouseForwarder());
+        border = new ScaleBorder();
+        setBorder(BorderFactory.createCompoundBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(),
+                                                                                        new LeftTitleBorder("")),
+                                                     BorderFactory.createCompoundBorder(border,
+                                                                                        BorderFactory.createLoweredBevelBorder())));
+
+        addComponentListener(new ComponentAdapter() {
+                    public void componentResized(ComponentEvent e) {
+                        resize();
+                    }
+                });
+    }
+
+
+    public RecordSectionDisplay(DataSetSeismogram[] seismos, TimeConfig tc,
+                                AmpConfig ac){
+        this();
+        setRegistrar(new Registrar(seismos, tc, ac));
+        add(seismos);
+    }
+
+    public synchronized void add(DataSetSeismogram[] seismos){
+        updating = true;
+        if(registrar == null){
+            setRegistrar(new Registrar(seismos, new BasicTimeConfig(), new RMeanAmpConfig()));
+        }else{
+            registrar.add(seismos);
+        }
+        if(layout == null){
+            setLayout(new BasicLayoutConfig(seismos));
+        }else{
+            layout.add(seismos);
+        }
+        for (int i = 0; i < seismos.length; i++){
+            dssPlotter.put(seismos[i], new DrawableSeismogram(this, seismos[i]));
+        }
+        updating = false;
+        repaint();
+    }
+
+    public void setRegistrar(Registrar registrar) {
+        if(this.registrar != null){
+            this.registrar.removeListener(this);
+            this.registrar.remove(getSeismograms());
+        }
+        timeScaleMap = new TimeScaleCalc(getSize().width, registrar);
+        border.setBottomScaleMapper(timeScaleMap);
+        registrar.add(getSeismograms());
+        registrar.addListener(this);
+        this.registrar = registrar;
+    }
+
+    public Registrar getRegistrar() {
+        return registrar;
+    }
+
+    public void setTimeConfig(TimeConfig tc){
+        if(registrar != null){
+            registrar.setTimeConfig(tc);
+        }else{
+            setRegistrar(new Registrar(getSeismograms(), tc, new RMeanAmpConfig()));
+        }
+    }
+
+    public TimeConfig getTimeConfig(){ return registrar.getTimeConfig(); }
+
+    public void setAmpConfig(AmpConfig ac){
+        if(registrar != null){
+            registrar.setAmpConfig(ac);
+        }else{
+            setRegistrar(new Registrar(getSeismograms(), new BasicTimeConfig(), ac));
+        }
+    }
+
+    public AmpConfig getAmpConfig(){ return registrar.getAmpConfig(); }
+
+    public void setLayout(LayoutConfig layout){
+        if(this.layout != null){
+            this.layout.removeListener(this);
+            this.layout.remove(getSeismograms());
+        }
+        layout.add(getSeismograms());
+        distanceScaler= new DistanceScaleMapper(getSize().height, 4, layout);
+        border.setLeftScaleMapper(distanceScaler);
+        layout.addListener(this);
+        this.layout = layout;
+    }
+
+    public synchronized DataSetSeismogram[] getSeismograms() {
+        return (DataSetSeismogram[])dssPlotter.keySet().toArray(new DataSetSeismogram[dssPlotter.keySet().size()]);
+    }
+
+    public void reset() {
+        registrar.reset();
+    }
+
+    public void reset(DataSetSeismogram[] seismos) {
+        registrar.reset(seismos);
+    }
+
+    public synchronized void clear() {
+        if(layout != null){
+            layout.remove(getSeismograms());
+        }
+        if(registrar != null){
+            registrar.remove(getSeismograms());
+        }
+        dssPlotter.clear();
+    }
+
+    public void removeAll(){
+        clear();
+        super.removeAll();
+    }
+
+    public synchronized void remove(DataSetSeismogram[] seismos) {
+        for (int i = 0; i < seismos.length; i++){
+            Iterator it = dssPlotter.keySet().iterator();
+            while(it.hasNext()){
+                if(it.next().equals(seismos[i])){
+                    it.remove();
+                }
+            }
+        }
+    }
+
+    public synchronized boolean contains(DataSetSeismogram seismo) {
+        DataSetSeismogram[] seismos = getSeismograms();
+        for (int i = 0; i < seismos.length; i++){
+            if(seismos[i].equals(seismo)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void update(ConfigEvent event) {
+        curTimeEvent = event.getTimeEvent();
+        curAmpEvent = event.getAmpEvent();
+        repaint();
+    }
+
+    public void updateTime(TimeEvent event) {
+        curTimeEvent = event;
+        repaint();
+    }
+
+    public void updateAmp(AmpEvent event) {
+        curAmpEvent = event;
+        repaint();
+    }
+
+    public void updateLayout(LayoutEvent event) {
+        curLayoutEvent = event;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g){
+        if(updating){
+            return;
+        }
+        Graphics2D g2 = (Graphics2D)g;
+        synchronized(this){
+            Dimension size = getSize();
+            Insets insets = getInsets();
+            size = new Dimension(size.width - insets.left - insets.right,
+                                 size.height - insets.top - insets.bottom);
+            g2.translate(insets.left, 0);
+            Iterator it = curLayoutEvent.iterator();
+            double curYPos = 0;
+            while(it.hasNext()){
+                LayoutData current = (LayoutData)it.next();
+                double neededYPos = current.getStart() * size.height;
+                double translate = neededYPos - curYPos;
+                g2.translate(0, translate);
+                curYPos = neededYPos;
+                int drawHeight = (int)((current.getEnd() - current.getStart())*size.height);
+                Dimension drawSize = new Dimension(size.width, drawHeight);
+                DrawableSeismogram cur = (DrawableSeismogram)dssPlotter.get(current.getSeis());
+                cur.draw(g2, drawSize, curTimeEvent, curAmpEvent);
+                cur.drawName(g2, 0, drawHeight);
+            }
+            g2.translate(-insets.left, -curYPos);
+            g2.setColor(Color.BLACK);
+        }
+    }
+
+    protected synchronized void resize(){
+        Insets insets = getInsets();
+        Dimension d = getSize();
+        d = new Dimension(d.width - insets.left - insets.right,
+                          d.height - insets.top - insets.bottom);
+        if(d.height <= 0 || d.width <= 0){
+            return;
+        }
+        if(timeScaleMap != null) timeScaleMap.setTotalPixels(d.width);
+        if(distanceScaler != null) distanceScaler.setTotalPixels(d.height);
+        repaint();
+    }
+
+    public void setCurrentTimeFlag(boolean visible){
+        //TODO
+    }
+
+    public void setOriginalVisibility(boolean visible) {
+        // TODO
+    }
+
+    public void applyFilter(ColoredFilter filter) {
+        // TODO
+    }
+
+    public void print() {
+        // TODO
+    }
+
+    public void setParticleAllowed(boolean allowed) {
+        // TODO
+    }
+
+    private Map dssPlotter = new HashMap();
+
+    private Registrar registrar;
+
+    private LayoutConfig layout;
+
+    private AmpEvent curAmpEvent;
+
+    private TimeEvent curTimeEvent;
+
+    private LayoutEvent curLayoutEvent = LayoutEvent.EMPTY_EVENT;
+
+    private TimeScaleCalc timeScaleMap;
+
+    private DistanceScaleMapper distanceScaler;
+
+    private ScaleBorder border;
+
+    private boolean updating;
+}
---
+++ src/edu/sc/seis/fissuresUtil/display/UnitRangeMapper.java
@@ -0,0 +1,156 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import edu.iris.Fissures.model.UnitImpl;
+import edu.iris.Fissures.model.UnitRangeImpl;
+import edu.sc.seis.fissuresUtil.display.AmpScaleMapper;
+import java.text.DecimalFormat;
+import org.apache.log4j.Logger;
+
+public class UnitRangeMapper implements ScaleMapper{
+
+    public UnitRangeMapper(int totalPixels, int hintPixels, boolean ascending){
+        this.totalPixels = totalPixels;
+        this.hintPixels = hintPixels;
+        this.ascending = ascending;
+    }
+
+    public int getPixelLocation(int i){
+        if(ascending){
+            return (int)Math.round(SimplePlotUtil.linearInterp(minTick, 0,
+                                                               calcRange, totalPixels,
+                                                               minTick + i * tickInc));
+        }else{
+            return (int)Math.round(SimplePlotUtil.linearInterp(minTick, 0,
+                                                               calcRange, totalPixels,
+                                                               minTick + (numTicks - i) * tickInc));
+        }
+    }
+
+    public String getLabel(int i) {
+        if (isLabelTick(i)) {
+            double value = minTick + i * tickInc;
+            double absValue = Math.abs(value);
+            // use regular notation
+            DecimalFormat df;
+            if (absValue < 10 && absValue != 0 ) {
+                // exponential notation
+                df = new DecimalFormat("0.00###");
+            } else {
+                df = new DecimalFormat("#.####");
+            }
+            return df.format(value);
+        } else {
+            return "";
+        }
+    }
+
+    public String getAxisLabel() {
+        return unitDisplayUtil.getNameForUnit(getUnit());
+    }
+
+    public UnitImpl getUnit() {
+        return range.getUnit();
+    }
+
+    public int getNumTicks() {
+        return numTicks;
+    }
+
+    public boolean isMajorTick(int i) {
+        return ((i % majorTickStep) == firstMajorTick);
+    }
+
+    public boolean isLabelTick(int i) {
+        return ((i % (2*majorTickStep)) == firstMajorTick);
+    }
+
+    protected void calculateTicks() {
+        if (totalPixels == 0) {
+            numTicks = 0;
+            return;
+        }
+        double rangeWidth = range.getMaxValue()-range.getMinValue();
+        if ( rangeWidth == 0) {
+            // not a real range
+            numTicks = 0;
+            return;
+        } // end of if ()
+        // find power of ten just larger than the goalTickInc
+        tickInc = Math.pow(10,
+                           Math.ceil(Math.log(rangeWidth) /
+                                         Math.log(10.0)));
+        double goalTickInc = (rangeWidth) / totalPixels * hintPixels;
+
+        //mostly major ticks are ten x minor ticks, but may be overridden
+        majorTickStep = 10;
+
+        if (tickInc >= 8*goalTickInc) {
+            tickInc /= 10;
+        }
+        if (tickInc >= 4*goalTickInc) {
+            tickInc /= 4;
+            majorTickStep = 4;
+        }
+        if (tickInc >= 2*goalTickInc) {
+            tickInc /= 2;
+            majorTickStep = 2;
+        }
+
+        minTick = tickInc *
+            Math.floor(range.getMinValue() / tickInc);
+        double minMajorTick = majorTickStep * tickInc *
+            Math.floor(range.getMinValue() /
+                           (majorTickStep * tickInc));
+        firstMajorTick = (int)Math.round((minTick-minMajorTick)/tickInc);
+        if (firstMajorTick < 0) {
+            firstMajorTick += majorTickStep;
+        }
+
+        numTicks = 1;
+        while (minTick + numTicks * tickInc <= range.getMaxValue() ) {
+            numTicks++;
+        }
+        calcRange = tickInc * numTicks + minTick;
+    }
+
+    public void setTotalPixels(int p) {
+        totalPixels = p;
+        calculateTicks();
+    }
+
+    public int getTotalPixels(){ return totalPixels; }
+
+    public void setUnitRange(UnitRangeImpl r) {
+        range = r;
+        calculateTicks();
+    }
+
+    public UnitRangeImpl getUnitRange() {
+        return range;
+    }
+
+    private UnitDisplayUtil unitDisplayUtil = new UnitDisplayUtil();
+
+    private int firstMajorTick = 0;
+
+    private int majorTickStep = 10;
+
+    private double tickInc;
+
+    private double minTick;
+
+    private double calcRange;
+
+    private int numTicks = 0;
+
+    private int totalPixels;
+
+    private int hintPixels;
+
+    private UnitRangeImpl range;
+
+    private final boolean ascending;
+
+    static Logger logger = Logger.getLogger(AmpScaleMapper.class);
+}
+
