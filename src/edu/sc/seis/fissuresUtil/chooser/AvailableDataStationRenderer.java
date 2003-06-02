b1bb784f810743e4b2d564a1ee2ae814fc2e6818
crotwell <crotwell@localhost>
2003-06-02 17:19:09 -0400

    [svn r4151] fixes to allow available station to work, very slow at present but runs in background

--- src/edu/sc/seis/fissuresUtil/chooser/AvailableDataStationRenderer.java
+++ src/edu/sc/seis/fissuresUtil/chooser/AvailableDataStationRenderer.java
@@ -27,17 +27,17 @@ import javax.swing.JList;
 import org.apache.log4j.Logger;
 
 public class AvailableDataStationRenderer extends NameListCellRenderer {
-    
+
     public AvailableDataStationRenderer(boolean useNames){
         super(useNames);
     }
-    
+
     public AvailableDataStationRenderer(boolean useNames,
                                         boolean useCodes,
                                         boolean codeIsFirst){
         super(useNames, useCodes, codeIsFirst);
     }
-    
+
     public AvailableDataStationRenderer(boolean useNames,
                                         DataCenterOperations dc,
                                        ChannelChooser channelChooser){
@@ -45,7 +45,7 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
         this.dc = dc;
         startThread();
     }
-    
+
     public AvailableDataStationRenderer(boolean useNames,
                                         boolean useCodes,
                                         boolean codeIsFirst,
@@ -56,17 +56,17 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
         this.channelChooser = channelChooser;
         startThread();
     }
-    
+
     protected void startThread() {
         StationChecker checker = new StationChecker();
         Thread t = new Thread(checker, "StationUpChecker");
         t.start();
     }
-    
+
     public void setJList(JList jlist) {
         this.jlist = jlist;
     }
-    
+
     public Component getListCellRendererComponent(JList list,
                                                   Object value,
                                                   int index,
@@ -78,7 +78,9 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
                                                          isSelected,
                                                          cellHasFocus);
         Station station = (Station)value;
-        if (stationsUpNow.get(station) != null) {
+        if (stationsUpNow.get(station) != null &&
+            stationsUpNow.get(station) instanceof Boolean) {
+
             Boolean val = (Boolean)stationsUpNow.get(station);
             logger.debug("Station available found "+
                              StationIdUtil.toString(station.get_id())+ val);
@@ -90,12 +92,15 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
         } else {
             logger.debug("Station available NOT found "+
                              StationIdUtil.toString(station.get_id()));
-            addToCheck(station);
+            if (stationsUpNow.get(station) == null) {
+                // only add if we are not already "working on it"
+                addToCheck(station);
+            }
         }
-        
+
         return c;
     }
-   
+
     /** Creates a request filter with several common channel ids to try
      *  and check for a station existing in a data center.
      */
@@ -111,7 +116,7 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
         }
         return request;
     }
-    
+
     /** Creates a request filter with several common channel ids to try
      *  and check for a station existing in a data center.
      */
@@ -150,31 +155,41 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
                                        range.end_time);
         return request;
     }
-    
+
     protected synchronized void addToCheck(Station station) {
         stationsToCheck.add(station);
         notifyAll();
     }
-    
+
     protected synchronized Station getToCheck() throws InterruptedException {
         while (stationsToCheck.isEmpty()) {
             wait();
         }
-        return (Station)stationsToCheck.removeLast();
+        Station sta =  (Station)stationsToCheck.removeLast();
+        stationsUpNow.put(sta, sta); // put station in as placeholder, means I'm working on it
+        return sta;
     }
-    
+
+    protected synchronized void finishedCheck(Station station, boolean val) {
+        if (val) {
+            stationsUpNow.put(station, Boolean.TRUE);
+        } else {
+            stationsUpNow.put(station, Boolean.FALSE);
+        }
+        if (jlist != null) {
+            jlist.repaint();
+        }
+    }
+
     class StationChecker implements Runnable {
         boolean quitThread = false;
-        
+
         public void run() {
             Station station;
             while ( ! quitThread) {
-                logger.debug("in station checker");
                 station = null;
                 try {
                     station = getToCheck();
-                    logger.debug("Checking Station available data for "+
-                                     StationIdUtil.toString(station.get_id()));
                     Channel[] chans = null;
                     NetworkAccess net =
                         channelChooser.getNetworkAccess(station.get_id().network_id);
@@ -182,16 +197,13 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
                         chans =
                             net.retrieve_for_station(station.get_id());
                     }
-                    
+
                     RequestFilter[] request = createFakeRequest(chans);
-                    
+
                     if (dc.available_data(request).length != 0) {
-                        stationsUpNow.put(station, Boolean.TRUE);
+                        finishedCheck(station, true);
                     } else {
-                        stationsUpNow.put(station, Boolean.FALSE);
-                    }
-                    if (jlist != null) {
-                        jlist.repaint();
+                        finishedCheck(station, false);
                     }
                 } catch (InterruptedException e) {
                 } catch (Exception e) {
@@ -200,21 +212,21 @@ public class AvailableDataStationRenderer extends NameListCellRenderer {
                 }
             }
         }
-        
+
     }
-    
+
     protected LinkedList stationsToCheck = new LinkedList();
-    
+
     protected HashMap stationsUpNow = new HashMap();
-    
+
     protected DataCenterOperations dc;
-    
+
     protected TimeInterval TEN_MINUTES = new TimeInterval(10, UnitImpl.MINUTE);
-    
+
     protected ChannelChooser channelChooser = null;
-        
+
     protected JList jlist = null;
-    
+
     static Logger logger = Logger.getLogger(AvailableDataStationRenderer.class);
 }
 
