0db184eb2ddb85b249b787922634a787174cffd0
crotwell <crotwell@localhost>
2003-06-02 10:55:34 -0400

    [svn r4148] add logging stmts

--- src/edu/sc/seis/fissuresUtil/chooser/DNDNetworkAccess.java
+++ src/edu/sc/seis/fissuresUtil/chooser/DNDNetworkAccess.java
@@ -1,9 +1,12 @@
 
 package edu.sc.seis.fissuresUtil.chooser;
 
-import java.awt.datatransfer.*;
-import edu.sc.seis.fissuresUtil.cache.CacheNetworkAccess;
 import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.sc.seis.fissuresUtil.cache.CacheNetworkAccess;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import org.apache.log4j.Logger;
 
 
 public class DNDNetworkAccess extends CacheNetworkAccess implements Transferable {
@@ -11,33 +14,39 @@ public class DNDNetworkAccess extends CacheNetworkAccess implements Transferable
 
     public DNDNetworkAccess(NetworkAccess net) {
         super(net);
-	df = new DataFlavor[2];
-	df[0] = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType+
-			       "; class="+DNDNetworkAccess.class.getName(), 
-			       "Seismic Network");
-	df[1] = DataFlavor.stringFlavor;
+        logger.debug("after super");
+    df = new DataFlavor[2];
+    df[0] = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType+
+                   "; class="+DNDNetworkAccess.class.getName(),
+                   "Seismic Network");
+        logger.debug("after data flavor for class");
+    df[1] = DataFlavor.stringFlavor;
+        logger.debug("after data flavor for string");
     }
 
     public DataFlavor[] getTransferDataFlavors() {
-	
-	return df;
+
+    return df;
     }
 
     public boolean isDataFlavorSupported(DataFlavor flavor) {
-	return flavor.equals(df[0]) || flavor.equals(df[1]);
+    return flavor.equals(df[0]) || flavor.equals(df[1]);
     }
 
 
     public Object getTransferData(DataFlavor flavor)
-	throws UnsupportedFlavorException,
-	java.io.IOException {
-	if (flavor.equals(df[0])) {
-	    return this;
-	} // end of if (flavor.equals(df[0]))
-	
-	return get_attributes().name;
-	
+    throws UnsupportedFlavorException,
+    java.io.IOException {
+    if (flavor.equals(df[0])) {
+        return this;
+    } // end of if (flavor.equals(df[0]))
+    logger.debug("getTransferData before get_attributes");
+    return get_attributes().name;
+
     }
 
     private DataFlavor[] df;
+
+    static Logger logger = Logger.getLogger(DNDNetworkAccess.class);
+
 }
