f9765993d34ad16263bfe606e2e111d3c9ef9019
Philip Crotwell <crotwell@seis.sc.edu>
2003-06-05 16:35:01 -0400

    [svn r4242] add method to get 3 data set seismograms that correspond to the channel ids from the 3 components of motion

--- src/edu/sc/seis/fissuresUtil/chooser/DataSetChannelGrouper.java
+++ src/edu/sc/seis/fissuresUtil/chooser/DataSetChannelGrouper.java
@@ -19,33 +19,59 @@ import org.apache.log4j.*;
 
 public class DataSetChannelGrouper {
     public DataSetChannelGrouper (){
-
+        
     }
-
+    
     public static ChannelId[] retrieveGrouping(DataSet dataset,
-                      ChannelId channelID) {
-    String[] paramNames = dataset.getParameterNames();
-    LinkedList list = new LinkedList();
-    String origChannelIdStr = ChannelIdUtil.toString(channelID);
-    origChannelIdStr = origChannelIdStr.substring(0, origChannelIdStr.indexOf(channelID.channel_code)+2);
-    String chanGroupString = edu.sc.seis.fissuresUtil.xml.StdDataSetParamNames.CHANNEL +
-        origChannelIdStr;
-    for (int i=0; i<paramNames.length; i++) {
-        if (paramNames[i].startsWith(chanGroupString)) {
-        logger.debug("found match parameter channelId "+paramNames[i]);
-        list.add(((edu.iris.Fissures.IfNetwork.Channel)dataset.getParameter(paramNames[i])).get_id());
-        } // end of if (paramNames[i].startsWith(origChannelIdStr))
-
-    } // end of for (int i=0; i<paramNames[i]; i++)
-    ChannelId[] channelIds = (ChannelId[])list.toArray(new ChannelId[0]);
-
-    ChannelGrouperImpl channelProxy = new ChannelGrouperImpl();
-    ChannelId[] channelGroup =
-        channelProxy.retrieve_grouping(channelIds, channelID);
-    return channelGroup;
+                                               ChannelId channelID) {
+        String[] paramNames = dataset.getParameterNames();
+        LinkedList list = new LinkedList();
+        String origChannelIdStr = ChannelIdUtil.toString(channelID);
+        origChannelIdStr = origChannelIdStr.substring(0, origChannelIdStr.indexOf(channelID.channel_code)+2);
+        String chanGroupString = edu.sc.seis.fissuresUtil.xml.StdDataSetParamNames.CHANNEL +
+            origChannelIdStr;
+        for (int i=0; i<paramNames.length; i++) {
+            if (paramNames[i].startsWith(chanGroupString)) {
+                logger.debug("found match parameter channelId "+paramNames[i]);
+                list.add(((edu.iris.Fissures.IfNetwork.Channel)dataset.getParameter(paramNames[i])).get_id());
+            } // end of if (paramNames[i].startsWith(origChannelIdStr))
+            
+        } // end of for (int i=0; i<paramNames[i]; i++)
+        ChannelId[] channelIds = (ChannelId[])list.toArray(new ChannelId[0]);
+        
+        ChannelGrouperImpl channelProxy = new ChannelGrouperImpl();
+        ChannelId[] channelGroup =
+            channelProxy.retrieve_grouping(channelIds, channelID);
+        return channelGroup;
     }
-
+    
+    /** Trys to find the 2 other seismograms that match the given dataset seismogram.
+     */
+    public static DataSetSeismogram[] retrieveSeismogramGrouping(DataSet dataset,
+                                                                 DataSetSeismogram dss) {
+        ChannelId channelId = dss.getRequestFilter().channel_id;
+        ChannelId[] channelGroup =
+            DataSetChannelGrouper.retrieveGrouping(dataset,
+                                                   channelId);
+        DataSetSeismogram[] chGrpSeismograms = new DataSetSeismogram[3];
+        String[] allSeisNames = dataset.getDataSetSeismogramNames();
+        for(int counter = 0; counter < channelGroup.length; counter++) {
+            for (int i=0; i < allSeisNames.length; i++) {
+                DataSetSeismogram allSeis =
+                    dataset.getDataSetSeismogram(allSeisNames[i]);
+                if (ChannelIdUtil.areEqual(channelGroup[counter], allSeis.getRequestFilter().channel_id) &&
+                    dss.getBeginTime().date_time.equals(allSeis.getBeginTime().date_time) &&
+                    dss.getEndTime().date_time.equals(allSeis.getEndTime().date_time)) {
+                    // found a match
+                    chGrpSeismograms[counter] = allSeis;
+                    break;
+                }
+            }
+        }
+        return chGrpSeismograms;
+    }
+    
     static Category logger =
-    Category.getInstance(DataSetChannelGrouper.class.getName());
-
+        Category.getInstance(DataSetChannelGrouper.class.getName());
+    
 }// DataSetChannelGrouper
