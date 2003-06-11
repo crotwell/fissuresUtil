402b5c1a3905f9d7202cfe4662b882d5524eb937
Philip Crotwell <crotwell@seis.sc.edu>
2003-06-11 09:40:45 -0400

    [svn r4369] fix attribute creation, make directories, debug stmts

--- src/edu/sc/seis/fissuresUtil/xml/DataSetToXML.java
+++ src/edu/sc/seis/fissuresUtil/xml/DataSetToXML.java
@@ -13,35 +13,40 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Attr;
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class DataSetToXML
-{
-
+public class DataSetToXML {
+    
     public void save(DataSet dataset, File saveDirectory)
-    throws IOException, ParserConfigurationException, MalformedURLException {
+        throws IOException, ParserConfigurationException, MalformedURLException {
         File dataDir = new File(saveDirectory, "data");
-        Document doc = createDocument(dataset, saveDirectory);
-        Writer xmlWriter = new Writer(true);
+        Element doc = createDocument(dataset, saveDirectory);
+        Writer xmlWriter = new Writer();
         BufferedWriter buf =
             new BufferedWriter(new FileWriter(new File(saveDirectory, dataset.getName()+".dsml")));
         xmlWriter.setOutput(buf);
         xmlWriter.write(doc);
         buf.close();
+        logger.debug("Done with save to "+saveDirectory.toString());
     }
-
-    public Document createDocument(DataSet dataset, File dataDirectory)
+    
+    public Element createDocument(DataSet dataset, File dataDirectory)
         throws ParserConfigurationException, MalformedURLException {
+        
+        if(!dataDirectory.exists() ) {
+            dataDirectory.mkdirs();
+        }
+        
         DocumentBuilder docBuilder = XMLDataSet.getDocumentBuilder();
         Document doc = docBuilder.newDocument();
         Element element = doc.createElement("dataset");
-        insert(element, dataset, dataDirectory);
         doc.appendChild(element);
-        return doc;
+        insert(element, dataset, dataDirectory);
+        return element;
     }
-
+    
     /** inserts the dataset, and all child datasets recursively, into the
      document, along with dataset seismograms and parameters if they can be
      stored. Note that all dataSetSeismograms are converted to
@@ -50,9 +55,7 @@ public class DataSetToXML
     public void insert(Element element, DataSet dataset, File directory)
         throws MalformedURLException {
         Document doc = element.getOwnerDocument();
-        Attr id = doc.createAttribute("datasetid");
-        id.setValue(dataset.getId());
-        element.appendChild(id);
+        element.setAttribute("datasetid", dataset.getId());
         element.appendChild(XMLUtil.createTextElement(doc,
                                                       "name",
                                                       dataset.getName()));
@@ -65,7 +68,7 @@ public class DataSetToXML
             insert(child, dataset.getDataSet(childDataSets[i]), directory);
             element.appendChild(child);
         }
-
+        
         String[] childDSS = dataset.getDataSetSeismogramNames();
         for (int i = 0; i < childDSS.length; i++) {
             DataSetSeismogram dss = dataset.getDataSetSeismogram(childDSS[i]);
@@ -79,7 +82,7 @@ public class DataSetToXML
             urlDSS.insertInto(child);
             element.appendChild(child);
         }
-
+        
         String[] paramNames = dataset.getParameterNames();
         for (int i = 0; i < paramNames.length; i++) {
             Element parameter =
@@ -89,17 +92,18 @@ public class DataSetToXML
                                 dataset.getParameter(paramNames[i]));
             element.appendChild(parameter);
         }
-
-
+        
+        
     }
-
+    
     /** inserts the child dataset as a datasetRef element. The URL is assumed
      *  to be in a subdirectory relative to the current dataset.
      */
     public void insertRef(Element element, DataSet dataset) {
     }
-
+    
     protected boolean saveLocally = true;
-
+    
+    Logger logger = Logger.getLogger(DataSetToXML.class);
 }
 
