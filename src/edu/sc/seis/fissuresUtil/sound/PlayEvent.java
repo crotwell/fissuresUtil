5db8b58f4b7feebd93fe15bf88c00f370f387807
groves <groves@localhost>
2003-06-23 18:13:38 -0400

    [svn r4525] added the clip to the play event

--- src/edu/sc/seis/fissuresUtil/sound/PlayEvent.java
+++ src/edu/sc/seis/fissuresUtil/sound/PlayEvent.java
@@ -1,23 +1,28 @@
 package edu.sc.seis.fissuresUtil.sound;
 
 import edu.iris.Fissures.model.TimeInterval;
+import javax.sound.sampled.Clip;
 
 
 public class PlayEvent{
 
-	private Object source;
-	private TimeInterval interval;
+    private Object source;
+    private TimeInterval interval;
+    private Clip clip;
 
-	public PlayEvent(Object src, TimeInterval timeInterval){
-		source = src;
-		interval = timeInterval;
-	}
+    public PlayEvent(Object src, TimeInterval timeInterval, Clip clip){
+        source = src;
+        interval = timeInterval;
+        this.clip = clip;
+    }
 
-	public TimeInterval getTimeInterval(){
-		return interval;
-	}
+    public TimeInterval getTimeInterval(){
+        return interval;
+    }
 
-	public Object getSource(){
-		return source;
-	}
+    public Object getSource(){
+        return source;
+    }
+
+    public Clip getClip(){ return clip; }
 }
