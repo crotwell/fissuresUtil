dc7e927105e4992378c5fde62c34fa2a945566dd
Charles Groves <groves@seis.sc.edu>
2003-06-26 09:48:15 -0400

    [svn r4544] broke displayremove down into a super class BigX.java with this as one type of x in addition to seismogramremove, another new class

--- src/edu/sc/seis/fissuresUtil/display/drawable/DisplayRemove.java
+++ src/edu/sc/seis/fissuresUtil/display/drawable/DisplayRemove.java
@@ -19,81 +19,16 @@ import java.awt.event.MouseMotionListener;
  * @author Created by Charlie Groves
  */
 
-public class DisplayRemove extends MouseAdapter implements Plotter, MouseMotionListener{
+public class DisplayRemove extends BigX{
 
     public DisplayRemove(SeismogramDisplay display){
+        super(display);
         this.display = display;
-        SeismogramDisplay.getMouseForwarder().addPermMouseListener(this);
-        SeismogramDisplay.getMouseMotionForwarder().addMouseMotionListener(this);
     }
 
-
-    public void draw(Graphics2D canvas, Dimension size, TimeEvent currentTime, AmpEvent currentAmp) {
-        if(visible){
-            canvas.setColor(drawColor);
-            canvas.setStroke(DisplayUtils.THREE_PIXEL_STROKE);
-            canvas.drawLine(xMin, yMin, xMax, yMax);
-            canvas.drawLine(xMin, yMax, xMax, yMin);
-        }
+    public void clicked(){
+        display.clear();
     }
-
-    public void toggleVisibility() {
-        visible = !visible;
-    }
-
-    public void setVisibility(boolean b) {
-        visible = b;
-    }
-
-    public void mouseClicked(MouseEvent e){
-        if(intersects(e)){
-            display.clear();
-            SeismogramDisplay.getMouseForwarder().removePermMouseListener(this);
-            SeismogramDisplay.getMouseMotionForwarder().removeMouseMotionListener(this);
-        }
-    }
-
-    public void mouseMoved(MouseEvent e) {
-        if(intersects(e)){
-            setDrawColor(Color.RED);
-        }else{
-           setDrawColor(Color.BLACK);
-        }
-    }
-
-    public void mouseDragged(MouseEvent e) {
-        if(intersects(e)){
-            setDrawColor(Color.RED);
-        }else if(e.getSource() == display){
-            setDrawColor(Color.BLACK);
-        }
-    }
-
-    private boolean intersects(MouseEvent e){
-        if(e.getSource() == display){
-            int clickX = e.getX() - display.getInsets().left;
-            int clickY = e.getY() - display.getInsets().top;
-            if(clickX >= xMin && clickX <= xMax &&
-               clickY >= yMin && clickY <= yMax){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void setDrawColor(Color newColor){
-        Color prevColor = drawColor;
-        drawColor = newColor;
-        if(prevColor != newColor){
-            display.repaint();
-        }
-    }
-
-    private Color drawColor = Color.BLACK;
-
-    private int xMax = 10, xMin = 5, yMax = 10, yMin = 5;
 
     private SeismogramDisplay display;
-
-    private boolean visible = true;
 }
