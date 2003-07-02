32bf3403b937b65308c400c3206c14e23ec5699b
groves <groves@localhost>
2003-07-02 14:39:05 -0400

    [svn r4617] initial

---
+++ src/edu/sc/seis/fissuresUtil/map/LayerProjectionUpdater.java
@@ -0,0 +1,39 @@
+package edu.sc.seis.fissuresUtil.map;
+
+/**
+ * LayerProjectionUpdater.java
+ *
+ * @author Created by Charlie Groves
+ */
+import com.bbn.openmap.Layer;
+import com.bbn.openmap.event.ProjectionEvent;
+import com.bbn.openmap.omGraphics.OMGraphic;
+import com.bbn.openmap.omGraphics.OMGraphicList;
+import edu.sc.seis.fissuresUtil.cache.WorkerThreadPool;
+
+public class LayerProjectionUpdater implements Runnable{
+    public static void update(ProjectionEvent e, OMGraphicList graphics,
+                              Layer layer){
+        threadPool.invokeLater(new LayerProjectionUpdater(e, graphics, layer));
+    }
+
+    private LayerProjectionUpdater(ProjectionEvent e,
+                                   OMGraphicList graphics,
+                                   Layer layer){
+        event = e;
+        this.graphics = graphics;
+        this.layer = layer;
+    }
+
+    public void run(){
+        layer.setProjection(event.getProjection());
+        graphics.regenerate(event.getProjection());
+        layer.repaint();
+    }
+
+    private ProjectionEvent event;
+    private OMGraphicList graphics;
+    private Layer layer;
+
+    private static WorkerThreadPool threadPool = new WorkerThreadPool("Map Layer Updater", 1);
+}
