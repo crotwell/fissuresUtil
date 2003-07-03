b493dc2ddfb342a7b870e3beec7f35168c310714
groves <groves@localhost>
2003-07-03 12:41:59 -0400

    [svn r4627] added use of event background loader pool and added synchronization

--- src/edu/sc/seis/fissuresUtil/map/EventLayer.java
+++ src/edu/sc/seis/fissuresUtil/map/EventLayer.java
@@ -7,6 +7,7 @@ package edu.sc.seis.fissuresUtil.map;
  */
 
 
+import com.bbn.openmap.Layer;
 import com.bbn.openmap.event.ProjectionEvent;
 import com.bbn.openmap.omGraphics.OMCircle;
 import com.bbn.openmap.omGraphics.OMGraphicList;
@@ -14,19 +15,25 @@ import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
 import edu.iris.Fissures.IfEvent.Origin;
 import edu.sc.seis.fissuresUtil.cache.CacheEvent;
+import edu.sc.seis.fissuresUtil.cache.EventBackgroundLoaderPool;
+import edu.sc.seis.fissuresUtil.cache.EventLoadedListener;
 import edu.sc.seis.fissuresUtil.display.EQDataEvent;
 import edu.sc.seis.fissuresUtil.display.EventDataListener;
 import edu.sc.seis.fissuresUtil.display.EventTableModel;
 import java.awt.Color;
 import org.apache.log4j.Logger;
 
-public class EventLayer extends MouseAdapterLayer implements EventDataListener{
+public class EventLayer extends Layer implements EventDataListener, EventLoadedListener{
     public EventLayer(EventTableModel tableModel){
+        this.tableModel = tableModel;
         tableModel.addEventDataListener(this);
+        eventDataChanged(new EQDataEvent(this, tableModel.getAllEvents()));
     }
 
     public void paint(java.awt.Graphics g) {
-        circles.render(g);
+        synchronized(circles){
+            circles.render(g);
+        }
     }
 
     public void projectionChanged(ProjectionEvent e) {
@@ -34,18 +41,28 @@ public class EventLayer extends MouseAdapterLayer implements EventDataListener{
     }
 
     public void eventDataChanged(EQDataEvent eqDataEvent) {
-        EventAccessOperations[] eao = eqDataEvent.getEvents();
-        for (int i = 0; i < eao.length; i++){
-            try{
-                circles.add(new OMEvent(eao[i]));
-            }catch(NoPreferredOrigin e){
-                logger.debug("No origin for an event");
+        EventAccessOperations[] events = eqDataEvent.getEvents();
+        EventBackgroundLoaderPool loader = tableModel.getLoader();
+        for (int i = 0; i < events.length; i++) {
+            loader.getEvent(events[i], (CacheEvent)events[i], this);
+        }
+    }
+
+    public void eventLoaded(CacheEvent event) {
+        try{
+            logger.debug("event loaded");
+            OMEvent omEvent = new OMEvent(event);
+            synchronized(circles){
+                circles.add(omEvent);
             }
+            repaint();
+        }catch(NoPreferredOrigin e){
+            logger.debug("No origin for an event");
         }
     }
 
     public void eventDataCleared() {
-        circles.clear();
+        synchronized(circles){circles.clear();}
         repaint();
     }
 
@@ -72,11 +89,7 @@ public class EventLayer extends MouseAdapterLayer implements EventDataListener{
 
     private OMGraphicList circles = new OMGraphicList();
 
-    private Logger logger = Logger.getLogger(EventLayer.class);
+    private static Logger logger = Logger.getLogger(EventLayer.class);
 
-    public String[] getMouseModeServiceList() {
-        // TODO
-        return null;
-    }
-
+    private EventTableModel tableModel;
 }
