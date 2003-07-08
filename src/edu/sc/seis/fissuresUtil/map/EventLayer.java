057755d77690b1535f588da5bb7302c5d77ec9f8
oliverpa <oliverpa@localhost>
2003-07-08 15:16:52 -0400

    [svn r4675] event circles are thicker and change color according to depth.  selection is transparent black.

--- src/edu/sc/seis/fissuresUtil/map/EventLayer.java
+++ src/edu/sc/seis/fissuresUtil/map/EventLayer.java
@@ -7,25 +7,22 @@ package edu.sc.seis.fissuresUtil.map;
  */
 
 
-import com.bbn.openmap.Layer;
+import edu.sc.seis.fissuresUtil.display.*;
+
 import com.bbn.openmap.MapBean;
 import com.bbn.openmap.event.CenterEvent;
 import com.bbn.openmap.event.ProjectionEvent;
 import com.bbn.openmap.event.SelectMouseMode;
 import com.bbn.openmap.omGraphics.OMCircle;
-import com.bbn.openmap.omGraphics.OMGraphicConstants;
 import com.bbn.openmap.omGraphics.OMGraphicList;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
 import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.cache.EventBackgroundLoaderPool;
 import edu.sc.seis.fissuresUtil.cache.EventLoadedListener;
-import edu.sc.seis.fissuresUtil.display.EQDataEvent;
-import edu.sc.seis.fissuresUtil.display.EQSelectionEvent;
-import edu.sc.seis.fissuresUtil.display.EQSelectionListener;
-import edu.sc.seis.fissuresUtil.display.EventDataListener;
-import edu.sc.seis.fissuresUtil.display.EventTableModel;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -157,13 +154,24 @@ public class EventLayer extends MouseAdapterLayer implements EventDataListener, 
 			float lat = prefOrigin.my_location.latitude;
 			float lon = prefOrigin.my_location.longitude;
 			float mag = prefOrigin.magnitudes[0].value;
-			mag *= mag;
-			bigCircle = new OMCircle(lat, lon, (int)Math.floor(mag), (int)Math.floor(mag));
-			OMCircle lilCircle = new OMCircle(lat, lon, 5, 5);
+
+			double scale = 2.2;
+			int lilDiameter = (int)Math.pow(scale, 3.0);
+			OMCircle lilCircle = new OMCircle(lat, lon, lilDiameter, lilDiameter);
+			if (mag <= 3.0){
+				bigCircle = new OMCircle(lat, lon, lilDiameter, lilDiameter);
+			}
+			else{
+				mag = (float)(Math.pow(scale, (double)mag));
+				bigCircle = new OMCircle(lat, lon, (int)Math.floor(mag), (int)Math.floor(mag));
+			}
 			event = new CacheEvent(eao);
-			setLinePaint(Color.BLUE);
-			lilCircle.setFillPaint(Color.RED);
-			setSelectPaint(Color.MAGENTA);
+
+			Color color = getDepthColor((QuantityImpl)prefOrigin.my_location.depth);
+			lilCircle.setLinePaint(Color.BLACK);
+			lilCircle.setFillPaint(color);
+			bigCircle.setStroke(DisplayUtils.THREE_PIXEL_STROKE);
+			bigCircle.setLinePaint(color);
 			add(bigCircle);
 			add(lilCircle);
 			generate(getProjection());
@@ -174,21 +182,31 @@ public class EventLayer extends MouseAdapterLayer implements EventDataListener, 
 		}
 
 		public void select() {
-			bigCircle.setFillPaint(Color.RED);
+			bigCircle.setFillPaint(new Color(0, 0, 0, 64));
 			try{
 				mapBean.center(new CenterEvent(this,
 											   event.get_preferred_origin().my_location.latitude,
 											   event.get_preferred_origin().my_location.longitude));
 			}catch(NoPreferredOrigin e){}
-			super.select();
 		}
 
 		public void deselect(){
-			bigCircle.setFillPaint(OMGraphicConstants.clear);
-			super.deselect();
+			bigCircle.setFillPaint(OMGraphicList.clear);
 		}
 
 		public OMCircle getBigCircle(){ return bigCircle; }
+
+		private Color getDepthColor(QuantityImpl depth){
+			double depthKM = depth.convertTo(UnitImpl.KILOMETER).value;
+			Color color = MEDIUM_DEPTH_EVENT;
+			if (depthKM <= 40.0){
+				color = SHALLOW_DEPTH_EVENT;
+			}
+			if (depthKM >= 150.0){
+				color = DEEP_DEPTH_EVENT;
+			}
+			return color;
+		}
 
 		private CacheEvent event;
 
@@ -208,6 +226,12 @@ public class EventLayer extends MouseAdapterLayer implements EventDataListener, 
     private ListSelectionModel selectionModel;
 
     private MapBean mapBean;
+
+	public static final Color SHALLOW_DEPTH_EVENT = new Color(243, 33, 78);
+
+	public static final Color MEDIUM_DEPTH_EVENT = new Color(246, 185, 42);
+
+	public static final Color DEEP_DEPTH_EVENT = new Color(245, 249, 27);
 }
 
 
