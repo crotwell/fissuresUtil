35c5ba2de166d77cb4d6bca5e1c71a36b54c3e04
crotwell <crotwell@localhost>
2003-07-09 16:42:46 -0400

    [svn r4700] add new method to get URL version of string for xlink usage

--- src/edu/sc/seis/fissuresUtil/xml/SeismogramFileTypes.java
+++ src/edu/sc/seis/fissuresUtil/xml/SeismogramFileTypes.java
@@ -10,25 +10,40 @@ package edu.sc.seis.fissuresUtil.xml;
  * @version
  */
 
+import edu.sc.seis.fissuresUtil.exceptionHandlerGUI.GlobalExceptionHandler;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class SeismogramFileTypes {
     private SeismogramFileTypes (String val){
-	this.val = val;
+    this.val = val;
     }
 
     public boolean equals(Object obj) {
-	if(! (obj instanceof SeismogramFileTypes) ) return false;
-	return ((SeismogramFileTypes)obj).getValue().equals(this.val);
+    if(! (obj instanceof SeismogramFileTypes) ) return false;
+    return ((SeismogramFileTypes)obj).getValue().equals(this.val);
     }
 
     public String getValue() {
-	return this.val;
+    return this.val;
     }
-   
+
+    public URL getURLValue() {
+        try {
+            return new URL(URL_PREFIX+getValue());
+        } catch (MalformedURLException e) {
+            // shouldn't ever happen as these are static strings
+            GlobalExceptionHandler.handleStatic("Trouble creating URL for file type "+getValue(), e);
+        }
+        return null;
+    }
 
     public static final SeismogramFileTypes SAC = new SeismogramFileTypes("sac");
 
     public static final SeismogramFileTypes MSEED = new SeismogramFileTypes("mseed");
-    
+
+    public static final String URL_PREFIX = "http://www.seis.sc.edu/xml/SeismogramFileTypes/";
+
     private String val;
-    
+
 }// SeismogramFileTypes
