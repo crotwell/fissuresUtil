4769278079307a18c0a2377c8dd2ee2b85b3e1e3
oliverpa <oliverpa@localhost>
2003-07-11 17:51:23 -0400

    [svn r4716] initial

---
+++ src/edu/sc/seis/fissuresUtil/map/tools/PanTool.java
@@ -0,0 +1,71 @@
+package edu.sc.seis.fissuresUtil.map.tools;
+import java.awt.event.MouseEvent;
+import java.awt.Cursor;
+
+
+
+public class PanTool extends OpenMapTool{
+
+	public PanTool(){
+
+	}
+
+	/**
+	 * Invoked when the mouse button has been clicked (pressed
+	 * and released) on a component.
+	 */
+	public void mouseClicked(MouseEvent e) {
+		// TODO
+	}
+
+	/**
+	 * Invoked when a mouse button has been released on a component.
+	 */
+	public void mouseReleased(MouseEvent e) {
+		// TODO
+	}
+
+	/**
+	 * Invoked when the mouse exits a component.
+	 */
+	public void mouseExited(MouseEvent e) {
+		// TODO
+	}
+
+	/**
+	 * Invoked when a mouse button has been pressed on a component.
+	 */
+	public void mousePressed(MouseEvent e) {
+		// TODO
+	}
+
+	/**
+	 * Gets the mouse cursor recommended for use when this mouse mode
+	 * is active.
+	 * @return Cursor the mouse cursor recommended for use when this
+	 * mouse mode is active.
+	 */
+	public Cursor getModeCursor() {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Invoked when the mouse enters a component.
+	 */
+	public void mouseEntered(MouseEvent e) {
+		// TODO
+	}
+
+	/**
+	 * Returns the id (MapMouseMode name).
+	 * This name should be unique for each MapMouseMode.
+	 * @return String ID
+	 */
+	public String getID() {
+		// TODO
+		return null;
+	}
+
+
+}
