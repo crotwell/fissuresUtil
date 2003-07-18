8e951f7116060e26f587b778bb4bb9837ad96334
Philip Oliver-Paull <oliverpa@seis.sc.edu>
2003-07-18 16:24:26 -0400

    [svn r4829] shows station names instead of codes

--- src/edu/sc/seis/fissuresUtil/display/TextInfoDisplay.java
+++ src/edu/sc/seis/fissuresUtil/display/TextInfoDisplay.java
@@ -26,55 +26,55 @@ public abstract class TextInfoDisplay
 {
 
     public TextInfoDisplay (){
-	setLayout(new BorderLayout());
-	//Create a text pane.
+    setLayout(new BorderLayout());
+    //Create a text pane.
         textPane = new JTextPane();
         paneScrollPane = new JScrollPane(textPane);
         paneScrollPane.setVerticalScrollBarPolicy(
                         JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         //paneScrollPane.setPreferredSize(new Dimension(250, 155));
         paneScrollPane.setMinimumSize(new Dimension(10, 15));
-	add(paneScrollPane, BorderLayout.CENTER);
-	initStylesForTextPane(textPane);
+    add(paneScrollPane, BorderLayout.CENTER);
+    initStylesForTextPane(textPane);
 
-	dropTarget = new DropTarget(textPane, this);
-	dropTarget.setActive(true);
+    dropTarget = new DropTarget(textPane, this);
+    dropTarget.setActive(true);
     }
 
 
     public void appendLabelValue(Document doc, String label, String value)
-	throws javax.swing.text.BadLocationException {
-//	    doc.insertString(doc.getLength(),
-//			     label+": ",
-//			     textPane.getStyle("label"));
-		doc.insertString(doc.getLength(),
-			     label,
-			     textPane.getStyle("label"));
-	    doc.insertString(doc.getLength(),
-			     value+"\n",
-			     textPane.getStyle("value"));
+    throws javax.swing.text.BadLocationException {
+//      doc.insertString(doc.getLength(),
+//               label+": ",
+//               textPane.getStyle("label"));
+        doc.insertString(doc.getLength(),
+                 label,
+                 textPane.getStyle("label"));
+        doc.insertString(doc.getLength(),
+                 value+"\n",
+                 textPane.getStyle("value"));
     }
 
     public  void appendLine(Document doc, String value)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(),
-			     value+"\n",
-			     textPane.getStyle("value"));
+    throws javax.swing.text.BadLocationException {
+        doc.insertString(doc.getLength(),
+                 value+"\n",
+                 textPane.getStyle("value"));
     }
 
 
     public void appendHeader(Document doc, String value)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(),
-			     value+"\n",
-			     textPane.getStyle("header"));
+    throws javax.swing.text.BadLocationException {
+        doc.insertString(doc.getLength(),
+                 value+"\n",
+                 textPane.getStyle("header"));
     }
 
     public void displayProblem(String problem) {
-	Document doc = textPane.getDocument();
+    Document doc = textPane.getDocument();
         try {
-	    doc.remove(0, doc.getLength());
-	    appendProblem(doc, problem);
+        doc.remove(0, doc.getLength());
+        appendProblem(doc, problem);
             toTop();
         } catch (BadLocationException ble) {
             System.err.println("Couldn't insert message.");
@@ -82,23 +82,23 @@ public abstract class TextInfoDisplay
     }
 
     public void appendProblem(Document doc, String problem)
-	throws javax.swing.text.BadLocationException {
-	    doc.insertString(doc.getLength(),
-			     "Problem: ",
-			     textPane.getStyle("label"));
-	    doc.insertString(doc.getLength(),
-			     problem+"\n",
-			     textPane.getStyle("problem"));
+    throws javax.swing.text.BadLocationException {
+        doc.insertString(doc.getLength(),
+                 "Problem: ",
+                 textPane.getStyle("label"));
+        doc.insertString(doc.getLength(),
+                 problem+"\n",
+                 textPane.getStyle("problem"));
     }
 
     public Document getDocument() {
-	return textPane.getDocument();
+    return textPane.getDocument();
     }
 
     public void clear() {
-	Document doc = textPane.getDocument();
+    Document doc = textPane.getDocument();
         try {
-	    doc.remove(0, doc.getLength());
+        doc.remove(0, doc.getLength());
             toTop();
         } catch (BadLocationException ble) {
             System.err.println("Couldn't insert message.");
