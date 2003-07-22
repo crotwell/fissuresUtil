0b79e0380f4e7cb5d6943120d1c95a45bf7a0453
oliverpa <oliverpa@localhost>
2003-07-22 09:50:08 -0400

    [svn r4845] added support for the distance layer

--- src/edu/sc/seis/fissuresUtil/map/OpenMap.java
+++ src/edu/sc/seis/fissuresUtil/map/OpenMap.java
@@ -9,14 +9,11 @@ package edu.sc.seis.fissuresUtil.map;
 import com.bbn.openmap.*;
 
 import com.bbn.openmap.event.MapMouseMode;
-import com.bbn.openmap.gui.OMToolSet;
-import com.bbn.openmap.gui.ToolPanel;
+import com.bbn.openmap.layer.EarthquakeLayer;
 import com.bbn.openmap.layer.shape.ShapeLayer;
-import com.bbn.openmap.proj.CADRG;
 import com.bbn.openmap.proj.Proj;
 import edu.sc.seis.fissuresUtil.chooser.ChannelChooser;
 import edu.sc.seis.fissuresUtil.display.EventTableModel;
-import edu.sc.seis.fissuresUtil.map.tools.PanTool;
 import edu.sc.seis.fissuresUtil.map.tools.ZoomTool;
 import java.awt.Color;
 import java.util.ArrayList;
@@ -27,121 +24,133 @@ import javax.swing.ListSelectionModel;
 public class OpenMap extends OpenMapComponent{
 
     public static final Color WATER = new Color(54, 179, 221);
-	public static final float DEFAULT_SCALE = 200000000f;
+    public static final float DEFAULT_SCALE = 200000000f;
 
-	private MapHandler mapHandler;
-	private LayerHandler lh;
-	private MapBean mapBean;
-	private MouseDelegator mouseDelegator;
-	private List tools = new ArrayList();
+    private MapHandler mapHandler;
+    private LayerHandler lh;
+    private EventLayer el;
+    private MapBean mapBean;
+    private MouseDelegator mouseDelegator;
+    private List tools = new ArrayList();
+    private EventTableModel tableModel;
 
     /**Creates a new openmap.  Both the channel chooser and the event table
-	 * model can be null.  If so, channels and events just won't get drawn
-	 */
+     * model can be null.  If so, channels and events just won't get drawn
+     */
     public OpenMap(EventTableModel etm, ListSelectionModel lsm, String shapefile){
-		try{
-			mapHandler = new MapHandler();
-			mapHandler.add(this);
-			// Create a MapBean
-			mapBean = getMapBean();
+        try{
+            mapHandler = new MapHandler();
+            mapHandler.add(this);
+            // Create a MapBean
+            mapBean = getMapBean();
+            tableModel = etm;
 
-			//get the projection and set its background color and center point
-//			Proj proj = new CADRG(new LatLonPoint(mapBean.DEFAULT_CENTER_LAT, mapBean.DEFAULT_CENTER_LON),
-//								  DEFAULT_SCALE,
-//								  mapBean.DEFAULT_WIDTH,
-//								  mapBean.DEFAULT_HEIGHT);
-			Proj proj = (Proj)mapBean.getProjection();
-			proj.setBackgroundColor(WATER);
-//			mapBean.setProjection(proj);
-			mapBean.setCenter(20, 200);
+            //get the projection and set its background color and center point
+//            Proj proj = new Orthographic(new LatLonPoint(mapBean.DEFAULT_CENTER_LAT, mapBean.DEFAULT_CENTER_LON),
+//                                         DEFAULT_SCALE,
+//                                         mapBean.DEFAULT_WIDTH,
+//                                         mapBean.DEFAULT_HEIGHT);
+            Proj proj = (Proj)mapBean.getProjection();
 
-			mapHandler.add(mapBean);
+            proj.setBackgroundColor(WATER);
+            //mapBean.setProjection(proj);
+            mapBean.setCenter(20, 200);
 
-			// Create and add a LayerHandler to the MapHandler.  The
-			// LayerHandler manages Layers, whether they are part of the
-			// map or not.  layer.setVisible(true) will add it to the map.
-			// The LayerHandler has methods to do this, too.  The
-			// LayerHandler will find the MapBean in the MapHandler.
-			lh = new LayerHandler();
-			mapHandler.add(lh);
+            mapHandler.add(mapBean);
 
-			if(etm != null){
-				EventLayer el = new EventLayer(etm, lsm, mapBean);
-				mapHandler.add(el);
-				lh.addLayer(el, 1);
-			}
+            // Create and add a LayerHandler to the MapHandler.  The
+            // LayerHandler manages Layers, whether they are part of the
+            // map or not.  layer.setVisible(true) will add it to the map.
+            // The LayerHandler has methods to do this, too.  The
+            // LayerHandler will find the MapBean in the MapHandler.
+            lh = new LayerHandler();
+            mapHandler.add(lh);
 
-			// Create a ShapeLayer to show world political boundaries.
-			ShapeLayer shapeLayer = new ShapeLayer();
+            if(etm != null){
+                el = new EventLayer(etm, lsm, mapBean);
+                mapHandler.add(el);
+                lh.addLayer(el, 1);
 
-			//Create shape layer properties
-			Properties shapeLayerProps = new Properties();
-			shapeLayerProps.put("prettyName", "Political Solid");
-			shapeLayerProps.put("lineColor", "000000");
-			shapeLayerProps.put("fillColor", "39DA87");
-			shapeLayerProps.put("shapeFile", shapefile + ".shp");
-			shapeLayerProps.put("spatialIndex", shapefile + ".ssx");
-			shapeLayer.setProperties(shapeLayerProps);
-			shapeLayer.setVisible(true);
-			mapHandler.add(shapeLayer);
+                DistanceLayer dl = new DistanceLayer();
+                el.addEQSelectionListener(dl);
+                mapHandler.add(dl);
+                lh.addLayer(dl);
+                etm.addEventDataListener(dl);
+            }
 
-			// Create the directional and zoom control tool
-			//OMToolSet omts = new OMToolSet();
-			// Create an OpenMap toolbar
-			//ToolPanel toolBar = new ToolPanel();
-			//Create an OpenMap Info Line
-			InformationDelegator infoDel = new InformationDelegator();
-			infoDel.setShowLights(false);
-			//Create an OpenMap Mouse Delegator
-			mouseDelegator = new MouseDelegator();
+            // Create a ShapeLayer to show world political boundaries.
+            ShapeLayer shapeLayer = new ShapeLayer();
 
-			// Add the ToolPanel and the OMToolSet to the MapHandler.  The
-			// OpenMapFrame will find the ToolPanel and attach it to the
-			// top part of its content pane, and the ToolPanel will find
-			// the OMToolSet and add it to itself.
-			//mapHandler.add(omts);
-			//mapHandler.add(toolBar);
+            //Create shape layer properties
+            Properties shapeLayerProps = new Properties();
+            shapeLayerProps.put("prettyName", "Political Solid");
+            shapeLayerProps.put("lineColor", "000000");
+            shapeLayerProps.put("fillColor", "39DA87");
+            shapeLayerProps.put("shapeFile", shapefile + ".shp");
+            shapeLayerProps.put("spatialIndex", shapefile + ".ssx");
+            shapeLayer.setProperties(shapeLayerProps);
+            shapeLayer.setVisible(true);
+            mapHandler.add(shapeLayer);
 
-			mapHandler.add(mouseDelegator);
-			mapHandler.add(infoDel);
-		} catch (MultipleSoloMapComponentException msmce) {
-			// The MapHandler is only allowed to have one of certain
-			// items.  These items implement the SoloMapComponent
-			// interface.  The MapHandler can have a policy that
-			// determines what to do when duplicate instances of the
-			// same type of object are added - replace or ignore.
+            // Create the directional and zoom control tool
+            //OMToolSet omts = new OMToolSet();
+            // Create an OpenMap toolbar
+            //ToolPanel toolBar = new ToolPanel();
+            //Create an OpenMap Info Line
+            InformationDelegator infoDel = new InformationDelegator();
+            infoDel.setShowLights(false);
+            //Create an OpenMap Mouse Delegator
+            mouseDelegator = new MouseDelegator();
 
-			// In this class, this will never happen, since we are
-			// controlling that one MapBean, LayerHandler,
-			// MouseDelegator, etc is being added to the MapHandler.
-		}
+            // Add the ToolPanel and the OMToolSet to the MapHandler.  The
+            // OpenMapFrame will find the ToolPanel and attach it to the
+            // top part of its content pane, and the ToolPanel will find
+            // the OMToolSet and add it to itself.
+            //mapHandler.add(omts);
+            //mapHandler.add(toolBar);
+
+            mapHandler.add(mouseDelegator);
+            mapHandler.add(infoDel);
+        } catch (MultipleSoloMapComponentException msmce) {
+            // The MapHandler is only allowed to have one of certain
+            // items.  These items implement the SoloMapComponent
+            // interface.  The MapHandler can have a policy that
+            // determines what to do when duplicate instances of the
+            // same type of object are added - replace or ignore.
+
+            // In this class, this will never happen, since we are
+            // controlling that one MapBean, LayerHandler,
+            // MouseDelegator, etc is being added to the MapHandler.
+        }
     }
 
-	public void addStationsFromChannelChooser(ChannelChooser chooser){
-		if(chooser != null){
-			StationLayer sl = new StationLayer(chooser);
-			mapHandler.add(sl);
-			lh.addLayer(sl,0);
-		}
-	}
+    public void addStationsFromChannelChooser(ChannelChooser chooser){
+        if(chooser != null){
+            StationLayer sl = new StationLayer(chooser);
+            mapHandler.add(sl);
+            lh.addLayer(sl,0);
+            el.addEQSelectionListener(sl);
+            tableModel.addEventDataListener(sl);
+        }
+    }
 
-	public MapBean getMapBean(){
-		if (mapBean == null){
-			mapBean = new MapBean();
-		}
-		return mapBean;
-	}
+    public MapBean getMapBean(){
+        if (mapBean == null){
+            mapBean = new MapBean();
+        }
+        return mapBean;
+    }
 
-	public void addMouseMode(MapMouseMode mode){
-		mouseDelegator.addMouseMode(mode);
-		if (mode instanceof ZoomTool){
-			((ZoomTool)mode).addZoomListener(getMapBean());
-		}
-	}
+    public void addMouseMode(MapMouseMode mode){
+        mouseDelegator.addMouseMode(mode);
+        if (mode instanceof ZoomTool){
+            ((ZoomTool)mode).addZoomListener(getMapBean());
+        }
+    }
 
-	public void setActiveMouseMode(MapMouseMode mode){
-		mouseDelegator.setActiveMouseMode(mode);
-	}
+    public void setActiveMouseMode(MapMouseMode mode){
+        mouseDelegator.setActiveMouseMode(mode);
+    }
 }
 
 
