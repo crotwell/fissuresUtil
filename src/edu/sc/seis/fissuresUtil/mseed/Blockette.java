b6efe5e93751964621d349874181673b9cd41b7d
crotwell <crotwell@localhost>
2003-07-24 11:08:38 -0400

    [svn r4854] made some small changes so the liss reader would work again

--- src/edu/sc/seis/fissuresUtil/mseed/Blockette.java
+++ src/edu/sc/seis/fissuresUtil/mseed/Blockette.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.lang.*;
 import java.lang.reflect.*;
 
-/** Superclass of all seed blockettes. The actual blockettes do not store 
+/** Superclass of all seed blockettes. The actual blockettes do not store
  * either their blockette type or their length in the case of ascii blockettes
  * or next blockettes offset in the case of data blockettes as these are either
  * already known (ie type) or may change after reading due to data changes.
@@ -16,59 +16,59 @@ public abstract class  Blockette {
     public Blockette() {
     }
 
-    public static Blockette parseBlockette(int type, byte[] bytes) 
-	throws IOException {
+    public static Blockette parseBlockette(int type, byte[] bytes)
+    throws IOException {
 
-	try {
+    try {
             //System.out.println(" Class.forName    Blockette"+type);
-	    Class blocketteClass = Class.forName("edu.iris.fissuresUtil.mseed.Blockette"+type);
+        Class blocketteClass = Class.forName("edu.sc.seis.fissuresUtil.mseed.Blockette"+type);
             //System.out.println(" Class.forName suceeded");
-	    
-	    Class[] argTypes = new Class[1];
-	    //	    argTypes[0] = Class.forName("byte[]");
-	    argTypes[0] = byte[].class;
-	    Constructor read = blocketteClass.getConstructor(argTypes);
-	    Object[] arguments = new Object[1];
-	    arguments[0] = bytes;
+
+        Class[] argTypes = new Class[1];
+        //      argTypes[0] = Class.forName("byte[]");
+        argTypes[0] = byte[].class;
+        Constructor read = blocketteClass.getConstructor(argTypes);
+        Object[] arguments = new Object[1];
+        arguments[0] = bytes;
             //System.out.println("Constructor  suceeded");
 
-	    Blockette blockette = (Blockette)read.newInstance(arguments);
+        Blockette blockette = (Blockette)read.newInstance(arguments);
 
 
             //System.out.println("read suceeded");
-	    return blockette;
-	} catch (ClassNotFoundException e) {
-	    // must not be installed, read an  unknownblockette
+        return blockette;
+    } catch (ClassNotFoundException e) {
+        // must not be installed, read an  unknownblockette
 System.out.println(" Class.forName failed: "+type);
-	    Blockette blockette = new BlocketteUnknown(bytes, type);
+        Blockette blockette = new BlocketteUnknown(bytes, type);
 
-	    return blockette;
-	} catch ( NoSuchMethodException e) {
-	    // must not be installed, skip this blockette
-	    return null;
-	} catch (InstantiationException e) {
-	    // must not be installed, skip this blockette
-	    return null;
-	} catch (IllegalAccessException  e) {
-	    // must not be installed, skip this blockette
-	    return null;
-	} catch (InvocationTargetException  e) {
-	    // must not be installed, skip this blockette
-	    return null;
-	}
+        return blockette;
+    } catch ( NoSuchMethodException e) {
+        // must not be installed, skip this blockette
+        return null;
+    } catch (InstantiationException e) {
+        // must not be installed, skip this blockette
+        return null;
+    } catch (IllegalAccessException  e) {
+        // must not be installed, skip this blockette
+        return null;
+    } catch (InvocationTargetException  e) {
+        // must not be installed, skip this blockette
+        return null;
+    }
     }
 
     public abstract int getType();
-	
+
     public abstract String getName();
 
     public abstract int getSize();
-	
+
     public abstract byte[] toBytes();
 
     public String toString() {
-	String s = getType()+": "+getName();
-	return s;
+    String s = getType()+": "+getName();
+    return s;
     }
 
 }
