785abf5364c93c7d5ee9f1b0a2881016fab60ed6
Charles Groves <groves@seis.sc.edu>
2003-07-25 12:35:14 -0400

    [svn r4885] moved from NamedPlotter

---
+++ src/edu/sc/seis/fissuresUtil/display/drawable/NamedDrawable.java
@@ -0,0 +1,18 @@
+package edu.sc.seis.fissuresUtil.display.drawable;
+
+import java.awt.Graphics2D;
+/**
+ * NamedPlotter.java
+ *
+ *
+ * Created: Mon Nov 11 17:09:15 2002
+ *
+ * @author <a href="mailto:">Charlie Groves</a>
+ * @version
+ */
+
+public interface NamedDrawable extends Drawable{
+
+    public boolean drawName(Graphics2D canvas, int xPosition, int yPosition);
+
+}// NamedPlotter
