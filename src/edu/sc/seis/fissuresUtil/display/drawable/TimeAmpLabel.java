1ebe7738b7b6be0452f0dd25f1742952c5a25d47
Charles Groves <groves@seis.sc.edu>
2003-07-26 16:23:57 -0400

    [svn r4915] use new NameDrawing interface

--- src/edu/sc/seis/fissuresUtil/display/drawable/TimeAmpLabel.java
+++ src/edu/sc/seis/fissuresUtil/display/drawable/TimeAmpLabel.java
@@ -6,6 +6,7 @@
 
 package edu.sc.seis.fissuresUtil.display.drawable;
 import edu.sc.seis.fissuresUtil.display.BasicSeismogramDisplay;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.display.VerticalSeismogramDisplay;
 import edu.sc.seis.fissuresUtil.display.registrar.AmpEvent;
 import edu.sc.seis.fissuresUtil.display.registrar.TimeEvent;
@@ -26,7 +27,7 @@ public class TimeAmpLabel implements NamedDrawable{
         visible = !visible;
     }
 
-    public boolean drawName(Graphics2D canvas, int xPosition, int yPosition) {
+    public Rectangle2D drawName(Graphics2D canvas, int xPosition, int yPosition) {
         if(visible){
             timeText = VerticalSeismogramDisplay.getTime();
             ampText = VerticalSeismogramDisplay.getAmp();
@@ -37,7 +38,9 @@ public class TimeAmpLabel implements NamedDrawable{
             stringBounds.setRect(canvas.getFontMetrics().getStringBounds(timeText, canvas));
             canvas.drawString(ampText, xPosition + stringBounds.width, yPosition - stringBounds.height);
         }
-        return visible;
+        //Returns an empty rectangle as this doesn't factor into top left corner
+        //space concerns
+        return DisplayUtils.EMPTY_RECTANGLE;
     }
 
     public void draw(Graphics2D canvas, Dimension size, TimeEvent currentTime, AmpEvent currentAmp) {
@@ -63,4 +66,5 @@ public class TimeAmpLabel implements NamedDrawable{
 
     private BasicSeismogramDisplay display;
 }
+
 
