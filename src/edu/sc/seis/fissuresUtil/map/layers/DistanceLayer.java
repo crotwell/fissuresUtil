b7dc1f7f8ba48aa62a021b8f42c855904275bd38
oliverpa <oliverpa@localhost>
2003-07-28 15:27:08 -0400

    [svn r4933] moved from maps directory for housekeeping puposes

---
+++ src/edu/sc/seis/fissuresUtil/map/layers/DistanceLayer.java
@@ -0,0 +1,155 @@
+/**
+ * DistanceLayer.java
+ *
+ * @author Created by Philip Oliver-Paull
+ */
+
+package edu.sc.seis.fissuresUtil.map.layers;
+import edu.sc.seis.fissuresUtil.map.*;
+
+import com.bbn.openmap.LatLonPoint;
+import com.bbn.openmap.Layer;
+import com.bbn.openmap.event.ProjectionEvent;
+import com.bbn.openmap.omGraphics.OMCircle;
+import com.bbn.openmap.omGraphics.OMGraphicList;
+import com.bbn.openmap.omGraphics.OMText;
+import com.bbn.openmap.proj.Length;
+import com.bbn.openmap.proj.Projection;
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
+import edu.sc.seis.fissuresUtil.display.EQDataEvent;
+import edu.sc.seis.fissuresUtil.display.EQSelectionEvent;
+import edu.sc.seis.fissuresUtil.display.EQSelectionListener;
+import edu.sc.seis.fissuresUtil.display.EventDataListener;
+import java.awt.Color;
+import java.awt.Point;
+
+public class DistanceLayer extends Layer implements EQSelectionListener, EventDataListener{
+
+    private OMGraphicList distCircles = new OMGraphicList();
+
+    public DistanceLayer(){
+    }
+
+    /**
+     * Invoked when there has been a fundamental change to the Map.
+     * <p>
+     * Layers are expected to recompute their graphics (if this makes sense),
+     * and then <code>repaint()</code> themselves.
+     * @param e ProjectionEvent
+     */
+    public void projectionChanged(ProjectionEvent e) {
+        LayerProjectionUpdater.update(e, distCircles, this);
+    }
+
+
+    /**
+     * Method eqSelectionChanged
+     *
+     * @param    eqSelectionEvent    an EQSelectionEvent
+     *
+     * FIXME: make this work for more than one event
+     */
+    public void eqSelectionChanged(EQSelectionEvent eqSelectionEvent) {
+        synchronized(distCircles){
+            distCircles.clear();
+        }
+        try{
+            Origin origin = eqSelectionEvent.getEvents()[0].get_preferred_origin();
+            LatLonPoint llp = new LatLonPoint(origin.my_location.latitude, origin.my_location.longitude);
+            makeDistCircles(llp);
+            repaint();
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    /**
+     * Method eventDataChanged
+     *
+     * @param    eqDataEvent         an EQDataEvent
+     *
+     */
+    public void eventDataChanged(EQDataEvent eqDataEvent) {}
+
+    /**
+     * Method eventDataCleared
+     *
+     */
+    public void eventDataCleared() {
+        synchronized(distCircles){
+            distCircles.clear();
+        }
+    }
+
+    private void makeDistCircle(LatLonPoint llp, double radiusDegrees){
+        Projection proj = getProjection();
+
+        OMCircle circle = new DistCircle(llp, radiusDegrees);
+        LatLonPoint labelPointUp = makeTextLabelLatLon(llp, radiusDegrees, proj, true);
+        if (proj.isPlotable(labelPointUp)){
+            synchronized(distCircles){
+                distCircles.add(new TextLabel(labelPointUp, Integer.toString((int)radiusDegrees)));
+            }
+        }
+        LatLonPoint labelPointDown = makeTextLabelLatLon(llp, radiusDegrees, proj, false);
+        if (proj.isPlotable(labelPointDown)){
+            synchronized(distCircles){
+                distCircles.add(new TextLabel(labelPointDown, Integer.toString((int)radiusDegrees)));
+            }
+        }
+        synchronized(distCircles){
+            distCircles.add(circle);
+        }
+    }
+
+    private void makeDistCircles(LatLonPoint llp){
+        makeDistCircle(llp, 30.0);
+        makeDistCircle(llp, 60.0);
+        makeDistCircle(llp, 90.0);
+    }
+
+    public void paint(java.awt.Graphics g){
+        synchronized(distCircles){
+            distCircles.render(g);
+        }
+    }
+
+    private static LatLonPoint makeTextLabelLatLon(LatLonPoint center, double radiusDegrees, Projection prj, boolean isUpper){
+        LatLonPoint textLLP;
+
+        if(isUpper){
+            textLLP = new LatLonPoint(center.getLatitude() + radiusDegrees,
+                                      center.getLongitude());
+        }
+        else{
+            textLLP = new LatLonPoint(center.getLatitude() - radiusDegrees,
+                                      center.getLongitude());
+        }
+
+        Point textXYPoint = prj.forward(textLLP);
+        textXYPoint.setLocation(textXYPoint.getX(), textXYPoint.getY() - 5);
+        textLLP = prj.inverse(textXYPoint);
+
+        return textLLP;
+    }
+
+    private class DistCircle extends OMCircle{
+
+        public DistCircle(LatLonPoint llp, double radiusDegrees){
+            super(llp.getLatitude(), llp.getLongitude(), (float)radiusDegrees, Length.DECIMAL_DEGREE);
+            this.setLinePaint(Color.magenta);
+            this.setStroke(DisplayUtils.ONE_PIXEL_STROKE);
+            generate(getProjection());
+        }
+
+    }
+
+    private class TextLabel extends OMText{
+        public TextLabel(LatLonPoint position, String text){
+            super(position.getLatitude(), position.getLongitude(), text, OMText.JUSTIFY_CENTER);
+        }
+    }
+}
+
---
+++ src/edu/sc/seis/fissuresUtil/map/layers/EventLayer.java
@@ -0,0 +1,385 @@
+package edu.sc.seis.fissuresUtil.map.layers;
+import edu.sc.seis.fissuresUtil.map.*;
+
+/**
+ * EventLayer.java
+ *
+ * @author Created by Charlie Groves
+ */
+
+
+import edu.sc.seis.fissuresUtil.display.*;
+
+import com.bbn.openmap.MapBean;
+import com.bbn.openmap.event.CenterEvent;
+import com.bbn.openmap.event.ProjectionEvent;
+import com.bbn.openmap.event.SelectMouseMode;
+import com.bbn.openmap.omGraphics.OMCircle;
+import com.bbn.openmap.omGraphics.OMGraphicList;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.Quantity;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
+import edu.sc.seis.fissuresUtil.cache.EventBackgroundLoaderPool;
+import edu.sc.seis.fissuresUtil.cache.EventLoadedListener;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TimeZone;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import org.apache.log4j.Logger;
+
+public class EventLayer extends MouseAdapterLayer implements EventDataListener, EventLoadedListener, EQSelectionListener{
+    public EventLayer(EventTableModel tableModel, ListSelectionModel lsm, MapBean mapBean){
+        this.tableModel = tableModel;
+        selectionModel = lsm;
+        addEQSelectionListener(this);
+        tableModel.addEventDataListener(this);
+        eventDataChanged(new EQDataEvent(this, tableModel.getAllEvents()));
+
+        selectionModel.addListSelectionListener(new ListSelectionListener(){
+                    public void valueChanged(ListSelectionEvent e) {
+                        EventAccessOperations[] selectedEvents = getSelectedEvents();
+                        if(selectedEvents.length > 0){
+                            fireEQSelectionChanged(new EQSelectionEvent(this, selectedEvents));
+                        }
+                    }
+
+                });
+
+        this.mapBean = mapBean;
+    }
+
+    public void paint(java.awt.Graphics g) {
+        synchronized(circles){
+            circles.render(g);
+        }
+    }
+
+    public void projectionChanged(ProjectionEvent e) {
+        LayerProjectionUpdater.update(e, circles, this);
+    }
+
+    public void eventDataChanged(EQDataEvent eqDataEvent) {
+        EventAccessOperations[] events = eqDataEvent.getEvents();
+        EventBackgroundLoaderPool loader = tableModel.getLoader();
+        for (int i = 0; i < events.length; i++) {
+            loader.getEvent(events[i], (CacheEvent)events[i], this);
+        }
+    }
+
+    public void eventLoaded(CacheEvent event) {
+        try{
+            OMEvent omEvent = new OMEvent(event);
+            synchronized(circles){
+                circles.add(omEvent);
+            }
+            repaint();
+        }catch(NoPreferredOrigin e){
+            logger.debug("No origin for an event");
+        }
+    }
+
+    public void eventDataCleared() {
+        synchronized(circles){circles.clear();}
+        repaint();
+    }
+
+    public void addEQSelectionListener(EQSelectionListener listener){
+        listenerList.add(EQSelectionListener.class, listener);
+        EventAccessOperations[] selectedEvents = getSelectedEvents();
+        if (selectedEvents.length > 0){
+            listener.eqSelectionChanged(new EQSelectionEvent(this, getSelectedEvents()));
+        }
+    }
+
+    public void fireEQSelectionChanged(EQSelectionEvent e){
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==EQSelectionListener.class) {
+                // Lazily create the event:
+                ((EQSelectionListener)listeners[i+1]).eqSelectionChanged(e);
+            }
+        }
+    }
+
+    //FIXME: make this work for more than one selected event at a time
+    public void eqSelectionChanged(EQSelectionEvent eqSelectionEvent) {
+        OMEvent selected = null;
+        List deselected = new ArrayList();
+        synchronized(circles){
+            Iterator it = circles.iterator();
+            while (it.hasNext()){
+                OMEvent current = (OMEvent)it.next();
+                try{
+                    //if (current.getEvent().get_preferred_origin().equals(eqSelectionEvent.getEvents()[0].get_preferred_origin())){
+                    if (DisplayUtils.originIsEqual(current.getEvent(), eqSelectionEvent.getEvents()[0])){
+                        selected = current;
+                    }else{
+                        deselected.add(current);
+                    }
+                }
+                catch(NoPreferredOrigin e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(selected != null){
+            selected.select();
+            synchronized(circles){
+                circles.moveIndexedToTop(circles.indexOf(selected));
+            }
+        }
+        Iterator it = deselected.iterator();
+        while(it.hasNext()){
+            ((OMEvent)it.next()).deselect();
+        }
+
+    }
+
+    private static String[] modeList = { SelectMouseMode.modeID } ;
+
+    public String[] getMouseModeServiceList() {
+        return modeList;
+    }
+
+    public boolean mouseClicked(MouseEvent e){
+        if (currentPopup != null){
+            currentPopup.setVisible(false);
+            currentPopup = null;
+        }
+
+        synchronized(circles){
+            Iterator it = circles.iterator();
+            List eventsUnderMouse = new ArrayList();
+            while(it.hasNext()){
+                OMEvent current = (OMEvent)it.next();
+                if(current.getBigCircle().contains(e.getX(), e.getY())){
+                    eventsUnderMouse.add(current.getEvent());
+                }
+            }
+            if (eventsUnderMouse.size() > 0){
+                if (eventsUnderMouse.size() == 1){
+                    int rowToSelect = tableModel.getRowForEvent((EventAccessOperations)eventsUnderMouse.get(0));
+                    if (rowToSelect != -1){
+                        selectionModel.setSelectionInterval(rowToSelect, rowToSelect);
+                    }
+                }
+                else{
+                    final JPopupMenu popup = new JPopupMenu();
+                    it = eventsUnderMouse.iterator();
+                    while (it.hasNext()){
+                        final EventAccessOperations current = (EventAccessOperations)it.next();
+                        final JMenuItem menuItem = new JMenuItem(getEventInfo(current));
+                        menuItem.addActionListener(new ActionListener(){
+                                    public void actionPerformed(ActionEvent e) {
+                                        int rowToSelect = tableModel.getRowForEvent(current);
+                                        if (rowToSelect != -1){
+                                            selectionModel.setSelectionInterval(rowToSelect, rowToSelect);
+                                        }
+                                        popup.setVisible(false);
+                                    }
+                                });
+                        menuItem.addMouseListener(new MouseAdapter(){
+
+                                    public void mouseEntered(MouseEvent e) {
+                                        menuItem.setArmed(true);
+                                    }
+
+                                    public void mouseExited(MouseEvent e) {
+                                        menuItem.setArmed(false);
+                                    }
+
+                                });
+                        popup.add(menuItem);
+                    }
+                    Point compLocation = e.getComponent().getLocationOnScreen();
+                    double[] popupLoc = {compLocation.getX(), compLocation.getY()};
+                    popup.setLocation((int)popupLoc[0] + e.getX(), (int)popupLoc[1] + e.getY());
+                    popup.setVisible(true);
+                    currentPopup = popup;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean mouseMoved(MouseEvent e){
+        synchronized(circles){
+            Iterator it = circles.iterator();
+            while(it.hasNext()){
+                OMEvent current = (OMEvent)it.next();
+                try{
+                    if(current.getBigCircle().contains(e.getX(), e.getY())){
+                        EventAccessOperations event = current.getEvent();
+                        fireRequestInfoLine(getEventInfo(event));
+                        return true;
+                    }
+                }
+                catch(Exception ex){}
+            }
+        }
+        fireRequestInfoLine(" ");
+        return false;
+    }
+
+    public static String getEventInfo(EventAccessOperations event){
+        StringBuffer buf = new StringBuffer();
+
+        //Get geographic name of origin
+        ParseRegions regions = new ParseRegions();
+        String location = regions.getGeographicRegionName(event.get_attributes().region.number);
+
+        //Get Date and format it accordingly
+        Origin origin;
+        try{
+            origin = event.get_preferred_origin();
+        }catch(NoPreferredOrigin e){
+            origin = event.get_origins()[0];
+        }
+        MicroSecondDate msd = new MicroSecondDate(origin.origin_time);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss z");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        sdf.format(msd);
+
+        //Get Magnitude
+        float mag = origin.magnitudes[0].value;
+
+        //get depth
+        Quantity depth = origin.my_location.depth;
+
+        buf.append("Event: ");
+        buf.append(location + " | ");
+        buf.append(sdf.format(msd) + " | ");
+        buf.append("Mag " + mag + " | ");
+
+        UnitDisplayUtil udu = new UnitDisplayUtil();
+        buf.append("Depth " + depth.value + " " + udu.getNameForUnit((UnitImpl)depth.the_units));
+        return buf.toString();
+    }
+
+    private class OMEvent extends OMGraphicList{
+        public OMEvent(EventAccessOperations eao) throws NoPreferredOrigin{
+            super(2);
+            Origin prefOrigin = eao.get_preferred_origin();
+            float lat = prefOrigin.my_location.latitude;
+            float lon = prefOrigin.my_location.longitude;
+            float mag = prefOrigin.magnitudes[0].value;
+
+            double scale = 1.8;
+            int lilDiameter = (int)Math.pow(scale, 3.0);
+            OMCircle lilCircle = new OMCircle(lat, lon, lilDiameter, lilDiameter);
+            if (mag <= 3.0){
+                bigCircle = new OMCircle(lat, lon, lilDiameter, lilDiameter);
+            }
+            else{
+                mag = (float)(Math.pow(scale, (double)mag));
+                bigCircle = new OMCircle(lat, lon, (int)Math.floor(mag), (int)Math.floor(mag));
+            }
+            event = new CacheEvent(eao);
+
+            Color color = getDepthColor((QuantityImpl)prefOrigin.my_location.depth);
+            lilCircle.setLinePaint(Color.BLACK);
+            lilCircle.setFillPaint(color);
+            bigCircle.setStroke(DisplayUtils.THREE_PIXEL_STROKE);
+            bigCircle.setLinePaint(color);
+            add(bigCircle);
+            add(lilCircle);
+            generate(getProjection());
+        }
+
+        public CacheEvent getEvent(){
+            return event;
+        }
+
+        public void select() {
+            bigCircle.setFillPaint(new Color(0, 0, 0, 64));
+            try{
+                mapBean.center(new CenterEvent(this,
+                                               event.get_preferred_origin().my_location.latitude,
+                                               event.get_preferred_origin().my_location.longitude));
+            }catch(NoPreferredOrigin e){}
+        }
+
+        public void deselect(){
+            bigCircle.setFillPaint(OMGraphicList.clear);
+        }
+
+        public OMCircle getBigCircle(){ return bigCircle; }
+
+        private Color getDepthColor(QuantityImpl depth){
+            double depthKM = depth.convertTo(UnitImpl.KILOMETER).value;
+            Color color = MEDIUM_DEPTH_EVENT;
+            if (depthKM <= 40.0){
+                color = SHALLOW_DEPTH_EVENT;
+            }
+            if (depthKM >= 150.0){
+                color = DEEP_DEPTH_EVENT;
+            }
+            return color;
+        }
+
+        private CacheEvent event;
+
+        private OMCircle bigCircle;
+    }
+
+    private EventTableModel getTableModel(){
+        return tableModel;
+    }
+
+    private EventAccessOperations[] getSelectedEvents(){
+        List selectedEvents = new ArrayList();
+        EventAccessOperations[] allEvents = getTableModel().getAllEvents();
+        for (int i = 0; i < allEvents.length; i++) {
+            if (selectionModel.isSelectedIndex(i)){
+                selectedEvents.add(allEvents[i]);
+            }
+        }
+        return (EventAccessOperations[])selectedEvents.toArray(new EventAccessOperations[0]);
+    }
+
+    private OMGraphicList circles = new OMGraphicList();
+
+    private static Logger logger = Logger.getLogger(EventLayer.class);
+
+    private EventTableModel tableModel;
+
+    private ListSelectionModel selectionModel;
+
+    private MapBean mapBean;
+
+    private JPopupMenu currentPopup;
+
+    public static final Color SHALLOW_DEPTH_EVENT = new Color(243, 33, 78);
+
+    public static final Color MEDIUM_DEPTH_EVENT = new Color(246, 185, 42);
+
+    public static final Color DEEP_DEPTH_EVENT = new Color(245, 249, 27);
+
+}
+
+
+
+
+
