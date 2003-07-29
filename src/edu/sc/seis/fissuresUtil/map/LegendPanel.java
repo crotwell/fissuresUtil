4ab54510e4ca9a67240c25e0b37ae3098f7465d1
oliverpa <oliverpa@localhost>
2003-07-29 15:33:45 -0400

    [svn r4973] initial..doesn't do much right now

---
+++ src/edu/sc/seis/fissuresUtil/map/LegendPanel.java
@@ -0,0 +1,35 @@
+/**
+ * LegendPanel.java
+ *
+ * @author Created by Philip Oliver-Paull
+ */
+
+package edu.sc.seis.fissuresUtil.map;
+
+import java.awt.GridLayout;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LegendPanel extends JPanel{
+
+    public LegendPanel(){
+        initComponents();
+    }
+
+    private void initComponents(){
+        setLayout(new GridLayout(1,2));
+        String imgPath = "fissuresUtil/data/maps/";
+
+        //Deep Events
+        URL imgURL = getClass().getClassLoader().getResource(imgPath + "deep-event.png");
+        ImageIcon imageIcon = new ImageIcon(imgURL);
+        JLabel imgLabel = new JLabel(imageIcon);
+        JLabel description = new JLabel("Deep Earthquake");
+        add(imgLabel);
+        add(description);
+    }
+
+}
+
