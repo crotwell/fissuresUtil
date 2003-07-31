8146a4f3afe606ffd1767168a0cbee89107adfa5
groves <groves@localhost>
2003-07-31 14:15:20 -0400

    [svn r5017] added toSTring hashcode and equals methods

--- src/edu/sc/seis/fissuresUtil/display/registrar/LayoutData.java
+++ src/edu/sc/seis/fissuresUtil/display/registrar/LayoutData.java
@@ -15,6 +15,34 @@ public class LayoutData{
 
     public double getEnd(){ return end; }
 
+    public String toString(){
+        if(name == null){
+            name = "LayoutData for " + seis + " perecentages: " + start + ", "  + end;
+        }
+        return name;
+    }
+
+    public boolean equals(Object obj){
+        if(getClass() == obj.getClass() &&
+           name.equals(obj.toString()) &&
+           seis.equals(((LayoutData)obj).getSeis())){
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        if(hashCode == 0){
+            hashCode = 67 * seis.hashCode();
+            hashCode += start + end;
+        }
+        return hashCode;
+    }
+
+    private int hashCode = 0;
+
+    private String name;
+
     private final DataSetSeismogram seis;
 
     private final double start, end;
