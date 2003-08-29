bf18c1afdbe593e80981f9346c1073a71bc7a458
Philip Crotwell <crotwell@seis.sc.edu>
2003-08-29 16:05:38 -0400

    [svn r5264] more info in seismogram display

--- src/edu/sc/seis/fissuresUtil/display/SeismogramInfoDisplay.java
+++ src/edu/sc/seis/fissuresUtil/display/SeismogramInfoDisplay.java
@@ -1,15 +1,26 @@
 package edu.sc.seis.fissuresUtil.display;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAttr;
+import edu.iris.Fissures.IfNetwork.Response;
+import edu.iris.Fissures.IfNetwork.Site;
+import edu.iris.Fissures.IfNetwork.Station;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.TimeRange;
 import edu.iris.Fissures.network.ChannelIdUtil;
+import edu.iris.Fissures.network.NetworkIdUtil;
+import edu.iris.Fissures.network.ResponsePrint;
+import edu.iris.Fissures.network.SiteIdUtil;
+import edu.iris.Fissures.network.StationIdUtil;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.bag.Statistics;
 import edu.sc.seis.fissuresUtil.exceptionHandlerGUI.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
+import edu.sc.seis.fissuresUtil.xml.StdAuxillaryDataNames;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.TimeZone;
 import javax.swing.text.BadLocationException;
@@ -30,7 +41,7 @@ public class SeismogramInfoDisplay extends TextInfoDisplay {
         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
         this.format = format;
     }
-    
+
     public void addSeismogram(DataSetSeismogram dss){
         HashMap values = new HashMap();
         Document doc = getDocument();
@@ -42,6 +53,52 @@ public class SeismogramInfoDisplay extends TextInfoDisplay {
             appendLabelValue(doc,"Channel ID", ChannelIdUtil.toString(rf.channel_id));
             appendLabelValue(doc,"Begin Time", "" + rfRange.getBeginTime());
             appendLabelValue(doc,"End Time" , "" + rfRange.getEndTime());
+
+            Channel chan = dss.getDataSet().getChannel(rf.channel_id);
+            if (chan != null) {
+                appendHeader(doc, "Channel");
+                appendLabelValue(doc, "Name", chan.name);
+                appendLabelValue(doc, "Orientation", chan.an_orientation.azimuth+"/"+chan.an_orientation.dip);
+                appendLabelValue(doc, "Effective Time", chan.effective_time.start_time.date_time+" to "+chan.effective_time.end_time.date_time);
+                appendLabelValue(doc, "Id", ChannelIdUtil.toString(chan.get_id()));
+                appendLabelValue(doc, "Sampling", chan.sampling_info.numPoints+" in "+chan.sampling_info.interval);
+                appendHeader(doc, "Site");
+                Site site = chan.my_site;
+                appendLabelValue(doc, "Id", SiteIdUtil.toString(site.get_id()));
+                appendLabelValue(doc, "Effective Time", site.effective_time.start_time.date_time+" to "+site.effective_time.end_time.date_time);
+                appendLabelValue(doc, "Comment", site.comment);
+                appendLabelValue(doc, "Location", site.my_location.latitude+"/"+site.my_location.longitude+" elev="+site.my_location.elevation+" depth="+site.my_location.depth);
+                appendHeader(doc, "Station");
+                Station sta = site.my_station;
+                appendLabelValue(doc, "Id", StationIdUtil.toString(sta.get_id()));
+                appendLabelValue(doc, "Effective Time", sta.effective_time.start_time.date_time+" to "+sta.effective_time.end_time.date_time);
+                appendLabelValue(doc, "Name", sta.name);
+                appendLabelValue(doc, "Description", sta.description);
+                appendLabelValue(doc, "Operator", sta.operator);
+                appendLabelValue(doc, "Comment", sta.comment);
+                appendLabelValue(doc, "Location", sta.my_location.latitude+"/"+sta.my_location.longitude+" elev="+sta.my_location.elevation+" depth="+sta.my_location.depth);
+                appendHeader(doc, "Network");
+                NetworkAttr net = sta.my_network;
+                appendLabelValue(doc, "Id", NetworkIdUtil.toString(net.get_id()));
+                appendLabelValue(doc, "Effective Time", net.effective_time.start_time.date_time+" to "+net.effective_time.end_time.date_time);
+                appendLabelValue(doc, "Name", sta.name);
+                appendLabelValue(doc, "Description", sta.description);
+                appendLabelValue(doc, "Owner", sta.operator);
+            }
+            Object respObj = dss.getAuxillaryData(StdAuxillaryDataNames.RESPONSE);
+            if (respObj != null && respObj instanceof Response) {
+                Response resp = (Response)respObj;
+                appendHeader(doc, "Response");
+                appendLabelValue(doc, "Sensitivity", resp.the_sensitivity.sensitivity_factor+" at "+resp.the_sensitivity.frequency+" Hz");
+                appendLabelValue(doc, "Number of Stages", ""+resp.stages.length);
+                TimeRange effTime;
+                if (chan != null) {
+                    effTime = chan.effective_time;
+                } else {
+                    effTime = new TimeRange(rf.start_time, rf.end_time);
+                }
+                appendLine(doc, ResponsePrint.printResponse(rf.channel_id, resp, effTime));
+            }
 //            Iterator e = format.iterator();
 //            while(e.hasNext()){
 //                String current = (String)e.next();
@@ -50,14 +107,14 @@ public class SeismogramInfoDisplay extends TextInfoDisplay {
         }
         catch(Exception e){ e.printStackTrace(); }
     }
-    
-    
+
+
     public void addSeismogram(LocalSeismogramImpl seis){
         try {
             Document doc = getDocument();
             appendLine(doc, "");
             appendHeader(doc, seis.toString());
-            
+
             appendLabelValue(doc, "ID", seis.get_id());
             appendLabelValue(doc, "Name", seis.getName());
             appendLabelValue(doc, "Channel", ChannelIdUtil.toStringNoDates(seis.channel_id));
@@ -66,20 +123,24 @@ public class SeismogramInfoDisplay extends TextInfoDisplay {
             appendLabelValue(doc, "End Time", ""+seis.getEndTime());
             appendLabelValue(doc, "Sampling", ""+seis.sampling_info);
             appendLabelValue(doc, "Unit", ""+seis.y_unit);
+            Statistics stat = new Statistics(seis);
+            appendLabelValue(doc, "Min", stat.min()+"");
+            appendLabelValue(doc, "Max", stat.max()+"");
+            appendLabelValue(doc, "Mean", stat.mean()+"");
         } catch (BadLocationException e) {
             GlobalExceptionHandler.handleStatic("Can't append seismogram info to display", e);
         }
     }
-    
+
     public void drop(DropTargetDropEvent e){}
-    
+
     public void dragScroll(DropTargetDragEvent e) {}
-    
+
     public void dropActionChanged(DropTargetDragEvent e) {}
-    
+
     SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss.S z");
-    
+
     protected LinkedList format;
-    
+
     DecimalFormat twoDecimal = new DecimalFormat("0.00");
 }// SeismogramInfoDisplay
