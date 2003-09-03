b7912da620fd90bd609147be0345276a90a5a40f
crotwell <crotwell@localhost>
2003-09-03 12:39:57 -0400

    [svn r5300] initial

---
+++ src/edu/sc/seis/fissuresUtil/display/registrar/RMeanAmpEvent.java
@@ -0,0 +1,30 @@
+/**
+ * RMeanAmpEvent.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.display.registrar;
+
+import edu.iris.Fissures.model.UnitRangeImpl;
+import edu.sc.seis.fissuresUtil.display.UnitDisplayUtil;
+import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
+
+public class RMeanAmpEvent extends BasicAmpEvent {
+
+    public RMeanAmpEvent(DataSetSeismogram[] seismos, UnitRangeImpl[] amps){
+        super(seismos, amps);
+    }
+
+    protected UnitRangeImpl calcGenericAmp(UnitRangeImpl inAmp, DataSetSeismogram seis) {
+        UnitRangeImpl basic = super.calcGenericAmp(inAmp, seis);
+        double mean = (basic.getMaxValue() - basic.getMinValue())/2;
+        double offset = basic.getMaxValue() - mean;
+        UnitRangeImpl out = new UnitRangeImpl(-1 * offset,
+                                              offset ,
+                                              basic.getUnit());
+        return UnitDisplayUtil.getBestForDisplay(out);
+    }
+
+}
+
