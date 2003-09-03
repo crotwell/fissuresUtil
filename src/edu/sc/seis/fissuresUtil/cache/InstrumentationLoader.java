57fb8d7423de6706763ae880daf0ed01d9ab8010
Philip Crotwell <crotwell@seis.sc.edu>
2003-09-03 12:41:55 -0400

    [svn r5302] try again if corba exception

--- src/edu/sc/seis/fissuresUtil/cache/InstrumentationLoader.java
+++ src/edu/sc/seis/fissuresUtil/cache/InstrumentationLoader.java
@@ -14,8 +14,8 @@ import edu.sc.seis.fissuresUtil.exceptionHandlerGUI.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import edu.sc.seis.fissuresUtil.xml.StdAuxillaryDataNames;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
 import org.apache.log4j.Logger;
 
 public class InstrumentationLoader extends Thread
@@ -35,11 +35,18 @@ public class InstrumentationLoader extends Thread
                                                       nextWork.seis.getBeginTime());
                 nextWork.seis.addAuxillaryData(StdAuxillaryDataNames.RESPONSE,
                                   inst.the_response);
-                logger.debug("added response to dss");
+                logger.debug("added response to dss for "+
+                                 ChannelIdUtil.toStringNoDates(nextWork.seis.getRequestFilter().channel_id));
             } catch (ChannelNotFound e) {
                 GlobalExceptionHandler.handleStatic("Could not load instrumentation for channel "+
                                 ChannelIdUtil.toString(nextWork.seis.getRequestFilter().channel_id),
                             e);
+            } catch (org.omg.CORBA.SystemException e) {
+                // might be a transient??? Try again if not too many tries
+                nextWork.numTries++;
+                if (nextWork.numTries < 10) {
+                    addToQueue(nextWork);
+                }
             } catch (Exception e) {
                 GlobalExceptionHandler.handleStatic("A problem occured loading the instrumentation for channel "+
                                 ChannelIdUtil.toString(nextWork.seis.getRequestFilter().channel_id),
@@ -58,6 +65,10 @@ public class InstrumentationLoader extends Thread
         while (queue.isEmpty()) {
             wait();
         }
+        // sort so high numbered retrys are at begining and grab last
+        // which has lowest retries
+        Collections.sort(queue, numTrysComparator);
+        Collections.reverse(queue);
         return (WorkUnit)queue.removeLast();
     }
 
@@ -68,6 +79,8 @@ public class InstrumentationLoader extends Thread
     }
 
     boolean noStopThread = true;
+
+    NumTrysComparator numTrysComparator = new NumTrysComparator();
 
     public void getInstrumentation(DataSetSeismogram seis, NetworkAccess net) {
         addToQueue(new WorkUnit(seis, net));
@@ -83,6 +96,27 @@ public class InstrumentationLoader extends Thread
         }
         DataSetSeismogram seis;
         NetworkAccess net;
+        int numTries = 0;
+    }
+
+    class NumTrysComparator implements Comparator {
+        /**
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         *         first argument is less than, equal to, or greater than the
+         *         second.
+         * @throws ClassCastException if the arguments' types prevent them from
+         *         being compared by this Comparator.
+         */
+        public int compare(Object o1, Object o2) {
+            WorkUnit wu1 = (WorkUnit)o1;
+            WorkUnit wu2 = (WorkUnit)o2;
+            if (wu1.numTries < wu2.numTries) return -1;
+            if (wu1.numTries > wu2.numTries) return 1;
+            return 0;
+        }
+
     }
 
     static Logger logger = Logger.getLogger(InstrumentationLoader.class);
