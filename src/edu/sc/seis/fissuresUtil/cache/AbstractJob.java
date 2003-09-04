ae4cef51eafa4bd2ef01ade77366b13204fafae3
Charles Groves <groves@seis.sc.edu>
2003-09-04 15:01:19 -0400

    [svn r5335] moved from Process files of the same name

---
+++ src/edu/sc/seis/fissuresUtil/cache/AbstractJob.java
@@ -0,0 +1,81 @@
+/**
+ * AbstractJob.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.cache;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AbstractJob implements Job{
+    public AbstractJob(String name){
+        this.name = name;
+    }
+
+
+    /**
+     * Method getStatus is used to indicate the current status of this long
+     * running Job
+     *
+     * @return   a String describing what this process is up to at the current
+     * time
+     *
+     */
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+        fireStatusUpdate();
+    }
+
+    /**
+     * Method getName returns a name for this process that succinctly describes
+     * its action
+     *
+     * @return   a String naming this process.  If the string is more than 30
+     * characters it will be truncated
+     *
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Method addStatusListener adds a status listener to this
+     * Process that will be notified whenver the status changes
+     *
+     */
+    public void addStatusListener(StatusListener listener){
+        listeners.add(listener);
+    }
+
+    private void fireStatusUpdate(){
+        Iterator it = listeners.iterator();
+        while(it.hasNext()){
+            ((StatusListener)it.next()).statusUpdated(this);
+        }
+    }
+
+    public boolean isFinished(){ return finished; }
+
+    protected void setFinished(){
+        finished = true;
+        setStatus(FINISHED);
+        fireStatusUpdate();
+    }
+
+    private boolean finished = false;
+
+    private List listeners = new ArrayList();
+
+    private String status = INITIALIZE;
+
+    private String name;
+}
+
---
+++ src/edu/sc/seis/fissuresUtil/cache/JobDisplay.java
@@ -0,0 +1,100 @@
+/**
+ * ProcessDisplay.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.cache;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableColumn;
+
+
+
+public class JobDisplay extends JTable implements StatusListener{
+
+    private JobDisplay(){
+        setModel(ptm);
+        ptm.setColumnSizes();
+    }
+
+    public static JobDisplay getDisplay(){
+        return display;
+    }
+
+    public void add(Job proc){
+        proc.addStatusListener(this);
+        ptm.add(proc);
+    }
+
+    public int numColumns(){
+        return ptm.getColumnCount();
+    }
+
+    public void statusUpdated(Job updated) {
+        ptm.fireTableChanged(new TableModelEvent(ptm));
+    }
+
+    private class ProcessTableModel extends AbstractTableModel{
+        public void  setColumnSizes() {
+            int columnCount = getColumnCount();
+            for(int counter = 0; counter < columnCount; counter++) {
+                String columnName =getColumnName(counter);
+                int width = 50;
+                if( columnName.equals("Name") ) width = 100;
+                else if( columnName.equals("Status") ) width = 200;
+                else if( columnName.equals("Finished")) width = 20;
+                getColumnModel().getColumn(counter).setPreferredWidth(width);
+                getColumnModel().getColumn(counter).setMinWidth(width);
+
+            }
+        }
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public String getColumnName(int columnIndex) {
+            return columnNames[columnIndex];
+        }
+
+        public Class getColumnClass(int columnIndex) {
+            if(columnIndex < 2){
+                return String.class;
+            }
+            return Boolean.class;
+        }
+
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            Job rowProcess = (Job)processes.get(rowIndex);
+            if(columnIndex == 0){
+                return rowProcess.getName();
+            }
+            if(columnIndex == 1){
+                return rowProcess.getStatus();
+            }
+            return Boolean.valueOf(rowProcess.isFinished());
+        }
+
+        public int getRowCount() {
+            return processes.size();
+        }
+
+        public void add(Job proc){
+            processes.add(proc);
+            fireTableChanged(new TableModelEvent(this));
+        }
+
+        private String[] columnNames = {"Name", "Status", "Finished"};
+
+        private List processes = new ArrayList();
+    }
+
+    private ProcessTableModel ptm = new ProcessTableModel();
+
+    private final static JobDisplay display = new JobDisplay();
+}
+
