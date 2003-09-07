330f9b345edd494c560a63ff56a97cf4e7eff888
oliverpa <oliverpa@localhost>
2003-09-06 23:36:10 -0400

    [svn r5430] added constructors for taking the center point and scale factors for the zoom tool

--- src/edu/sc/seis/fissuresUtil/map/tools/ZoomTool.java
+++ src/edu/sc/seis/fissuresUtil/map/tools/ZoomTool.java
@@ -14,74 +14,83 @@ import java.awt.Cursor;
 
 public class ZoomTool extends OpenMapTool{
 
-	private float zoomFactor;
-	private OpenMap map;
-	private float mapScale;
-	private MapBean mapBean;
-	private EventListenerList listenerList = new EventListenerList();
-	private String id = "Zoom";
+    private float zoomFactor;
+    private OpenMap map;
+    private float mapScale;
+    private LatLonPoint center;
+    private MapBean mapBean;
+    private EventListenerList listenerList = new EventListenerList();
+    private String id = "Zoom";
 
-	public ZoomTool(OpenMap om, float factor, String id){
-		zoomFactor = factor;
-		map = om;
-		mapScale = map.DEFAULT_SCALE;
-		mapBean = map.getMapBean();
-		if (id != null){
-			this.id = id;
-		}
-	}
+    public ZoomTool(OpenMap om, float factor, String id){
+        this(om, factor, OpenMap.DEFAULT_SCALE, id);
+    }
 
-	public void addZoomListener(ZoomListener zl){
-		listenerList.add(ZoomListener.class, zl);
-	}
+    public ZoomTool(OpenMap om, float factor, float scale, String id){
+        this(om, factor, scale, new LatLonPoint(0,0), id);
+    }
+    public ZoomTool(OpenMap om, float factor, float scale, LatLonPoint center, String id){
+        zoomFactor = factor;
+        map = om;
+        mapScale = scale;
+        this.center = center;
+        mapBean = map.getMapBean();
+        if (id != null){
+            this.id = id;
+        }
+    }
 
-	public void removeZoomListener(ZoomListener zl){
-		listenerList.remove(ZoomListener.class, zl);
-	}
+    public void addZoomListener(ZoomListener zl){
+        listenerList.add(ZoomListener.class, zl);
+    }
 
-	private void fireZoomChanged(ZoomEvent ze){
-		// Guaranteed to return a non-null array
-		Object[] listeners = listenerList.getListenerList();
-		// Process the listeners last to first, notifying
-		// those that are interested in this event
-		for (int i = listeners.length-2; i>=0; i-=2) {
-			if (listeners[i]==ZoomListener.class) {
-				// Lazily create the event:
-				if (ze != null){
-					((ZoomListener)listeners[i+1]).zoom(ze);
-				}
-			}
-		}
-	}
+    public void removeZoomListener(ZoomListener zl){
+        listenerList.remove(ZoomListener.class, zl);
+    }
 
-	public void mouseClicked(MouseEvent e){
-		if (isActive()){
-			LatLonPoint llp = mapBean.getCoordinates(e);
-			mapBean.center(new CenterEvent(this, llp.getLatitude(), llp.getLongitude()));
-			zoom();
-		}
-	}
+    private void fireZoomChanged(ZoomEvent ze){
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==ZoomListener.class) {
+                // Lazily create the event:
+                if (ze != null){
+                    ((ZoomListener)listeners[i+1]).zoom(ze);
+                }
+            }
+        }
+    }
 
-	public void zoom(){
-		fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapBean.getScale()/zoomFactor));
-	}
+    public void mouseClicked(MouseEvent e){
+        if (isActive()){
+            LatLonPoint llp = mapBean.getCoordinates(e);
+            mapBean.center(new CenterEvent(this, llp.getLatitude(), llp.getLongitude()));
+            zoom();
+        }
+    }
 
-	public void reset(){
-		mapBean.setCenter(20, 200);
-		fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapScale));
-	}
+    public void zoom(){
+        fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapBean.getScale()/zoomFactor));
+    }
 
-	public void setActive(boolean active){
-		super.setActive(active);
-	}
+    public void reset(){
+        mapBean.setCenter(center.getLatitude(), center.getLongitude());
+        fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapScale));
+    }
 
-	/**
-	 * Returns the id (MapMouseMode name).
-	 * This name should be unique for each MapMouseMode.
-	 * @return String ID
-	 */
-	public String getID() {
-		return id;
-	}
+    public void setActive(boolean active){
+        super.setActive(active);
+    }
+
+    /**
+     * Returns the id (MapMouseMode name).
+     * This name should be unique for each MapMouseMode.
+     * @return String ID
+     */
+    public String getID() {
+        return id;
+    }
 
 }
