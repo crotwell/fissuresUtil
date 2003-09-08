1b49f4598b7f773fb29a9172b85d63a47047ee86
crotwell <crotwell@localhost>
2003-09-08 12:48:23 -0400

    [svn r5470] avoid divide by zero

--- src/edu/sc/seis/fissuresUtil/display/TimeScaleCalc.java
+++ src/edu/sc/seis/fissuresUtil/display/TimeScaleCalc.java
@@ -37,6 +37,7 @@ public class TimeScaleCalc implements ScaleMapper, TimeListener {
             return;
         }
         int majTickNum = totalPixels/60;
+        if (majTickNum < 2) {majTickNum = 2;}// prevent divid by zero
         majTickTime = timeIntv/majTickNum;
         majTickRatio = 10;
         daysInBorder = false;
