883d3b5bef5d61ef6eeec2e39fba7b8d9a5a44be
crotwell <crotwell@localhost>
2003-09-11 22:59:33 -0400

    [svn r5628] check ffor null

--- src/edu/sc/seis/fissuresUtil/display/ParseRegions.java
+++ src/edu/sc/seis/fissuresUtil/display/ParseRegions.java
@@ -18,61 +18,63 @@ import java.util.Properties;
 
 public class ParseRegions {
     public ParseRegions (){
-	load();
+        load();
     }
     
     /** Gets the Geographic region number for a name. Returns 0 if the
-	name cannot be found. */
+     name cannot be found. */
     public int getRegionValue(String region) {
-	String geoNum = feProps.getProperty(region.replace(' ','_'));
-	if (geoNum != null) {
-	    geoNum = geoNum.substring("GeogRegion".length(), geoNum.length());
-	    return Integer.parseInt(geoNum);
-	} // end of if (feProps.get(region.replace(' ','_')))
-	return 0;
+        String geoNum = feProps.getProperty(region.replace(' ','_'));
+        if (geoNum != null) {
+            geoNum = geoNum.substring("GeogRegion".length(), geoNum.length());
+            return Integer.parseInt(geoNum);
+        } // end of if (feProps.get(region.replace(' ','_')))
+        return 0;
     }
-
+    
     public String getGeographicRegionName(int geoNum) {
-        String propValue = 
-	    feProps.getProperty("GeogRegion"+geoNum);
-	if (propValue != null && propValue.length() > 1) {
+        String propValue =
+            feProps.getProperty("GeogRegion"+geoNum);
+        if (propValue != null && propValue.length() > 1) {
             return propValue;
-	}
-	return "GeoRegion"+geoNum;
+        }
+        return "GeoRegion"+geoNum;
     }
-
+    
     public String getSeismicRegionName(int seisNum) {
         String propValue =
-           feProps.getProperty("SeismicRegion"+seisNum);
-	if (propValue != null && propValue.length() > 1) {
+            feProps.getProperty("SeismicRegion"+seisNum);
+        if (propValue != null && propValue.length() > 1) {
             return propValue;
-	}
-	return "SeisRegion"+seisNum;
+        }
+        return "SeisRegion"+seisNum;
     }
-
+    
     public String getRegionName(FlinnEngdahlRegion region){
-	if (region.type.equals(FlinnEngdahlType.SEISMIC_REGION)) {
-	    return getSeismicRegionName(region.number);
-	}
-	if (region.type.equals(FlinnEngdahlType.GEOGRAPHIC_REGION)) {
-	    return getGeographicRegionName(region.number);
-	}
-	return "Unknown";
+        if (region != null && region.type != null) {
+            if (region.type.equals(FlinnEngdahlType.SEISMIC_REGION)) {
+                return getSeismicRegionName(region.number);
+            }
+            if (region.type.equals(FlinnEngdahlType.GEOGRAPHIC_REGION)) {
+                return getGeographicRegionName(region.number);
+            }
+        }
+        return "Unknown";
     }
-
+    
     protected void load() {
-	try {
-	    ClassLoader loader = getClass().getClassLoader();
-	    InputStream fstream = 
-		loader.getResourceAsStream("edu/sc/seis/fissuresUtil/display/FERegions.prop");
-	    feProps = new Properties();
-	    feProps.load(fstream);
-	} catch (IOException e) {
-	    System.err.println("Cannot load FE regions");
-	    e.printStackTrace();
-	} // end of catch
-    }	
-	
+        try {
+            ClassLoader loader = getClass().getClassLoader();
+            InputStream fstream =
+                loader.getResourceAsStream("edu/sc/seis/fissuresUtil/display/FERegions.prop");
+            feProps = new Properties();
+            feProps.load(fstream);
+        } catch (IOException e) {
+            System.err.println("Cannot load FE regions");
+            e.printStackTrace();
+        } // end of catch
+    }
+    
     protected Properties feProps;
-
+    
 }// parseRegions
