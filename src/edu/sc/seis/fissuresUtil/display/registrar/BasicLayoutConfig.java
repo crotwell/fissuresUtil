82054778a29e80a0ce9b76be1e4b3269c00c4090
groves <groves@localhost>
2003-09-23 02:39:45 -0400

    [svn r5694] moved distance calculation to DisplayUtils from here

--- src/edu/sc/seis/fissuresUtil/display/registrar/BasicLayoutConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/registrar/BasicLayoutConfig.java
@@ -1,17 +1,11 @@
 package edu.sc.seis.fissuresUtil.display.registrar;
 import java.util.*;
 
-import edu.iris.Fissures.IfEvent.EventAccessOperations;
-import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
-import edu.iris.Fissures.IfNetwork.Channel;
-import edu.iris.Fissures.IfNetwork.ChannelId;
-import edu.iris.Fissures.IfNetwork.Site;
-import edu.iris.Fissures.Location;
 import edu.iris.Fissures.model.QuantityImpl;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.model.UnitRangeImpl;
 import edu.iris.Fissures.network.ChannelIdUtil;
-import edu.sc.seis.fissuresUtil.bag.DistAz;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import javax.swing.JOptionPane;
 
@@ -19,31 +13,31 @@ import javax.swing.JOptionPane;
 
 public class BasicLayoutConfig implements LayoutConfig{
     public BasicLayoutConfig(){ }
-
+    
     public BasicLayoutConfig(DataSetSeismogram[] seismos){
         add(seismos);
     }
-
+    
     public synchronized void addListener(LayoutListener listener) {
         listeners.add(listener);
         fireLayoutEvent();
     }
-
+    
     public synchronized void removeListener(LayoutListener listener) {
         listeners.remove(listener);
     }
-
+    
     public void fireLayoutEvent(){
         fireLayoutEvent(generateLayoutEvent());
     }
-
+    
     private synchronized void fireLayoutEvent(LayoutEvent event) {
         Iterator it = listeners.iterator();
         while(it.hasNext()){
             ((LayoutListener)it.next()).updateLayout(event);
         }
     }
-
+    
     /*
      *Checks the given seismograms for already being in the config, and if not,
      *adds them and fires a new LayoutEvent with them
@@ -53,7 +47,7 @@ public class BasicLayoutConfig implements LayoutConfig{
         boolean someAdded = false;
         for (int i = 0; i < seismos.length; i++){
             if(!(distanceMap.containsKey(seismos[i]))){
-                QuantityImpl dist = calculateDistance(seismos[i]);
+                QuantityImpl dist = DisplayUtils.calculateDistance(seismos[i]);
                 if(dist == null){
                     noDist.add(seismos[i]);
                 }else{
@@ -71,9 +65,9 @@ public class BasicLayoutConfig implements LayoutConfig{
         }else if(seis.size() <= 0){
             unableToDisplayDialog(noDist);
         }
-
+        
     }
-
+    
     private String getNoDistString(List noDist) {
         String s = "";
         Iterator it = noDist.iterator();
@@ -85,7 +79,7 @@ public class BasicLayoutConfig implements LayoutConfig{
             } else {
                 s += "an event, ";
             }
-
+            
             if (dss.getDataSet().getChannel(dss.getRequestFilter().channel_id) == null) {
                 s += "no channel for "+ChannelIdUtil.toString(dss.getRequestFilter().channel_id);
             } else {
@@ -95,21 +89,21 @@ public class BasicLayoutConfig implements LayoutConfig{
         }
         return s;
     }
-
+    
     private void noDistDialog(List noDist){
         JOptionPane.showMessageDialog(null,
                                       "Some of the seismograms added to the record section have no distances in their data set so they will not be displayed.\n"+getNoDistString(noDist),
                                       "Unable to Display some Seismograms",
                                       JOptionPane.WARNING_MESSAGE);
     }
-
+    
     public void unableToDisplayDialog(List noDist){
         JOptionPane.showMessageDialog(null,
                                       "All of the seismograms added to the record section have no distances in their data set so it can not be displayed.\n"+getNoDistString(noDist),
                                       "Unable to display any seismograms",
                                       JOptionPane.WARNING_MESSAGE);
     }
-
+    
     /**
      *Attempts to remove all given seismograms from this Config.  If any are
      * removed, a layout event is fired
@@ -130,31 +124,31 @@ public class BasicLayoutConfig implements LayoutConfig{
             fireLayoutEvent();
         }
     }
-
+    
     public synchronized boolean contains(DataSetSeismogram seismo) {
         if(seis.contains(seismo)){
             return true;
         }
         return false;
     }
-
+    
     public synchronized void clear() {
         if(seis.size() > 0){
             seis.clear();
             fireLayoutEvent();
         }
     }
-
+    
     public DataSetSeismogram[] getSeismograms() {
         return (DataSetSeismogram[])seis.toArray(new DataSetSeismogram[seis.size()]);
     }
-
+    
     public void reset() {
         DataSetSeismogram[] seismograms = getSeismograms();
         seis.clear();
         add(seismograms);
     }
-
+    
     public void reset(DataSetSeismogram[] seismos) {
         List reset = new ArrayList();
         for (int i = 0; i < seismos.length; i++){
@@ -165,11 +159,11 @@ public class BasicLayoutConfig implements LayoutConfig{
         }
         add((DataSetSeismogram[])reset.toArray(new DataSetSeismogram[reset.size()]));
     }
-
+    
     public double getScale() {
         return scale;
     }
-
+    
     /**
      *sets the amount by which every seismogram in the layout is being scaled
      *@param scale - the factor by which the seismogram height is multiplied
@@ -180,7 +174,7 @@ public class BasicLayoutConfig implements LayoutConfig{
             fireLayoutEvent();
         }
     }
-
+    
     public synchronized LayoutEvent generateLayoutEvent(){
         DataSetSeismogram[] seis = getSeismograms();
         if(seis.length > 0){
@@ -229,42 +223,19 @@ public class BasicLayoutConfig implements LayoutConfig{
                 data[i] = new LayoutData(cur,
                                          centerPercentage - percentageOffset,
                                          centerPercentage + percentageOffset);
-
+                
             }
             UnitRangeImpl range = new UnitRangeImpl(startDist, endDist, UnitImpl.DEGREE);
             return new LayoutEvent(data, range);
         }
         return LayoutEvent.EMPTY_EVENT;
     }
-
-    public static QuantityImpl calculateDistance(DataSetSeismogram seis){
-        EventAccessOperations event = seis.getDataSet().getEvent();
-        ChannelId chanId = seis.getRequestFilter().channel_id;
-        Channel seismoChannel = seis.getDataSet().getChannel(chanId);
-        if(seismoChannel != null && event != null){
-            Site seisSite = seismoChannel.my_site;
-            Location seisLoc =  seisSite.my_location;
-            Location eventLoc = null;
-            try{
-                eventLoc = event.get_preferred_origin().my_location;
-            }catch(NoPreferredOrigin e){//if no preferred origin, just use the first
-                if (event.get_origins().length > 0) {
-                    eventLoc = event.get_origins()[0].my_location;
-                }
-            }
-            if (eventLoc == null) { return null; }
-            DistAz distAz = new DistAz(seisLoc.latitude, seisLoc.longitude,
-                                       eventLoc.latitude, eventLoc.longitude);
-            return new QuantityImpl(distAz.delta, UnitImpl.DEGREE);
-        }
-        return null;
-    }
-
+    
     private Map distanceMap = new HashMap();
-
+    
     private List listeners = new ArrayList();
-
+    
     private List seis =  new ArrayList();
-
+    
     private double scale = 1;
 }
