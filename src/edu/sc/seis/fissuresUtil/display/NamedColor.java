0fd7bfb1dd7a05efe8c3e939971837f1cfcda0df
Charles Groves <groves@seis.sc.edu>
2003-09-26 11:35:49 -0400

    [svn r5800] fixed ugly font in pick zone problem on macs

--- src/edu/sc/seis/fissuresUtil/display/NamedColor.java
+++ src/edu/sc/seis/fissuresUtil/display/NamedColor.java
@@ -14,24 +14,31 @@ import java.awt.Color;
 
 public class NamedColor extends Color {
     public NamedColor(Color color, String name){
-        this(color.getRed(), color.getBlue(), color.getGreen(), color.getAlpha(), name);
+		this(color.getRed(), color.getBlue(), color.getGreen(), color.getAlpha(), name);
     }
-
+	
     public NamedColor(Color color, int alpha, String name){
-        this(color.getRed(), color.getBlue(), color.getGreen(), alpha, name);
+		this(color.getRed(), color.getBlue(), color.getGreen(), alpha, name);
     }
-
+	
     public NamedColor(int r, int b, int g, int a, String name){
-        super(r, g, b, a);
-        this.name = name;
+		super(r, g, b, a);
+		this.name = name;
     }
-
+	
+	/**
+	 * @return  the Color behind this named color
+	 */
+	public Color getColor() {
+		return new Color(getRed(), getGreen(), getBlue(), getAlpha());
+	}
+	
     public String toString(){ return name; }
-
+	
     public void setName(String name){ this.name = name; }
-
+	
     public String getName(){ return name; }
-
+	
     private String name;
-
+	
 }// NamedColor
