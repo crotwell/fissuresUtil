004c15733a6a8afd9895cc2932a2ebbd59448b34
Charles Groves <groves@seis.sc.edu>
2003-09-28 21:12:49 -0400

    [svn r5832] fixed drawing for gaps, synchronized colors across filters, added a basic status table

--- src/edu/sc/seis/fissuresUtil/display/ParticleMotionView.java
+++ src/edu/sc/seis/fissuresUtil/display/ParticleMotionView.java
@@ -3,19 +3,23 @@ import edu.sc.seis.fissuresUtil.display.registrar.*;
 import java.awt.*;
 import java.util.*;
 
-import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.QuantityImpl;
 import edu.iris.Fissures.model.UnitRangeImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.freq.NamedFilter;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import edu.sc.seis.fissuresUtil.xml.FilteredDataSetSeismogram;
-import edu.sc.seis.fissuresUtil.xml.RequestFilterChangeListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.util.List;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 import org.apache.log4j.Logger;
 
 /**
@@ -50,7 +54,8 @@ public class ParticleMotionView extends JComponent implements TimeListener{
     public void add(NamedFilter filter){
         Iterator it = parMos.iterator();
         while(it.hasNext()){
-            ((ParticleMotion)it.next()).add(filter);
+            ParticleMotion cur = (ParticleMotion)it.next();
+            cur.add(filter, getColor(cur.getTimeConfig(), filter));
         }
         repaint();
     }
@@ -103,7 +108,7 @@ public class ParticleMotionView extends JComponent implements TimeListener{
     }
     
     
-    public synchronized void drawAzimuth(ParticleMotion particleMotion, Graphics2D graphics2D) {
+    public void drawAzimuth(ParticleMotion particleMotion, Graphics2D graphics2D) {
         if(!particleMotion.isHorizontalPlane()) return;
         Shape sector = getSectorShape();
         graphics2D.setPaint(Color.LIGHT_GRAY);
@@ -116,19 +121,6 @@ public class ParticleMotionView extends JComponent implements TimeListener{
     public void drawTitles(LocalSeismogramImpl hseis, LocalSeismogramImpl vseis) {
         pmd.setHorizontalTitle(hseis.getName());
         pmd.setVerticalTitle(vseis.getName());
-    }
-    
-    public synchronized Shape getParticleMotionPath(int[] x, int[] y) {
-        int len = x.length;
-        if(y.length < len) { len = y.length;}
-        GeneralPath generalPath = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
-        if(len != 0) {
-            generalPath.moveTo(x[0], y[0]);
-        }
-        for(int counter = 1; counter < len; counter++) {
-            generalPath.lineTo(x[counter], y[counter]);
-        }
-        return (Shape)generalPath;
     }
     
     public void drawAzimuths(Graphics2D g2D) {
@@ -144,15 +136,17 @@ public class ParticleMotionView extends JComponent implements TimeListener{
         while(it.hasNext()){
             Double key = (Double)it.next();
             Color parMoCo =(Color)azimuths.get(key);
-            g2D.setColor(new Color(parMoCo.getRed(), parMoCo.getGreen(),
-                                   parMoCo.getBlue(), 96));
-            GeneralPath generalPath = new GeneralPath();
-            double degrees = key.doubleValue();
-            int x = (int)(fmin * Math.cos(Math.toRadians(degrees)));
-            int y = (int)(fmax * Math.sin(Math.toRadians(degrees)));
-            generalPath.moveTo(newx+x, newy-y);
-            generalPath.lineTo(newx-x, newy+y);
-            g2D.draw(generalPath);
+            if(parMoCo != null){
+                g2D.setColor(new Color(parMoCo.getRed(), parMoCo.getGreen(),
+                                       parMoCo.getBlue(), 96));
+                GeneralPath generalPath = new GeneralPath();
+                double degrees = key.doubleValue();
+                int x = (int)(fmin * Math.cos(Math.toRadians(degrees)));
+                int y = (int)(fmax * Math.sin(Math.toRadians(degrees)));
+                generalPath.moveTo(newx+x, newy-y);
+                generalPath.lineTo(newx-x, newy+y);
+                g2D.draw(generalPath);
+            }
         }
     }
     
@@ -183,20 +177,38 @@ public class ParticleMotionView extends JComponent implements TimeListener{
         return (Shape)generalPath;
     }
     
-    public synchronized void addParticleMotionDisplay(DataSetSeismogram hseis,
-                                                      DataSetSeismogram vseis,
-                                                      TimeConfig tc,
-                                                      Color color,
-                                                      String key,
-                                                      boolean horizPlane) {
+    public synchronized void add(DataSetSeismogram hseis,
+                                 DataSetSeismogram vseis,
+                                 TimeConfig tc,
+                                 Color color,
+                                 String key,
+                                 boolean horizPlane) {
         ParticleMotion newParMo = new ParticleMotion(hseis, vseis, tc, color, key,horizPlane);
         parMos.add(newParMo);
         Iterator it = SeismogramDisplay.activeFilters.iterator();
         while(it.hasNext()){
-            newParMo.add((NamedFilter)it.next());
+            NamedFilter cur = (NamedFilter)it.next();
+            color = getColor(tc, cur);
+            newParMo.add(cur, color);
         }
         updateAmps();
+        if(statusTable != null)
+            statusTable.add(newParMo);
     }
+    
+    private Color getColor(TimeConfig tc, NamedFilter filter){
+        Map filterToColor = (Map)timeConfigToFilterColor.get(tc);
+        if(filterToColor == null){
+            filterToColor = new HashMap();
+            timeConfigToFilterColor.put(tc, filterToColor);
+        }
+        if(!filterToColor.containsKey(filter))
+            filterToColor.put(filter,
+                              SeismogramDisplay.COLORS[filterCount++ % SeismogramDisplay.COLORS.length]);
+        return (Color)filterToColor.get(filter);
+    }
+    
+    private Map timeConfigToFilterColor = new HashMap();
     
     public void addSector(double degreeone, double degreetwo) {
         sectors.add(new java.awt.geom.Point2D.Double(degreeone, degreetwo));
@@ -242,6 +254,12 @@ public class ParticleMotionView extends JComponent implements TimeListener{
         return rtnValues;
     }
     
+    public JTable getStatusTable(){
+        if(statusTable == null)
+            statusTable = new ParticleMotionStatusTable(parMos);
+        return statusTable;
+    }
+    
     private String displayKey;
     
     private Map keysToAmpConfigs = new HashMap();
@@ -256,17 +274,32 @@ public class ParticleMotionView extends JComponent implements TimeListener{
     
     UnitRangeImpl vertRange = DisplayUtils.ONE_RANGE;
     
-    Point2D.Float startPoint;
-    
-    Point2D.Float endPoint;
-    
     private ParticleMotionDisplay pmd;
     
-    class ParticleMotion implements TimeListener, AmpListener,
+    private ParticleMotionStatusTable statusTable;
+    
+    private static int filterCount = SeismogramDisplay.COLORS.length/2;
+    
+    private static Logger logger = Logger.getLogger(ParticleMotionView.class);
+    
+    private int i = 0;
+    
+    private class ParticleMotion implements TimeListener, AmpListener,
         SeismogramContainerListener{
         public ParticleMotion(DataSetSeismogram hSeis, DataSetSeismogram vSeis,
                               TimeConfig tc, Color color, String key,
                               boolean horizPlane) {
+            
+            if(hSeis instanceof FilteredDataSetSeismogram){
+                name = hSeis.getName();
+            }else{
+                name = hSeis.getRequestFilter().channel_id.station_code;
+            }
+            this.color = color;
+            hSeis = (DataSetSeismogram)hSeis.clone();
+            hSeis.setName(hSeis.getName() + i++);
+            vSeis = (DataSetSeismogram)vSeis.clone();
+            vSeis.setName(vSeis.getName() + i++);
             horiz = new SeismogramContainer(this,hSeis);
             vert = new SeismogramContainer(this, vSeis);
             this.tc = tc;
@@ -278,7 +311,6 @@ public class ParticleMotionView extends JComponent implements TimeListener{
                                        hSeis.getRequestFilter().channel_id,
                                        color);
             }
-            this.color = color;
             tc.fireTimeEvent();
         }
         
@@ -320,19 +352,48 @@ public class ParticleMotionView extends JComponent implements TimeListener{
                 }
                 g2D.setColor(color);
                 g2D.setStroke(DisplayUtils.TWO_PIXEL_STROKE);
-                int[]hPixels =  SimplePlotUtil.getPlottableSimple(horiz.getIterator(tr),
-                                                                  ae.getAmp(horiz.getDataSetSeismogram()),
-                                                                  size);
-                int[] vPixels = SimplePlotUtil.getPlottableSimple(vert.getIterator(tr),
-                                                                  ae.getAmp(vert.getDataSetSeismogram()),
-                                                                  size);
-                SimplePlotUtil.flipArray(hPixels, size.width);
-                g2D.draw(getParticleMotionPath(hPixels, vPixels));
+                GeneralPath generalPath = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
+                boolean prevPointBad = true;//first point needs to be a move to
+                SeismogramIterator hIt = horiz.getIterator(tr);
+                double hMin = ae.getAmp(horiz.getDataSetSeismogram()).getMinValue();
+                double hMax = ae.getAmp(horiz.getDataSetSeismogram()).getMaxValue();
+                SeismogramIterator vIt = vert.getIterator(tr);
+                double vMin = ae.getAmp(vert.getDataSetSeismogram()).getMinValue();
+                double vMax = ae.getAmp(vert.getDataSetSeismogram()).getMaxValue();
+                while(hIt.hasNext() && vIt.hasNext()){
+                    float hVal = getVal(hIt, hMin, hMax, size.height);
+                    float vVal = getVal(vIt, vMin, vMax, size.height);
+                    if(hVal == Integer.MAX_VALUE || vVal == Integer.MAX_VALUE){
+                        prevPointBad = true;
+                    }else{
+                        hVal *= -1;
+                        hVal += size.height;
+                        if(prevPointBad){
+                            generalPath.moveTo(hVal, vVal);
+                            prevPointBad = false;
+                        }else{
+                            generalPath.lineTo(hVal, vVal);
+                        }
+                    }
+                }
+                g2D.draw(generalPath);
             }
             Iterator it = filterToParMo.keySet().iterator();
             while(it.hasNext()){
                 ((ParticleMotion)filterToParMo.get(it.next())).draw(g, size);
             }
+        }
+        
+        private int getVal(SeismogramIterator it, double minAmp, double maxAmp,
+                           int size){
+            double itVal = ((QuantityImpl)it.next()).getValue();
+            if(Double.isNaN(itVal)){//Gap in trace
+                itVal = Integer.MAX_VALUE;
+            }else{
+                itVal= Math.round(SimplePlotUtil.linearInterp(minAmp, 0, maxAmp,
+                                                              size, itVal));
+            }
+            return (int)itVal;
         }
         
         public void updateData() {
@@ -351,20 +412,35 @@ public class ParticleMotionView extends JComponent implements TimeListener{
             return this.horizPlane;
         }
         
-        public void add(NamedFilter filter){
-            if(!filterToParMo.containsKey(filter))
+        public void add(NamedFilter filter, Color color){
+            if(!filterToParMo.containsKey(filter)){
                 filterToParMo.put(filter, new ParticleMotion(FilteredDataSetSeismogram.getFiltered(horiz.getDataSetSeismogram(), filter),
                                                              FilteredDataSetSeismogram.getFiltered(vert.getDataSetSeismogram(), filter),
                                                              tc,
-                                                             SeismogramDisplay.COLORS[filterCount++ % SeismogramDisplay.COLORS.length],
+                                                             color,
                                                              key,
                                                              horizPlane));
+                if(statusTable != null)
+                    statusTable.setComponent(this, generateLabel());
+            }
+        }
+        
+        private JComponent generateLabel(){
+            JPanel labelPanel = new JPanel(new GridLayout(filterToParMo.keySet().size() + 1, 1));
+            labelPanel.add(ParticleMotionView.generateLabel(this));
+            Iterator it = filterToParMo.keySet().iterator();
+            while(it.hasNext()){
+                labelPanel.add(ParticleMotionView.generateLabel((ParticleMotion)filterToParMo.get(it.next())));
+            }
+            return labelPanel;
         }
         
         public void remove(NamedFilter filter){
             if(filterToParMo.containsKey(filter)){
                 ((ParticleMotion)filterToParMo.get(filter)).tearDownConfigs();
                 filterToParMo.remove(filter);
+                if(statusTable != null)
+                    statusTable.setComponent(this, generateLabel());
             }
         }
         
@@ -377,8 +453,15 @@ public class ParticleMotionView extends JComponent implements TimeListener{
             }
         }
         
+        public Color getColor(){ return color; }
+        
+        public TimeConfig getTimeConfig(){ return tc; }
+        
         Map filterToParMo = new HashMap();
         
+        public String toString(){ return name; }
+        
+        private String name;
         private SeismogramContainer horiz, vert;
         public String key = new String();
         private boolean visible = true;
@@ -389,9 +472,138 @@ public class ParticleMotionView extends JComponent implements TimeListener{
         private TimeConfig tc;
     }
     
-    private static int filterCount = SeismogramDisplay.COLORS.length/2;
+    private static JLabel generateLabel(ParticleMotion parMo){
+        JLabel label = new JLabel(parMo.toString(), JLabel.CENTER);
+        label.setForeground(parMo.getColor());
+        return label;
+    }
     
-    private static int i = 0;
-    
-    private static Logger logger = Logger.getLogger(ParticleMotionView.class);
+    private class ParticleMotionStatusTable extends JTable{
+        
+        public ParticleMotionStatusTable(List parMos){
+            setModel(new ViewTableModel());
+            setDefaultRenderer(String.class, new TableCellRenderer(){
+                        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                            return ((CellStruct)cellStructs.get(row)).comp;
+                        }
+                    });
+            setColumnWidths();
+            Iterator it = parMos.iterator();
+            while(it.hasNext())
+                  add((ParticleMotion)it.next());
+        }
+        
+        public void setComponent(ParticleMotion parMo, JComponent comp){
+            synchronized(cellStructs){
+                TimeConfig tc = parMo.getTimeConfig();
+                Iterator it = cellStructs.iterator();
+                while(it.hasNext()){
+                    CellStruct cur = (CellStruct)it.next();
+                    if(cur.tc.equals(tc)){
+                        cur.comp = comp;
+                        rowHeight = comp.getPreferredSize().height;
+                        revalidate();
+                        return;
+                    }
+                }
+            }
+        }
+        
+        public void add(ParticleMotion parMo){
+            synchronized(cellStructs){
+                TimeConfig tc = parMo.getTimeConfig();
+                Iterator it = cellStructs.iterator();
+                while(it.hasNext()){
+                    CellStruct cur = (CellStruct)it.next();
+                    if(cur.tc.equals(tc)){
+                        setOrientation(parMo.key, cur);
+                        return;
+                    }
+                }
+                CellStruct cell = new CellStruct(tc);
+                cellStructs.add(cell);
+                cell.name = parMo.toString();
+                cell.comp = new JLabel(cell.name, JLabel.CENTER);
+                cell.comp.setForeground(parMo.getColor());
+                setOrientation(parMo.key, cell);
+            }
+        }
+        
+        private void setOrientation(String key, CellStruct cell){
+            if(key.equals(DisplayUtils.NORTHEAST))
+                cell.orientations[0] = Boolean.TRUE;
+            else if(key.equals(DisplayUtils.UPNORTH))
+                cell.orientations[1] = Boolean.TRUE;
+            else
+                cell.orientations[2] = Boolean.TRUE;
+        }
+        
+        private void setColumnWidths(){
+            int columnCount = getColumnCount();
+            for(int counter = 0; counter < columnCount; counter++) {
+                String columnName =getColumnName(counter);
+                int width = 50;
+                if( columnName.equals("Trace") ) width = 100;
+                else width = 20;
+                getColumnModel().getColumn(counter).setPreferredWidth(width);
+                getColumnModel().getColumn(counter).setMinWidth(width);
+            }
+        }
+        
+        private class ViewTableModel extends AbstractTableModel{
+            public int getColumnCount() {
+                return keys.length + 1;
+            }
+            
+            public int getRowCount() {
+                return cellStructs.size();
+            }
+            
+            
+            public String getColumnName(int columnIndex) {
+                if(columnIndex>0)
+                    return keys[columnIndex - 1];
+                else
+                    return "Trace";
+            }
+            
+            public Class getColumnClass(int columnIndex) {
+                if(columnIndex == 0)
+                    return String.class;
+                else
+                    return Boolean.class;
+            }
+            
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                synchronized(cellStructs){
+                    if(cellStructs.size() > rowIndex)
+                        if(columnIndex == 0)
+                            return ((CellStruct)cellStructs.get(rowIndex)).name;
+                        else
+                            return ((CellStruct)cellStructs.get(rowIndex)).orientations[columnIndex -1];
+                }
+                return null;
+            }
+            
+            
+            private String[] keys = {DisplayUtils.NORTHEAST,
+                    DisplayUtils.UPNORTH,
+                    DisplayUtils.UPEAST};
+            
+        }
+        
+        private class CellStruct{
+            public CellStruct(TimeConfig tc){
+                this.tc = tc;
+            }
+            
+            public TimeConfig tc;
+            public Boolean[] orientations = { Boolean.FALSE, Boolean.FALSE,
+                    Boolean.FALSE };
+            public JComponent comp;
+            public String name;
+        }
+        
+        private List cellStructs = new ArrayList();
+    }
 }// ParticleMotionView
