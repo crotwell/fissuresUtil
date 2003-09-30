cd479bdfe5e9d0b247b153787822aecd3d5e2e9c
groves <groves@localhost>
2003-09-30 17:58:53 -0400

    [svn r5912] initial

---
+++ src/edu/sc/seis/fissuresUtil/display/ReverseMultiWindowDisplay.java
@@ -0,0 +1,18 @@
+/**
+ * ReverseMultiWindowDisplay.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.display;
+
+public class ReverseMultiWindowDisplay extends MultiSeismogramWindowDisplay{
+    public ReverseMultiWindowDisplay(SeismogramSorter sorter){
+        super(sorter);
+    }
+    
+    protected void addBSD(BasicSeismogramDisplay disp, int pos){
+        super.addBSD(disp, getComponentCount() - pos);
+    }
+}
+
