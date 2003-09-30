a259e06d9626015c682deef70a56f9e753516aa2
Charles Groves <groves@seis.sc.edu>
2003-09-30 12:55:39 -0400

    [svn r5877] added seis to allow time to be calculated off actual seis time

--- src/edu/sc/seis/fissuresUtil/display/drawable/Event.java
+++ src/edu/sc/seis/fissuresUtil/display/drawable/Event.java
@@ -4,6 +4,7 @@ import edu.sc.seis.TauP.Arrival;
 import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.display.registrar.AmpEvent;
 import edu.sc.seis.fissuresUtil.display.registrar.TimeEvent;
+import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
@@ -22,25 +23,25 @@ import java.util.List;
  */
 
 public class Event implements NamedDrawable{
-    public Event(Arrival[] arrivals, MicroSecondDate originTime){
-        this(arrivals, originTime, null);
+    public Event(Arrival[] arrivals, MicroSecondDate originTime, DataSetSeismogram seis){
+        this(arrivals, originTime, seis, null);
     }
-
-    public Event(Arrival[] arrivals, MicroSecondDate originTime, String name){
+    
+    public Event(Arrival[] arrivals, MicroSecondDate originTime, DataSetSeismogram seis, String name){
         flags = new ArrayList(arrivals.length);
         for (int i = 0; i < arrivals.length; i++){
             MicroSecondDate time = new MicroSecondDate((long)(arrivals[i].getTime() * 1000000) +
                                                            originTime.getMicroSecondTime());
-            flags.add(new Flag(time, arrivals[i].getName()));
+            flags.add(new Flag(time, arrivals[i].getName(), seis));
         }
-        flags.add(new Flag(originTime, "Origin"));
+        flags.add(new Flag(originTime, "Origin", seis));
         this.name = name;
     }
-
+    
     public void setVisibility(boolean b) {
         visible = b;
     }
-
+    
     public void setColor(Color color){
         Iterator it = flags.iterator();
         while(it.hasNext()){
@@ -48,9 +49,9 @@ public class Event implements NamedDrawable{
         }
         this.color = color;
     }
-
+    
     public Color getColor(){ return color; }
-
+    
     public Rectangle2D drawName(Graphics2D canvas, int xPosition, int yPosition) {
         if(visible && name != null){
             canvas.drawString(name, xPosition, yPosition);
@@ -60,7 +61,7 @@ public class Event implements NamedDrawable{
         }
         return DisplayUtils.EMPTY_RECTANGLE;
     }
-
+    
     public void draw(Graphics2D canvas, Dimension size, TimeEvent currentTime, AmpEvent currentAmp) {
         if(visible){
             Iterator it = flags.iterator();
@@ -69,12 +70,12 @@ public class Event implements NamedDrawable{
             }
         }
     }
-
+    
     private Color color = Color.RED;
-
+    
     private List flags;
-
+    
     private boolean visible = true;
-
+    
     private String name;
 }
