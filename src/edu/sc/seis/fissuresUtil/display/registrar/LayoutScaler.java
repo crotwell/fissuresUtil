af609bcef5ce0b0f904a335ee66095efece9423f
Charles Groves <groves@seis.sc.edu>
2003-10-01 19:19:12 -0400

    [svn r5956] scaling now works off of the concept of always having some room to play with in the slider

--- src/edu/sc/seis/fissuresUtil/display/registrar/LayoutScaler.java
+++ src/edu/sc/seis/fissuresUtil/display/registrar/LayoutScaler.java
@@ -8,7 +8,6 @@ package edu.sc.seis.fissuresUtil.display.registrar;
 
 import edu.sc.seis.fissuresUtil.display.RecordSectionDisplay;
 import java.util.Hashtable;
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
@@ -16,26 +15,45 @@ import javax.swing.event.ChangeListener;
 
 public class LayoutScaler extends JSlider implements ChangeListener{
     public LayoutScaler(RecordSectionDisplay recSec){
-        super(JSlider.VERTICAL, MIN_SCALE, MAX_SCALE, INITIAL_SCALE);
-        setMajorTickSpacing(10);
-        setPaintTicks(true);
-
-        //Create the label table
-        Hashtable labelTable = new Hashtable();
-        labelTable.put(new Integer(MIN_SCALE), new JLabel("Min"));
-        labelTable.put(new Integer(MAX_SCALE), new JLabel("Max"));
-        setLabelTable(labelTable);
+        super(JSlider.VERTICAL);
+        setMinPoint();
+        setValue(INITIAL_SCALE);
         setPaintLabels(true);
+        setPaintTicks(true);
         this.recSec = recSec;
         addChangeListener(this);
+        recSec.setLayoutScaler(this);
     }
-
+    
     public void stateChanged(ChangeEvent ce){
-        recSec.scalingChanged(getValue());
+        if(!changingValues){
+            recSec.scalingChanged(getValue());
+        }
     }
-
+    
+    public void setMinPoint(){
+        setMinPoint(MIN_SCALE, INITIAL_SCALE);
+    }
+    
+    private boolean changingValues = false;
+    
+    public void setMinPoint(int minpoint, int curValue){
+        changingValues = true;
+        setMinimum(minpoint);
+        int maxpoint = minpoint + minpoint*10;
+        setMaximum(maxpoint);
+        setValue(curValue);
+        changingValues = false;
+        setMajorTickSpacing((maxpoint - minpoint)/10);
+        //Create the label table
+        Hashtable labelTable = new Hashtable();
+        labelTable.put(new Integer(minpoint), new JLabel("Min"));
+        labelTable.put(new Integer(maxpoint), new JLabel("Max"));
+        setLabelTable(labelTable);
+    }
+    
     private RecordSectionDisplay recSec;
-
-    public static final int MIN_SCALE = 10, MAX_SCALE = 100, INITIAL_SCALE = 45;
+    
+    public static final int MIN_SCALE = 10, INITIAL_SCALE = 45;
 }
 
