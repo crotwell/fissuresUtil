5dfca7d6f648a8c979527507749754a28b5b8839
Charles Groves <groves@seis.sc.edu>
2003-10-17 14:50:10 -0400

    [svn r6146] added support for multiple sections

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/FileWriterReporter.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/FileWriterReporter.java
@@ -8,7 +8,9 @@ package edu.sc.seis.fissuresUtil.exceptionHandler;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.List;
+import java.io.IOException;
 
 
 
@@ -21,19 +23,36 @@ public class FileWriterReporter implements ExceptionReporter{
         this.file = file;
     }
     
-    public void report(String message, Throwable e, Map parsedContents) {
-        try {
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(fw);
-            String str = message+"\n";
-            bw.write(str, 0, str.length());
-            str = ExceptionReporterUtils.getTrace(e);
-            bw.write(str, 0, str.length());
-            str = ExceptionReporterUtils.getSysInfo();
-            bw.write(str, 0, str.length());
-            bw.close();
-            fw.close();
-        } catch(Exception ex) {}
+    public void report(String message, Throwable e, List sections) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        String str = message+ "\n\n";
+        String stackTrace = "Stack Trace";
+        str +=   stackTrace + makeDivider(stackTrace.length()) + ExceptionReporterUtils.getTrace(e);
+        bw.write(constructString(str, sections));
+        bw.close();
+    }
+    
+    private String constructString(String initialBit, List sections){
+        Iterator it = sections.iterator();
+        while(it.hasNext()){
+            initialBit += "\n" + constructString((Section)it.next());
+        }
+        return initialBit;
+    }
+    
+    private String constructString(Section sec) {
+        String result = sec.getName() + makeDivider(sec.getName().length());
+        return result += sec.getContents();
+    }
+    
+    private String makeDivider(int len){
+        StringBuffer div = new StringBuffer(len + 2);
+        div.append('\n');
+        for (int i = 0; i < len; i++) {
+            div.append('=');
+        }
+        div.append('\n');
+        return div.toString();
     }
     
     private File file;
