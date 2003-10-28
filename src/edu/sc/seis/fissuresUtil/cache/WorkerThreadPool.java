52973e835d751f48fe51230e02dd67a43d91d44d
groves <groves@localhost>
2003-10-28 15:44:54 -0500

    [svn r6252] added support for extenders

--- src/edu/sc/seis/fissuresUtil/cache/WorkerThreadPool.java
+++ src/edu/sc/seis/fissuresUtil/cache/WorkerThreadPool.java
@@ -9,59 +9,64 @@ package edu.sc.seis.fissuresUtil.cache;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import java.util.LinkedList;
 
-public class WorkerThreadPool
-{
+public class WorkerThreadPool{
     public WorkerThreadPool(String name, int numThreads){
         this(name, numThreads, Thread.NORM_PRIORITY);
     }
-
-
+    
+    
     public WorkerThreadPool(String name, int numThreads, int priority) {
         this.name = name;
-        for (int i = 0; i < numThreads; i++) {
-            BackgroundWorker t = new BackgroundWorker(name + i, priority);
-            t.start();
-            workers.add(t);
-        }
+        this.priority = priority;
+        for (int i = 0; i < numThreads; i++) { createWorker(); }
     }
-
+    
+    protected synchronized void createWorker(){
+        BackgroundWorker t = new BackgroundWorker(name + workers.size(),
+                                                  priority);
+        t.start();
+        workers.add(t);
+    }
+    
     public synchronized void invokeLater(Runnable runnable) {
         if(!queue.contains(runnable)){
             queue.addFirst(runnable);
         }
         notifyAll();
     }
-
+    
     public static WorkerThreadPool getDefaultPool() {
         if (defaultPool == null) {
             defaultPool = new WorkerThreadPool("default workers", 2);
         }
         return defaultPool;
     }
-
+    
     static WorkerThreadPool defaultPool;
-
-
+    
+    
     protected synchronized Runnable getFromQueue() throws InterruptedException {
         while (queue.isEmpty()) {
             wait();
         }
         return (Runnable)queue.removeLast();
     }
-
+    
     protected LinkedList queue = new LinkedList();
-
+    
     protected LinkedList workers = new LinkedList();
-
-    private String name;
-
+    
+    protected String name;
+    
+    private int priority;
+    
     protected class BackgroundWorker extends Thread {
         protected BackgroundWorker(String name, int priority) {
             super(name);
             setPriority(priority);
             setDaemon(true);
         }
-
+        
         public void run() {
             while (noQuit) {
                 try {
@@ -72,7 +77,7 @@ public class WorkerThreadPool
                 }
             }
         }
-
+        
         boolean noQuit = true;
     }
 }
