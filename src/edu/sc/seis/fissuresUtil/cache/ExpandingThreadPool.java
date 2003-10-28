df2bc7bf899a563d6fc3d1124fa712293aa9efee
Charles Groves <groves@seis.sc.edu>
2003-10-28 16:48:43 -0500

    [svn r6262] fixed race conditon in expansion of thread pool

--- src/edu/sc/seis/fissuresUtil/cache/ExpandingThreadPool.java
+++ src/edu/sc/seis/fissuresUtil/cache/ExpandingThreadPool.java
@@ -6,18 +6,24 @@
 
 package edu.sc.seis.fissuresUtil.cache;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExpandingThreadPool extends WorkerThreadPool{
     public ExpandingThreadPool(String name, int maxSize){
-        super(name, maxSize);
+        super(name, 1);
         this.maxSize = maxSize;
     }
-    //FIXME this is a race condition
-    /*public synchronized void invokeLater(Runnable r){
-        super.invokeLater(r);
-        if(queue.contains(r) && workers.size() < maxSize){
+    
+    public synchronized void invokeLater(Runnable r){
+        formerRunnables.add(r);
+        if(active.size() == workers.size() && workers.size() < maxSize){
             createWorker();
         }
-     }*/
+        super.invokeLater(r);
+    }
+    
+    private Set formerRunnables = new HashSet();
     
     private int maxSize;
 }
