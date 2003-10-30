ebd0624b183c6f6fdcee326a54f4593a171e0592
Philip Crotwell <crotwell@seis.sc.edu>
2003-10-30 15:44:25 -0500

    [svn r6303] remove throws

--- src/edu/sc/seis/fissuresUtil/namingService/FissuresNamingService.java
+++ src/edu/sc/seis/fissuresUtil/namingService/FissuresNamingService.java
@@ -37,28 +37,28 @@ public interface FissuresNamingService {
     NetworkDC getNetworkDC(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
 
     DataCenter getSeismogramDC(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-    
+
     PlottableDC getPlottableDC(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-    
-    org.omg.CORBA.Object getRoot()throws org.omg.CORBA.ORBPackage.InvalidName ;
+
+    org.omg.CORBA.Object getRoot();
 
     EventDC getEventDC(String dns, String objectname)  throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
 
     String[] getInterfaceNames(String dns) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-    
+
     String[] getInstanceNames(String dns, String interfacename) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-  
+
     String[] getDNSNames(String dns) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
 
     org.omg.CORBA.Object getNetworkDCObject(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
 
     org.omg.CORBA.Object getSeismogramDCObject(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-    
+
     org.omg.CORBA.Object getPlottableDCObject(String dns, String objectname) throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
-    
-  
+
+
     org.omg.CORBA.Object getEventDCObject(String dns, String objectname)  throws NotFound, CannotProceed, InvalidName, org.omg.CORBA.ORBPackage.InvalidName;
 
-    
-  
+
+
 }// FissuresNamingService
