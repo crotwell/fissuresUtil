f8769fde2121ca5905437dfdaef33c0c6161c137
crotwell <crotwell@localhost>
2003-11-03 16:46:08 -0500

    [svn r6337] initial, was inner class of DataHeader

---
+++ src/edu/sc/seis/fissuresUtil/mseed/Btime.java
@@ -0,0 +1,17 @@
+/**
+ * Btime.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.mseed;
+
+public class Btime {
+    public int year = 1900;
+    public int jday = 1;
+    public int hour = 0;
+    public int min = 0;
+    public int sec = 0;
+    public int tenthMilli = 0;
+}
+
