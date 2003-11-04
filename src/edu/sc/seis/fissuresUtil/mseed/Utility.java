bffb01c749db6dfa84ba87eb126cdf5195c8d7d9
Philip Crotwell <crotwell@seis.sc.edu>
2003-11-04 09:54:56 -0500

    [svn r6350] reindent

--- src/edu/sc/seis/fissuresUtil/mseed/Utility.java
+++ src/edu/sc/seis/fissuresUtil/mseed/Utility.java
@@ -20,14 +20,14 @@ public class Utility  {
             return (short)(((hi & 0xff) << 8) + (low & 0xff));
         }
     }
-    
+
     public static int bytesToInt(byte a) {
-	return (int)a;
+        return (int)a;
     }
-   
+
     public static int uBytesToInt(byte a) {
         // we and with 0xff in order to get the sign correct (pos)
-	return a & 0xff;
+        return a & 0xff;
     }
 
     public static int bytesToInt(byte a, byte b, boolean swapBytes) {
@@ -47,8 +47,8 @@ public class Utility  {
         }
     }
 
-    public static int bytesToInt(byte a, byte b, byte c, 
-                                    boolean swapBytes) {
+    public static int bytesToInt(byte a, byte b, byte c,
+                                 boolean swapBytes) {
         if (swapBytes) {
             return (a & 0xff ) +
                 ((b & 0xff) << 8 ) +
@@ -60,8 +60,8 @@ public class Utility  {
         }
     }
 
-    public static int bytesToInt(byte a, byte b, byte c, byte d, 
-                                    boolean swapBytes) {
+    public static int bytesToInt(byte a, byte b, byte c, byte d,
+                                 boolean swapBytes) {
         if (swapBytes) {
             return ((a & 0xff) ) +
                 ((b & 0xff) << 8 ) +
@@ -72,72 +72,64 @@ public class Utility  {
                 ((b & 0xff) << 16 ) +
                 ((c & 0xff) << 8 ) +
                 ((d & 0xff) );
-        }    
+        }
     }
 
-    public static byte[] intToByteArray(int a)
-    {
-	byte[] returnByteArray = new byte[4];//int is 4 bytes
+    public static byte[] intToByteArray(int a) {
+        byte[] returnByteArray = new byte[4];//int is 4 bytes
         returnByteArray[0] = (byte)((a & 0xff000000)>>24);
-	returnByteArray[1] = (byte)((a & 0x00ff0000)>>16);
-	returnByteArray[2] = (byte)((a & 0x0000ff00)>>8);
-	returnByteArray[3] = (byte) ((a & 0x000000ff));
-	return returnByteArray;
-      
+        returnByteArray[1] = (byte)((a & 0x00ff0000)>>16);
+        returnByteArray[2] = (byte)((a & 0x0000ff00)>>8);
+        returnByteArray[3] = (byte) ((a & 0x000000ff));
+        return returnByteArray;
+
     }
 
-    public static byte[]  pad(byte[] source,int requiredBytes, byte paddingByte)
-    {
-	if(source.length == requiredBytes)
-	{
-	    return source;
-	}
-	else{
+    public static byte[]  pad(byte[] source,int requiredBytes, byte paddingByte) {
+        if(source.length == requiredBytes) {
+            return source;
+        }
+        else{
 
-	byte[] returnByteArray = new byte[requiredBytes];
-	System.arraycopy(source, 0, returnByteArray, 0, source.length);
-	for(int i = source.length; i<requiredBytes; i++)
-	    {
-		returnByteArray[i] = (byte)paddingByte;
-	    }
-	return returnByteArray;
-	}
+            byte[] returnByteArray = new byte[requiredBytes];
+            System.arraycopy(source, 0, returnByteArray, 0, source.length);
+            for(int i = source.length; i<requiredBytes; i++) {
+                returnByteArray[i] = (byte)paddingByte;
+            }
+            return returnByteArray;
+        }
     }
 
-    public static byte[] format(byte[] source, int start, int end)
-    {
-	byte[] returnByteArray = new byte[start-end+1];
-	int j = 0;
-	for(int i = start; i < end; i ++,j++)
-	{
-	    returnByteArray[j] = source[i];
-	}
-	return returnByteArray;
+    public static byte[] format(byte[] source, int start, int end) {
+        byte[] returnByteArray = new byte[start-end+1];
+        int j = 0;
+        for(int i = start; i < end; i ++,j++) {
+            returnByteArray[j] = source[i];
+        }
+        return returnByteArray;
     }
 
-    public static void main (String[] args)
-    {
-	int a = 256;
-	byte a1 = (byte)((a & 0xff000000)>>24);
-	byte a2 = (byte)((a & 0x00ff0000)>>16);
-	byte a3 = (byte)((a & 0x0000ff00)>>8);
-	byte a4 = (byte) ((a & 0x000000ff));
-	System.out.println("first byte is " + a1);
-	System.out.println("2 byte is " + a2);
-	System.out.println("3 byte is " + a3);
-	System.out.println("4  byte is " + a4);
-	byte[] source = new byte[5];
-	for(int i=0; i< 5; i++)
-	    source[i] = (byte)10;
-	byte[] output = Utility.pad(source, 5, (byte)32);
-// 	for(int j = 0; j< output.length; j++)
-// 	    {
-// 		System.out.println("byte"+j+"  " + output[j]);
-// 	    }
-	for(int k=output.length-1; k > -1; k--)
-	    {
-		System.out.println("byte" + k +" " + output[k]);
-	    }
+    public static void main (String[] args) {
+        int a = 256;
+        byte a1 = (byte)((a & 0xff000000)>>24);
+        byte a2 = (byte)((a & 0x00ff0000)>>16);
+        byte a3 = (byte)((a & 0x0000ff00)>>8);
+        byte a4 = (byte) ((a & 0x000000ff));
+        System.out.println("first byte is " + a1);
+        System.out.println("2 byte is " + a2);
+        System.out.println("3 byte is " + a3);
+        System.out.println("4  byte is " + a4);
+        byte[] source = new byte[5];
+        for(int i=0; i< 5; i++)
+            source[i] = (byte)10;
+        byte[] output = Utility.pad(source, 5, (byte)32);
+        //  for(int j = 0; j< output.length; j++)
+        //      {
+        //      System.out.println("byte"+j+"  " + output[j]);
+        //      }
+        for(int k=output.length-1; k > -1; k--) {
+            System.out.println("byte" + k +" " + output[k]);
+        }
 
 
     }
