4ec78350371d6f9bd6c92fe52bd5da4bb79c68a5
Philip Crotwell <crotwell@seis.sc.edu>
2003-12-04 12:47:20 -0500

    [svn r6532] initial

---
+++ src/edu/sc/seis/fissuresUtil/cache/NSEventDC.java
@@ -0,0 +1,94 @@
+/**
+ * NSEventDC.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.cache;
+import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
+import edu.iris.Fissures.IfEvent.EventDCOperations;
+import edu.iris.Fissures.IfEvent.EventDC;
+import edu.iris.Fissures.IfEvent.EventFinder;
+import org.apache.log4j.Logger;
+import edu.iris.Fissures.IfEvent.EventChannelFinder;
+
+
+
+public class NSEventDC implements EventDCOperations {
+
+    public NSEventDC(String serverDNS,
+                     String serverName,
+                     FissuresNamingService fissuresNamingService) {
+        this.serverDNS = serverDNS;
+        this.serverName = serverName;
+        this.fissuresNamingService = fissuresNamingService;
+    } // NSEventDC constructor
+
+    public String getServerDNS() {
+        return serverDNS;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public EventDC getEventDC() {
+        if ( eventDC == null) {
+            try {
+                eventDC = fissuresNamingService.getEventDC(serverDNS,
+                                                           serverName);
+            } catch (org.omg.CosNaming.NamingContextPackage.NotFound e) {
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "+serverName+" "+serverDNS+" "+e.toString(),
+                                                  0,
+                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            } catch (org.omg.CosNaming.NamingContextPackage.CannotProceed e) {
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "+serverName+" "+serverDNS+" "+e.toString(),
+                                                  0,
+                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            } catch (org.omg.CORBA.ORBPackage.InvalidName e) {
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "+serverName+" "+serverDNS+" "+e.toString(),
+                                                  0,
+                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            } catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "+serverName+" "+serverDNS+" "+e.toString(),
+                                                  0,
+                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            } // end of try-catch
+        } // end of if ()
+        return eventDC;
+    }
+
+    public EventChannelFinder a_channel_finder() {
+        try {
+            return getEventDC().a_channel_finder();
+        } catch (Exception e) {
+            // retry in case regetting from name service helps
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
+            eventDC = null;
+            return getEventDC().a_channel_finder();
+        } // end of try-catch
+    }
+
+    public EventFinder a_finder() {
+        try {
+            return getEventDC().a_finder();
+        } catch (Exception e) {
+            // retry in case regetting from name service helps
+            logger.warn("Exception in a_finder(), regetting from nameservice to try again.", e);
+            eventDC = null;
+            return getEventDC().a_finder();
+        } // end of try-catch
+    }
+
+    protected EventDC eventDC = null;
+
+    protected String serverDNS;
+
+    protected String serverName;
+
+    protected FissuresNamingService fissuresNamingService;
+
+    static Logger logger =
+        Logger.getLogger(NSEventDC.class);
+}
+
