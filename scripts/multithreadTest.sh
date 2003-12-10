b980d8335d89ef36f995b169839f278a2e1d0208
Philip Crotwell <crotwell@seis.sc.edu>
2003-12-10 17:10:01 -0500

    [svn r6633] simplified classpath

--- scripts/multithreadTest.sh
+++ scripts/multithreadTest.sh
@@ -3,25 +3,18 @@
 MAVEN=~/.maven/repository
 
 
-JACORB=~/External/JacORB/lib/jacorb.jar
-
-HSQLDB=$MAVEN/hsqldb/jars/hsqldb-1.7.1.jar
-
 SEEDCODEC=$MAVEN/SeedCodec/jars/SeedCodec-1.0Beta.jar
 FISSURESUTIL=$MAVEN/fissuresUtil/jars/fissuresUtil-1.0.2beta.jar
 FISSURESIMPL=$MAVEN/fissuresImpl/jars/fissuresImpl-1.1.1beta.jar
 FISSURESIDL=$MAVEN/fissuresIDL/jars/fissuresIDL-1.0.jar
 LOG4J=$MAVEN/log4j/jars/log4j-1.2.8.jar
-TAUP=$MAVEN/TauP/jars/TauP-SNAPSHOT.jar
-XALAN=$MAVEN/xalan/jars/xalan-2.5.1.jar
-XERCES=$MAVEN/xerces/jars/xerces-2.4.0.jar
-XMLAPI=$MAVEN/xml-apis/jars/xml-apis-1.0.b2.jar
-JAICORE=$MAVEN/jars/jai_core.jar
-JAICODEC=$MAVEN/jars/jai_codec.jar
 
-#~/External/JacORB/bin/jaco -cp ${HSQLDB}:${XMLAPI}:${XERCES}:${XALAN}:${TAUP}:${LOG4J}:${SEEDCODEC}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${JAICORE}:${JAICODEC}:${CLASSPATH} edu.sc.seis.fissuresUtil.simple.MultiThreadNetworkClient -props ./alpha.prop
+# run with JacORB
+#~/External/JacORB/bin/jaco -cp {LOG4J}:${SEEDCODEC}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL} edu.sc.seis.fissuresUtil.simple.MultiThreadNetworkClient -props ./alpha.prop
 
-java -cp ${HSQLDB}:${XMLAPI}:${XERCES}:${XALAN}:${TAUP}:${LOG4J}:${SEEDCODEC}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${JAICORE}:${JAICODEC}:${CLASSPATH} edu.sc.seis.fissuresUtil.simple.MultiThreadNetworkClient -props ./alpha.prop
+
+# run with built in java orb
+java -cp ${LOG4J}:${SEEDCODEC}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL} edu.sc.seis.fissuresUtil.simple.MultiThreadNetworkClient -props ./alpha.prop
 
 
 echo done.
