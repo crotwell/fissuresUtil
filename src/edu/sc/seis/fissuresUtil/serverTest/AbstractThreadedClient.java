b41187bac82419592188634b13591c8237b1aa9a
groves <groves@localhost>
2003-12-11 09:55:51 -0500

    [svn r6638] rearranged structure

--- src/edu/sc/seis/fissuresUtil/serverTest/AbstractThreadedClient.java
+++ src/edu/sc/seis/fissuresUtil/serverTest/AbstractThreadedClient.java
@@ -1,15 +1,20 @@
 package edu.sc.seis.fissuresUtil.serverTest;
 
-import edu.sc.seis.fissuresUtil.serverTest.AbstractThreadedClient;
+import edu.iris.Fissures.IfNetwork.ChannelId;
+import edu.iris.Fissures.IfNetwork.NetworkId;
+import edu.iris.Fissures.IfNetwork.StationId;
+import edu.iris.Fissures.Time;
 import edu.sc.seis.fissuresUtil.simple.AbstractClient;
 import org.apache.log4j.Logger;
 
 public abstract class AbstractThreadedClient extends AbstractClient{
-    public AbstractThreadedClient(){ this.runnables = createRunnables(); }
-  
     public abstract Runnable[] createRunnables();
     
-    public void runAll(){
+    /**Runs all the runnables returned by client.createRunnables() against each
+     * other pair wise
+     */
+    public static void runAll(AbstractThreadedClient client){
+        Runnable[] runnables = client.createRunnables();
         for (int i = 0; i < runnables.length; i++) {
             for (int j = i; j < runnables.length; j++) {
                 Thread one = new Thread(runnables[i], runnables[i] + " thread");
@@ -32,18 +37,21 @@ public abstract class AbstractThreadedClient extends AbstractClient{
     protected class Repeater implements Runnable{
         public Repeater(Runnable r){ this.r = r; }
         
-        private Runnable r;
-        
-        public void run() { for (int i = 0; i < 3; i++){
+        public void run() {
+            for (int i = 0; i < 3; i++){
                 logger.info("run " + (i + 1) + " of 3 for " + r);
                 r.run();
             }
         }
         
-        public String toString(){ return "repeater for " + r; }
+        public String toString(){ return "" + r; }
+        
+        private Runnable r;
     }
     
-    private  Runnable[] runnables;
+    protected static NetworkId fakeNet = new NetworkId("II", new Time("19861024000000.0000GMT", 0));
+    protected static StationId fakeStation = new StationId(fakeNet, "AAK", new Time("19901012000000.0000GMT", 0));
+    protected static ChannelId fakeChan = new ChannelId(fakeNet, "AAK", "  ", "BHE", new Time("19901012000000.0000GMT", 0));
     
     private static Logger logger = Logger.getLogger(AbstractThreadedClient.class);
 }
