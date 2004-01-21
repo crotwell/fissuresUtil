b5a1f0e3e12713d54dbc6a1d5d9008a22a4ef75a
Philip Crotwell <crotwell@seis.sc.edu>
2004-01-21 16:54:11 -0500

    [svn r6862] add test for narrow

--- src/edu/sc/seis/fissuresUtil/simple/ThreadedNetClient.java
+++ src/edu/sc/seis/fissuresUtil/simple/ThreadedNetClient.java
@@ -3,51 +3,62 @@ package edu.sc.seis.fissuresUtil.simple;
 import edu.iris.Fissures.IfNetwork.ChannelNotFound;
 import edu.iris.Fissures.IfNetwork.NetworkAccessHelper;
 import org.apache.log4j.Logger;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
 
 public class ThreadedNetClient extends SimpleNetworkClient{
     public void exercise(){
         super.exercise();
         Tester.runAll(createRunnables());
     }
-    
+
     public Runnable[] createRunnables() {
-        Runnable[] runnables = new Runnable[5];
-        runnables[0] = new RetrieveForStation();
-        runnables[1] = new GetAttributes();
-        runnables[2] = new IORConverter();
-        runnables[3] = new RetrieveStations();
-        runnables[4] = new RetrieveChannel();
+        Runnable[] runnables = new Runnable[6];
+        runnables[0] = new NetworkAccessNarrow();
+        runnables[1] = new RetrieveForStation();
+        runnables[2] = new GetAttributes();
+        runnables[3] = new IORConverter();
+        runnables[4] = new RetrieveStations();
+        runnables[5] = new RetrieveChannel();
         return runnables;
     }
-    
+
+    private class NetworkAccessNarrow implements Runnable{
+        public void run() {
+            String ior = Initializer.getORB().object_to_string((org.omg.CORBA.Object)net);
+            org.omg.CORBA.Object obj = Initializer.getORB().string_to_object(ior);
+            NetworkAccess temp = NetworkAccessHelper.narrow(obj); }
+
+        public String toString(){ return "NetworkAccess narrow"; }
+    }
+
     private class RetrieveForStation implements Runnable{
         public void run() { net.retrieve_for_station(Initializer.fakeStation); }
-        
+
         public String toString(){ return "retrieve_for_station"; }
     }
-    
+
     private class GetAttributes implements Runnable{
         public void run() { get_attributes(false); }
-        
+
         public String toString(){ return "get_attributes"; }
     }
-    
+
     private class IORConverter implements Runnable{
         public void run() {
             String ior = Initializer.getORB().object_to_string((org.omg.CORBA.Object)net);
             org.omg.CORBA.Object obj = Initializer.getORB().string_to_object(ior);
             NetworkAccessHelper.narrow(obj);
         }
-        
+
         public String toString(){ return "IORConverter"; }
     }
-    
+
     private class RetrieveStations implements Runnable{
         public void run() { retrieve_stations(false); }
-        
+
         public String toString(){ return "retrieve_stations"; }
     }
-    
+
     private class RetrieveChannel implements Runnable{
         public void run() {
             try {
@@ -58,12 +69,12 @@ public class ThreadedNetClient extends SimpleNetworkClient{
                 System.exit(1);
             }
         }
-        
+
         public String toString(){ return "retrieve_channel"; }
     }
-    
+
     private static Logger logger = Logger.getLogger(ThreadedNetClient.class);
-    
+
     public static void main(String[] args){
         Initializer.init(args);
         Tester.runAll(new ThreadedNetClient().createRunnables());
