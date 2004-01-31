39c37bb460e44eea8541eefe600dbbe5e76cc74b
Philip Crotwell <crotwell@seis.sc.edu>
2004-01-31 12:17:15 -0500

    [svn r6933] initial, does nothing yet

---
+++ src/edu/sc/seis/fissuresUtil/bag/Bag.java
@@ -0,0 +1,21 @@
+package edu.sc.seis.fissuresUtil.bag;
+
+/**
+ * Bag.java
+ *
+ *
+ * Created: Tue Mar 25 12:50:39 2003
+ *
+ * @author <a href="mailto:crotwell@owl.seis.sc.edu">Philip Crotwell</a>
+ * @version 1.0
+ */
+public class Bag {
+    public Bag() {
+        
+    } // Bag constructor
+    
+    public static void main(String[] args) {
+        
+    } // end of main()
+    
+} // Bag
