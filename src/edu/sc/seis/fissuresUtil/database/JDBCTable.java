3c94576df9090231c263c059a31935cf3ca816e8
Charles Groves <groves@seis.sc.edu>
2004-02-10 17:03:38 -0500

    [svn r7062] moved from anhinga

---
+++ src/edu/sc/seis/fissuresUtil/database/JDBCTable.java
@@ -0,0 +1,38 @@
+
+package edu.sc.seis.fissuresUtil.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * JDBCTable.java
+ *
+ * All methods are unsyncronized, the calling application should make sure
+ * that a single instance of this class is not accessed by more than one
+ * thread at a time. Because of the use of prepared statements and a single
+ * connection per instance, this class IS NOT THREAD-SAFE!
+ *
+ * Created: Fri Jan 26 10:00:16 2001
+ *
+ * @author Philip Crotwell
+ * @version
+ */
+
+public class JDBCTable  {
+    public JDBCTable(String tableName, Connection conn) {
+        this.tableName = tableName;
+        this.conn = conn;
+    }
+    
+    public String getTableName() { return tableName; }
+    
+    public Connection getConnection() { return conn; }
+    
+    public void commit() throws SQLException { conn.commit(); }
+    
+    public void rollback() throws SQLException { conn.rollback(); }
+    
+    protected String tableName;
+    
+    protected Connection conn;
+} // JDBCTable
