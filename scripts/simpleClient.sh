e8e8568a220732e1b45c19e8d97be056fe66d449
crotwell <crotwell@localhost>
2004-02-10 09:29:04 -0500

    [svn r7048] bump har versions

--- scripts/simpleClient.sh
+++ scripts/simpleClient.sh
@@ -3,21 +3,11 @@
 MAVEN=~/.maven/repository
 
 
-JACORB=$MAVEN/JacORB/jars/JacORB-1.4.1.jar
-# for orbacus
-OB=$MAVEN/OB/jars/OB-4.1.0.jar
-OBNAMING=$MAVEN/OBNaming/jars/OBNaming-4.1.0.jar
-# openorb
-OPENORB=$MAVEN/openorb/jars/openorb-1.3.1.jar
-LOGKIT=$MAVEN/logkit/jars/logkit-1.2.jar
-AVALONFRAMEWORK=$MAVEN/avalon/jars/avalon-framework-4.1.4.jar
-EXCALIBUR=$MAVEN/excalibur/jars/excalibur-configuration-1.0.jar
-
 HSQLDB=$MAVEN/hsqldb/jars/hsqldb-1.7.1.jar
 
 SEEDCODEC=$MAVEN/SeedCodec/jars/SeedCodec-1.0Beta.jar
-FISSURESUTIL=$MAVEN/fissuresUtil/jars/fissuresUtil-1.0.2beta.jar
-FISSURESIMPL=$MAVEN/fissuresImpl/jars/fissuresImpl-1.1.1beta.jar
+FISSURESUTIL=$MAVEN/fissuresUtil/jars/fissuresUtil-1.0.6beta.jar
+FISSURESIMPL=$MAVEN/fissuresImpl/jars/fissuresImpl-1.1.4beta.jar
 FISSURESIDL=$MAVEN/fissuresIDL/jars/fissuresIDL-1.0.jar
 LOG4J=$MAVEN/log4j/jars/log4j-1.2.8.jar
 TAUP=$MAVEN/TauP/jars/TauP-SNAPSHOT.jar
