e7ea2320a20e55058c854c420a433e9eec98bb1b
Philip Crotwell <crotwell@seis.sc.edu>
2004-02-25 15:52:24 -0500

    [svn r7286] remove import of old fissures utility package

--- src/edu/sc/seis/fissuresUtil/chooser/SeisTimeFilterSelector.java
+++ src/edu/sc/seis/fissuresUtil/chooser/SeisTimeFilterSelector.java
@@ -5,7 +5,6 @@ import edu.iris.Fissures.IfSeismogramDC.*;
 import edu.iris.Fissures.network.*;
 import edu.iris.Fissures.IfNetwork.*;
 import edu.iris.Fissures.model.*;
-import edu.iris.Fissures.utility.*;
 import edu.iris.Fissures.*;
 import java.awt.*;
 import java.util.*;
@@ -30,60 +29,60 @@ import org.apache.log4j.*;
 
 
 public class SeisTimeFilterSelector  {
-    
+
     public SeisTimeFilterSelector() {
     }
 
     public LocalSeismogram[] getFromGivenFilters(DataCenterOperations dataCenter,
-						 RequestFilter[] filterseq) throws FissuresException {
-	for (int i=0; i<filterseq.length; i++) {
+                         RequestFilter[] filterseq) throws FissuresException {
+    for (int i=0; i<filterseq.length; i++) {
             logger.info("request "+ChannelIdUtil.toString(filterseq[i].channel_id));
             logger.info("   from "+filterseq[i].start_time.date_time);
             logger.info("   to   "+filterseq[i].end_time.date_time);
         }
-	LocalSeismogram[] ls = dataCenter.retrieve_seismograms(filterseq);
+    LocalSeismogram[] ls = dataCenter.retrieve_seismograms(filterseq);
         logger.info("Got "+ls.length+" seismograms.");
         return ls;
 
 
     }
 
-   
+
     public RequestFilter[] makeFiltersGivenChannelId(edu.iris.Fissures.IfNetwork.ChannelId channel_id,
-						     int year,
-						     int jday,
-						     edu.iris.Fissures.Dimension pixel_size,
-						     edu.iris.Fissures.Time sTime,
-						     edu.iris.Fissures.Time eTime) {
-	
-	
-	StringBuffer dayBuffer = new StringBuffer();
-	DecimalFormat numberFormat = new DecimalFormat();
-	numberFormat.setMinimumIntegerDigits(3);
-	numberFormat.format((long)jday, dayBuffer, new FieldPosition(0));
+                             int year,
+                             int jday,
+                             edu.iris.Fissures.Dimension pixel_size,
+                             edu.iris.Fissures.Time sTime,
+                             edu.iris.Fissures.Time eTime) {
 
-	String startTimeStr = new java.lang.Integer(year).toString();
 
-	startTimeStr =  startTimeStr  + dayBuffer + "T00:00:00.000Z";
-	edu.iris.Fissures.Time startTime = sTime;
-	String endTimeStr = new java.lang.Integer(year).toString();
-	endTimeStr = endTimeStr  + dayBuffer + "T23:59:59.999z";
-	edu.iris.Fissures.Time endTime = eTime;
-	RequestFilter[] filters;
-	TimeRange window = new TimeRange(startTime, endTime);
-	filters = new RequestFilter[1];
-	filters[0] = 
-	    new RequestFilter(channel_id,
-			      window.start_time,
-			      window.end_time);
+    StringBuffer dayBuffer = new StringBuffer();
+    DecimalFormat numberFormat = new DecimalFormat();
+    numberFormat.setMinimumIntegerDigits(3);
+    numberFormat.format((long)jday, dayBuffer, new FieldPosition(0));
 
-	return filters;
+    String startTimeStr = new java.lang.Integer(year).toString();
+
+    startTimeStr =  startTimeStr  + dayBuffer + "T00:00:00.000Z";
+    edu.iris.Fissures.Time startTime = sTime;
+    String endTimeStr = new java.lang.Integer(year).toString();
+    endTimeStr = endTimeStr  + dayBuffer + "T23:59:59.999z";
+    edu.iris.Fissures.Time endTime = eTime;
+    RequestFilter[] filters;
+    TimeRange window = new TimeRange(startTime, endTime);
+    filters = new RequestFilter[1];
+    filters[0] =
+        new RequestFilter(channel_id,
+                  window.start_time,
+                  window.end_time);
+
+    return filters;
     }
 
 
 
 
- 
+
 
     static Category logger = Category.getInstance(SeisTimeFilterSelector.class.getName());
 
