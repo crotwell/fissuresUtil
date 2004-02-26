19419c0af88ef020dfb1868f64bd863cb082f06c
groves <groves@localhost>
2004-02-26 15:51:49 -0500

    [svn r7331] use getCenter instead of createCenter

--- src/edu/sc/seis/fissuresUtil/display/drawable/SeismogramRemover.java
+++ src/edu/sc/seis/fissuresUtil/display/drawable/SeismogramRemover.java
@@ -12,7 +12,7 @@ import java.awt.Graphics2D;
 
 public class SeismogramRemover extends BigX{
     public SeismogramRemover(DataSetSeismogram seis, SeismogramDisplay display){
-        super(display.getCenterPanel());
+        super(display.getCenter());
         this.display = display;
         this.seismogram = seis;
     }
