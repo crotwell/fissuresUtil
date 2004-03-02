c48e86140032c6b830b58a3ca58668ed2e93cfee
groves <groves@localhost>
2004-03-02 10:46:42 -0500

    [svn r7364] validate when the borders are changed

--- src/edu/sc/seis/fissuresUtil/display/VerticalSeismogramDisplay.java
+++ src/edu/sc/seis/fissuresUtil/display/VerticalSeismogramDisplay.java
@@ -72,6 +72,7 @@ public abstract class VerticalSeismogramDisplay extends SeismogramDisplay{
                                   BOTTOM_CENTER);
                 }
             }
+            validate();
         }
 
         private SeismogramDisplay get(int pos){
