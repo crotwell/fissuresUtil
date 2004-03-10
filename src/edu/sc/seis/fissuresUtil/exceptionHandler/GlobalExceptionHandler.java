3bdac84983f0dae7fe76779842f31774372a9506
crotwell <crotwell@localhost>
2004-03-10 17:22:06 -0500

    [svn r7572] remove uneeded geebugs message

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/GlobalExceptionHandler.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/GlobalExceptionHandler.java
@@ -17,7 +17,7 @@ import org.apache.log4j.Logger;
 
 public class GlobalExceptionHandler {
     public static void handle(Throwable thrown) {
-        handle("An uncaught exception has occured. Please report this to geebugs@seis.sc.edu", thrown);
+        handle("An uncaught exception has occured.", thrown);
     }
 
     public static void handle(String message, Throwable thrown) {
