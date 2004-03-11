727ab6e6326742298ee3a4eb6b723adcb8dbf0f1
oliverpa <oliverpa@localhost>
2004-03-11 14:50:34 -0500

    [svn r7587] got rid of outdated documentation

--- src/edu/sc/seis/fissuresUtil/map/colorizer/event/DefaultEventColorizer.java
+++ src/edu/sc/seis/fissuresUtil/map/colorizer/event/DefaultEventColorizer.java
@@ -15,10 +15,6 @@ public class DefaultEventColorizer implements EventColorizer{
 	
 	public static Color DEFAULT_EVENT = Color.RED;
 	
-	/*
-	 *	@param eventList can be null
-	 *  @param event cannot be null
-	 */
 	public Paint[] colorize(EventAccessOperations[] events) {
 		Paint[] paints = new Color[events.length];
 		for (int i = 0; i < events.length; i++) {
