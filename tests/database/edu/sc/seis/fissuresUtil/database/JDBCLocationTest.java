b0ec0124b2cdad4d6457b666c06ab1490bd5c01f
Charles Groves <groves@seis.sc.edu>
2004-03-16 13:45:55 -0500

    [svn r7657] moved from regular fissuresUtil tests so that these dont have to be run as often

---
+++ tests/database/edu/sc/seis/fissuresUtil/database/JDBCLocationTest.java
@@ -0,0 +1,49 @@
+package edu.sc.seis.fissuresUtil.database;
+
+import edu.iris.Fissures.Location;
+import edu.sc.seis.fissuresUtil.database.JDBCLocation;
+import edu.sc.seis.mockFissures.MockLocation;
+import java.sql.Connection;
+import java.sql.SQLException;
+import junit.framework.TestCase;
+
+public class JDBCLocationTest extends TestCase {
+    public JDBCLocationTest(String testname) { super(testname); }
+    
+    protected void setUp() throws SQLException {
+        Connection conn = ConnMgr.createConnection();
+        jdbcLocation = new JDBCLocation(conn);
+    }
+    
+    public void testPutAndGet() throws SQLException, NotFound {
+        Location[] locs = MockLocation.createMultiple();
+        for (int i = 0; i < locs.length; i++) {
+            int dbid = jdbcLocation.put(locs[i]);
+            Location gottenLoc = jdbcLocation.get(dbid);
+            assertEquals(locs[i].depth, gottenLoc.depth);
+            assertEquals(locs[i].elevation, gottenLoc.elevation);
+            assertEquals(locs[i].latitude, gottenLoc.latitude, 0.001f);
+            assertEquals(locs[i].longitude, gottenLoc.longitude, 0.001f);
+        }
+    }
+    
+    public void testGetDBId() throws NotFound, SQLException {
+        Location[] locs = MockLocation.createMultiple();
+        for (int i = 0; i < locs.length; i++) {
+            int dbid = jdbcLocation.put(locs[i]);
+            int gottenid = jdbcLocation.getDBId(locs[i]);
+            assertEquals(dbid, gottenid);
+        }
+    }
+    
+    public void testDoubleInsert() throws SQLException {
+        Location[] locs = MockLocation.createMultiple();
+        for (int i = 0; i < locs.length; i++) {
+            int dbidA = jdbcLocation.put(locs[i]);
+            int dbidB = jdbcLocation.put(locs[i]);
+            assertEquals(dbidA, dbidB);
+        }
+    }
+    
+    protected JDBCLocation jdbcLocation;
+}
---
+++ tests/database/edu/sc/seis/fissuresUtil/database/network/JDBCSiteTest.java
@@ -0,0 +1,26 @@
+/**
+ * JDBCSiteTest.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.database.network;
+
+import edu.iris.Fissures.IfNetwork.Site;
+import edu.sc.seis.fissuresUtil.database.NotFound;
+import edu.sc.seis.mockFissures.IfNetwork.MockSite;
+import java.sql.SQLException;
+import junit.framework.TestCase;
+
+public class JDBCSiteTest extends TestCase{
+    public void testDoublePut() throws SQLException, NotFound{
+        JDBCSite siteTable = new JDBCSite();
+        Site site = MockSite.createSite();
+        int dbidA = siteTable.put(site);
+        int dbidB = siteTable.put(site);
+        int gottenId = siteTable.getDBId(site.get_id(), site.my_station.get_id());
+        assertEquals(dbidA, dbidB);
+        assertEquals(dbidB, gottenId);
+    }
+}
+
