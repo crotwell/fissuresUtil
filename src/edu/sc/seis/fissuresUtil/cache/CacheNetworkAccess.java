8e95312a094cbd2537e558e2a7f1502f30756fe7
groves <groves@localhost>
2004-03-18 13:04:49 -0500

    [svn r7696] added ProxyNetworkAccess superclass with getNetwork and reset method and put the passthroughs in it

--- src/edu/sc/seis/fissuresUtil/cache/CacheNetworkAccess.java
+++ src/edu/sc/seis/fissuresUtil/cache/CacheNetworkAccess.java
@@ -1,10 +1,7 @@
-
 package edu.sc.seis.fissuresUtil.cache;
 
 import edu.iris.Fissures.IfNetwork.*;
 
-import edu.iris.Fissures.AuditElement;
-import edu.iris.Fissures.NotImplemented;
 import edu.iris.Fissures.Time;
 import edu.iris.Fissures.network.NetworkIdUtil;
 import edu.iris.Fissures.network.SiteIdUtil;
@@ -16,26 +13,22 @@ import java.util.Iterator;
 import java.util.List;
 import org.apache.log4j.Logger;
 
-public class CacheNetworkAccess implements NetworkAccess {
+public class CacheNetworkAccess extends ProxyNetworkAccess {
+    public CacheNetworkAccess(NetworkAccess net){ super(net); }
 
-    public CacheNetworkAccess(NetworkAccess net) {
-        if (net == null) {
-            throw new NullPointerException("network is null");
-        } // end of if (net == null)
-        this.net = net;
+    /**
+     * Resetting a CacheNetworkAccess clears the cache and calls reset on the
+     * network access it's holding if it is also a ProxyNetworkAccess
+     */
+    public void reset() {
+        attr = null;
+        stations = null;
+        channelMap.clear();
+        super.reset();
     }
 
-    public NetworkAccess getNetworkAccess() { return net; }
-
-    //
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/get_attributes:1.0
-    //
-    /***/
-
     public NetworkAttr get_attributes() {
-        if (attr == null) {
-            attr = net.get_attributes();
-        }
+        if (attr == null) { attr = net.get_attributes(); }
         return attr;
     }
 
@@ -51,6 +44,7 @@ public class CacheNetworkAccess implements NetworkAccess {
         }
         return stations;
     }
+
     /** retreives the channels for the stations, but uses the cached copy
      *  if it has been previously retrieved. The channels are also cleaned
      *  of duplicate site objects to free memory.
@@ -221,8 +215,9 @@ public class CacheNetworkAccess implements NetworkAccess {
             while(it.hasNext()){
                 Time cur = (Time)it.next();
                 if(cur.date_time.equals(unknownTime.date_time) &&
-                   cur.leap_seconds_version == unknownTime.leap_seconds_version)
+                   cur.leap_seconds_version == unknownTime.leap_seconds_version){
                     return cur;
+                }
             }
             knownTimes.add(unknownTime);
         }
@@ -231,137 +226,9 @@ public class CacheNetworkAccess implements NetworkAccess {
 
     private static List knownTimes = Collections.synchronizedList(new ArrayList());
 
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_grouping:1.0
-//
-    /***/
-
-    public ChannelId[]
-        retrieve_grouping(ChannelId id)
-        throws ChannelNotFound {
-        return net.retrieve_grouping(id);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_groupings:1.0
-//
-    /***/
-
-    public ChannelId[][]
-        retrieve_groupings() {
-        return net.retrieve_groupings();
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_channel:1.0
-//
-    /***/
-
-    public Channel retrieve_channel(ChannelId id) throws ChannelNotFound {
-        return net.retrieve_channel(id);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_channels_by_code:1.0
-//
-    /***/
-
-    public Channel[] retrieve_channels_by_code(String station_code,
-                                               String site_code,
-                                               String channel_code)
-        throws ChannelNotFound {
-        return net.retrieve_channels_by_code(station_code, site_code, channel_code);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/locate_channels:1.0
-//
-    /***/
-
-    public Channel[] locate_channels(edu.iris.Fissures.Area the_area,
-                                     SamplingRange sampling,
-                                     OrientationRange orientation) {
-        return net.locate_channels(the_area, sampling, orientation);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_instrumentation:1.0
-//
-    /***/
-
-    public Instrumentation
-        retrieve_instrumentation(ChannelId id,
-                                 edu.iris.Fissures.Time the_time)
-        throws ChannelNotFound {
-        return net.retrieve_instrumentation(id, the_time);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_calibrations:1.0
-//
-    /***/
-
-    public Calibration[]
-        retrieve_calibrations(ChannelId id,
-                              edu.iris.Fissures.TimeRange the_time)
-        throws ChannelNotFound,
-        edu.iris.Fissures.NotImplemented {
-        return net.retrieve_calibrations(id, the_time);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_time_corrections:1.0
-//
-    /***/
-
-    public TimeCorrection[]
-        retrieve_time_corrections(ChannelId id,
-                                  edu.iris.Fissures.TimeRange time_range)
-        throws ChannelNotFound,
-        edu.iris.Fissures.NotImplemented {
-        return net.retrieve_time_corrections(id, time_range);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/retrieve_all_channels:1.0
-//
-    /***/
-
-    public ChannelId[]
-        retrieve_all_channels(int seq_max,
-                              ChannelIdIterHolder iter) {
-
-        return net.retrieve_all_channels(seq_max, iter);
-    }
-
-//
-    // IDL:iris.edu/Fissures/IfNetwork/NetworkAccess/get_audit_trail_for_channel:1.0
-//
-    /***/
-
-    public edu.iris.Fissures.AuditElement[]
-        get_audit_trail_for_channel(ChannelId id)
-        throws ChannelNotFound,
-        edu.iris.Fissures.NotImplemented {
-        return net.get_audit_trail_for_channel(id);
-    }
-
-//
-    // IDL:iris.edu/Fissures/AuditSystemAccess/get_audit_trail:1.0
-//
-    /***/
-
-    public AuditElement[]
-        get_audit_trail()
-        throws NotImplemented {
-        return net.get_audit_trail();
-    }
-
-    NetworkAccess net;
-    NetworkAttr attr;
-    Station[] stations;
-    HashMap channelMap = new HashMap();
-
-    static Logger logger = Logger.getLogger(CacheNetworkAccess.class);
+    private NetworkAccess net;
+    private NetworkAttr attr;
+    private Station[] stations;
+    private HashMap channelMap = new HashMap();
+    private static Logger logger = Logger.getLogger(CacheNetworkAccess.class);
 }
-
