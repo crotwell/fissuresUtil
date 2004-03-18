9166b4f30019be5fae3f9b2feb0d9ae4fe07b01f
groves <groves@localhost>
2004-03-18 13:06:02 -0500

    [svn r7699] initial.  This is a combination of cache, retry and ns network accesses.  it should represent the most common config of network access usage

---
+++ src/edu/sc/seis/fissuresUtil/cache/BulletproofNetworkAccess.java
@@ -0,0 +1,24 @@
+package edu.sc.seis.fissuresUtil.cache;
+
+import edu.iris.Fissures.IfNetwork.NetworkDCOperations;
+import edu.iris.Fissures.IfNetwork.NetworkId;
+import edu.iris.Fissures.IfNetwork.NetworkNotFound;
+
+/**
+ * BulletproofNetworkAccess combines our three delicious ProxyNetworkAccess
+ * classes in a hard candy shell.  The order is NSNetworkAccess inside a
+ * RetryNetworkAccess inside a CacheNetworkAccess.  This means that all of the
+ * results you get from the network access will be cached, if there are some
+ * transient network issues we'll retry 3 times, and if the network access
+ * reference goes stale we'll reget it from the naming service.  It'd be wise
+ * to use a NSNetworkDC as the type of NetworkDCOperations as it allows the
+ * NetworkDC to be refreshed from the naming service if the reference goes stale
+ *
+ */
+public class BulletproofNetworkAccess extends ProxyNetworkAccess{
+    public BulletproofNetworkAccess(NetworkDCOperations netDC, NetworkId id)
+        throws NetworkNotFound{
+        super(new CacheNetworkAccess(new RetryNetworkAccess(new NSNetworkAccess(id, netDC), 3)));
+    }
+}
+
