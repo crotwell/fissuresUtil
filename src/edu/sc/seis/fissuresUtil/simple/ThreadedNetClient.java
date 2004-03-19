3a6354ba4fcd6d94890754c1c287960afa41aa0d
groves <groves@localhost>
2004-03-19 12:46:53 -0500

    [svn r7719] use testChannel and station from superclass.  catch throwable

--- src/edu/sc/seis/fissuresUtil/simple/ThreadedNetClient.java
+++ src/edu/sc/seis/fissuresUtil/simple/ThreadedNetClient.java
@@ -2,22 +2,35 @@ package edu.sc.seis.fissuresUtil.simple;
 
 import edu.iris.Fissures.IfNetwork.ChannelNotFound;
 import edu.iris.Fissures.IfNetwork.NetworkAccessHelper;
+import edu.iris.Fissures.network.StationIdUtil;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import edu.iris.Fissures.IfNetwork.NetworkAccess;
 
 public class ThreadedNetClient extends SimpleNetworkClient{
+    public ThreadedNetClient(){
+        super();
+    }
+
+    public ThreadedNetClient(String networkCode, String serverDNS, String serverName){
+        super(networkCode, serverDNS, serverName);
+    }
     public void exercise(){
         super.exercise();
-        Tester.runAll(createRunnables());
+        try {
+            Tester.runAll(createRunnables());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
     }
 
     public Runnable[] createRunnables() {
         Runnable[] runnables = new Runnable[6];
-        runnables[0] = new NetworkAccessNarrow();
-        runnables[1] = new RetrieveForStation();
+        runnables[0] =  new RetrieveForStation();
+        runnables[1] =new RetrieveStations();
         runnables[2] = new GetAttributes();
         runnables[3] = new IORConverter();
-        runnables[4] = new RetrieveStations();
+        runnables[4] = new NetworkAccessNarrow();
         runnables[5] = new RetrieveChannel();
         return runnables;
     }
@@ -26,13 +39,16 @@ public class ThreadedNetClient extends SimpleNetworkClient{
         public void run() {
             String ior = Initializer.getORB().object_to_string((org.omg.CORBA.Object)net);
             org.omg.CORBA.Object obj = Initializer.getORB().string_to_object(ior);
-            NetworkAccess temp = NetworkAccessHelper.narrow(obj); }
+            NetworkAccessHelper.narrow(obj);
+        }
 
         public String toString(){ return "NetworkAccess narrow"; }
     }
 
     private class RetrieveForStation implements Runnable{
-        public void run() { net.retrieve_for_station(Initializer.fakeStation); }
+        public void run() {
+            net.retrieve_for_station(testStation.get_id());
+        }
 
         public String toString(){ return "retrieve_for_station"; }
     }
@@ -62,7 +78,7 @@ public class ThreadedNetClient extends SimpleNetworkClient{
     private class RetrieveChannel implements Runnable{
         public void run() {
             try {
-                net.retrieve_channel(Initializer.fakeChan);
+                net.retrieve_channel(testChannel.get_id());
             } catch (ChannelNotFound e) {
                 System.err.println("This channel should always be found");
                 e.printStackTrace();
@@ -76,7 +92,12 @@ public class ThreadedNetClient extends SimpleNetworkClient{
     private static Logger logger = Logger.getLogger(ThreadedNetClient.class);
 
     public static void main(String[] args){
+        BasicConfigurator.configure();
         Initializer.init(args);
-        Tester.runAll(new ThreadedNetClient().createRunnables());
+        try {
+            Tester.runAll(new ThreadedNetClient().createRunnables());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
