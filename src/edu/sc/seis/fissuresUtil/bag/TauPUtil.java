1a4eaa47aabb82332b45a26df9fedb373c153a54
crotwell <crotwell@localhost>
2004-03-26 14:40:27 -0500

    [svn r7862] initial

---
+++ src/edu/sc/seis/fissuresUtil/bag/TauPUtil.java
@@ -0,0 +1,44 @@
+/**
+ * TauPUtil.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.bag;
+
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.IfNetwork.Station;
+import edu.iris.Fissures.Location;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.TauP.Arrival;
+import edu.sc.seis.TauP.TauModelException;
+import edu.sc.seis.TauP.TauP_Time;
+
+public class TauPUtil {
+
+    public TauPUtil(String modelName) throws TauModelException {
+        taup_time = new TauP_Time(modelName);
+    }
+
+    public Arrival[] calcTravelTimes(Station station, Origin origin, String[] phaseNames) throws TauModelException {
+        return calcTravelTimes(station.my_location, origin, phaseNames);
+    }
+
+    public synchronized Arrival[] calcTravelTimes(Location stationLoc, Origin origin, String[] phaseNames) throws TauModelException {
+        QuantityImpl depth = (QuantityImpl)origin.my_location.depth;
+        depth = depth.convertTo(UnitImpl.KILOMETER);
+        DistAz distAz = new DistAz(stationLoc.latitude,
+                                   stationLoc.longitude,
+                                   origin.my_location.latitude,
+                                   origin.my_location.longitude);
+        taup_time.setSourceDepth(depth.getValue());
+        taup_time.setPhaseNames(phaseNames );
+        taup_time.calculate(distAz.delta);
+        Arrival[] arrivals = taup_time.getArrivals();
+        return arrivals;
+    }
+
+    TauP_Time taup_time;
+}
+
