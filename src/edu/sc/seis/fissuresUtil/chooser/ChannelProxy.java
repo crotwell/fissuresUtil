dd2f241197dfa3176d9ecef7d172a2223b9a06c3
groves <groves@localhost>
2004-03-31 15:20:14 -0500

    [svn r7940] cleaned up unused code

--- src/edu/sc/seis/fissuresUtil/chooser/ChannelProxy.java
+++ src/edu/sc/seis/fissuresUtil/chooser/ChannelProxy.java
@@ -1,12 +1,10 @@
 package edu.sc.seis.fissuresUtil.chooser;
 
-import edu.sc.seis.fissuresUtil.namingService.*;
-import edu.sc.seis.fissuresUtil.xml.*;
-
-import edu.iris.Fissures.IfNetwork.*;
-import edu.iris.Fissures.network.*;
-
-import org.omg.CORBA.*;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.ChannelId;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.NetworkDCOperations;
+import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
 
 /**
  * ChannelProxy.java
@@ -19,47 +17,43 @@ import org.omg.CORBA.*;
  */
 
 public class ChannelProxy implements ChannelGrouper{
-    public ChannelProxy (){
-
-    }
-
     public  Channel[] retrieve_grouping(org.omg.CORBA_2_3.ORB orb, ChannelId channelId) {
-    Channel[] group;
-    try {
-        FissuresNamingService fissuresNamingService = new FissuresNamingService(orb);
-        NetworkDCOperations[] networkReferences = fissuresNamingService.getAllNetworkDC();
-        // ChannelId channelId = channel.get_id();
-        for(int counter = 0; counter < networkReferences.length; counter++) {
+        Channel[] group;
         try {
-             NetworkAccess networkAccess = networkReferences[counter].a_finder().retrieve_by_id(channelId.network_id);
-            Channel channel = networkAccess.retrieve_channel(channelId);
-            Channel[] channels = networkAccess.retrieve_for_station(channel.my_site.my_station.get_id());
-            ChannelGrouperImpl channelGrouperImpl = new ChannelGrouperImpl();
-            group = channelGrouperImpl.retrieve_grouping(channels, channel);
-            if(group.length == 3) return group;
-        } catch(Throwable e) {
+            FissuresNamingService fissuresNamingService = new FissuresNamingService(orb);
+            NetworkDCOperations[] networkReferences = fissuresNamingService.getAllNetworkDC();
+            // ChannelId channelId = channel.get_id();
+            for(int counter = 0; counter < networkReferences.length; counter++) {
+                try {
+                    NetworkAccess networkAccess = networkReferences[counter].a_finder().retrieve_by_id(channelId.network_id);
+                    Channel channel = networkAccess.retrieve_channel(channelId);
+                    Channel[] channels = networkAccess.retrieve_for_station(channel.my_site.my_station.get_id());
+                    ChannelGrouperImpl channelGrouperImpl = new ChannelGrouperImpl();
+                    group = channelGrouperImpl.retrieve_grouping(channels, channel);
+                    if(group.length == 3) return group;
+                } catch(Throwable e) {
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+        } catch(Exception e) {
             e.printStackTrace();
-            continue;
+            return new Channel[0];
         }
-        }
-    } catch(Exception e) {
-        e.printStackTrace();
-        return new Channel[0];
-    }
         return null;
     }
 
 
     public Channel[] retrieve_grouping(edu.sc.seis.fissuresUtil.xml.DataSet dataSet, ChannelId channelId) {
 
-    return null;
-    //dataSet
+        return null;
+        //dataSet
 
     }
 
 
     public ChannelId[] retrieve_grouping(ChannelId[] channelIds, ChannelId channelId) {
-    return retrieve_grouping(channelIds, channelId);
+        return retrieve_grouping(channelIds, channelId);
     }
 
 
