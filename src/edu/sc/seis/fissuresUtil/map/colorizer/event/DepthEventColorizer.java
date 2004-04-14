c11a5180b781ccf5d0fd9a7d06132c5cd6947b83
groves <groves@localhost>
2004-04-14 13:39:34 -0400

    [svn r8156] use new event color names

--- src/edu/sc/seis/fissuresUtil/map/colorizer/event/DepthEventColorizer.java
+++ src/edu/sc/seis/fissuresUtil/map/colorizer/event/DepthEventColorizer.java
@@ -16,33 +16,33 @@ import java.awt.Color;
 import java.awt.Paint;
 
 public class DepthEventColorizer implements EventColorizer{
-	
-	public Paint[] colorize(EventAccessOperations[] events) {
-		Paint[] paints = new Color[events.length];
-		for (int i = 0; i < events.length; i++) {
-			try {
-				paints[i] = getDepthColor(events[i]);
-			} catch (NoPreferredOrigin e) {
-				paints[i] = DefaultEventColorizer.DEFAULT_EVENT;
-			}
-		}
-		return paints;
-	}
-	
-    public static Color getDepthColor(EventAccessOperations eao) throws NoPreferredOrigin{
-		Origin prefOrigin = eao.get_preferred_origin();
-		QuantityImpl depth = (QuantityImpl)prefOrigin.my_location.depth;
-		double depthKM = depth.convertTo(UnitImpl.KILOMETER).value;
-		Color color = DisplayUtils.MEDIUM_DEPTH_EVENT;
-		if (depthKM <= 40.0){
-			color = DisplayUtils.SHALLOW_DEPTH_EVENT;
-		}
-		if (depthKM >= 150.0){
-			color = DisplayUtils.DEEP_DEPTH_EVENT;
-		}
-		return color;
+
+    public Paint[] colorize(EventAccessOperations[] events) {
+        Paint[] paints = new Color[events.length];
+        for (int i = 0; i < events.length; i++) {
+            try {
+                paints[i] = getDepthColor(events[i]);
+            } catch (NoPreferredOrigin e) {
+                paints[i] = DefaultEventColorizer.DEFAULT_EVENT;
+            }
+        }
+        return paints;
     }
-	
-	
+
+    public static Color getDepthColor(EventAccessOperations eao) throws NoPreferredOrigin{
+        Origin prefOrigin = eao.get_preferred_origin();
+        QuantityImpl depth = (QuantityImpl)prefOrigin.my_location.depth;
+        double depthKM = depth.convertTo(UnitImpl.KILOMETER).value;
+        Color color = DisplayUtils.EVENT_ORANGE;
+        if (depthKM <= 40.0){
+            color = DisplayUtils.EVENT_RED;
+        }
+        if (depthKM >= 150.0){
+            color = DisplayUtils.EVENT_YELLOW;
+        }
+        return color;
+    }
+
+
 }
 
--- src/edu/sc/seis/fissuresUtil/map/colorizer/event/FreshnessEventColorizer.java
+++ src/edu/sc/seis/fissuresUtil/map/colorizer/event/FreshnessEventColorizer.java
@@ -7,30 +7,31 @@
 package edu.sc.seis.fissuresUtil.map.colorizer.event;
 
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import java.awt.Color;
 import java.awt.Paint;
 
 public class FreshnessEventColorizer implements EventColorizer{
-	
-	public static Color FRESH_EVENT = Color.RED;
-	public static Color OLD_EVENT = Color.ORANGE;
-	public static Color REALLY_OLD_EVENT = Color.YELLOW;
-	
-	public Paint[] colorize(EventAccessOperations[] events) {
-		Paint[] paints = new Color[events.length];
-		for (int i = 0; i < events.length; i++) {
-			if (i <= events.length - 11){
-				paints[i] = REALLY_OLD_EVENT;
-			}
-			else if (i <= events.length - 6){
-				paints[i] = OLD_EVENT;
-			}
-			else if (i <= events.length - 1){
-				paints[i] = FRESH_EVENT;
-			}
-		}
-		return paints;
-	}
-	
+
+    public static Color FRESH_EVENT = DisplayUtils.EVENT_RED;
+    public static Color OLD_EVENT = DisplayUtils.EVENT_ORANGE;
+    public static Color REALLY_OLD_EVENT = DisplayUtils.EVENT_YELLOW;
+
+    public Paint[] colorize(EventAccessOperations[] events) {
+        Paint[] paints = new Color[events.length];
+        for (int i = 0; i < events.length; i++) {
+            if (i <= events.length - 11){
+                paints[i] = REALLY_OLD_EVENT;
+            }
+            else if (i <= events.length - 6){
+                paints[i] = OLD_EVENT;
+            }
+            else if (i <= events.length - 1){
+                paints[i] = FRESH_EVENT;
+            }
+        }
+        return paints;
+    }
+
 }
 
