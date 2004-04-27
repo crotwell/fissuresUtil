04558a6edf747af3062b14648dd0316f40c2bdbd
Philip Crotwell <crotwell@seis.sc.edu>
2004-04-27 16:13:52 -0400

    [svn r8307] dont store the seismogram, just the id and channel

--- src/edu/sc/seis/fissuresUtil/bag/LongShortTrigger.java
+++ src/edu/sc/seis/fissuresUtil/bag/LongShortTrigger.java
@@ -6,6 +6,7 @@
 
 package edu.sc.seis.fissuresUtil.bag;
 
+import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
@@ -13,16 +14,27 @@ import java.io.Serializable;
 
 public class LongShortTrigger implements Serializable {
 
+    /** This uses the seismogram to get the channelId, seismogramId and trigger time, but does
+     *  not retain a references to the seismogram to avoid memory leaks. */
     public LongShortTrigger(LocalSeismogramImpl seis, int index, float value, float sta, float lta) {
-        this(seis, index, value,
+        this(seis.getChannelID(),
+             seis.get_id(),
+             index,
+             value,
              seis.getBeginTime().add((TimeInterval)seis.getSampling().getPeriod().multiplyBy(index)),
-            sta, lta);
+             sta,
+             lta);
     }
 
-    public LongShortTrigger(LocalSeismogramImpl seis, int index, float value,
+    public LongShortTrigger(ChannelId channelId,
+                            String seisId,
+                            int index,
+                            float value,
                             MicroSecondDate when,
-                           float sta, float lta){
-        this.seis = seis;
+                            float sta,
+                            float lta){
+        this.seisId = seisId;
+        this.channelId = channelId;
         this.index = index;
         this.when = when;
         this.value = value;
@@ -58,19 +70,30 @@ public class LongShortTrigger implements Serializable {
     }
 
     /**
-     * Returns the seismogram associated with the trigger.
+     * Returns the seismogram id associated with the trigger.
      *
-     * @return    a  LocalSeismogramImpl
+     * @return    a  LocalSeismogram id
      */
-    public LocalSeismogramImpl getSeis() {
-        return seis;
+    public String getSeisId() {
+        return seisId;
+    }
+
+    /**
+     * Returns the channel id associated with the trigger.
+     *
+     * @return    a  ChannelId
+     */
+    public String getChannelId() {
+        return seisId;
     }
 
     public float getSTA() { return sta; }
 
     public float getLTA() { return lta; }
 
-    private transient LocalSeismogramImpl seis;
+    private String seisId;
+
+    private ChannelId channelId;
 
     private MicroSecondDate when;
 
@@ -82,5 +105,6 @@ public class LongShortTrigger implements Serializable {
 
     private float lta;
 }
+
 
 
