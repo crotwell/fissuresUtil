7155debeed13b36c082dbc80160991d6ac40772b
Philip Crotwell <crotwell@seis.sc.edu>
2004-04-29 16:26:47 -0400

    [svn r8379] initial

---
+++ src/edu/sc/seis/fissuresUtil/cache/ServerNameDNS.java
@@ -0,0 +1,15 @@
+/**
+ * ServerNameDNS.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.fissuresUtil.cache;
+
+public interface ServerNameDNS {
+
+    public String getServerName();
+
+    public String getServerDNS();
+}
+
