8f573a08d6906429d753caec5cbebb53e6e5d611
groves <groves@localhost>
2004-04-30 15:07:35 -0400

    [svn r8417] initial

---
+++ src/edu/sc/seis/fissuresUtil/display/SeismicRegion.java
@@ -0,0 +1,35 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import edu.iris.Fissures.FlinnEngdahlRegion;
+import edu.iris.Fissures.FlinnEngdahlType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeismicRegion extends FlinnEngdahlRegion{
+    public SeismicRegion(String name, int num){
+        this(name, num, new GeographicRegion[]{});
+    }
+
+    public SeismicRegion(String name, int num, GeographicRegion[] children){
+        type =  FlinnEngdahlType.SEISMIC_REGION;
+        number = num;
+        this.name = name;
+        for (int i = 0; i < children.length; i++) { kids.add(children[i]); }
+    }
+
+    public int getNumber(){ return number; }
+
+    public String getName(){ return name; }
+
+    public void add(GeographicRegion child){ kids.add(child); }
+
+    public GeographicRegion[] getChildren(){
+        return (GeographicRegion[])kids.toArray(new GeographicRegion[kids.size()]);
+    }
+
+    public String toString(){ return getName(); }
+
+    private List kids = new ArrayList();
+    private String name;
+}
+
