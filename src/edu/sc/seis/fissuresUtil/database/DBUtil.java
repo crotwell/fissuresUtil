1c6f2390499356d387fb9941dfeea8fb804ecdf1
Charles Groves <groves@seis.sc.edu>
2004-05-04 15:46:51 -0400

    [svn r8482] added commaDelimit method

--- src/edu/sc/seis/fissuresUtil/database/DBUtil.java
+++ src/edu/sc/seis/fissuresUtil/database/DBUtil.java
@@ -4,8 +4,29 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class DBUtil{
+    public static String commaDelimit(int numQuestionMarks){
+        List qs = new ArrayList(numQuestionMarks);
+        for (int i = 0; i < numQuestionMarks; i++) { qs.add("?"); }
+        return commaDelimit(qs);
+    }
+
+    public static String commaDelimit(Collection strings) {
+        String commaDelimited = "";
+        Iterator it = strings.iterator();
+        boolean first = true;
+        while(it.hasNext()){
+            if(first){ first = false; }
+            else{ commaDelimited += ", "; }
+            commaDelimited += it.next();
+        }
+        return commaDelimited;
+    }
     public static boolean tableExists(String tableName, Connection conn)
         throws SQLException{
         DatabaseMetaData dbmd = conn.getMetaData();
@@ -15,13 +36,13 @@ public class DBUtil{
         }
         return false;
     }
-    
+
     public static void printTableExistence(String name, Connection conn)throws SQLException{
         if(tableExists(name, conn)) System.out.println(name + " exists");
         else System.out.println(name + " does not exist");
     }
-    
-    
+
+
     public static void printExistingTables(Connection conn)throws SQLException{
         DatabaseMetaData dbmd = conn.getMetaData();
         System.out.println("********************************************");
