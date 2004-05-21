1170f008a30f3d9893408021b851f8de06eff293
Charles Groves <groves@seis.sc.edu>
2004-05-21 12:36:20 -0400

    [svn r8850] use WorkerThreadPool instead of EventBackgroundLoader

--- src/edu/sc/seis/fissuresUtil/map/layers/EventTableLayer.java
+++ src/edu/sc/seis/fissuresUtil/map/layers/EventTableLayer.java
@@ -8,7 +8,6 @@ package edu.sc.seis.fissuresUtil.map.layers;
 
 import com.bbn.openmap.MapBean;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
-import edu.sc.seis.fissuresUtil.cache.EventBackgroundLoaderPool;
 import edu.sc.seis.fissuresUtil.display.EQDataEvent;
 import edu.sc.seis.fissuresUtil.display.EQSelectionEvent;
 import edu.sc.seis.fissuresUtil.display.EventTableModel;
@@ -20,56 +19,52 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class EventTableLayer extends EventLayer{
-	
+
     private EventTableModel tableModel;
     private ListSelectionModel selectionModel;
-	
+
     public EventTableLayer(EventTableModel tableModel,
-						   ListSelectionModel lsm,
-						   MapBean mapBean,
-						   EventColorizer colorizer){
-		super(mapBean, colorizer);
-		this.tableModel = tableModel;
-		selectionModel = lsm;
-		addEQSelectionListener(this);
-		tableModel.addEventDataListener(this);
-		eventDataChanged(new EQDataEvent(this, tableModel.getAllEvents()));
-		
-		selectionModel.addListSelectionListener(new ListSelectionListener(){
-					public void valueChanged(ListSelectionEvent e) {
-						EventAccessOperations[] selectedEvents = getSelectedEvents();
-						if(selectedEvents.length > 0){
-							fireEQSelectionChanged(new EQSelectionEvent(this, selectedEvents));
-						}
-					}
-					
-				});
+                           ListSelectionModel lsm,
+                           MapBean mapBean,
+                           EventColorizer colorizer){
+        super(mapBean, colorizer);
+        this.tableModel = tableModel;
+        selectionModel = lsm;
+        addEQSelectionListener(this);
+        tableModel.addEventDataListener(this);
+        eventDataChanged(new EQDataEvent(this, tableModel.getAllEvents()));
+
+        selectionModel.addListSelectionListener(new ListSelectionListener(){
+                    public void valueChanged(ListSelectionEvent e) {
+                        EventAccessOperations[] selectedEvents = getSelectedEvents();
+                        if(selectedEvents.length > 0){
+                            fireEQSelectionChanged(new EQSelectionEvent(this, selectedEvents));
+                        }
+                    }
+
+                });
     }
-	
+
     public void selectEvent(EventAccessOperations evo){
-		int rowToSelect = tableModel.getRowForEvent(evo);
-		if (rowToSelect != -1){
-			selectionModel.setSelectionInterval(rowToSelect, rowToSelect);
-		}
+        int rowToSelect = tableModel.getRowForEvent(evo);
+        if (rowToSelect != -1){
+            selectionModel.setSelectionInterval(rowToSelect, rowToSelect);
+        }
     }
-	
+
     public EventTableModel getTableModel(){
-		return tableModel;
+        return tableModel;
     }
-	
-	public EventBackgroundLoaderPool getLoader(){
-		return tableModel.getLoader();
-    }
-	
+
     public EventAccessOperations[] getSelectedEvents(){
-		List selectedEvents = new ArrayList();
-		EventAccessOperations[] allEvents = tableModel.getAllEvents();
-		for (int i = 0; i < allEvents.length; i++) {
-			if (selectionModel.isSelectedIndex(i)){
-				selectedEvents.add(allEvents[i]);
-			}
-		}
-		return (EventAccessOperations[])selectedEvents.toArray(new EventAccessOperations[0]);
+        List selectedEvents = new ArrayList();
+        EventAccessOperations[] allEvents = tableModel.getAllEvents();
+        for (int i = 0; i < allEvents.length; i++) {
+            if (selectionModel.isSelectedIndex(i)){
+                selectedEvents.add(allEvents[i]);
+            }
+        }
+        return (EventAccessOperations[])selectedEvents.toArray(new EventAccessOperations[0]);
     }
 }
 
