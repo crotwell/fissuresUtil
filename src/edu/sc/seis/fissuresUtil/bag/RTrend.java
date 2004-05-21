7465ca006a0e0f6a47b53e8817b151c0585f398b
Philip Crotwell <crotwell@seis.sc.edu>
2004-05-20 22:22:55 -0400

    [svn r8833] added to tests to fix least squares/rtrend bug

--- src/edu/sc/seis/fissuresUtil/bag/RTrend.java
+++ src/edu/sc/seis/fissuresUtil/bag/RTrend.java
@@ -22,80 +22,80 @@ public class RTrend  implements LocalSeismogramFunction {
     }
 
     public LocalSeismogramImpl apply(LocalSeismogramImpl seis) throws FissuresException {
-    if (seis.can_convert_to_short()) {
-        short[] sSeries = seis.get_as_shorts();
-        return new LocalSeismogramImpl(seis, apply(sSeries));
-    } else if (seis.can_convert_to_long()) {
-        int[] iSeries = seis.get_as_longs();
-        return new LocalSeismogramImpl(seis, apply(iSeries));
-    } else if (seis.can_convert_to_float()) {
-        float[] fSeries = seis.get_as_floats();
-        return new LocalSeismogramImpl(seis, apply(fSeries));
-    } else {
-        double[] dSeries = seis.get_as_doubles();
-        return new LocalSeismogramImpl(seis, apply(dSeries));
-    } // end of else
+        if (seis.can_convert_to_short()) {
+            short[] sSeries = seis.get_as_shorts();
+            return new LocalSeismogramImpl(seis, apply(sSeries));
+        } else if (seis.can_convert_to_long()) {
+            int[] iSeries = seis.get_as_longs();
+            return new LocalSeismogramImpl(seis, apply(iSeries));
+        } else if (seis.can_convert_to_float()) {
+            float[] fSeries = seis.get_as_floats();
+            return new LocalSeismogramImpl(seis, apply(fSeries));
+        } else {
+            double[] dSeries = seis.get_as_doubles();
+            return new LocalSeismogramImpl(seis, apply(dSeries));
+        } // end of else
     }
 
     public  float[] apply(float[] data) {
-    float[] out = new float[data.length];
-    System.arraycopy(data, 0, out, 0, data.length);
-    applyInPlace(out);
-    return out;
+        float[] out = new float[data.length];
+        System.arraycopy(data, 0, out, 0, data.length);
+        applyInPlace(out);
+        return out;
     }
 
     public  void applyInPlace(float[] data) {
-    Statistics stat = new Statistics(data);
-    double[] trend = stat.linearLeastSquares();
-    for (int i=0; i<data.length; i++) {
-        data[i] -= (float)(trend[0] + i * trend[1]);
-    } // end of for (int i=0; i<data.length; i++)
+        Statistics stat = new Statistics(data);
+        double[] trend = stat.linearLeastSquares();
+        for (int i=0; i<data.length; i++) {
+            data[i] -= (float)(trend[0] + i * trend[1]);
+        } // end of for (int i=0; i<data.length; i++)
 
     }
 
     public  double[] apply(double[] data) {
-    double[] out = new double[data.length];
-    System.arraycopy(data, 0, out, 0, data.length);
-    applyInPlace(out);
-    return out;
+        double[] out = new double[data.length];
+        System.arraycopy(data, 0, out, 0, data.length);
+        applyInPlace(out);
+        return out;
     }
 
     public  void applyInPlace(double[] data) {
-    Statistics stat = new Statistics(data);
-    double[] trend = stat.linearLeastSquares();
-    for (int i=0; i<data.length; i++) {
-        data[i] -= (trend[0] + i * trend[1]);
-    } // end of for (int i=0; i<data.length; i++)
+        Statistics stat = new Statistics(data);
+        double[] trend = stat.linearLeastSquares();
+        for (int i=0; i<data.length; i++) {
+            data[i] -= (trend[0] + i * trend[1]);
+        } // end of for (int i=0; i<data.length; i++)
     }
 
     public  int[] apply(int[] data) {
-    int[] out = new int[data.length];
-    System.arraycopy(data, 0, out, 0, data.length);
-    applyInPlace(out);
-    return out;
+        int[] out = new int[data.length];
+        System.arraycopy(data, 0, out, 0, data.length);
+        applyInPlace(out);
+        return out;
     }
 
     public  void applyInPlace(int[] data) {
-    Statistics stat = new Statistics(data);
-    double[] trend = stat.linearLeastSquares();
-    for (int i=0; i<data.length; i++) {
-        data[i] -= (int)(trend[0] + i * trend[1]);
-    } // end of for (int i=0; i<data.length; i++)
+        Statistics stat = new Statistics(data);
+        double[] trend = stat.linearLeastSquares();
+        for (int i=0; i<data.length; i++) {
+            data[i] -= (int)Math.round(trend[0] + i * trend[1]);
+        } // end of for (int i=0; i<data.length; i++)
     }
 
     public  short[] apply(short[] data) {
-    short[] out = new short[data.length];
-    System.arraycopy(data, 0, out, 0, data.length);
-    applyInPlace(out);
-    return out;
+        short[] out = new short[data.length];
+        System.arraycopy(data, 0, out, 0, data.length);
+        applyInPlace(out);
+        return out;
     }
 
     public  void applyInPlace(short[] data) {
-    Statistics stat = new Statistics(data);
-    double[] trend = stat.linearLeastSquares();
-    for (int i=0; i<data.length; i++) {
-        data[i] -= (short)(trend[0] + i * trend[1]);
-    } // end of for (int i=0; i<data.length; i++)
+        Statistics stat = new Statistics(data);
+        double[] trend = stat.linearLeastSquares();
+        for (int i=0; i<data.length; i++) {
+            data[i] -= (short)Math.round(trend[0] + i * trend[1]);
+        } // end of for (int i=0; i<data.length; i++)
     }
 
 
--- tests/unit/edu/sc/seis/fissuresUtil/bag/RTrendTest.java
+++ tests/unit/edu/sc/seis/fissuresUtil/bag/RTrendTest.java
@@ -11,6 +11,9 @@ public class RTrendTest extends TestCase  {
     short[] shortTestData;
     float[] floatTestData;
     double[] doubleTestData;
+    short[] bumps;
+    float bumpSlope;
+    float bimpIntercept;
 
     public RTrendTest(String name)  {
         super(name);
@@ -22,7 +25,7 @@ public class RTrendTest extends TestCase  {
 
     protected void setUp() throws Exception  {
         super.setUp();
-        short size = 40;
+        short size = 400;
         intTestData = new int[size];
         shortTestData = new short[size];
         floatTestData = new float[size];
@@ -33,6 +36,28 @@ public class RTrendTest extends TestCase  {
             floatTestData[i] = i;
             doubleTestData[i] = i;
         } // end of for (int i=0; i<intTestData.length; i++)
+
+        // linear least squares applet
+        // http://www.dartmouth.edu/~chemlab/info/resources/linear/linear.html
+        bumpSlope = 4.2367647f;
+        bimpIntercept = 9.4117647f;
+        bumps = new short[16];
+        bumps[0] = 7;
+        bumps[1] = -12;
+        bumps[2] = 46;
+        bumps[3] = 30;
+        bumps[4] = 17;
+        bumps[5] = 33;
+        bumps[6] = 27;
+        bumps[7] = 39;
+        bumps[8] = 51;
+        bumps[9] = 48;
+        bumps[10] = 51;
+        bumps[11] = 60;
+        bumps[12] = 59;
+        bumps[13] = 70;
+        bumps[14] = 78;
+        bumps[15] = 55;
         rtrend = createInstance();
     }
 
@@ -41,24 +66,49 @@ public class RTrendTest extends TestCase  {
         super.tearDown();
     }
 
+    public void testApplyBumps() throws Exception  {
+        short[] sOut = rtrend.apply(bumps);
+        for (int i = 0; i < sOut.length; i++) {
+            assertEquals("short "+i, (short)Math.round(bumps[i]-(bumpSlope*i+bimpIntercept)), sOut[i]);
+        }
+    }
+
+    public void testApplyIntBumps() throws Exception  {
+        int[] iBumps = new int[bumps.length];
+        for (int i = 0; i < bumps.length; i++) {
+            iBumps[i] = bumps[i];
+        }
+        int[] sOut = rtrend.apply(iBumps);
+        for (int i = 0; i < sOut.length; i++) {
+            assertEquals("intBumps "+i, (int)Math.round(bumps[i]-(bumpSlope*i+bimpIntercept)), sOut[i]);
+        }
+    }
     public void testApplyShort() throws Exception  {
         short[] sOut = rtrend.apply(shortTestData);
-        assertEquals("short", 0, sOut[0]);
+        for (int i = 0; i < sOut.length; i++) {
+            assertEquals("short", 0, sOut[i]);
+        }
     }
 
     public void testApplyInt() throws Exception  {
         int[] iOut = rtrend.apply(intTestData);
-        assertEquals("int", 0, iOut[0]);
+        for (int i = 0; i < iOut.length; i++) {
+            assertEquals("int", 0, iOut[i]);
+        }
     }
 
     public void testApplyFloat() throws Exception  {
         float[] fOut = rtrend.apply(floatTestData);
-        assertEquals("float", 0, fOut[0], 0.0000001);
+        for (int i = 0; i < fOut.length; i++) {
+            assertEquals("float", 0, fOut[i], 0.0000001);
+        }
     }
 
     public void testApplyDouble() throws Exception  {
         double[] dOut = rtrend.apply(doubleTestData);
-        assertEquals("double", 0, dOut[0], 0.000001);
+        for (int i = 0; i < dOut.length; i++) {
+            assertEquals("double", 0, dOut[i], 0.000001);
+        }
     }
 
 }
