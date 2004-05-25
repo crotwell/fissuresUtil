d5a42fbed8b69cfa47b3f5d4a522ef6357c7d026
Charles Groves <groves@seis.sc.edu>
2004-05-25 16:42:37 -0400

    [svn r8911] added huuuuge and smaaaaal values

--- src/edu/sc/seis/fissuresUtil/display/borders/AbstractUnitRangeBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/AbstractUnitRangeBorder.java
@@ -16,7 +16,7 @@ public abstract class AbstractUnitRangeBorder extends Border implements TitlePro
 
     protected List createFormats() {
         List formats = new ArrayList();
-        for (double i = .1; i <= 100000000; i *= 10) {
+        for (double i = .000000001; i <= 100000000; i *= 10) {
             formats.add(new UnitRangeFormatter(i, 2));
             formats.add(new UnitRangeFormatter(i*5, 5));
         }
@@ -29,11 +29,16 @@ public abstract class AbstractUnitRangeBorder extends Border implements TitlePro
 
         public UnitRangeFormatter(double division, int ticksPerDivision){
             super(division, ticksPerDivision);
-            if (division < 10 && division != 0 ) {
+            if(division < .01){
                 // exponential notation
+                df = new DecimalFormat("#.###E00");
+            }else if (division < 10 && division != 0 ) {
                 df = new DecimalFormat("0.00###");
-            } else {
+            } else if(division < 100000){
                 df = new DecimalFormat("#.####");
+            }else{
+                // exponential notation
+                df = new DecimalFormat("#.###E00");
             }
         }
 
