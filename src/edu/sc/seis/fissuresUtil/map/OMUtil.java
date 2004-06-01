e1e405e35ed45740a0b9ccae4f61ffea67f00489
oliverpa <oliverpa@localhost>
2004-06-01 14:16:21 -0400

    [svn r9020] added a method that reverses a graphic list

--- src/edu/sc/seis/fissuresUtil/map/OMUtil.java
+++ src/edu/sc/seis/fissuresUtil/map/OMUtil.java
@@ -11,28 +11,43 @@ import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.fissuresUtil.map.colorizer.event.EventColorizer;
 import edu.sc.seis.fissuresUtil.map.graphics.OMEvent;
 import java.awt.Paint;
+import java.util.Iterator;
 
 public class OMUtil {
-	
-	public static void setEventColors(OMGraphicList omEvents, EventColorizer colorizer){
-		synchronized(omEvents){
-			Paint[] paints = colorizer.colorize(extractEvents(omEvents));
-			for (int i = 0; i < paints.length; i++) {
-				((OMEvent)omEvents.getOMGraphicAt(i)).setPaint(paints[i]);
-			}
-		}
-	}
-	
-	public static EventAccessOperations[] extractEvents(OMGraphicList omEvents){
-		EventAccessOperations[] events;
-		synchronized(omEvents){
-			events = new EventAccessOperations[omEvents.size()];
-			for (int i = 0; i < events.length; i++) {
-				events[i] = ((OMEvent)omEvents.getOMGraphicAt(i)).getEvent();
-			}
-		}
-		return events;
-	}
-	
+
+    public static void setEventColors(OMGraphicList omEvents, EventColorizer colorizer){
+        synchronized(omEvents){
+            Paint[] paints = colorizer.colorize(extractEvents(omEvents));
+            Iterator it = omEvents.iterator();
+            OMEvent cur = (OMEvent)it.next();
+            for (int i = 0;
+                 i < omEvents.size() && it.hasNext();
+                 i++, cur = (OMEvent)it.next()) {
+
+                cur.setPaint(paints[i]);
+            }
+            reverseGraphicOrder(omEvents);
+        }
+    }
+
+    public static EventAccessOperations[] extractEvents(OMGraphicList omEvents){
+        EventAccessOperations[] events;
+        synchronized(omEvents){
+            events = new EventAccessOperations[omEvents.size()];
+            for (int i = 0; i < events.length; i++) {
+                events[i] = ((OMEvent)omEvents.getOMGraphicAt(i)).getEvent();
+            }
+        }
+        return events;
+    }
+
+
+    public static void reverseGraphicOrder(OMGraphicList graphics){
+        synchronized(graphics){
+            for (int i = 0; i < graphics.size(); i++) {
+                graphics.moveIndexedToFirst(i);
+            }
+        }
+    }
 }
 
