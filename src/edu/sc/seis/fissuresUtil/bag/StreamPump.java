1df60a9ace0a3ef2c77f6f8484c8c3a0e66a0fb8
Philip Crotwell <crotwell@seis.sc.edu>
2004-06-14 15:32:51 -0400

    [svn r9154] initial

---
+++ src/edu/sc/seis/fissuresUtil/bag/StreamPump.java
@@ -0,0 +1,59 @@
+/**
+ * StreamPump takes an input stream, and an optional output writer, and
+ * reads from the input stream and writes it to the output. It does
+ * this in a separate thread, useful also for gobbling the stuff in
+ * an input stream that you do not really care about, but need to empty to
+ * prevent the streams buffers from filling.
+ *
+ * @author Philip Crotwell
+ */
+
+package edu.sc.seis.fissuresUtil.bag;
+
+import java.io.*;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+
+public class StreamPump extends Thread {
+
+    public StreamPump(InputStream in) {
+        this (new BufferedInputStream(in), null);
+    }
+
+    public StreamPump(BufferedReader in) {
+        this (in, null);
+    }
+
+
+    public StreamPump(InputStream in, BufferedWriter out) {
+        this (new BufferedReader(new InputStreamReader(in)), out);
+    }
+
+    public StreamPump(BufferedReader in, BufferedWriter out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    public void run() {
+        try {
+            String s;
+
+            while ( (s = in.readLine()) != null) {
+                if (out != null) {
+                    out.write(s);
+                    out.newLine();
+                }
+            }
+            if (out != null) {
+                out.flush();
+            }
+        } catch (Throwable e) {
+            GlobalExceptionHandler.handle(e);
+        }
+    }
+
+    private BufferedReader in;
+
+    private BufferedWriter out;
+
+}
+
