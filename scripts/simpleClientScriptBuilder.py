2c327fa1a022b93549d6acc70cbd73d48681c7f5
groves <groves@localhost>
2004-06-16 15:48:28 -0400

    [svn r9189] updated files

--- scripts/simpleClientScriptBuilder.py
+++ scripts/simpleClientScriptBuilder.py
@@ -6,21 +6,31 @@ class simpleScriptParameters(scriptBuilder.jacorbParameters):
     def __init__(self, mods, mainclass, name):
         scriptBuilder.jacorbParameters.__init__(self)
         for mod in mods: self.update(mod)
-        propLoc = self.add('PROP', 'nameservice.prop', 'initial')
+        propLoc = self.add('PROP', 'simpleClient.prop', 'initial')
         self.args.insert(0, '-props')
         self.args.insert(1, propLoc.interp)
         self.name = name
         self.mainclass = mainclass
+
+
+def buildNetScripts(proj):
+    mainclasses = [("edu.sc.seis.fissuresUtil.simple.SimpleNetworkClient", "simpleNet"),
+                   ("edu.sc.seis.fissuresUtil.simple.ThreadedNetClient", "threadedNet")]
+    return __build(proj, mainclasses)
+    
         
 def buildScripts(proj):
-    depCopy.copy(proj)
-    filenames = []
     mainclasses = [("edu.sc.seis.fissuresUtil.simple.ThreadedEventClient", "threadedEvent"),
                ("edu.sc.seis.fissuresUtil.simple.SimpleEventClient", "simpleEvent"),
                ("edu.sc.seis.fissuresUtil.simple.SimpleNetworkClient", "simpleNet"),
                ("edu.sc.seis.fissuresUtil.simple.ThreadedNetClient", "threadedNet"),
                ("edu.sc.seis.fissuresUtil.simple.SimpleSeismogramClient", "simpleSeis"),
                ("edu.sc.seis.fissuresUtil.simple.ThreadedSeisClient", "threadedSeis")]
+    return __build(proj, mainclasses)
+
+def __build(proj, mainclasses):
+    depCopy.copy(proj)
+    filenames = []
     for mainclass, name in mainclasses:
         scriptBuilder.setVarSh()
         shParams = simpleScriptParameters([], mainclass, name)
