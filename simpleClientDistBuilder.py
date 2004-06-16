5c4524214f685d02e34f1d1050e167b89979b506
groves <groves@localhost>
2004-06-16 15:46:55 -0400

    [svn r9187] added more options

--- simpleClientDistBuilder.py
+++ simpleClientDistBuilder.py
@@ -4,10 +4,20 @@ sys.path.extend(["../devTools/maven", './scripts'])
 import distBuilder, ProjectParser, simpleClientScriptBuilder
 
 def buildDist(proj):
-    scripts = simpleClientScriptBuilder.buildScripts(proj)
+    __build(proj, simpleClientScriptBuilder.buildScripts(proj))
+
+def buildNetDist(proj):
+    __build(proj, simpleClientScriptBuilder.buildNetScripts(proj), 'netClients')
+
+def __build(proj, scripts, name='simpleClients'):
     extras = [(script, script) for script in  scripts]
-    distBuilder.buildDist(proj, extras, 'simpleClients')
+    extras.append(('scripts/simpleClient.prop', 'simpleClient.prop'))
+    extras.append(('src', 'src'))
+    distBuilder.buildDist(proj, extras, name)
     for script in scripts: os.remove(script)
+    
 
 if __name__ == "__main__":
-    buildDist(ProjectParser.ProjectParser('./project.xml'))
+    proj = ProjectParser.ProjectParser('./project.xml')
+    if len(sys.argv) > 1: buildNetDist(proj)
+    else : buildDist(proj)
