b3cb83661a9ecf68ce1d009220a75a5907704efd
groves <groves@localhost>
2004-07-01 21:47:34 -0400

    [svn r9430] changed the colorize event to work on an OMGraphicList

--- src/edu/sc/seis/fissuresUtil/map/colorizer/event/DepthEventColorizer.java
+++ src/edu/sc/seis/fissuresUtil/map/colorizer/event/DepthEventColorizer.java
@@ -6,40 +6,33 @@
 
 package edu.sc.seis.fissuresUtil.map.colorizer.event;
 
-import edu.iris.Fissures.IfEvent.EventAccessOperations;
-import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
+import com.bbn.openmap.omGraphics.OMGraphicList;
 import edu.iris.Fissures.IfEvent.Origin;
 import edu.iris.Fissures.model.QuantityImpl;
 import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.display.DisplayUtils;
+import edu.sc.seis.fissuresUtil.map.graphics.OMEvent;
 import java.awt.Color;
-import java.awt.Paint;
+import java.util.Iterator;
 
 public class DepthEventColorizer implements EventColorizer{
 
-    public Paint[] colorize(EventAccessOperations[] events) {
-        Paint[] paints = new Color[events.length];
-        for (int i = 0; i < events.length; i++) {
-            try {
-                paints[i] = getDepthColor(events[i]);
-            } catch (NoPreferredOrigin e) {
-                paints[i] = DefaultEventColorizer.DEFAULT_EVENT;
-            }
+    public void colorize(OMGraphicList events) {
+        Iterator it = events.iterator();
+        while(it.hasNext()){
+            OMEvent cur = (OMEvent)it.next();
+            cur.setPaint(getDepthColor(cur.getEvent()));
         }
-        return paints;
     }
 
-    public static Color getDepthColor(EventAccessOperations eao) throws NoPreferredOrigin{
-        Origin prefOrigin = eao.get_preferred_origin();
+    public static Color getDepthColor(CacheEvent eao){
+        Origin prefOrigin = eao.getOrigin();
         QuantityImpl depth = (QuantityImpl)prefOrigin.my_location.depth;
         double depthKM = depth.convertTo(UnitImpl.KILOMETER).value;
         Color color = DisplayUtils.EVENT_ORANGE;
-        if (depthKM <= 40.0){
-            color = DisplayUtils.EVENT_RED;
-        }
-        if (depthKM >= 150.0){
-            color = DisplayUtils.EVENT_YELLOW;
-        }
+        if (depthKM <= 40.0){ color = DisplayUtils.EVENT_RED; }
+        if (depthKM >= 150.0){ color = DisplayUtils.EVENT_YELLOW; }
         return color;
     }
 
--- src/edu/sc/seis/fissuresUtil/map/colorizer/event/FreshnessEventColorizer.java
+++ src/edu/sc/seis/fissuresUtil/map/colorizer/event/FreshnessEventColorizer.java
@@ -6,10 +6,11 @@
 
 package edu.sc.seis.fissuresUtil.map.colorizer.event;
 
-import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import com.bbn.openmap.omGraphics.OMGraphicList;
 import edu.sc.seis.fissuresUtil.display.DisplayUtils;
+import edu.sc.seis.fissuresUtil.map.graphics.OMEvent;
 import java.awt.Color;
-import java.awt.Paint;
+import java.util.Iterator;
 
 public class FreshnessEventColorizer implements EventColorizer{
 
@@ -17,20 +18,14 @@ public class FreshnessEventColorizer implements EventColorizer{
     public static Color OLD_EVENT = DisplayUtils.EVENT_ORANGE;
     public static Color REALLY_OLD_EVENT = DisplayUtils.EVENT_YELLOW;
 
-    public Paint[] colorize(EventAccessOperations[] events) {
-        Paint[] paints = new Color[events.length];
-        for (int i = 0; i < events.length; i++) {
-            if (i <= events.length - 11){
-                paints[i] = REALLY_OLD_EVENT;
-            }
-            else if (i <= events.length - 6){
-                paints[i] = OLD_EVENT;
-            }
-            else if (i <= events.length - 1){
-                paints[i] = FRESH_EVENT;
-            }
+    public void colorize(OMGraphicList events) {
+        int size = events.size();
+        for (int i = 0; i < size; i++) {
+            OMEvent cur = (OMEvent)events.getOMGraphicAt(i);
+            if (i <= size - 11){ cur.setPaint(REALLY_OLD_EVENT); }
+            else if (i <= size - 6){ cur.setPaint(OLD_EVENT); }
+            else if (i <= size - 1){ cur.setPaint(FRESH_EVENT); }
         }
-        return paints;
     }
 
 }
