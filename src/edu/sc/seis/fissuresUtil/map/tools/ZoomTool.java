d6ea43e233dd26d83915c5f8044389038a53b913
oliverpa <oliverpa@localhost>
2004-07-19 13:01:19 -0400

    [svn r9662] center on currently selected event

--- src/edu/sc/seis/fissuresUtil/map/tools/ZoomTool.java
+++ src/edu/sc/seis/fissuresUtil/map/tools/ZoomTool.java
@@ -1,105 +1,127 @@
 package edu.sc.seis.fissuresUtil.map.tools;
 
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.EventListenerList;
+
 import com.bbn.openmap.LatLonPoint;
 import com.bbn.openmap.Layer;
 import com.bbn.openmap.MapBean;
 import com.bbn.openmap.event.CenterEvent;
 import com.bbn.openmap.event.ZoomEvent;
 import com.bbn.openmap.event.ZoomListener;
+
+import edu.iris.Fissures.IfEvent.*;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.map.OpenMap;
 import edu.sc.seis.fissuresUtil.map.layers.ChannelChooserLayer;
-import java.awt.event.MouseEvent;
-import javax.swing.event.EventListenerList;
 
-public class ZoomTool extends OpenMapTool{
+public class ZoomTool extends OpenMapTool {
 
-    private float zoomFactor;
-    private OpenMap map;
-    private float mapScale;
-    private LatLonPoint center;
-    private MapBean mapBean;
-    private EventListenerList listenerList = new EventListenerList();
-    private String id = "Zoom";
+	private float zoomFactor;
 
-    public ZoomTool(OpenMap om, float factor, String id){
-        this(om, factor, OpenMap.DEFAULT_SCALE, id);
-    }
+	private OpenMap map;
 
-    public ZoomTool(OpenMap om, float factor, float scale, String id){
-        this(om, factor, scale, new LatLonPoint(0,0), id);
-    }
-    public ZoomTool(OpenMap om, float factor, float scale, LatLonPoint center, String id){
-        zoomFactor = factor;
-        map = om;
-        mapScale = scale;
-        this.center = center;
-        mapBean = map.getMapBean();
-        if (id != null){
-            this.id = id;
-        }
-    }
+	private float mapScale;
 
-    public void addZoomListener(ZoomListener zl){
-        listenerList.add(ZoomListener.class, zl);
-    }
+	private MapBean mapBean;
 
-    public void removeZoomListener(ZoomListener zl){
-        listenerList.remove(ZoomListener.class, zl);
-    }
+	private EventListenerList listenerList = new EventListenerList();
 
-    private void fireZoomChanged(ZoomEvent ze){
-        // Guaranteed to return a non-null array
-        Object[] listeners = listenerList.getListenerList();
-        // Process the listeners last to first, notifying
-        // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==ZoomListener.class) {
-                // Lazily create the event:
-                if (ze != null){
-                    ((ZoomListener)listeners[i+1]).zoom(ze);
-                }
-            }
-        }
-    }
+	private String id = "Zoom";
 
-    public void mouseClicked(MouseEvent e){
-        if (isActive()){
-            LatLonPoint llp = mapBean.getCoordinates(e);
-            mapBean.center(new CenterEvent(this, llp.getLatitude(), llp.getLongitude()));
-            zoom();
-        }
-    }
+	public ZoomTool(OpenMap om, float factor, String id) {
+		this(om, factor, OpenMap.DEFAULT_SCALE, id);
+	}
 
-    public void zoom(){
-        fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapBean.getScale()/zoomFactor));
-    }
+	public ZoomTool(OpenMap om, float factor, float scale, String id) {
+		zoomFactor = factor;
+		map = om;
+		mapScale = scale;
+		mapBean = map.getMapBean();
+		if (id != null) {
+			this.id = id;
+		}
+	}
 
-    public void reset(){
-        mapBean.setCenter(center.getLatitude(), center.getLongitude());
-        fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapScale));
-        Layer[] layers = map.getLayers();
-        for (int i = 0; i < layers.length; i++) {
-            if (layers[i] instanceof ChannelChooserLayer){
-                ((ChannelChooserLayer)layers[i]).getChannelChooser().recheckNetworkAvailability();
-                break;
-            }
-        }
-    }
+	public void addZoomListener(ZoomListener zl) {
+		listenerList.add(ZoomListener.class, zl);
+	}
 
-    public void setActive(boolean active){
-        super.setActive(active);
-    }
+	public void removeZoomListener(ZoomListener zl) {
+		listenerList.remove(ZoomListener.class, zl);
+	}
 
-    /**
-     * Returns the id (MapMouseMode name).
-     * This name should be unique for each MapMouseMode.
-     * @return String ID
-     */
-    public String getID() {
-        return id;
-    }
-	
-	public String getPrettyName(){
+	private void fireZoomChanged(ZoomEvent ze) {
+		// Guaranteed to return a non-null array
+		Object[] listeners = listenerList.getListenerList();
+		// Process the listeners last to first, notifying
+		// those that are interested in this event
+		for (int i = listeners.length - 2; i >= 0; i -= 2) {
+			if (listeners[i] == ZoomListener.class) {
+				// Lazily create the event:
+				if (ze != null) {
+					((ZoomListener) listeners[i + 1]).zoom(ze);
+				}
+			}
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (isActive()) {
+			LatLonPoint llp = mapBean.getCoordinates(e);
+			mapBean.center(new CenterEvent(this, llp.getLatitude(), llp
+					.getLongitude()));
+			zoom();
+		}
+	}
+
+	public void zoom() {
+		fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapBean
+				.getScale()
+				/ zoomFactor));
+	}
+
+	public void reset() {
+		LatLonPoint center = mapBean.getCenter(); //center on mapBean's current
+		// center point
+		//however, if there is an event (or events) selected, center on the
+		// first selected event
+		if (map.getEventLayer() != null
+				&& map.getEventLayer().getSelectedEvents().length > 0) {
+			EventAccessOperations event = map.getEventLayer()
+					.getSelectedEvents()[0];
+			Origin orig = CacheEvent.extractOrigin(event);
+			center.setLatitude(0f);
+			center.setLongitude((float)orig.my_location.longitude);
+		}
+		mapBean.setCenter(center.getLatitude(), center.getLongitude());
+		fireZoomChanged(new ZoomEvent(this, ZoomEvent.ABSOLUTE, mapScale));
+		Layer[] layers = map.getLayers();
+		for (int i = 0; i < layers.length; i++) {
+			if (layers[i] instanceof ChannelChooserLayer) {
+				((ChannelChooserLayer) layers[i]).getChannelChooser()
+						.recheckNetworkAvailability();
+				break;
+			}
+		}
+	}
+
+	public void setActive(boolean active) {
+		super.setActive(active);
+	}
+
+	/**
+	 * Returns the id (MapMouseMode name). This name should be unique for each
+	 * MapMouseMode.
+	 * 
+	 * @return String ID
+	 */
+	public String getID() {
+		return id;
+	}
+
+	public String getPrettyName() {
 		return id;
 	}
 
