e50ebaf92037533fd010473a17e5edef84135d8e
groves <groves@localhost>
2004-07-19 13:47:47 -0400

    [svn r9666] always delve down inside of nested cachings to get the real event access

--- src/edu/sc/seis/fissuresUtil/cache/CacheEvent.java
+++ src/edu/sc/seis/fissuresUtil/cache/CacheEvent.java
@@ -1,4 +1,3 @@
-
 package edu.sc.seis.fissuresUtil.cache;
 
 import java.text.DateFormat;
@@ -21,37 +20,41 @@ import edu.sc.seis.fissuresUtil.display.UnitDisplayUtil;
 
 /**
  * CacheEvent.java
- *
- *
- * Created: Mon Jan  8 16:33:52 2001
- *
+ * 
+ * 
+ * Created: Mon Jan 8 16:33:52 2001
+ * 
  * @author Philip Crotwell
  * @version
  */
 
 public class CacheEvent implements EventAccessOperations {
     public CacheEvent(EventAttr attr, Origin[] origins, Origin preferred) {
-        if(attr==null) {
-            throw new IllegalArgumentException("EventAttr cannot be null");
-        }
-        if (origins== null) {
-            throw new IllegalArgumentException("origins cannot be null");
-        }
+        if (attr == null) { throw new IllegalArgumentException(
+                "EventAttr cannot be null"); }
+        if (origins == null) { throw new IllegalArgumentException(
+                "origins cannot be null"); }
         this.attr = attr;
         this.origins = origins;
         this.preferred = preferred;
     }
 
     public CacheEvent(EventAccessOperations event) {
-        if (event==null) {
-            throw new IllegalArgumentException("EventAccess cannot be null");
-        }
+        if (event == null) { throw new IllegalArgumentException(
+                "EventAccess cannot be null"); }
         this.event = event;
     }
 
-    public EventAccessOperations getEventAccess() { return event; }
+    public EventAccessOperations getEventAccess() {
+        if (event instanceof CacheEvent) {
+            logger.debug("CacheEvent nested inside of CacheEvent!  NO!  NO!");
+            return ((CacheEvent) event).getEventAccess();
+        } else {
+            return event;
+        }
+    }
 
-    public EventFactory a_factory(){
+    public EventFactory a_factory() {
         if (event != null) {
             return event.a_factory();
         } else {
@@ -121,10 +124,8 @@ public class CacheEvent implements EventAccessOperations {
         if (event != null) {
             return event.get_origin(the_origin);
         } else {
-            for (int i=0; i<origins.length; i++) {
-                if (origins[i].get_id().equals(the_origin)) {
-                    return origins[i];
-                }
+            for (int i = 0; i < origins.length; i++) {
+                if (origins[i].get_id().equals(the_origin)) { return origins[i]; }
             }
         }
         throw new OriginNotFound();
@@ -141,48 +142,42 @@ public class CacheEvent implements EventAccessOperations {
         return preferred;
     }
 
-    public Locator[] get_locators(String an_origin)
-        throws OriginNotFound, NotImplemented {
-        if (event != null) {
-            return event.get_locators(an_origin);
-        }
+    public Locator[] get_locators(String an_origin) throws OriginNotFound,
+            NotImplemented {
+        if (event != null) { return event.get_locators(an_origin); }
         throw new org.omg.CORBA.NO_IMPLEMENT();
     }
 
     public AuditElement[] get_audit_trail_for_origin(String the_origin)
-        throws OriginNotFound, NotImplemented {
-        if (event != null) {
-            return event.get_audit_trail_for_origin(the_origin);
-        }
+            throws OriginNotFound, NotImplemented {
+        if (event != null) { return event.get_audit_trail_for_origin(the_origin); }
         throw new NotImplemented();
     }
 
-    public boolean equals(Object o){
-        if(o == this){ return true; }
-        else if (getEventAccess() != null &&
-                 o instanceof CacheEvent &&
-                     ((CacheEvent)o).getEventAccess() != null &&
-                     getEventAccess().equals(((CacheEvent)o).getEventAccess())) {
+    public boolean equals(Object o) {
+        if (o == this) {
             return true;
-        }else if(o instanceof EventAccessOperations){
-            EventAccessOperations oEvent = (EventAccessOperations)o;
-            if(get_attributes().equals(oEvent.get_attributes())){
+        } else if (getEventAccess() != null && o instanceof CacheEvent
+                && ((CacheEvent) o).getEventAccess() != null
+                && getEventAccess().equals(((CacheEvent) o).getEventAccess())) {
+            return true;
+        } else if (o instanceof EventAccessOperations) {
+            EventAccessOperations oEvent = (EventAccessOperations) o;
+            if (get_attributes().equals(oEvent.get_attributes())) {
                 Origin thisOrigin = getOrigin();
-                if(thisOrigin == null && thisOrigin == extractOrigin(oEvent)){
+                if (thisOrigin == null && thisOrigin == extractOrigin(oEvent)) {
                     return true;
-                }else if(thisOrigin.equals(extractOrigin(oEvent))){
-                    return true;
-                }
+                } else if (thisOrigin.equals(extractOrigin(oEvent))) { return true; }
             }
         }
         return false;
     }
 
-    public int hashCode(){
-        if(!hashSet){
+    public int hashCode() {
+        if (!hashSet) {
             int result = 52;
-            result = 48*result + getOrigin().hashCode();
-            result = 48*result + get_attributes().hashCode();
+            result = 48 * result + getOrigin().hashCode();
+            result = 48 * result + get_attributes().hashCode();
             hashValue = result;
             hashSet = true;
         }
@@ -190,62 +185,65 @@ public class CacheEvent implements EventAccessOperations {
     }
 
     private boolean hashSet = false;
+
     private int hashValue;
 
     /**
-     * @return  true if both the attributes and the preferred origin are cached
+     * @return true if both the attributes and the preferred origin are cached
      */
-    public boolean isLoaded(){
+    public boolean isLoaded() {
         return attr != null && preferred != null;
     }
 
-    public Origin getOrigin(){ return extractOrigin(this); }
+    public Origin getOrigin() {
+        return extractOrigin(this);
+    }
 
     /**
-     *  This gets around the NoPreferredOrigin exception
+     * This gets around the NoPreferredOrigin exception
      */
-    public static Origin extractOrigin(EventAccessOperations ev){
-        try{
+    public static Origin extractOrigin(EventAccessOperations ev) {
+        try {
             return ev.get_preferred_origin();
-        }
-        catch(NoPreferredOrigin e){
+        } catch (NoPreferredOrigin e) {
             Origin[] oArray = ev.get_origins();
-            if (oArray.length> 0) {
+            if (oArray.length > 0) {
                 return oArray[0];
-            }
-            else {
+            } else {
                 throw new RuntimeException("No preferred origin", e);
             }
         }
     }
 
-    public String toString(){ return getEventInfo(this); }
+    public String toString() {
+        return getEventInfo(this);
+    }
 
     /**
-     *@ returns a string for the form "Event: Location | Time | Magnitude | Depth"
+     * @ returns a string for the form "Event: Location | Time | Magnitude |
+     *   Depth"
      */
-    public static String getEventInfo(EventAccessOperations event){
+    public static String getEventInfo(EventAccessOperations event) {
         return getEventInfo(event, NO_ARG_STRING);
     }
 
     public static String getEventInfo(EventAccessOperations event, String format) {
-        return getEventInfo(event, format, new SimpleDateFormat("MM/dd/yyyy HH:mm:sss z"));
+        return getEventInfo(event, format, new SimpleDateFormat(
+                "MM/dd/yyyy HH:mm:sss z"));
     }
 
     /**
-     *@ formats a string for the given event.  To insert information about a
-     * certain item magic strings are used in the format string
-     * Magic Strings
-     * LOC adds the location of the event
-     * TIME adds the event time
-     * MAG adds event magnitude
-     * DEPTH adds the depth
-     *
-     *For example the string
-     *"Event: " + LOC + " | " + TIME + " | Mag: " + MAG + " | Depth: " + DEPTH + " " + DEPTH_UNIT
-     *produces the same thing as the no format call to getEventInfo
+     * @ formats a string for the given event. To insert information about a
+     *   certain item magic strings are used in the format string Magic Strings
+     *   LOC adds the location of the event TIME adds the event time MAG adds
+     *   event magnitude DEPTH adds the depth
+     * 
+     * For example the string "Event: " + LOC + " | " + TIME + " | Mag: " + MAG + " |
+     * Depth: " + DEPTH + " " + DEPTH_UNIT produces the same thing as the no
+     * format call to getEventInfo
      */
-    public static String getEventInfo(EventAccessOperations event, String format, DateFormat sdf){
+    public static String getEventInfo(EventAccessOperations event,
+            String format, DateFormat sdf) {
         //Get geographic name of origin
         ParseRegions regions = ParseRegions.getInstance();
         String location = regions.getGeographicRegionName(event.get_attributes().region.number);
@@ -258,7 +256,7 @@ public class CacheEvent implements EventAccessOperations {
 
         //Get Magnitude
         float mag = Float.NaN;
-        if (origin.magnitudes.length > 0){
+        if (origin.magnitudes.length > 0) {
             mag = origin.magnitudes[0].value;
         }
 
@@ -271,26 +269,27 @@ public class CacheEvent implements EventAccessOperations {
         StringBuffer buf = new StringBuffer(format);
         for (int i = 0; i < magicStrings.length; i++) {
             int index = buf.indexOf(magicStrings[i]);
-            if(index != -1){
+            if (index != -1) {
                 buf.delete(index, index + magicStrings[i].length());
-                if(magicStrings[i].equals(LOC)){
+                if (magicStrings[i].equals(LOC)) {
                     buf.insert(index, location);
-                }else if(magicStrings[i].equals(TIME)){
+                } else if (magicStrings[i].equals(TIME)) {
                     buf.insert(index, originTimeString);
-                }else if(magicStrings[i].equals(MAG)){
-                    if (Float.isNaN(mag)){
+                } else if (magicStrings[i].equals(MAG)) {
+                    if (Float.isNaN(mag)) {
                         buf.insert(index, "...");
-                    }
-                    else {
+                    } else {
                         buf.insert(index, mag);
                     }
-                }else if(magicStrings[i].equals(DEPTH)){
+                } else if (magicStrings[i].equals(DEPTH)) {
                     buf.insert(index, depthFormatter.format(depth.value));
-                }else if(magicStrings[i].equals(DEPTH_UNIT)){
-                    buf.insert(index, UnitDisplayUtil.getNameForUnit((UnitImpl)depth.the_units));
-                }else if(magicStrings[i].equals(LAT)){
+                } else if (magicStrings[i].equals(DEPTH_UNIT)) {
+                    buf.insert(
+                            index,
+                            UnitDisplayUtil.getNameForUnit((UnitImpl) depth.the_units));
+                } else if (magicStrings[i].equals(LAT)) {
                     buf.insert(index, latitude);
-                }else if(magicStrings[i].equals(LON)){
+                } else if (magicStrings[i].equals(LON)) {
                     buf.insert(index, longitude);
                 }
             }
@@ -300,15 +299,22 @@ public class CacheEvent implements EventAccessOperations {
 
     private static DecimalFormat depthFormatter = new DecimalFormat("###0.00");
 
-    public static final String LOC = "LOC", TIME = "TIME", MAG = "MAG", DEPTH = "DEPTH", DEPTH_UNIT = "DEPTH_UNIT", LAT = "LAT", LON = "LON";
+    public static final String LOC = "LOC", TIME = "TIME", MAG = "MAG",
+            DEPTH = "DEPTH", DEPTH_UNIT = "DEPTH_UNIT", LAT = "LAT",
+            LON = "LON";
 
-    private static final String[] magicStrings = { LOC, TIME, MAG, DEPTH, DEPTH_UNIT, LAT, LON};
+    private static final String[] magicStrings = { LOC, TIME, MAG, DEPTH,
+            DEPTH_UNIT, LAT, LON };
 
-    private static final String NO_ARG_STRING = "Event: " + LOC + " | " + TIME + " | Mag: " + MAG + " | Depth " + DEPTH + " " + DEPTH_UNIT;
+    private static final String NO_ARG_STRING = "Event: " + LOC + " | " + TIME
+            + " | Mag: " + MAG + " | Depth " + DEPTH + " " + DEPTH_UNIT;
 
     protected EventAccessOperations event;
+
     protected EventAttr attr;
+
     protected Origin[] origins;
+
     protected Origin preferred;
 
     private static final Logger logger = Logger.getLogger(CacheEvent.class);
