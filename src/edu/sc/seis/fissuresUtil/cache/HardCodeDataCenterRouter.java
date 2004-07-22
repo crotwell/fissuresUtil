a5b7b985786ae50b3e0a9507d350db1614f6e670
groves <groves@localhost>
2004-07-22 17:20:38 -0400

    [svn r9750] removed bogus javadoc

--- src/edu/sc/seis/fissuresUtil/cache/HardCodeDataCenterRouter.java
+++ src/edu/sc/seis/fissuresUtil/cache/HardCodeDataCenterRouter.java
@@ -6,23 +6,24 @@
 
 package edu.sc.seis.fissuresUtil.cache;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import edu.iris.Fissures.FissuresException;
+import edu.iris.Fissures.Time;
 import edu.iris.Fissures.IfSeismogramDC.DataCenterCallBack;
 import edu.iris.Fissures.IfSeismogramDC.DataCenterOperations;
 import edu.iris.Fissures.IfSeismogramDC.LocalSeismogram;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
-import edu.iris.Fissures.Time;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.network.ChannelIdUtil;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
-import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
-import java.util.LinkedList;
-import java.util.List;
-import org.apache.log4j.Logger;
 
 /**
  NOTE this is BAD code, but configures GEE to go to the SCEPP datacenter
@@ -51,45 +52,16 @@ public class HardCodeDataCenterRouter extends DataCenterRouter implements DataCe
     static final int BUD_INDEX = 1;
     static final int POND_INDEX = 2;
 
-
-    /**
-     * Method queue_seismograms
-     *
-     * @param    p                   a  RequestFilter[]
-     *
-     * @return   a String
-     *
-     * @throws   FissuresException
-     *
-     */
     public String queue_seismograms(RequestFilter[] p0) throws FissuresException {
         // TODO
         return null;
     }
 
-    /**
-     * Method retrieve_queue
-     *
-     * @param    p                   a  String
-     *
-     * @return   a LocalSeismogram[]
-     *
-     * @throws   FissuresException
-     *
-     */
     public LocalSeismogram[] retrieve_queue(String p0) throws FissuresException {
         // TODO
         return null;
     }
 
-    /**
-     * Method available_data
-     *
-     * @param    p                   a  RequestFilter[]
-     *
-     * @return   a RequestFilter[]
-     *
-     */
     public RequestFilter[] available_data(RequestFilter[] filters) {
         DataCenterRoute[] route = makeRoutes(filters);
         LinkedList allSeis = new LinkedList();
@@ -104,6 +76,7 @@ public class HardCodeDataCenterRouter extends DataCenterRouter implements DataCe
                     try {
                         ls =
                             route[i].getDataCenter().available_data(rf);
+                            
                     } catch (org.omg.CORBA.SystemException e) {
                         try {
                             ls =
@@ -142,28 +115,10 @@ public class HardCodeDataCenterRouter extends DataCenterRouter implements DataCe
         return (RequestFilter[])allSeis.toArray(new RequestFilter[0]);
     }
 
-    /**
-     * Method cancel_request
-     *
-     * @param    p                   a  String
-     *
-     * @throws   FissuresException
-     *
-     */
     public void cancel_request(String p0) throws FissuresException {
         // TODO
     }
 
-    /**
-     * Method retrieve_seismograms
-     *
-     * @param    p                   a  RequestFilter[]
-     *
-     * @return   a LocalSeismogram[]
-     *
-     * @throws   FissuresException
-     *
-     */
     public LocalSeismogram[] retrieve_seismograms(RequestFilter[] filters) throws FissuresException {
         DataCenterRoute[] route = makeRoutes(filters);
         LinkedList allSeis = new LinkedList();
@@ -210,34 +165,11 @@ public class HardCodeDataCenterRouter extends DataCenterRouter implements DataCe
         return (LocalSeismogram[])allSeis.toArray(new LocalSeismogramImpl[0]);
     }
 
-    /**
-     * Method request_status
-     *
-     * @param    p                   a  String
-     *
-     * @return   a String
-     *
-     * @throws   FissuresException
-     *
-     */
     public String request_status(String p0) throws FissuresException {
         // TODO
         return null;
     }
 
-    /**
-     * Method request_seismograms
-     *
-     * @param    p                   a  RequestFilter[]
-     * @param    p                   a  DataCenterCallBack
-     * @param    p                   a  boolean
-     * @param    p                   a  Time
-     *
-     * @return   a String
-     *
-     * @throws   FissuresException
-     *
-     */
     public String request_seismograms(RequestFilter[] p0, DataCenterCallBack p1, boolean p2, Time p3) throws FissuresException {
         // TODO
         return null;
