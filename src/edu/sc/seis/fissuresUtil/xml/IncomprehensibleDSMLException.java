d12cf35eb2ef80d19bf37bdef93f818fb5b2d2ab
Philip Crotwell <crotwell@seis.sc.edu>
2004-07-27 11:17:06 -0400

    [svn r9818] initial

---
+++ src/edu/sc/seis/fissuresUtil/xml/IncomprehensibleDSMLException.java
@@ -0,0 +1,21 @@
+/**
+ * IncomprehensibleDSMLException.java
+ *
+ * @author Philip Crotwell
+ */
+
+package edu.sc.seis.fissuresUtil.xml;
+
+public class IncomprehensibleDSMLException extends Exception {
+
+    public IncomprehensibleDSMLException(String s) {
+        super(s);
+    }
+
+    public IncomprehensibleDSMLException(String s, Throwable t) {
+        super(s,t);
+    }
+
+}
+
+
