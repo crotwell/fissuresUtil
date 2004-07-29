45fc2c100289f97261613d3679818d821ac910ae
groves <groves@localhost>
2004-07-29 15:27:05 -0400

    [svn r9888] initial

---
+++ tests/edu/sc/seis/fissuresUtil/chooser/BestChannelUtilTest.java
@@ -0,0 +1,32 @@
+/*
+ * Created on Jul 20, 2004
+ */
+package edu.sc.seis.fissuresUtil.chooser;
+
+import org.apache.log4j.BasicConfigurator;
+
+import junit.framework.TestCase;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.Station;
+import edu.sc.seis.mockFissures.IfNetwork.MockNetworkAccess;
+
+/**
+ * @author Charlie Groves
+ */
+public class BestChannelUtilTest extends TestCase {
+    public void testGetHorizontalChannels() {
+        NetworkAccess na = MockNetworkAccess.createNetworkAccess();
+        Station[] stations = na.retrieve_stations();
+        for (int i = 0; i < stations.length; i++) {
+            Station station = stations[i];
+            Channel[] channels = na.retrieve_for_station(station.get_id());
+            assertEquals(2,
+                    BestChannelUtil.getHorizontalChannels(channels, "B").length);
+        }
+    }
+    
+    static{
+        BasicConfigurator.configure();
+    }
+}
---
+++ tests/edu/sc/seis/fissuresUtil/chooser/ChannelChooserTest.java
@@ -0,0 +1,93 @@
+/*
+ * Created on Jul 19, 2004
+ */
+package edu.sc.seis.fissuresUtil.chooser;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.NetworkNotFound;
+import edu.iris.Fissures.IfNetwork.Station;
+import edu.sc.seis.fissuresUtil.cache.ProxyNetworkDC;
+import edu.sc.seis.fissuresUtil.cache.RetryNetworkDC;
+import edu.sc.seis.mockFissures.IfNetwork.MockNetworkDC;
+import edu.sc.seis.mockFissures.IfNetwork.MockNetworkId;
+
+/**
+ * @author Charlie Groves
+ */
+public class ChannelChooserTest extends TestCase {
+    public ChannelChooserTest() throws NetworkNotFound {
+        BasicConfigurator.configure();
+        MockNetworkDC mockDC = new MockNetworkDC();
+        ProxyNetworkDC[] dcs = new ProxyNetworkDC[] { new RetryNetworkDC(
+                mockDC, 3) };
+        cc = new ChannelChooser(dcs);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {}
+        NetworkAccess threeCompNet = mockDC.a_finder().retrieve_by_id(MockNetworkId.createNetworkID());
+        threeCompStation = threeCompNet.retrieve_stations()[0];
+        NetworkAccess vertNet = mockDC.a_finder().retrieve_by_id(MockNetworkId.createOtherNetworkID());
+        vertOnlyStation = vertNet.retrieve_stations()[0];
+    }
+
+    public void testSingleStationVerticalChannelSelection() {
+        cc.orientationList.setSelectedIndex(ChannelChooser.VERTICAL_ONLY);
+        cc.select(vertOnlyStation);
+        assertEquals(1, cc.getSelectedChannels().length);
+    }
+
+    public void testDualStationVerticalChannelSelection() {
+        cc.orientationList.setSelectedIndex(ChannelChooser.VERTICAL_ONLY);
+        cc.select(threeCompStation);
+        cc.select(vertOnlyStation);
+        assertEquals(2, cc.getSelectedChannels().length);
+    }
+
+    public void testHorizontalChannelSelectionWithData() {
+        cc.orientationList.setSelectedIndex(ChannelChooser.HORIZONTAL_ONLY);
+        cc.select(threeCompStation);
+        assertEquals(2, cc.getSelectedChannels().length);
+    }
+
+    public void testHorizontalChannelSelectionWithNoData() {
+        cc.orientationList.setSelectedIndex(ChannelChooser.HORIZONTAL_ONLY);
+        cc.select(vertOnlyStation);
+        assertEquals(0, cc.getSelectedChannels().length);
+    }
+
+    public void testThreeComponentSelection() {
+        cc.select(threeCompStation);
+        cc.orientationList.setSelectedIndex(ChannelChooser.THREE_COMPONENT);
+        assertEquals(3, cc.getSelectedChannels().length);
+    }
+
+    public void testThreeComponentSelectionWithNoData() {
+        cc.select(vertOnlyStation);
+        cc.orientationList.setSelectedIndex(ChannelChooser.THREE_COMPONENT);
+        assertEquals(0, cc.getSelectedChannels().length);
+    }
+
+    public void testBestSelection() {
+        cc.select(vertOnlyStation);
+        cc.orientationList.setSelectedIndex(ChannelChooser.BEST_CHANNELS);
+        assertEquals(1, cc.getSelectedChannels().length);
+    }
+    
+    
+    public void tearDown() {
+        cc.deselect(threeCompStation);
+        cc.deselect(vertOnlyStation);
+    }
+
+    private ChannelChooser cc;
+    
+    private Station threeCompStation;
+    private Station vertOnlyStation;
+
+    private static Logger logger = Logger.getLogger(ChannelChooserTest.class);
+}
