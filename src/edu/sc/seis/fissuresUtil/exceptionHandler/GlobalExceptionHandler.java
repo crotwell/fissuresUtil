4e7b8aff90e5794f1fe813e6266d387ad65e3637
groves <groves@localhost>
2004-08-03 15:42:54 -0400

    [svn r10013] catch exception in paranoid method from logger in case of disk being full

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/GlobalExceptionHandler.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 /**
  * GlobalExceptionHandler.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.exceptionHandler;
 
 import java.util.*;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,47 +14,56 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 
 public class GlobalExceptionHandler {
+
     public static void handle(Throwable thrown) {
         handle("Houston, we have a problem...", thrown);
     }
 
-    public static int getNumHandled(){ return numHandled; }
+    public static int getNumHandled() {
+        return numHandled;
+    }
 
     public static void handle(String message, Throwable thrown) {
         try {
-            if(reporters.size() == 0){
+            if(reporters.size() == 0) {
                 System.err.println(message);
                 thrown.printStackTrace(System.err);
-                logger.error("handle exception, but there are no Reporters.", thrown);
-            }else{
+                logger.error("handle exception, but there are no Reporters.",
+                             thrown);
+            } else {
                 List parsedContents = new ArrayList(sectionToContents.size());
                 Iterator it = sectionToContents.keySet().iterator();
-                while(it.hasNext()){
+                while(it.hasNext()) {
                     String name = (String)it.next();
-                    parsedContents.add(new Section(name, parse(sectionToContents.get(name))));
+                    parsedContents.add(new Section(name,
+                                                   parse(sectionToContents.get(name))));
                 }
-                if(showSysInfo){
-                    parsedContents.add(new Section("System Information", ExceptionReporterUtils.getSysInfo()));
+                if(showSysInfo) {
+                    parsedContents.add(new Section("System Information",
+                                                   ExceptionReporterUtils.getSysInfo()));
                 }
                 List reporterExceptions = new ArrayList();
-                synchronized(reporters){
+                synchronized(reporters) {
                     numHandled++;
                     it = reporters.iterator();
-                    while(it.hasNext()){
+                    while(it.hasNext()) {
                         try {
-                            ((ExceptionReporter)it.next()).report(message, thrown, parsedContents);
-                        } catch (Throwable e) {
+                            ((ExceptionReporter)it.next()).report(message,
+                                                                  thrown,
+                                                                  parsedContents);
+                        } catch(Throwable e) {
                             it.remove();
                             reporterExceptions.add(e);
                         }
                     }
                 }
                 it = reporterExceptions.iterator();
-                while(it.hasNext()){
-                    handle("An exception reporter caused this exception.  It has been removed from the GlobalExceptionHandler", (Throwable)it.next());
+                while(it.hasNext()) {
+                    handle("An exception reporter caused this exception.  It has been removed from the GlobalExceptionHandler",
+                           (Throwable)it.next());
                 }
             }
-        } catch (Throwable e) {
+        } catch(Throwable e) {
             paranoid(e, thrown);
         }
     }
@@ -69,55 +76,51 @@ public class GlobalExceptionHandler {
         return extractors;
     }
 
-    public static void add(ExceptionReporter reporter){
+    public static void add(ExceptionReporter reporter) {
         reporters.add(reporter);
     }
 
-    public static void add(String sectionName, File file){
+    public static void add(String sectionName, File file) {
         sectionToContents.put(sectionName, file);
     }
 
-    public static void append(String sectionName, String contents){
+    public static void append(String sectionName, String contents) {
         List contentsList = null;
-        if(sectionToContents.containsKey(sectionName)){
+        if(sectionToContents.containsKey(sectionName)) {
             contentsList = (List)sectionToContents.get(sectionName);
-        }else{
+        } else {
             contentsList = new LinkedList();
             sectionToContents.put(sectionName, contentsList);
         }
-        if(contentsList.size() > 1000){
+        if(contentsList.size() > 1000) {
             contentsList.remove(0);
         }
         contentsList.add(contents);
     }
 
-    /** This supposedly sets a global exception handler in the awt thread only,
-     * so that uncaught exceptions
-     * can be processed/saved/viewed. Will not necessarily work for future
-     * releases (> 1.4). Perhaps it will, perhaps not.
-     * NOTE: It does not work for exceptions in other threads. Java1.5 is supposed
-     * to have a mechanism to do this.
+    /**
+     * This supposedly sets a global exception handler in the awt thread only,
+     * so that uncaught exceptions can be processed/saved/viewed. Will not
+     * necessarily work for future releases (> 1.4). Perhaps it will, perhaps
+     * not. NOTE: It does not work for exceptions in other threads. Java1.5 is
+     * supposed to have a mechanism to do this.
      */
     public static void registerWithAWTThread() {
         System.setProperty("sun.awt.exception.handler",
                            "edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler");
-
     }
 
     private static String parse(Object item) throws IOException {
-        if(item instanceof List)
-            return createString((List)item);
-        else if(item instanceof File)
-            return createString((File)item);
-        else
-            throw new IllegalArgumentException();
+        if(item instanceof List) return createString((List)item);
+        else if(item instanceof File) return createString((File)item);
+        else throw new IllegalArgumentException();
     }
 
     private static String createString(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         StringBuffer message = new StringBuffer();
         String line = reader.readLine();
-        while(line != null){
+        while(line != null) {
             message.append(line + "\n");
             line = reader.readLine();
         }
@@ -127,7 +130,7 @@ public class GlobalExceptionHandler {
     private static String createString(List stringList) {
         StringBuffer message = new StringBuffer();
         Iterator it = stringList.iterator();
-        while(it.hasNext()){
+        while(it.hasNext()) {
             message.append((String)it.next() + "\n");
         }
         return message.toString();
@@ -135,13 +138,17 @@ public class GlobalExceptionHandler {
 
     private static void paranoid(Throwable e, Throwable thrown) {
         // this is for paranoid coders
-        System.err.println("Caught an exception in the exception handler: "+e.toString());
+        System.err.println("Caught an exception in the exception handler: "
+                + e.toString());
         e.printStackTrace(System.err);
-        System.err.println("Original exception was:" +thrown.toString());
+        System.err.println("Original exception was:" + thrown.toString());
         thrown.printStackTrace(System.err);
-
-        logger.error("Caught an exception in the exception handler: ",e);
-        logger.error("Original exception was:" ,thrown);
+        try {
+            logger.error("Caught an exception in the exception handler: ", e);
+            logger.error("Original exception was:", thrown);
+        } catch(Throwable loggerException) {
+            //well, lets hope System.err is good enough.  
+        }
     }
 
     private static Map sectionToContents = new HashMap();
@@ -155,11 +162,9 @@ public class GlobalExceptionHandler {
     private static boolean showSysInfo = true;
 
     private static int numHandled = 0;
-
     static {
         // always send error to log4j
         add(new Log4jReporter());
         add(new DefaultExtractor());
     }
 }
-
