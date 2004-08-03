d6c4d8046f5338b5a7b554ed2164f8202504687a
Charles Groves <groves@seis.sc.edu>
2004-08-03 15:42:01 -0400

    [svn r10012] added thread group for all threads in a pool, restart threads on invokeLater if some have died

--- src/edu/sc/seis/fissuresUtil/cache/WorkerThreadPool.java
+++ src/edu/sc/seis/fissuresUtil/cache/WorkerThreadPool.java
@@ -1,9 +1,8 @@
 /**
  * WorkerThread.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.cache;
 
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
@@ -13,65 +12,92 @@ import java.util.LinkedList;
 import java.util.Set;
 import org.apache.log4j.Logger;
 
-public class WorkerThreadPool{
-    public WorkerThreadPool(String name, int numThreads){
+public class WorkerThreadPool {
+
+    public WorkerThreadPool(String name, int numThreads) {
         this(name, numThreads, Thread.NORM_PRIORITY);
     }
 
     public WorkerThreadPool(String name, int numThreads, int priority) {
-        this.numThreads = numThreads;
-        for (int i = 0; i < numThreads; i++) {
-            BackgroundWorker bw = new BackgroundWorker(name + i, priority);
+        this.poolSize = numThreads;
+        tg = new ThreadGroup(name);
+        tg.setMaxPriority(priority);
+        fillPool();
+    }
+
+    public synchronized void invokeLater(Runnable runnable) {
+        if(!queue.contains(runnable)) {
+            queue.addFirst(runnable);
+        }
+        fillPool();
+        notifyAll();
+    }
+
+    private void fillPool() {
+        int numNewThreads = poolSize - tg.activeCount();
+        for(int i = 0; i < numNewThreads; i++) {
+            BackgroundWorker bw = new BackgroundWorker(tg, ""
+                    + totalNumCreated++, tg.getMaxPriority());
+            logger.debug("Adding thread " + bw + " to a pool");
             idle.add(bw);
             bw.start();
         }
     }
 
-    public synchronized void invokeLater(Runnable runnable) {
-        if(!queue.contains(runnable)){ queue.addFirst(runnable); }
-        notifyAll();
-    }
-
     public static WorkerThreadPool getDefaultPool() {
-        if (defaultPool == null) {
+        if(defaultPool == null) {
             defaultPool = new WorkerThreadPool("default workers", 2);
         }
         return defaultPool;
     }
 
     private synchronized Runnable getFromQueue() throws InterruptedException {
-        while (queue.isEmpty()) { wait(); }
+        while(queue.isEmpty()) {
+            wait();
+        }
         idle.remove(Thread.currentThread());
         return (Runnable)queue.removeLast();
     }
 
-    public synchronized int getNumWaiting(){ return queue.size(); }
-
-    public int getNumIdle(){ return idle.size(); }
-
-    public synchronized boolean isEmployed(){
-        return getNumWaiting() != 0 || getNumIdle() < numThreads;
+    public synchronized int getNumWaiting() {
+        return queue.size();
     }
 
-    private int numThreads;
+    public int getNumIdle() {
+        return idle.size();
+    }
+
+    public synchronized boolean isEmployed() {
+        return getNumWaiting() != 0 || getNumIdle() < poolSize;
+    }
+
+    private int totalNumCreated = 0;
+
+    private int poolSize;
+
+    private ThreadGroup tg;
+
     private LinkedList queue = new LinkedList();
+
     private Set idle = Collections.synchronizedSet(new HashSet());
+
     private static WorkerThreadPool defaultPool;
 
     private class BackgroundWorker extends Thread {
-        protected BackgroundWorker(String name, int priority) {
-            super(name);
+
+        protected BackgroundWorker(ThreadGroup group, String name, int priority) {
+            super(group, name);
             setPriority(priority);
             setDaemon(true);
         }
 
         public void run() {
-            while (true) {
+            while(true) {
                 try {
                     getFromQueue().run();
-                } catch (Throwable e) {
+                } catch(Throwable e) {
                     GlobalExceptionHandler.handle(e);
-                }finally{
+                } finally {
                     idle.add(this);
                 }
             }
