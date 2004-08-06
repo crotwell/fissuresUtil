59a1a526d54eb54fae190695cb9a5f880eb81a69
groves <groves@localhost>
2004-08-06 12:01:04 -0400

    [svn r10073] use new microsecondtimerange jazz

--- src/edu/sc/seis/fissuresUtil/display/borders/TimeBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/TimeBorder.java
@@ -1,6 +1,6 @@
 package edu.sc.seis.fissuresUtil.display.borders;
-import java.util.*;
 
+import java.util.*;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
@@ -12,10 +12,13 @@ import java.awt.Dimension;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-public class TimeBorder extends Border implements TitleProvider{
-    public TimeBorder(SeismogramDisplay disp){ this(disp, TOP); }
+public class TimeBorder extends Border implements TitleProvider {
 
-    public TimeBorder(SeismogramDisplay disp, int position){
+    public TimeBorder(SeismogramDisplay disp) {
+        this(disp, TOP);
+    }
+
+    public TimeBorder(SeismogramDisplay disp, int position) {
         super(position, ASCENDING);
         add((TitleProvider)this);
         this.disp = disp;
@@ -25,24 +28,28 @@ public class TimeBorder extends Border implements TitleProvider{
 
     public String getTitle() {
         MicroSecondTimeRange time = disp.getTimeConfig().getTime();
-        if(roundTheEpoch.intersects(time)) return "Relative time";
-        else{
-            Date middleDate = time.getBeginTime().add(new TimeInterval(time.getInterval().divideBy(2)));
+        if(roundTheEpoch.intersects(time))
+            return "Relative time";
+        else {
+            Date middleDate = time.getBeginTime()
+                    .add(new TimeInterval(time.getInterval().divideBy(2)));
             calendar.setTime(middleDate);
             return axisFormat.format(calendar.getTime()) + " (GMT)";
         }
     }
 
-    protected UnitRangeImpl getRange(){
-        return disp.getTimeConfig().getTime().convertTo(UnitImpl.MILLISECOND);
+    protected UnitRangeImpl getRange() {
+        return disp.getTimeConfig().getTime().getMillis();
     }
 
-    protected List createFormats(){
+    protected List createFormats() {
         List borderFormats = new ArrayList();
         borderFormats.add(new TimeBorderFormat("mm:ss.SSS",
-                                               new TimeInterval(1, UnitImpl.MILLISECOND)));
+                                               new TimeInterval(1,
+                                                                UnitImpl.MILLISECOND)));
         borderFormats.add(new TimeBorderFormat("mm:ss.SSS",
-                                               new TimeInterval(10, UnitImpl.MILLISECOND)));
+                                               new TimeInterval(10,
+                                                                UnitImpl.MILLISECOND)));
         createSecondFormats(secDivs, borderFormats);
         createMinuteSecondFormats(minSecDivs, borderFormats);
         createMinuteFormats(minDivs, borderFormats);
@@ -54,69 +61,98 @@ public class TimeBorder extends Border implements TitleProvider{
     //labelled ticks and 10 ticks between each labelled tick
     private static double[] secDivs = {.01, .02, .05, .1, .25, .5, 1, 2.5};
 
-    //this is used to create foramts with the first number used to determine how
-    //many seconds between each major tick, and the second to determine how many
+    //this is used to create foramts with the first number used to determine
+    // how
+    //many seconds between each major tick, and the second to determine how
+    // many
     //ticks for each major tick
-    private static double[][] minSecDivs = { {5, 5}, {10, 5}, {20, 4}, {30, 6} };
+    private static double[][] minSecDivs = { {5, 5}, {10, 5}, {20, 4}, {30, 6}};
 
-    //this is used to create foramts with the first number used to determine how
-    //many minutes between each major tick, and the second to determine how many
+    //this is used to create foramts with the first number used to determine
+    // how
+    //many minutes between each major tick, and the second to determine how
+    // many
     //ticks for each major tick
-    private static double[][] minDivs = { {1, 6},{2, 6},{5, 5},{10,10},{20,10},{30,10}};
+    private static double[][] minDivs = { {1, 6},
+                                         {2, 6},
+                                         {5, 5},
+                                         {10, 10},
+                                         {20, 10},
+                                         {30, 10}};
 
-    //this is used to create foramts with the first number used to determine how
+    //this is used to create foramts with the first number used to determine
+    // how
     //many hours between each major tick, and the second to determine how many
     //ticks for each major tick
-    private static double[][] hourDivs = {{1, 6},{2, 4},{6, 6},{12,6},{24,4},{48,4}, {96, 4}, };
+    private static double[][] hourDivs = { {1, 6},
+                                          {2, 4},
+                                          {6, 6},
+                                          {12, 6},
+                                          {24, 4},
+                                          {48, 4},
+                                          {96, 4},};
 
-    private void createSecondFormats(double[] secondsPerDivision, List recip){
-        for (int i = 0; i < secondsPerDivision.length; i++) {
+    private void createSecondFormats(double[] secondsPerDivision, List recip) {
+        for(int i = 0; i < secondsPerDivision.length; i++) {
             createSecondFormat(secondsPerDivision[i], recip);
         }
     }
 
-    private void createSecondFormat(double secondsPerDivision, List recip){
-        TimeInterval inter =  new TimeInterval(secondsPerDivision, UnitImpl.SECOND);
+    private void createSecondFormat(double secondsPerDivision, List recip) {
+        TimeInterval inter = new TimeInterval(secondsPerDivision,
+                                              UnitImpl.SECOND);
         recip.add(new TimeBorderFormat("mm:ss.SSS", inter));
     }
 
-    private void createMinuteSecondFormats(double[][] divs, List recip){
-        for (int i = 0; i < divs.length; i++) {
-            createHourFormat(divs[i][0], (int)divs[i][1], recip, UnitImpl.SECOND);
+    private void createMinuteSecondFormats(double[][] divs, List recip) {
+        for(int i = 0; i < divs.length; i++) {
+            createHourFormat(divs[i][0],
+                             (int)divs[i][1],
+                             recip,
+                             UnitImpl.SECOND);
         }
     }
 
-    private void createMinuteFormats(double[][] minDivs, List recip){
-        for (int i = 0; i < minDivs.length; i++) {
-            createHourFormat(minDivs[i][0], (int)minDivs[i][1], recip, UnitImpl.MINUTE);
+    private void createMinuteFormats(double[][] minDivs, List recip) {
+        for(int i = 0; i < minDivs.length; i++) {
+            createHourFormat(minDivs[i][0],
+                             (int)minDivs[i][1],
+                             recip,
+                             UnitImpl.MINUTE);
         }
     }
-    private void createHourFormat(double minutesPerDivision, int divPerLabel, List recip, UnitImpl unit){
+
+    private void createHourFormat(double minutesPerDivision,
+                                  int divPerLabel,
+                                  List recip,
+                                  UnitImpl unit) {
         TimeInterval inter = new TimeInterval(minutesPerDivision, unit);
         recip.add(new TimeBorderFormat("HH:mm:ss", inter, divPerLabel));
     }
 
-    private void createHourFormats(double[][] minDivs, List recip){
-        for (int i = 0; i < minDivs.length; i++) {
+    private void createHourFormats(double[][] minDivs, List recip) {
+        for(int i = 0; i < minDivs.length; i++) {
             createDayFormat(minDivs[i][0], (int)minDivs[i][1], recip);
         }
     }
 
-    private void createDayFormat(double hoursPerDivision, int divPerLabel, List recip){
+    private void createDayFormat(double hoursPerDivision,
+                                 int divPerLabel,
+                                 List recip) {
         TimeInterval inter = new TimeInterval(hoursPerDivision, UnitImpl.HOUR);
         recip.add(new TimeBorderFormat("MM/dd HH:mm", inter, divPerLabel));
     }
 
     private SeismogramDisplay disp;
 
-     class TimeBorderFormat extends BorderFormat{
-        public TimeBorderFormat(String format, TimeInterval labelInterval){
+    class TimeBorderFormat extends BorderFormat {
+
+        public TimeBorderFormat(String format, TimeInterval labelInterval) {
             this(format, labelInterval, 10);
         }
 
-        public TimeBorderFormat(String format,
-                                TimeInterval labelInterval,
-                                int ticksPerDivision){
+        public TimeBorderFormat(String format, TimeInterval labelInterval,
+                int ticksPerDivision) {
             super(labelInterval.convertTo(UnitImpl.MILLISECOND).getValue(),
                   ticksPerDivision);
             this.format = new SimpleDateFormat(format);
@@ -127,20 +163,25 @@ public class TimeBorder extends Border implements TitleProvider{
             return format.format(new Date((long)value));
         }
 
-        public String getMaxString() { return format.format(FULL_DATE); }
+        public String getMaxString() {
+            return format.format(FULL_DATE);
+        }
 
         private DateFormat format;
     }
 
-    private Calendar calendar  = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+    private Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+
     private SimpleDateFormat axisFormat = new SimpleDateFormat("MM/dd/yyyy");
+
     //Five days before the epoch to 10 after
     public static MicroSecondTimeRange roundTheEpoch = new MicroSecondTimeRange(new MicroSecondDate(0),
-                                                                                new TimeInterval(20, UnitImpl.DAY));
+                                                                                new TimeInterval(20,
+                                                                                                 UnitImpl.DAY));
 
     //FULL_DATE has 2 digits for hours, minutes and seconds, and 999 millis
     private static Date FULL_DATE;
-    static{
+    static {
         Calendar cal = GregorianCalendar.getInstance();
         cal.setTimeZone(TimeZone.getTimeZone("EST"));
         cal.set(1999, 12, 31, 12, 59, 59);
