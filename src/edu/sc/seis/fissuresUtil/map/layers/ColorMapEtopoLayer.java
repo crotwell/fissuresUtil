5192936eb9f5832f4d801b394ed4675035f1ab71
Charles Groves <groves@seis.sc.edu>
2004-08-18 12:59:30 -0400

    [svn r10187] added ability to reread color table, ignore slope for now

--- src/edu/sc/seis/fissuresUtil/map/layers/ColorMapEtopoLayer.java
+++ src/edu/sc/seis/fissuresUtil/map/layers/ColorMapEtopoLayer.java
@@ -20,7 +20,17 @@ public class ColorMapEtopoLayer extends ETOPOJarLayer {
     public ColorMapEtopoLayer(String colorMapFilename)
             throws FileNotFoundException, IOException {
         super();
-        readInColorTable(colorMapFilename);
+        setColorTable(colorMapFilename);
+    }
+
+    public void setColorTable(String colorMapFilename)
+            throws FileNotFoundException, IOException {
+        int[][] colorTable = readInColorTable(colorMapFilename);
+        elevationLimit = colorTable[0];
+        redValues = colorTable[1];
+        greenValues = colorTable[2];
+        blueValues = colorTable[3];
+        slopeColors = null;
     }
 
     protected Color getColor(short elevation, byte slopeVal) {
@@ -39,8 +49,10 @@ public class ColorMapEtopoLayer extends ETOPOJarLayer {
                 // positive slope
                 for(int j = 4; j < 8; j++) {
                     // set
-                    if(j == 4) slopeColors[i][j] = base;
-                    else slopeColors[i][j] = slopeColors[i][j - 1].brighter();
+                    if(j == 4)
+                        slopeColors[i][j] = base;
+                    else
+                        slopeColors[i][j] = slopeColors[i][j - 1].brighter();
                 }
                 // call the "darker" method on the base color for negative
                 // slopes
@@ -55,19 +67,19 @@ public class ColorMapEtopoLayer extends ETOPOJarLayer {
         // compute slope idx
         int slopeIdx = ((int)slopeVal + 127) >> 5;
         // return color
-        return slopeColors[elIdx][slopeIdx];
+        return slopeColors[elIdx][4];
     }
     
     /* returns the color lookup index based on elevation */
     protected int getElevIndex(short el) {
-        for (int i=0;i<elevationLimit.length-1;i++)
-            if (el<elevationLimit[i+1])
+        for(int i = 0; i < elevationLimit.length - 1; i++)
+            if(el < elevationLimit[i + 1])
                 return i;
-        return elevationLimit.length-1;
+        return elevationLimit.length - 1;
     }
 
-    private void readInColorTable(String filename) throws IOException,
-            FileNotFoundException {
+    private static int[][] readInColorTable(String filename)
+            throws IOException, FileNotFoundException {
         //read lines of file into a list
         File cMapFile = new File(filename);
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(cMapFile)));
@@ -77,27 +89,24 @@ public class ColorMapEtopoLayer extends ETOPOJarLayer {
             colorRangeList.add(line);
             System.out.println(line);
         }
+        reader.close();
         int numValues = colorRangeList.size() + 1;
         //initialize arrays
-        elevationLimit = new int[numValues];
-        redValues = new int[numValues];
-        greenValues = new int[numValues];
-        blueValues = new int[numValues];
+        int[] elevationLimit = new int[numValues];
+        int[] redValues = new int[numValues];
+        int[] greenValues = new int[numValues];
+        int[] blueValues = new int[numValues];
         StringTokenizer tok = null;
         for(int i = 0; i <= colorRangeList.size(); i++) {
             if(i < colorRangeList.size()) {
                 tok = new StringTokenizer((String)colorRangeList.get(i));
             }
-            System.out.println(i + " " + tok);
-            getValuesFromTokenizer(tok, i);
+            elevationLimit[i] = Integer.parseInt(tok.nextToken());
+            redValues[i] = Integer.parseInt(tok.nextToken());
+            greenValues[i] = Integer.parseInt(tok.nextToken());
+            blueValues[i] = Integer.parseInt(tok.nextToken());
         }
-    }
-
-    private void getValuesFromTokenizer(StringTokenizer tok, int i) {
-        elevationLimit[i] = Integer.parseInt(tok.nextToken());
-        redValues[i] = Integer.parseInt(tok.nextToken());
-        greenValues[i] = Integer.parseInt(tok.nextToken());
-        blueValues[i] = Integer.parseInt(tok.nextToken());
+        return new int[][] {elevationLimit, redValues, greenValues, blueValues};
     }
 
     private int[] elevationLimit;
