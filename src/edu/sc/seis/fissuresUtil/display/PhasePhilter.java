559007718affb71fff63da3dcdb310cdd89613d7
Philip Oliver-Paull <oliverpa@seis.sc.edu>
2004-08-25 15:25:45 -0400

    [svn r10236] initial

---
+++ src/edu/sc/seis/fissuresUtil/display/PhasePhilter.java
@@ -0,0 +1,34 @@
+package edu.sc.seis.fissuresUtil.display;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.sc.seis.TauP.Arrival;
+
+
+/**
+ * @author oliverpa
+ * Created on Aug 25, 2004
+ */
+public class PhasePhilter {
+    
+    public static Arrival[] filter(Arrival[] arrivals, TimeInterval offset) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < arrivals.length; i++) {
+            list.add(arrivals[i]);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            ListIterator it = list.listIterator(i);
+            Arrival a = (Arrival)it.next(); // get first arrival for comparison
+            while (it.hasNext()) {
+                Arrival b = (Arrival)it.next();
+                if (b.getName().equals(a.getName()) &&
+                    Math.abs(b.getTime() - a.getTime()) < offset.value) {
+                    it.remove();
+                }
+            }
+        }
+        return (Arrival[])list.toArray(new Arrival[list.size()]);
+    }
+    
+}
