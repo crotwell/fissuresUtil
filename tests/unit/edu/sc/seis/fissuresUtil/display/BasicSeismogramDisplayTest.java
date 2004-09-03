7689bdeedfe1591d50484aea400e7f353c4c50ba
Charles Groves <groves@seis.sc.edu>
2004-09-03 14:14:39 -0400

    [svn r10347] dont catch IOExceptions

--- tests/unit/edu/sc/seis/fissuresUtil/display/BasicSeismogramDisplayTest.java
+++ tests/unit/edu/sc/seis/fissuresUtil/display/BasicSeismogramDisplayTest.java
@@ -1,9 +1,8 @@
 /**
  * BasicSeismogramDisplayTest.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.display;
 
 import java.io.File;
@@ -16,35 +15,30 @@ import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.xml.MemoryDataSetSeismogram;
 
 public class BasicSeismogramDisplayTest extends TestCase {
-    public BasicSeismogramDisplayTest(String name){
+
+    public BasicSeismogramDisplayTest(String name) {
         super(name);
         BasicConfigurator.configure(new NullAppender());
     }
-    public void testOutputToPNG(){
-        try{
-            File outPNG = new File("./testOutput.png");
-            outPNG.deleteOnExit();
-            sd.outputToPNG(outPNG);
-        }
-        catch(IOException e){
-            System.out.println("something unforseen has happened");
-            e.printStackTrace();
-        }
+
+    public void testOutputToPNG() throws IOException {
+        File outPNG = new File("./testOutput.png");
+        outPNG.deleteOnExit();
+        sd.outputToPNG(outPNG);
     }
 
-    public void testOutputToPDF() throws FileNotFoundException{
+    public void testOutputToPDF() throws FileNotFoundException {
         File outPDF = new File("./testOutput.pdf");
         outPDF.deleteOnExit();
         SeismogramPDFBuilder.createPDF(sd, outPDF, 1);
     }
 
-    public void setUp(){
+    public void setUp() {
         LocalSeismogramImpl lsi = SimplePlotUtil.createTestData();
         MemoryDataSetSeismogram memDSS = new MemoryDataSetSeismogram(lsi);
         sd = new BasicSeismogramDisplay();
-        sd.add(new MemoryDataSetSeismogram[]{memDSS});
+        sd.add(new MemoryDataSetSeismogram[] {memDSS});
     }
 
     private SeismogramDisplay sd;
 }
-
