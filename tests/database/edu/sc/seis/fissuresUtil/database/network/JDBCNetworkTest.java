c9fe444b96704e57e7d9339d4401d3a89a341a9a
crotwell <crotwell@localhost>
2004-09-15 14:38:48 -0400

    [svn r10542] add testGetNetworkId

--- tests/database/edu/sc/seis/fissuresUtil/database/network/JDBCNetworkTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/network/JDBCNetworkTest.java
@@ -9,6 +9,7 @@ package edu.sc.seis.fissuresUtil.database.network;
 import java.sql.SQLException;
 import junit.framework.TestCase;
 import edu.iris.Fissures.IfNetwork.NetworkAttr;
+import edu.iris.Fissures.IfNetwork.NetworkId;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockNetworkAttr;
 
@@ -22,6 +23,15 @@ public class JDBCNetworkTest extends TestCase{
         int gottenId = net.getDBId(attr.get_id());
         assertEquals(dbidA, dbidB);
         assertEquals(dbidB, gottenId);
+    }
+    
+    public void testGetNetworkId() throws SQLException, NotFound{
+        JDBCNetwork net = new JDBCNetwork();
+        NetworkAttr attr = MockNetworkAttr.createNetworkAttr();
+        int dbid = net.put(attr.get_id());
+        NetworkId netId = net.getNetworkId(dbid);
+        assertEquals(attr.get_id().network_code, netId.network_code);
+        assertEquals(attr.get_id().begin_time.date_time, netId.begin_time.date_time);
     }
 }
 
