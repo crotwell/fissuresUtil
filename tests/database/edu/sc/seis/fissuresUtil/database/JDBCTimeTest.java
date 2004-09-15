9ba29515c725f592f26991058bdd0532a3356114
Philip Crotwell <crotwell@seis.sc.edu>
2004-09-15 14:48:56 -0400

    [svn r10544] initial

---
+++ tests/database/edu/sc/seis/fissuresUtil/database/JDBCTimeTest.java
@@ -0,0 +1,32 @@
+package edu.sc.seis.fissuresUtil.database;
+
+import java.sql.SQLException;
+import edu.iris.Fissures.model.TimeUtils;
+import edu.iris.Fissures.model.UnitImpl;
+import junit.framework.TestCase;
+
+
+/**
+ * @author crotwell
+ * Created on Sep 15, 2004
+ */
+public class JDBCTimeTest extends TestCase {
+    
+    public JDBCTimeTest(String testname) { super(testname);  }
+    
+    public void setUp() throws SQLException{
+        table = new JDBCTime(ConnMgr.createConnection());
+    }
+    
+    public void testPutAndGet() throws SQLException, NotFound{
+        int id = table.put(TimeUtils.future.getFissuresTime());
+        assertEquals(TimeUtils.future.getFissuresTime().date_time, table.get(id).date_time);
+    }
+    
+    public void testDoublePut() throws NotFound, SQLException {
+        assertEquals(table.put(TimeUtils.future.getFissuresTime()),
+                     table.put(TimeUtils.future.getFissuresTime()));
+    }
+    
+    private JDBCTime table;
+}
