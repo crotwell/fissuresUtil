106f5ee71a34e834dbd5100526e9cec35663b187
Philip Crotwell <crotwell@seis.sc.edu>
2004-10-21 23:04:43 -0400

    [svn r10960] doesnt compile, lots of code stink removed, almost ready to actually look at the programming!

--- src/edu/sc/seis/fissuresUtil/anhinga/event/EventDCImpl.java
+++ src/edu/sc/seis/fissuresUtil/anhinga/event/EventDCImpl.java
@@ -1,57 +1,22 @@
-
-
-package edu.sc.seis.anhinga.event;
-
-
+package edu.sc.seis.fissuresUtil.anhinga.event;
 
 import edu.iris.Fissures.IfEvent.EventChannelFinder;
 import edu.iris.Fissures.IfEvent.EventDCPOA;
 import edu.iris.Fissures.IfEvent.EventFinder;
 
-/**
- * EventDCImpl.java
- *
- *
- * Created: Mon Jun 11 14:23:39 2001
- *
- * @author Srinivasa Telukutla
- * @version
- */
-
 public class EventDCImpl extends EventDCPOA {
 
-     //
-    // IDL:iris.edu/Fissures/IfEvent/EventDC/a_finder:1.0
-    //
-
-
-    public EventFinder
-    a_finder() {
-
-    return eventFinder;
-    //throw new org.omg.CORBA.NO_IMPLEMENT();
-
+    public EventFinder a_finder() {
+        return eventFinder;
     }
 
-    //
-    // IDL:iris.edu/Fissures/IfEvent/EventDC/a_channel_finder:1.0
-    //
-
-
-    public EventChannelFinder
-    a_channel_finder() {
-
+    public EventChannelFinder a_channel_finder() {
         throw new org.omg.CORBA.NO_IMPLEMENT();
     }
 
-
-     public void setEventFinder(EventFinder eventFinder) {
-
-    this.eventFinder = eventFinder;
+    public void setEventFinder(EventFinder eventFinder) {
+        this.eventFinder = eventFinder;
     }
 
     private EventFinder eventFinder = null;
-
-
-
 } // EventDCImpl
--- src/edu/sc/seis/fissuresUtil/anhinga/event/EventFactoryImpl.java
+++ src/edu/sc/seis/fissuresUtil/anhinga/event/EventFactoryImpl.java
@@ -1,4 +1,4 @@
-package edu.sc.seis.anhinga.event;
+package edu.sc.seis.fissuresUtil.anhinga.event;
 
 import java.sql.SQLException;
 import org.apache.log4j.Logger;
@@ -9,110 +9,77 @@ import edu.iris.Fissures.IfEvent.EventFactory;
 import edu.iris.Fissures.IfEvent.EventFactoryPOA;
 import edu.iris.Fissures.IfEvent.EventFinder;
 import edu.iris.Fissures.IfEvent.EventHelper;
-import edu.sc.seis.anhinga.database.JDBCEventAttr;
-import edu.sc.seis.anhinga.database.JDBCFlinnEngdahl;
-import edu.sc.seis.anhinga.database.JDBCParameterRef;
+import edu.sc.seis.fissuresUtil.database.event.JDBCEventAccess;
 
 /**
- * EventFactoryImpl.java
- *
- *
- * Created: Mon Oct  1 13:12:13 2001
- *
- * @author <a href="mailto: "Srinivasa Telukutla</a>
+ * EventFactoryImpl.java Created: Mon Oct 1 13:12:13 2001
+ * 
+ * @author <a href="mailto: "Srinivasa Telukutla </a>
  * @version
  */
+public class EventFactoryImpl extends EventFactoryPOA {
 
-public class EventFactoryImpl extends EventFactoryPOA{
-    public EventFactoryImpl( JDBCParameterRef jdbcParameterRef, JDBCEventAttr jdbcEventAttr,JDBCFlinnEngdahl jdbcFlinnEngdahl, org.omg.PortableServer.POA poa){
-    this.jdbcParameterRef = jdbcParameterRef;
-    this.jdbcEventAttr = jdbcEventAttr;
-    this.jdbcFlinnEngdahl = jdbcFlinnEngdahl;
-    this.poa = poa;
-
+    public EventFactoryImpl(JDBCEventAccess jdbcEventAccess,
+            org.omg.PortableServer.POA poa) {
+        this.jdbcEventAccess = jdbcEventAccess;
+        this.poa = poa;
     }
-     //
-    // IDL:iris.edu/Fissures/IfEvent/EventFactory/create:1.0
-    //
-    /***/
 
     public Event create(EventAttr attributes,
-            edu.iris.Fissures.AuditInfo[] audit_info){
-    //enImpl
-    logger.debug("In The method create");
-    Event event = null;
-    try {
-        int eventid;
-        eventid = jdbcEventAttr.put(attributes);
-        logger.debug("The event id obtained is "+eventid);
-         org.omg.CORBA.Object obj  = poa.create_reference_with_id(new Integer(eventid).toString().getBytes(), EventHelper.id());
-         logger.debug("After creating the obj reference with id");
-         event = EventHelper.narrow(obj);
-         if(event == null) logger.debug("The event to be returned is null");
-         else
-         logger.debug("IOR: "+_orb().object_to_string(event));
-
-        // return event;
-    } catch(SQLException sqle) {
-        logger.error("SQLException caught while inserting eventattr into database from EventFactoryImpl create method", sqle);
-    } catch(edu.sc.seis.anhinga.database.NotFound nfe) {
-        logger.error("NOTFOUNDException caught while inserting eventattr into database from EventFactoryImpl create method", nfe);
-    }catch(Exception e) {
-
-        logger.error(" CAUGHT THE EXCEPTION ", e);
-
-    }
-
-    return event;
-
+                        edu.iris.Fissures.AuditInfo[] audit_info) {
+        logger.debug("In The method create");
+        Event event = null;
+        try {
+            int eventid;
+            eventid = jdbcEventAccess.put(attributes);
+            logger.debug("The event id obtained is " + eventid);
+            org.omg.CORBA.Object obj = poa.create_reference_with_id(new Integer(eventid).toString()
+                                                                            .getBytes(),
+                                                                    EventHelper.id());
+            logger.debug("After creating the obj reference with id");
+            event = EventHelper.narrow(obj);
+            if(event == null) logger.debug("The event to be returned is null");
+            else logger.debug("IOR: " + _orb().object_to_string(event));
+            // return event;
+        } catch(SQLException sqle) {
+            logger.error("SQLException caught while inserting eventattr into database from EventFactoryImpl create method",
+                         sqle);
+        } catch(edu.sc.seis.anhinga.database.NotFound nfe) {
+            logger.error("NOTFOUNDException caught while inserting eventattr into database from EventFactoryImpl create method",
+                         nfe);
+        } catch(Exception e) {
+            logger.error(" CAUGHT THE EXCEPTION ", e);
+        }
+        return event;
     }
 
     public EventFactory a_factory() {
-    return eventFactory; //this._this();
-    }
-  //
-    // IDL:iris.edu/Fissures/IfEvent/EventDC/a_finder:1.0
-    //
-    /***/
-
-    public EventFinder a_finder(){
-    /*EventFinderImpl eventFinderImpl = new EventFinderImpl((JDBCEventAttr jdbcEventAttr,
-               int eventid,
-               JDBCOrigin jdbcOrigin,
-               JDBCLocator jdbcLocator,
-               JDBCLocation jdbcLocation,
-               JDBCCatalog jdbcCatalog,
-               org.omg.PortableServer.POA poa)*/
-    return eventFinder;
+        return eventFactory;
     }
 
-    //
-    // IDL:iris.edu/Fissures/IfEvent/EventDC/a_channel_finder:1.0
-    //
-    /***/
+    public EventFinder a_finder() {
+        return eventFinder;
+    }
 
-    public EventChannelFinder
-    a_channel_finder(){return null;}
+    public EventChannelFinder a_channel_finder() {
+        return null;
+    }
 
-
-     public void setEventFinder(EventFinder eventFinder) {
-
-    this.eventFinder = eventFinder;
+    public void setEventFinder(EventFinder eventFinder) {
+        this.eventFinder = eventFinder;
     }
 
     public void setEventFactory(EventFactory eventFactory) {
-
-    this.eventFactory = eventFactory;
+        this.eventFactory = eventFactory;
     }
 
     private EventFinder eventFinder = null;
 
     private EventFactory eventFactory = null;
 
-    private JDBCParameterRef jdbcParameterRef;
-    private JDBCEventAttr jdbcEventAttr;
-    private JDBCFlinnEngdahl jdbcFlinnEngdahl;
+    private JDBCEventAccess jdbcEventAccess;
+
     private org.omg.PortableServer.POA poa;
 
-    private static Logger logger = Logger.getLogger(EventFactoryImpl.class);
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(EventFactoryImpl.class);
 }// EventFactoryImpl
