e37021f2fe4e4a9bd0629b6370d99c956c596513
groves <groves@localhost>
2004-10-25 12:54:12 -0400

    [svn r10989] long gone

--- bsdtest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp ./dist/lib/fissuresUtil.jar:../fissures/dist/lib/FissuresIDL.jar:../fissures/dist/lib/FissuresImpl.jar:../jars/taup.jar:../jars/log4j.jar edu.sc.seis.fissuresUtil.display.TestBSD
