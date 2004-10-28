2bb9e39f102fcf6678ceaa327aab771efa9b552e
groves <groves@localhost>
2004-10-28 14:43:10 -0400

    [svn r11038] extend JDBCTest

--- tests/database/edu/sc/seis/fissuresUtil/database/event/JDBCEventAccessTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/event/JDBCEventAccessTest.java
@@ -1,26 +1,30 @@
 package edu.sc.seis.fissuresUtil.database.event;
 
-
 import java.sql.SQLException;
-import junit.framework.TestCase;
 import org.apache.log4j.BasicConfigurator;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.database.ConnMgr;
+import edu.sc.seis.fissuresUtil.database.JDBCTest;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 import edu.sc.seis.fissuresUtil.mockFissures.IfEvent.MockEventAccessOperations;
 
-public class JDBCEventAccessTest extends TestCase{
-    public JDBCEventAccessTest(String name) throws SQLException{
-        super(name);
+public class JDBCEventAccessTest extends JDBCTest {
+
+    public void setUp() throws SQLException {
         this.eventTable = new JDBCEventAccess(ConnMgr.createConnection());
         BasicConfigurator.configure();
     }
 
-    public void testGetAll() throws SQLException, NotFound{
+    public void tearDown() throws SQLException {
+        super.tearDown();
+        JDBCEventAccess.emptyCache();
+    }
+
+    public void testGetAll() throws SQLException, NotFound {
         EventAccessOperations[] events = MockEventAccessOperations.createEvents();
-        for (int i = 0; i < events.length; i++){
-            int dbid= put(events[i], i);
+        for(int i = 0; i < events.length; i++) {
+            int dbid = put(events[i], i);
             eventTable.getEvent(dbid);
             put(events[i], i);
         }
@@ -30,22 +34,23 @@ public class JDBCEventAccessTest extends TestCase{
     public void testPut() throws SQLException, NotFound {
         EventAccessOperations[] events = MockEventAccessOperations.createEvents();
         CacheEvent[] caches = new CacheEvent[events.length];
-        for (int i = 0; i < events.length; i++) {
+        for(int i = 0; i < events.length; i++) {
             caches[i] = new CacheEvent(events[i]);
         }
-        for (int i = 0; i < events.length; i++) {
+        for(int i = 0; i < events.length; i++) {
             int dbid = put(events[i], i);
             assertEquals(caches[i], eventTable.getEvent(dbid));
-            assertEquals(caches[i].hashCode(), eventTable.getEvent(dbid).hashCode());
-            assertEquals("event"+i, eventTable.getIOR(dbid));
+            assertEquals(caches[i].hashCode(), eventTable.getEvent(dbid)
+                    .hashCode());
+            assertEquals("event" + i, eventTable.getIOR(dbid));
             assertEquals("localhost", eventTable.getServer(dbid));
             assertEquals("test/dns", eventTable.getDNS(dbid));
         }
     }
 
-    public void testDoublePut() throws SQLException, NotFound{
+    public void testDoublePut() throws SQLException, NotFound {
         EventAccessOperations[] events = MockEventAccessOperations.createEvents();
-        for (int i = 0; i < events.length; i++) {
+        for(int i = 0; i < events.length; i++) {
             int dbidA = put(events[i], i);
             int dbidB = put(events[i], i);
             int gottenId = eventTable.getDBId(events[i]);
@@ -54,16 +59,16 @@ public class JDBCEventAccessTest extends TestCase{
         }
     }
 
-    public void testGetDBId() throws SQLException, NotFound{
+    public void testGetDBId() throws SQLException, NotFound {
         EventAccessOperations[] events = MockEventAccessOperations.createEvents();
-        for (int i = 0; i < events.length; i++) {
+        for(int i = 0; i < events.length; i++) {
             int dbid = put(events[i], i);
             assertEquals(dbid, eventTable.getDBId(events[i]));
         }
     }
 
-    private int put(EventAccessOperations event, int i) throws SQLException{
-        return eventTable.put(event, "event"+i, "localhost", "test/dns");
+    private int put(EventAccessOperations event, int i) throws SQLException {
+        return eventTable.put(event, "event" + i, "localhost", "test/dns");
     }
 
     private JDBCEventAccess eventTable;
