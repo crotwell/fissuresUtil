ac844a660fd684c1887117d2b7cabae9c5326b21
groves <groves@localhost>
2004-10-28 13:25:52 -0400

    [svn r11028] extend JDBCTest

--- tests/database/edu/sc/seis/fissuresUtil/database/network/JDBCSiteTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/network/JDBCSiteTest.java
@@ -6,20 +6,33 @@
 package edu.sc.seis.fissuresUtil.database.network;
 
 import java.sql.SQLException;
-import junit.framework.TestCase;
 import edu.iris.Fissures.IfNetwork.Site;
+import edu.sc.seis.fissuresUtil.database.JDBCTest;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockSite;
 
-public class JDBCSiteTest extends TestCase {
-
+public class JDBCSiteTest extends JDBCTest {
+    public void setUp() throws SQLException{
+        siteTable = new JDBCSite();
+        site = MockSite.createSite();
+    }
+    
     public void testDoublePut() throws SQLException, NotFound {
-        JDBCSite siteTable = new JDBCSite();
-        Site site = MockSite.createSite();
         int dbidA = siteTable.put(site);
         int dbidB = siteTable.put(site);
         int gottenId = siteTable.getDBId(site.get_id(), site.my_station);
         assertEquals(dbidA, dbidB);
         assertEquals(dbidB, gottenId);
     }
+    
+    public void testGetByCode() throws SQLException, NotFound{
+        int dbidA = siteTable.put(site);
+        int[] possibleStations = siteTable.getStationTable().getDBIds(site.get_id().network_id, site.my_station.get_code());
+        int[] dbids = siteTable.getDBIds(possibleStations, site.get_code());
+        assertEquals(1, dbids.length);
+        assertEquals(dbids[0], dbidA);
+    }
+    
+    private JDBCSite siteTable;
+    private Site site;
 }
