01541816c82092a79ae19fe9e6c9417b434f1a53
Charles Groves <groves@seis.sc.edu>
2004-10-28 11:04:27 -0400

    [svn r11014] use new statement preparer in JDBCTable, add methods to check for pieces of channel id

--- src/edu/sc/seis/fissuresUtil/database/network/JDBCSite.java
+++ src/edu/sc/seis/fissuresUtil/database/network/JDBCSite.java
@@ -1,4 +1,3 @@
-
 package edu.sc.seis.fissuresUtil.database.network;
 
 import java.sql.Connection;
@@ -9,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.log4j.Logger;
 import edu.iris.Fissures.TimeRange;
-import edu.iris.Fissures.IfNetwork.NetworkId;
 import edu.iris.Fissures.IfNetwork.Site;
 import edu.iris.Fissures.IfNetwork.SiteId;
 import edu.iris.Fissures.IfNetwork.Station;
@@ -23,124 +21,42 @@ import edu.sc.seis.fissuresUtil.database.JDBCTime;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 
 /**
- * JDBCSite.java
- *
- * All methods are unsyncronized, the calling application should make sure
- * that a single instance of this class is not accessed by more than one
- * thread at a time. Because of the use of prepared statements and a single
- * connection per instance, this class IS NOT THREAD-SAFE!
- *
- * Created: Fri Jan 26 12:01:39 2001
- *
+ * JDBCSite.java All methods are unsynchronized, the calling application should
+ * make sure that a single instance of this class is not accessed by more than
+ * one thread at a time. Because of the use of prepared statements and a single
+ * connection per instance, this class IS NOT THREAD-SAFE! Created: Fri Jan 26
+ * 12:01:39 2001
+ * 
  * @author Philip Crotwell
  * @version
  */
-
 public class JDBCSite extends NetworkTable {
-    public JDBCSite() throws SQLException{
+
+    public JDBCSite() throws SQLException {
         this(ConnMgr.createConnection());
     }
 
-    public JDBCSite(Connection conn) throws SQLException{
-        this(conn, new JDBCLocation(conn), new JDBCStation(conn),
+    public JDBCSite(Connection conn) throws SQLException {
+        this(conn,
+             new JDBCLocation(conn),
+             new JDBCStation(conn),
              new JDBCTime(conn));
     }
 
-
     public JDBCSite(Connection conn, JDBCLocation locTable,
-                    JDBCStation stationTable, JDBCTime time) throws SQLException {
+            JDBCStation stationTable, JDBCTime time) throws SQLException {
         super("site", conn);
         this.locTable = locTable;
         this.stationTable = stationTable;
         this.time = time;
         seq = new JDBCSequence(conn, "SiteSeq");
-        if(!DBUtil.tableExists("site", conn)){
+        if(!DBUtil.tableExists("site", conn)) {
             conn.createStatement().executeUpdate(ConnMgr.getSQL("site.create"));
         }
-        putAll = conn.prepareStatement("INSERT INTO site (site_id, sta_id, " +
-                                           "site_code, site_begin_id, site_end_id, "+
-                                           "site_comment, loc_id) " +
-                                           "VALUES (?, ?, ?, ?, ?, ?, ?)");
-        putId = conn.prepareStatement("INSERT INTO site (site_id, sta_id, " +
-                                          "site_code, site_begin_id) " +
-                                          "VALUES (?, ?, ?, ?)");
-        String getAllIdsQuery = "SELECT site_id, sta_id, site_code, site_begin_id FROM site";
-        getAllIds = conn.prepareStatement(getAllIdsQuery);
-        getAllIdsForSta = conn.prepareStatement(getAllIdsQuery + " WHERE sta_id = ?");
-        getAllIdsForNet = conn.prepareStatement(getAllIdsQuery + ", station WHERE site.sta_id = station.sta_id AND station.net_id = ?");
-        String getAllQuery = "SELECT * FROM site";
-        getAllSites = conn.prepareStatement(getAllQuery);
-        getAllSitesForSta = conn.prepareStatement(getAllQuery + " WHERE sta_id = ?");
-        getAllSitesForNet = conn.prepareStatement(getAllQuery + ", station WHERE site.sta_id =  station.sta_id AND station.net_id = ?");
-        getIfCommentExists = conn.prepareStatement("SELECT site_id FROM site " +
-                                                       "WHERE site_id = ? AND " +
-                                                       "site_comment IS NOT NULL");
-        getByDBId = conn.prepareStatement("SELECT * FROM site WHERE site_id = ?");
-        getSiteIdByDBId = conn.prepareStatement("SELECT site_id, sta_id, site_code, site_begin_id FROM site WHERE site_id = ?");
-        getDBId = conn.prepareStatement("SELECT site_id FROM site WHERE sta_id = ? AND " +
-                                            "site_code = ? AND site_begin_id = ?");
-        updateSite = conn.prepareStatement("UPDATE site SET site_end_id = ?, " +
-                                               "site_comment = ?, loc_id = ? WHERE site_id = ?");
+        prepareStatements();
     }
 
-    public SiteId[] getAllSiteIds() throws SQLException {
-        return extractAllSiteIds(getAllIds);
-    }
-
-    /**
-     *@returns - a 0 length array if the station isn't found
-     */
-    public SiteId[] getAllSiteIds(StationId sta) throws SQLException{
-        try {
-            int sta_id = stationTable.getDBId(sta);
-            getAllIdsForSta.setInt(1, sta_id);
-            return extractAllSiteIds(getAllIdsForSta);
-        } catch (NotFound e) { return new SiteId[]{};  }
-    }
-
-    public SiteId[] getAllSiteIds(NetworkId net) throws SQLException{
-        try {
-            int net_id = stationTable.getNetTable().getDBId(net);
-            getAllIdsForNet.setInt(1, net_id);
-            return extractAllSiteIds(getAllIdsForSta);
-        } catch (NotFound e) { return new SiteId[]{};  }
-    }
-
-    public Site[] getAllSites() throws SQLException, NotFound {
-        return extractAllSites(getAllSites);
-    }
-
-    public Site[] getAllSites(StationId sta) throws SQLException{
-        try{
-            int sta_id = stationTable.getDBId(sta);
-            getAllSitesForSta.setInt(1, sta_id);
-            return extractAllSites(getAllSitesForSta);
-        } catch (NotFound e) {return new Site[]{}; }
-    }
-
-    public Site[] getAllSites(NetworkId net) throws SQLException{
-        try{
-            int net_id = stationTable.getNetTable().getDBId(net);
-            getAllSitesForNet.setInt(1, net_id);
-            return extractAllSites(getAllSitesForNet);
-        } catch (NotFound e) {return new Site[]{}; }
-    }
-
-    private Site[] extractAllSites(PreparedStatement query) throws SQLException, NotFound{
-        ResultSet rs = query.executeQuery();
-        List aList = new ArrayList();
-        while(rs.next()){ aList.add(extract(rs, locTable, stationTable, time)); }
-        return (Site[])aList.toArray(new Site[aList.size()]);
-    }
-
-    private SiteId[] extractAllSiteIds(PreparedStatement query) throws SQLException{
-        ResultSet rs = query.executeQuery();
-        List aList = new ArrayList();
-        while (rs.next()){ aList.add(extractId(rs, stationTable, time)); }
-        return  (SiteId[])aList.toArray(new SiteId[aList.size()]);
-    }
-
-    public int put(Site site)  throws SQLException {
+    public int put(Site site) throws SQLException {
         int dbid;
         try {
             dbid = getDBId(site.get_id(), site.my_station);
@@ -153,7 +69,7 @@ public class JDBCSite extends NetworkTable {
                 updateSite.setInt(index, dbid);
                 updateSite.executeUpdate();
             }
-        } catch (NotFound notFound) {
+        } catch(NotFound notFound) {
             // no id found so ok to add the whole thing
             dbid = seq.next();
             putAll.setInt(1, dbid);
@@ -163,89 +79,129 @@ public class JDBCSite extends NetworkTable {
         return dbid;
     }
 
-    public Site get(int dbid)  throws SQLException, NotFound {
+    public Site get(int dbid) throws SQLException, NotFound {
         getByDBId.setInt(1, dbid);
         ResultSet rs = getByDBId.executeQuery();
-        if (rs.next()){ return extract(rs, locTable, stationTable, time);}
-        throw new NotFound("No Site found for database id = "+dbid);
+        if(rs.next()) { return extract(rs, locTable, stationTable, time); }
+        throw new NotFound("No Site found for database id = " + dbid);
     }
 
-    public SiteId getSiteId(int dbid)  throws SQLException, NotFound {
+    public SiteId getSiteId(int dbid) throws SQLException, NotFound {
         getSiteIdByDBId.setInt(1, dbid);
         ResultSet rs = getSiteIdByDBId.executeQuery();
-        if (rs.next()){ return extractId(rs, stationTable, time);}
-        throw new NotFound("No SiteId found for database id = "+dbid);
-    }
-    
-    public Site get(SiteId id, Station staId)throws SQLException, NotFound {
-        return get(getDBId(id, staId));
+        if(rs.next()) { return extractId(rs, stationTable, time); }
+        throw new NotFound("No SiteId found for database id = " + dbid);
     }
 
-    public int getDBId(SiteId id, Station staId)  throws SQLException, NotFound {
+    public int getDBId(SiteId id, Station staId) throws SQLException, NotFound {
         insertId(id, staId, getDBId, 1, stationTable, time);
         ResultSet rs = getDBId.executeQuery();
-        if(rs.next()){ return rs.getInt("site_id"); }
+        if(rs.next()) { return rs.getInt("site_id"); }
         throw new NotFound("No such Site id in the db");
     }
 
-    public static Site extract(ResultSet rs, JDBCLocation locTable,
-                               JDBCStation stationTable, JDBCTime time) throws SQLException, NotFound {
-        SiteId id = extractId(rs,stationTable, time);
-        return new SiteImpl(id, locTable.get(rs.getInt("loc_id")),
+    public int[] getDBIds(int[] possibleStaDbIds, String site_code)
+            throws SQLException, NotFound {
+        List ids = new ArrayList();
+        for(int i = 0; i < possibleStaDbIds.length; i++) {
+            getDBIdsForStaAndCode.setInt(1, possibleStaDbIds[i]);
+            getDBIdsForStaAndCode.setString(2, site_code);
+            ResultSet rs = getDBIdsForStaAndCode.executeQuery();
+            while(rs.next()) {
+                ids.add(new Integer(rs.getInt("site_id")));
+            }
+        }
+        if(ids.size() > 0) {
+            int[] intIds = new int[ids.size()];
+            for(int i = 0; i < ids.size(); i++) {
+                intIds[i] = ((Integer)ids.get(i)).intValue();
+            }
+            return intIds;
+        }
+        throw new NotFound("No sites in the database of code " + site_code
+                + " for given station ids");
+    }
+
+    public static Site extract(ResultSet rs,
+                               JDBCLocation locTable,
+                               JDBCStation stationTable,
+                               JDBCTime time) throws SQLException, NotFound {
+        SiteId id = extractId(rs, stationTable, time);
+        return new SiteImpl(id,
+                            locTable.get(rs.getInt("loc_id")),
                             new TimeRange(id.begin_time,
                                           time.get(rs.getInt("site_end_id"))),
                             stationTable.get(rs.getInt("sta_id")),
                             rs.getString("site_comment"));
     }
 
-    public static SiteId extractId(ResultSet rs, JDBCStation staTable,
-                                   JDBCTime time) throws SQLException{
+    public static SiteId extractId(ResultSet rs,
+                                   JDBCStation staTable,
+                                   JDBCTime time) throws SQLException {
         try {
             StationId staId = staTable.getStationId(rs.getInt("sta_id"));
-            return new SiteId(staId.network_id, staId.station_code,
+            return new SiteId(staId.network_id,
+                              staId.station_code,
                               rs.getString("site_code"),
                               time.get(rs.getInt("site_begin_id")));
-        }catch (NotFound e) {
+        } catch(NotFound e) {
             throw new RuntimeException("There is a foreign key constraint requiring that a sta_id be in the station table, but it just returned a not found for one such key.  This probably indicates a db problem!",
                                        e);
         }
     }
 
-    public static int insertAll(Site site, PreparedStatement stmt, int index,
-                                JDBCStation stationTable, JDBCLocation locTable,
-                                JDBCTime time)throws SQLException {
-        index = insertId(site.get_id(), site.my_station, stmt, index,
-                         stationTable, time);
+    public static int insertAll(Site site,
+                                PreparedStatement stmt,
+                                int index,
+                                JDBCStation stationTable,
+                                JDBCLocation locTable,
+                                JDBCTime time) throws SQLException {
+        index = insertId(site.get_id(),
+                         site.my_station,
+                         stmt,
+                         index,
+                         stationTable,
+                         time);
         index = insertOnlySite(site, stmt, index, locTable, time);
         return index;
     }
 
-    public static int insertOnlySite(Site site, PreparedStatement stmt,
-                                     int index, JDBCLocation locTable,
-                                     JDBCTime time)throws SQLException{
+    public static int insertOnlySite(Site site,
+                                     PreparedStatement stmt,
+                                     int index,
+                                     JDBCLocation locTable,
+                                     JDBCTime time) throws SQLException {
         stmt.setInt(index++, time.put(site.effective_time.end_time));
         stmt.setString(index++, site.comment);
         stmt.setInt(index++, locTable.put(site.my_location));
         return index;
     }
 
-    public static int insertId(SiteId id, Station sta, PreparedStatement stmt, int index,
-                               JDBCStation stationTable, JDBCTime time) throws SQLException{
+    public static int insertId(SiteId id,
+                               Station sta,
+                               PreparedStatement stmt,
+                               int index,
+                               JDBCStation stationTable,
+                               JDBCTime time) throws SQLException {
         stmt.setInt(index++, stationTable.put(sta));
         stmt.setString(index++, id.site_code);
         stmt.setInt(index++, time.put(id.begin_time));
         return index;
-
     }
 
-    public JDBCStation getStationTable(){ return stationTable; }
+    public JDBCStation getStationTable() {
+        return stationTable;
+    }
 
-    private PreparedStatement getAllIds, getAllIdsForSta, getIfCommentExists, getByDBId, getSiteIdByDBId,
-        getDBId, updateSite, putAll, putId, getAllIdsForNet, getAllSites, getAllSitesForSta,
-        getAllSitesForNet;
+    private PreparedStatement getIfCommentExists, getByDBId, getSiteIdByDBId,
+            getDBId, updateSite, putAll, getDBIdsForStaAndCode;
+
     private JDBCSequence seq;
+
     private JDBCLocation locTable;
+
     private JDBCStation stationTable;
+
     private JDBCTime time;
 
     private static final Logger logger = Logger.getLogger(JDBCSite.class);
