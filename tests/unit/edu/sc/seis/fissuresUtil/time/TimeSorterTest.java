2aa93117dfa487ea8da9e63578c2eee71c32462c
Charles Groves <groves@seis.sc.edu>
2004-10-28 22:00:07 -0400

    [svn r11056] moved from DisplayUtilsTest

---
+++ tests/unit/edu/sc/seis/fissuresUtil/time/TimeSorterTest.java
@@ -0,0 +1,108 @@
+/**
+ * DisplayUtilsTest.java
+ * 
+ * @author Created by Omnicore CodeGuide
+ */
+package edu.sc.seis.fissuresUtil.time;
+
+import junit.framework.TestCase;
+import junitx.framework.ArrayAssert;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
+import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
+import edu.sc.seis.fissuresUtil.display.SimplePlotUtil;
+
+public class TimeSorterTest extends TestCase {
+
+    public void testSortByDateSorted() {
+        LocalSeismogramImpl[] seisArray = createThreeSeisArray();
+        ArrayAssert.assertEquals(seisArray, SortTool.sortByDate(seisArray));
+    }
+
+    public void testSortByDateUnsorted() {
+        LocalSeismogramImpl[] originalSeis = createThreeSeisArray();
+        LocalSeismogramImpl[] unsortedSeis = createUnsortedThreeSeisArray(originalSeis);
+        LocalSeismogramImpl[] sortedSeis = SortTool.sortByDate(unsortedSeis);
+        ArrayAssert.assertEquals(originalSeis, sortedSeis);
+    }
+
+    public void testGetFullTimeRange() {
+        LocalSeismogramImpl[] seis = createThreeSeisArray();
+        MicroSecondTimeRange fullTime = new MicroSecondTimeRange(new MicroSecondDate(0),
+                                                                 new MicroSecondDate(seis[2].getEndTime()));
+        assertEquals(fullTime,
+                     RangeTool.getFullTime(createUnsortedThreeSeisArray(seis)));
+    }
+
+    public void testAreContiguous() {
+        LocalSeismogramImpl first = SimplePlotUtil.createSpike(new MicroSecondDate(0));
+        LocalSeismogramImpl second = SimplePlotUtil.createSpike(first.getEndTime());
+        assertTrue("Touching end times should be contiguous",
+                   RangeTool.areContiguous(first, second));
+        assertTrue("Touching end times should be contiguous",
+                   RangeTool.areContiguous(second, first));
+        TimeInterval sampleInterval = first.getSampling().getPeriod();
+        LocalSeismogramImpl third = SimplePlotUtil.createSpike(first.getEndTime()
+                .add(sampleInterval));
+        assertTrue(RangeTool.areContiguous(first, third));
+        assertTrue(RangeTool.areContiguous(third, first));
+    }
+
+    public void testAreNotContiguous() {
+        LocalSeismogramImpl first = SimplePlotUtil.createSpike(new MicroSecondDate(0));
+        LocalSeismogramImpl second = SimplePlotUtil.createSpike(new MicroSecondDate(5));
+        assertFalse(RangeTool.areContiguous(first, second));
+        assertFalse(RangeTool.areContiguous(second, first));
+        TimeInterval sampleInterval = first.getSampling().getPeriod();
+        TimeInterval tripleInterval = (TimeInterval)sampleInterval.multiplyBy(3.0);
+        LocalSeismogramImpl third = SimplePlotUtil.createSpike(first.getEndTime()
+                .add(tripleInterval));
+        assertFalse(RangeTool.areContiguous(first, third));
+        assertFalse(RangeTool.areContiguous(third, first));
+    }
+
+    public void testAreOverlapping() {
+        LocalSeismogramImpl first = SimplePlotUtil.createSpike(new MicroSecondDate(0));
+        LocalSeismogramImpl second = SimplePlotUtil.createSpike(new MicroSecondDate(5));
+        assertTrue(RangeTool.areOverlapping(first, second));
+        assertTrue(RangeTool.areOverlapping(second, first));
+    }
+
+    public void testAreNotOverlapping() {
+        LocalSeismogramImpl first = SimplePlotUtil.createSpike(new MicroSecondDate(0));
+        TimeInterval sampleInterval = first.getSampling().getPeriod();
+        LocalSeismogramImpl second = SimplePlotUtil.createSpike(first.getEndTime()
+                .add(sampleInterval));
+        assertFalse(RangeTool.areOverlapping(first, second));
+        assertFalse(RangeTool.areOverlapping(second, first));
+    }
+
+    public static LocalSeismogramImpl[] createThreeSeisArray() {
+        LocalSeismogramImpl[] seis = {firstSeis, secondSeis, thirdSeis};
+        return seis;
+    }
+
+    public static LocalSeismogramImpl[] createOtherSeisArray() {
+        LocalSeismogramImpl[] seis = {fourthSeis, fifthSeis, sixthSeis};
+        return seis;
+    }
+
+    private LocalSeismogramImpl[] createUnsortedThreeSeisArray(LocalSeismogramImpl[] seis) {
+        LocalSeismogramImpl[] unsorted = {seis[2], seis[1], seis[0]};
+        return unsorted;
+    }
+
+    private static LocalSeismogramImpl firstSeis = SimplePlotUtil.createSpike(new MicroSecondDate(0));
+
+    private static LocalSeismogramImpl secondSeis = SimplePlotUtil.createSpike(new MicroSecondDate(5000));
+
+    private static LocalSeismogramImpl thirdSeis = SimplePlotUtil.createSpike(new MicroSecondDate(100000));
+
+    private static LocalSeismogramImpl fourthSeis = SimplePlotUtil.createSpike(new MicroSecondDate(2500));
+
+    private static LocalSeismogramImpl fifthSeis = SimplePlotUtil.createSpike(new MicroSecondDate(1000000));
+
+    private static LocalSeismogramImpl sixthSeis = SimplePlotUtil.createSpike(new MicroSecondDate(9000));
+}
