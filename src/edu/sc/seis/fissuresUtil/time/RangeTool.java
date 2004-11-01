65d234451c2ed5ea69c3c3802ffaab73ca81547f
groves <groves@localhost>
2004-11-01 17:45:29 -0500

    [svn r11074] added PlottableChunk stuff

--- src/edu/sc/seis/fissuresUtil/time/RangeTool.java
+++ src/edu/sc/seis/fissuresUtil/time/RangeTool.java
@@ -2,7 +2,9 @@ package edu.sc.seis.fissuresUtil.time;
 
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.database.plottable.PlottableChunk;
 import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
 
@@ -11,20 +13,41 @@ import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
  */
 public class RangeTool {
 
+    public static boolean areContiguous(PlottableChunk one, PlottableChunk two) {
+        TimeInterval secondPerSample = new TimeInterval(1 / one.getSamplesPerSecond(),
+                                                        UnitImpl.SECOND);
+        return areContiguous(one.getTimeRange(),
+                             two.getTimeRange(),
+                             secondPerSample);
+    }
+
     public static boolean areContiguous(LocalSeismogramImpl one,
                                         LocalSeismogramImpl two) {
+        return areContiguous(new MicroSecondTimeRange(one),
+                             new MicroSecondTimeRange(two),
+                             one.getSampling().getPeriod());
+    }
+
+    public static boolean areContiguous(MicroSecondTimeRange one,
+                                        MicroSecondTimeRange two,
+                                        TimeInterval interval) {
         if(!RangeTool.areOverlapping(one, two)) {
+            TimeInterval doubleInterval = (TimeInterval)interval.multiplyBy(2.0);
             if(one.getEndTime().before(two.getBeginTime())) {
-                return RangeTool.areContiguous(one.getEndTime(),
-                                                two.getBeginTime(),
-                                                one.getSampling().getPeriod());
+                return one.getEndTime()
+                        .add(doubleInterval)
+                        .after(two.getBeginTime());
             } else {
-                return RangeTool.areContiguous(two.getEndTime(),
-                                                one.getBeginTime(),
-                                                one.getSampling().getPeriod());
+                return two.getEndTime()
+                        .add(doubleInterval)
+                        .after(one.getBeginTime());
             }
         }
         return false;
+    }
+    
+    public static boolean areOverlapping(PlottableChunk one, PlottableChunk two){
+        return areOverlapping(one.getTimeRange(), two.getTimeRange());
     }
 
     public static boolean areOverlapping(MicroSecondTimeRange one,
@@ -45,25 +68,28 @@ public class RangeTool {
         return areOverlapping(oneTr, twoTr);
     }
 
-    public static boolean areContiguous(MicroSecondDate one,
-                                        MicroSecondDate two,
-                                        TimeInterval interval) {
-        TimeInterval doubleInterval = (TimeInterval)interval.multiplyBy(2.0);
-        if(one.add(doubleInterval).after(two)) { return true; }
-        return false;
-    }
-
     /**
      * @returns A time range encompassing the earliest begin time of the passed
      *          in seismograms to the latest end time
      */
     public static MicroSecondTimeRange getFullTime(LocalSeismogramImpl[] seis) {
         if(seis.length == 0) { return DisplayUtils.ZERO_TIME; }
-        MicroSecondDate beginTime = SortTool.sortByDate(seis)[0].getBeginTime();
+        MicroSecondDate beginTime = SortTool.byBeginTimeAscending(seis)[0].getBeginTime();
         MicroSecondDate endTime = new MicroSecondDate(0);
         for(int i = 0; i < seis.length; i++) {
             if(seis[i].getEndTime().after(endTime)) {
                 endTime = seis[i].getEndTime();
+            }
+        }
+        return new MicroSecondTimeRange(beginTime, endTime);
+    }
+    public static MicroSecondTimeRange getFullTime(PlottableChunk[] pc) {
+        if(pc.length == 0) { return DisplayUtils.ZERO_TIME; }
+        MicroSecondDate beginTime = SortTool.byBeginTimeAscending(pc)[0].getBeginTime();
+        MicroSecondDate endTime = new MicroSecondDate(0);
+        for(int i = 0; i < pc.length; i++) {
+            if(pc[i].getEndTime().after(endTime)) {
+                endTime = pc[i].getEndTime();
             }
         }
         return new MicroSecondTimeRange(beginTime, endTime);
