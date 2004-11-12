3a925c5d77114fa5b384e9cc0922a1ca3651c46d
Charles Groves <groves@seis.sc.edu>
2004-11-12 16:14:22 -0500

    [svn r11146] use pixels internally

--- src/edu/sc/seis/fissuresUtil/database/plottable/JDBCPlottable.java
+++ src/edu/sc/seis/fissuresUtil/database/plottable/JDBCPlottable.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -49,11 +50,15 @@ public class JDBCPlottable extends PlottableTable {
 
     public void put(PlottableChunk[] chunks) throws SQLException, IOException {
         MicroSecondTimeRange stuffInDB = RangeTool.getFullTime(chunks);
-        //TODO get everything for start and end day as well, in case the db has
-        // more
+        MicroSecondDate startTime = PlottableChunk.stripToDay(stuffInDB.getBeginTime());
+        MicroSecondDate strippedEnd = PlottableChunk.stripToDay(stuffInDB.getEndTime());
+        if(!strippedEnd.equals(stuffInDB.getEndTime())){
+            strippedEnd = strippedEnd.add(PlottableChunk.ONE_DAY);
+        }
+        stuffInDB = new MicroSecondTimeRange(startTime, strippedEnd);
         PlottableChunk[] dbChunks = get(stuffInDB,
                                         chunks[0].getChannel(),
-                                        chunks[0].getSamplesPerDay());
+                                        chunks[0].getPixelsPerDay());
         PlottableChunk[] everything = new PlottableChunk[chunks.length
                 + dbChunks.length];
         System.arraycopy(dbChunks, 0, everything, 0, dbChunks.length);
@@ -67,7 +72,7 @@ public class JDBCPlottable extends PlottableTable {
         logger.debug("Adding " + everything.length + " chunks split on days");
         int rowsDropped = drop(stuffInDB,
                                chunks[0].getChannel(),
-                               chunks[0].getSamplesPerDay());
+                               chunks[0].getPixelsPerDay());
         logger.debug("Dropped " + rowsDropped
                 + " rows of stuff that new data covered");
         for(int i = 0; i < everything.length; i++) {
@@ -75,11 +80,11 @@ public class JDBCPlottable extends PlottableTable {
             int stmtIndex = 1;
             PlottableChunk chunk = everything[i];
             put.setInt(stmtIndex++, chanTable.put(chunk.getChannel()));
-            put.setInt(stmtIndex++, chunk.getSamplesPerDay());
+            put.setInt(stmtIndex++, chunk.getPixelsPerDay());
             put.setTimestamp(stmtIndex++, chunk.getBeginTime().getTimestamp());
             put.setTimestamp(stmtIndex++, chunk.getEndTime().getTimestamp());
             int[] y = chunk.getData().y_coor;
-            put.setInt(stmtIndex++, y.length);
+            put.setInt(stmtIndex++, y.length/2);
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             DataOutputStream dos = new DataOutputStream(out);
             for(int k = 0; k < y.length; k++) {
@@ -105,10 +110,10 @@ public class JDBCPlottable extends PlottableTable {
         return Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.US);
     }
 
-    private static int getSamples(int samplesPerDay, MicroSecondTimeRange tr) {
+    private static int getPixels(int pixelsPerDay, MicroSecondTimeRange tr) {
         TimeInterval inter = tr.getInterval();
         inter = (TimeInterval)inter.convertTo(UnitImpl.DAY);
-        double samples = samplesPerDay * inter.getValue();
+        double samples = pixelsPerDay * inter.getValue();
         return (int)Math.floor(samples);
     }
 
@@ -155,7 +160,7 @@ public class JDBCPlottable extends PlottableTable {
 
     public PlottableChunk[] get(MicroSecondTimeRange requestRange,
                                 ChannelId id,
-                                int samplesPerDay) throws SQLException,
+                                int pixelsPerDay) throws SQLException,
             IOException {
         int chanDbId;
         try {
@@ -169,52 +174,53 @@ public class JDBCPlottable extends PlottableTable {
         get.setTimestamp(index++, requestRange.getEndTime().getTimestamp());
         get.setTimestamp(index++, requestRange.getBeginTime().getTimestamp());
         get.setInt(index++, chanDbId);
-        get.setInt(index++, samplesPerDay);
+        get.setInt(index++, pixelsPerDay);
         ResultSet rs = get.executeQuery();
         List chunks = new ArrayList();
-        int requestSamples = getSamples(samplesPerDay, requestRange);
-        logger.debug("Request made for " + requestSamples + " from "
-                + requestRange + " at " + samplesPerDay);
+        int requestPixels = getPixels(pixelsPerDay, requestRange);
+        logger.debug("Request made for " + requestPixels + " from "
+                + requestRange + " at " + pixelsPerDay + "ppd");
         while(rs.next()) {
             Timestamp ts = rs.getTimestamp("start_time");
             MicroSecondDate rowBeginTime = new MicroSecondDate(ts);
-            int offsetIntoRequestSamples = SimplePlotUtil.getPixel(requestSamples,
+            int offsetIntoRequestPixels = SimplePlotUtil.getPixel(requestPixels,
                                                                    requestRange,
                                                                    rowBeginTime);
-            int numSamples = rs.getInt("sample_count");
-            int firstSampleForRequest = 0;
-            if(offsetIntoRequestSamples < 0) {
+            int numPixels = rs.getInt("pixel_count");
+            int firstPixelForRequest = 0;
+            if(offsetIntoRequestPixels < 0) {
                 //This db row has data starting before the request, start at
                 // pertinent point
-                firstSampleForRequest = -1 * offsetIntoRequestSamples;
+                firstPixelForRequest = -1 * offsetIntoRequestPixels;
             }
-            int lastSampleForRequest = numSamples;
-            if(offsetIntoRequestSamples + numSamples > requestSamples) {
+            int lastPixelForRequest = numPixels;
+            if(offsetIntoRequestPixels + numPixels > requestPixels) {
                 //This row has more data than was requested in it, only get
                 // enough to fill the request
-                lastSampleForRequest = requestSamples
-                        - offsetIntoRequestSamples;
+                lastPixelForRequest = requestPixels
+                        - offsetIntoRequestPixels;
             }
-            int samplesUsed = lastSampleForRequest - firstSampleForRequest;
-            int[] x = new int[samplesUsed];
-            int[] y = new int[samplesUsed];
+            int pixelsUsed = lastPixelForRequest - firstPixelForRequest;
+            int[] x = new int[pixelsUsed*2];
+            int[] y = new int[pixelsUsed*2];
             byte[] dataBytes = rs.getBytes("data");
             DataInputStream dis = new DataInputStream(new ByteArrayInputStream(dataBytes));
-            for(int i = 0; i < firstSampleForRequest; i++) {
+            for(int i = 0; i < firstPixelForRequest; i++) {
+                dis.readInt();
                 dis.readInt();
             }
-            for(int i = 0; i < samplesUsed; i++) {
-                x[i] = (firstSampleForRequest + i) / 2;
+            for(int i = 0; i < pixelsUsed * 2; i++) {
+                x[i] = firstPixelForRequest + i/2;
                 y[i] = dis.readInt();
             }
             Plottable p = new Plottable(x, y);
             PlottableChunk pc = new PlottableChunk(p,
-                                                   PlottableChunk.getSample(rowBeginTime,
-                                                                            samplesPerDay)
-                                                           + firstSampleForRequest,
+                                                   PlottableChunk.getPixel(rowBeginTime,
+                                                                            pixelsPerDay)
+                                                           + firstPixelForRequest,
                                                    PlottableChunk.getJDay(rowBeginTime),
                                                    PlottableChunk.getYear(rowBeginTime),
-                                                   samplesPerDay,
+                                                   pixelsPerDay,
                                                    id);
             chunks.add(pc);
             logger.debug("Returning " + pc + " from chunk starting at "
