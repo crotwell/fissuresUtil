b786aea9aedfc586dac961329e9d69d91da420b2
groves <groves@localhost>
2004-11-16 15:57:35 -0500

    [svn r11173] changed stations to render as netcode.stationcode - stationname if useCodes and useNames is on

--- src/edu/sc/seis/fissuresUtil/chooser/NameListCellRenderer.java
+++ src/edu/sc/seis/fissuresUtil/chooser/NameListCellRenderer.java
@@ -12,21 +12,23 @@ import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 public class NameListCellRenderer extends DefaultListCellRenderer {
 
-    public NameListCellRenderer(boolean useNames){
-        this(useNames, ! useNames, true);
+    public NameListCellRenderer(boolean useNames) {
+        this(useNames, !useNames, true);
     }
 
     /**
      * Creates a new <code>NameListCellRenderer</code> instance.
-     *
-     * @param useNames true if names should be used
-     * @param useCodes true if codes should be used
-     * @param codeIsFirst true if the string should be code - name, false if
-     *    it should be name - code
+     * 
+     * @param useNames
+     *            true if names should be used
+     * @param useCodes
+     *            true if codes should be used
+     * @param codeIsFirst
+     *            true if the string should be code - name, false if it should
+     *            be name - code
      */
-    public NameListCellRenderer(boolean useNames,
-                                boolean useCodes,
-                                boolean codeIsFirst){
+    public NameListCellRenderer(boolean useNames, boolean useCodes,
+            boolean codeIsFirst) {
         this.useNames = useNames;
         this.useCodes = useCodes;
         this.codeIsFirst = codeIsFirst;
@@ -34,20 +36,39 @@ public class NameListCellRenderer extends DefaultListCellRenderer {
 
     private String getDisplayString(String name, String code) {
         String out = "";
-        if (useNames) {
+        if(useNames) {
             out = name;
-            if ((name == null || name.length() == 0) && ! useCodes) {
+            if((name == null || name.length() == 0) && !useCodes) {
                 out = code;
             }
-            if ( useCodes && codeIsFirst) {
-                out = code +" - "+out;
+            if(useCodes && codeIsFirst) {
+                out = code + " - " + out;
             }
-            if ( useCodes && ! codeIsFirst) {
-                out = out + " - "+ code;
+            if(useCodes && !codeIsFirst) {
+                out = out + " - " + code;
             }
             return out;
         }
         return code;
+    }
+
+    public String getStringToDisplay(Station sta) {
+        if(useNames) {
+            if(useCodes) {
+                if(codeIsFirst) {
+                    return getCodes(sta) + " - " + sta.name;
+                } else {
+                    return sta.name + " - " + getCodes(sta);
+                }
+            } else {
+                return sta.name;
+            }
+        }
+        return getCodes(sta);
+    }
+
+    public static String getCodes(Station sta) {
+        return sta.get_id().network_id.network_code + "." + sta.get_code();
     }
 
     public Component getListCellRendererComponent(JList list,
@@ -56,42 +77,39 @@ public class NameListCellRenderer extends DefaultListCellRenderer {
                                                   boolean isSelected,
                                                   boolean cellHasFocus) {
         String name = "XXXX";
-        if (value instanceof NetworkAccess) {
+        if(value instanceof NetworkAccess) {
             try {
-                String netCode =
-                    ((NetworkAccess)value).get_attributes().get_code();
-                if (netCode.startsWith("X") ||
-                    netCode.startsWith("Y") ||
-                    netCode.startsWith("Z")) {
-                    edu.iris.Fissures.Time start =
-                        ((NetworkAccess)value).get_attributes().get_id().begin_time;
-                    netCode += start.date_time.substring(2,4);
+                String netCode = ((NetworkAccess)value).get_attributes()
+                        .get_code();
+                if(netCode.startsWith("X") || netCode.startsWith("Y")
+                        || netCode.startsWith("Z")) {
+                    edu.iris.Fissures.Time start = ((NetworkAccess)value).get_attributes()
+                            .get_id().begin_time;
+                    netCode += start.date_time.substring(2, 4);
                 }
-                name =
-                    getDisplayString(((NetworkAccess)value).get_attributes().name,
-                                     netCode);
-            } catch (Throwable e) {
-                GlobalExceptionHandler.handle("in renderer for network "+name, e);
+                name = getDisplayString(((NetworkAccess)value).get_attributes().name,
+                                        netCode);
+            } catch(Throwable e) {
+                GlobalExceptionHandler.handle("in renderer for network " + name,
+                                              e);
             }
         }
-        if (value instanceof Station) {
-            name = getDisplayString(((Station)value).name,
-                                        ((Station)value).get_code());
+        if(value instanceof Station) {
+            name = getStringToDisplay((Station)value);
+            //getDisplayString(((Station)value).name,
+            //                            ((Station)value).get_code());
         }
-        if (value instanceof Site) {
+        if(value instanceof Site) {
             // sites do not have names
             name = ((Site)value).get_code();
         }
-
-        if (value instanceof Channel) {
+        if(value instanceof Channel) {
             name = getDisplayString(((Channel)value).name,
-                                        ((Channel)value).get_code());
+                                    ((Channel)value).get_code());
         }
-
-        if (value instanceof String) {
+        if(value instanceof String) {
             name = (String)value;
         } // end of if (value instanceof String)
-
         //logger.debug("render "+name);
         Component c = super.getListCellRendererComponent(list,
                                                          name,
@@ -99,6 +117,10 @@ public class NameListCellRenderer extends DefaultListCellRenderer {
                                                          isSelected,
                                                          cellHasFocus);
         return c;
+    }
+    
+    public void setUseCodes(boolean useCodes){
+        this.useCodes = useCodes; 
     }
 
     boolean useNames;
@@ -108,5 +130,4 @@ public class NameListCellRenderer extends DefaultListCellRenderer {
     boolean codeIsFirst;
 
     private static Logger logger = Logger.getLogger(NameListCellRenderer.class);
-
 }
