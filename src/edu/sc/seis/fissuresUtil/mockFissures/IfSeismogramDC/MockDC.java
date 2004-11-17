8b1fa92a6fa6ee4d1dde36ef818dda5e8d19b561
groves <groves@localhost>
2004-11-17 16:31:15 -0500

    [svn r11193] dummy corba stuff

--- src/edu/sc/seis/fissuresUtil/mockFissures/IfSeismogramDC/MockDC.java
+++ src/edu/sc/seis/fissuresUtil/mockFissures/IfSeismogramDC/MockDC.java
@@ -1,7 +1,18 @@
 package edu.sc.seis.fissuresUtil.mockFissures.IfSeismogramDC;
 
+import org.omg.CORBA.Context;
+import org.omg.CORBA.ContextList;
+import org.omg.CORBA.DomainManager;
+import org.omg.CORBA.ExceptionList;
+import org.omg.CORBA.NVList;
+import org.omg.CORBA.NamedValue;
+import org.omg.CORBA.Object;
+import org.omg.CORBA.Policy;
+import org.omg.CORBA.Request;
+import org.omg.CORBA.SetOverrideType;
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.Time;
+import edu.iris.Fissures.IfSeismogramDC.DataCenter;
 import edu.iris.Fissures.IfSeismogramDC.DataCenterCallBack;
 import edu.iris.Fissures.IfSeismogramDC.DataCenterPOA;
 import edu.iris.Fissures.IfSeismogramDC.LocalSeismogram;
@@ -12,7 +23,7 @@ import edu.sc.seis.fissuresUtil.display.SimplePlotUtil;
 /**
  * @author groves Created on Nov 9, 2004
  */
-public class MockDC extends DataCenterPOA{
+public class MockDC extends DataCenterPOA implements DataCenter{
 
     public RequestFilter[] available_data(RequestFilter[] a_filterseq) {
         return a_filterseq;
@@ -55,5 +66,43 @@ public class MockDC extends DataCenterPOA{
 
     public String request_status(String a_request) throws FissuresException {
         throw new FissuresException();
+    }
+
+    public void _release() {}
+
+    public int _hash(int maximum) {
+        return 0;
+    }
+
+    public DomainManager[] _get_domain_managers() {
+        return null;
+    }
+
+    public Object _duplicate() {
+        return null;
+    }
+
+    public boolean _is_equivalent(Object other) {
+        return false;
+    }
+
+    public Policy _get_policy(int policy_type) {
+        return null;
+    }
+
+    public Request _request(String operation) {
+        return null;
+    }
+
+    public Object _set_policy_override(Policy[] policies, SetOverrideType set_add) {
+        return null;
+    }
+
+    public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result) {
+        return null;
+    }
+
+    public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result, ExceptionList exclist, ContextList ctxlist) {
+        return null;
     }
 }
