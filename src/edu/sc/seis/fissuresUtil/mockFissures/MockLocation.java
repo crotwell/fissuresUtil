58900664094aa7bee39577fb7ec0e6bf49a34654
groves <groves@localhost>
2004-11-24 00:36:37 -0500

    [svn r11226] added location grid creator

--- src/edu/sc/seis/fissuresUtil/mockFissures/MockLocation.java
+++ src/edu/sc/seis/fissuresUtil/mockFissures/MockLocation.java
@@ -4,24 +4,45 @@ import edu.iris.Fissures.Location;
 import edu.iris.Fissures.LocationType;
 import edu.iris.Fissures.Quantity;
 
-public class MockLocation{
+public class MockLocation {
+
     public static final Location SIMPLE = create();
-    
-    public static final Location BERLIN = create(52.31f, 13.24f, Defaults.TEN_K,
+
+    public static final Location BERLIN = create(52.31f,
+                                                 13.24f,
+                                                 Defaults.TEN_K,
                                                  Defaults.TEN_K);
-    
-    public static Location create(){ return create(0, 0); }
-    
-    public static Location create(float lat, float lon){
+
+    public static Location create() {
+        return create(0f, 0f);
+    }
+
+    public static Location create(float lat, float lon) {
         return create(lat, lon, Defaults.ZERO_K, Defaults.ZERO_K);
     }
-    
-    public static Location create(float lat, float lon, Quantity depth,
-                                  Quantity elev){
+
+    public static Location create(float lat,
+                                  float lon,
+                                  Quantity depth,
+                                  Quantity elev) {
         return new Location(lat, lon, elev, depth, LocationType.from_int(1));
     }
-    
-    public static Location[] createMultiple(){
+
+    public static Location[] create(int rows, int cols) {
+        Location[] locs = new Location[rows * cols];
+        float lonStep = 360 / cols;
+        float latStep = 180 / rows;
+        for(int i = 0; i < rows; i++) {
+            float lat = -90 + latStep * i;
+            int rowOffset = i * cols;
+            for(int j = 0; j < cols; j++) {
+                locs[rowOffset + j] = MockLocation.create(lat, lonStep * j);
+            }
+        }
+        return locs;
+    }
+
+    public static Location[] createMultiple() {
         Location[] locs = new Location[3];
         locs[0] = SIMPLE;
         locs[1] = BERLIN;
