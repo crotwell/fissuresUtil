6b12ecd85af570d0262286d80db60bb5ea52228d
groves <groves@localhost>
2004-11-27 16:04:28 -0500

    [svn r11255] ripped out threaded updates

--- src/edu/sc/seis/fissuresUtil/map/LayerProjectionUpdater.java
+++ src/edu/sc/seis/fissuresUtil/map/LayerProjectionUpdater.java
@@ -9,25 +9,12 @@ import com.bbn.openmap.Layer;
 import com.bbn.openmap.event.ProjectionEvent;
 import com.bbn.openmap.omGraphics.OMGraphicList;
 import com.bbn.openmap.proj.Projection;
-import edu.sc.seis.fissuresUtil.cache.WorkerThreadPool;
 
-public class LayerProjectionUpdater implements Runnable {
-
+public class LayerProjectionUpdater{
     public static void update(ProjectionEvent e,
                               OMGraphicList graphics,
                               Layer layer) {
-        threadPool.invokeLater(new LayerProjectionUpdater(e, graphics, layer));
-    }
-
-    private LayerProjectionUpdater(ProjectionEvent e, OMGraphicList graphics,
-            Layer layer) {
-        event = e;
-        this.graphics = graphics;
-        this.layer = layer;
-    }
-
-    public void run() {
-        Projection projection = event.getProjection();
+        Projection projection = e.getProjection();
         layer.setProjection(projection);
         if(graphics != null) {
             synchronized(graphics) {
@@ -36,13 +23,4 @@ public class LayerProjectionUpdater implements Runnable {
         }
         layer.repaint();
     }
-
-    private ProjectionEvent event;
-
-    private OMGraphicList graphics;
-
-    private Layer layer;
-
-    private static WorkerThreadPool threadPool = new WorkerThreadPool("Map Layer Updater",
-                                                                      1);
 }
