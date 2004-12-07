e3e72bd09b3396a392ab58c8c602f4cc64ac0be5
groves <groves@localhost>
2004-12-07 13:52:54 -0500

    [svn r11374] removed getWidth methood

--- src/edu/sc/seis/fissuresUtil/display/borders/AbstractUnitRangeBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/AbstractUnitRangeBorder.java
@@ -5,55 +5,55 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class AbstractUnitRangeBorder extends Border implements TitleProvider{
-    public AbstractUnitRangeBorder(int side, int order){
+public abstract class AbstractUnitRangeBorder extends Border implements
+        TitleProvider {
+
+    public AbstractUnitRangeBorder(int side, int order) {
         super(side, order);
         add((TitleProvider)this);
-        setPreferredSize(new Dimension(width, 50));
+        setPreferredSize(new Dimension(80, 50));
     }
 
     protected List createFormats() {
         List formats = new ArrayList();
-        for (double i = .000000001; i <= 100000000; i *= 10) {
+        for(double i = .000000001; i <= 100000000; i *= 10) {
             formats.add(new UnitRangeFormatter(i, 2));
-            formats.add(new UnitRangeFormatter(i*5, 5));
+            formats.add(new UnitRangeFormatter(i * 5, 5));
         }
         return formats;
     }
-    public int getWidth(){
-        return width;
-    }
 
     public abstract String getTitle();
-    
-    private int width=80;
 
-    private class UnitRangeFormatter extends BorderFormat{
+    private class UnitRangeFormatter extends BorderFormat {
 
-        public UnitRangeFormatter(double division, int ticksPerDivision){
+        public UnitRangeFormatter(double division, int ticksPerDivision) {
             super(division, ticksPerDivision);
-            if(division < .01){
+            if(division < .01) {
                 // exponential notation
                 df = new DecimalFormat("#.###E00");
-            }else if (division < 10 && division != 0 ) {
+            } else if(division < 10 && division != 0) {
                 df = new DecimalFormat("0.00###");
-            } else if(division < 100000){
+            } else if(division < 100000) {
                 df = new DecimalFormat("#.####");
-            }else{
+            } else {
                 // exponential notation
                 df = new DecimalFormat("#.###E00");
             }
         }
 
-        public String getMaxString() { return df.format(divSize * 10); }
+        public String getMaxString() {
+            return df.format(divSize * 10);
+        }
 
-        public String getLabel(double value) { return df.format(value); }
+        public String getLabel(double value) {
+            return df.format(value);
+        }
 
         public String toString() {
-            return "UnitRange "+getDivSize();
+            return "UnitRange " + getDivSize();
         }
 
         private DecimalFormat df;
     }
 }
-
