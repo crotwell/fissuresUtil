b123f015b60839d19ce7a430d4a72be2f945e15d
Charles Groves <groves@seis.sc.edu>
2004-12-07 10:11:06 -0500

    [svn r11362] switch to log4j from System.out since props all have it

--- src/edu/sc/seis/fissuresUtil/namingService/NameServiceCopy.java
+++ src/edu/sc/seis/fissuresUtil/namingService/NameServiceCopy.java
@@ -1,9 +1,8 @@
 /**
  * NameServiceCopy.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.namingService;
 
 import org.omg.CORBA_2_3.ORB;
@@ -20,115 +19,132 @@ import edu.sc.seis.fissuresUtil.simple.Initializer;
 
 public class NameServiceCopy {
 
-    /**
-     *
-     */
-    public static void main(String[] args) throws CannotProceed, InvalidName, NotFound, org.omg.CORBA.ORBPackage.InvalidName {
+    private static void logErrExit(String msg) {
+        System.err.println(msg);
+        logger.error(msg);
+        System.exit(1);
+    }
 
+    public static void main(String[] args) throws CannotProceed, InvalidName,
+            NotFound, org.omg.CORBA.ORBPackage.InvalidName {
         // this parse the args, reads properties, and inits the orb
         Initializer.init(args);
-
         boolean skipServerPing = false;
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("--noping")) {
-                System.out.println("Skipping server ping");
+        for(int i = 0; i < args.length; i++) {
+            if(args[i].equals("--noping")) {
+                logger.info("Skipping server ping");
                 skipServerPing = true;
-            } else if (args[i].equals("-h") || args[i].equals("--help")) {
+            } else if(args[i].equals("-h") || args[i].equals("--help")) {
                 System.out.println("-props propfile  Connfiguration properties");
                 System.out.println("--noping         Do not ping servers before copy");
                 System.out.println("-h --help        Print this help message");
+                System.exit(0);
             }
         }
         String dnsToCopy = System.getProperty("nameServiceCopy.dns");
-        if (dnsToCopy == null) {
-            System.err.println("System property nameServiceCopy.dns must be set");
-            System.exit(1);
+        if(dnsToCopy == null) {
+            logErrExit("System property nameServiceCopy.dns must be set");
         }
         String copyToNS = System.getProperty("nameServiceCopy.copyTo");
-        if (copyToNS == null) {
-            System.err.println("System property nameServiceCopy.copyTo must be set");
-            System.exit(1);
+        if(copyToNS == null) {
+            logErrExit("System property nameServiceCopy.copyTo must be set");
         }
-
-        System.out.println("Copying all from "+dnsToCopy+" to "+copyToNS);
-
+        logger.info("Copying all from " + dnsToCopy + " to " + copyToNS);
         FissuresNamingService fisNS = Initializer.getNS();
-        if (fisNS.getNameService()== null) {
-            System.out.println("Problem, ns is null");
-            System.exit(1);
+        if(fisNS.getNameService() == null) {
+            logErrExit("Problem, ns is null");
         }
         fisNS.addOtherNameServiceCorbaLoc(copyToNS);
         ORB orb = Initializer.getORB();
         org.omg.CORBA.Object ncObj = orb.string_to_object(copyToNS);
         NamingContextExt copyToNameContext = NamingContextExtHelper.narrow(ncObj);
-
-        //"corbaloc:iiop:dmc.iris.washington.edu:6371/NameService");
-        //fisNS.addOtherNameServiceCorbaLoc("corbaloc:iiop:roo.seis.sc.edu:6371/NameService");
-
         NSSeismogramDC[] seisDC = fisNS.getAllSeismogramDC();
-        System.out.println("Got "+seisDC.length+" seis datacenters");
-        for (int i = 0; i < seisDC.length; i++) {
-            if (seisDC[i].getServerDNS().equals(dnsToCopy)) {
-                if ( skipServerPing || ! seisDC[i].getCorbaObject()._non_existent()) {
-                    System.out.println("Rebind "+seisDC[i].getServerDNS()+", "+seisDC[i].getServerName());
-                    fisNS.rebind(seisDC[i].getServerDNS(), seisDC[i].getServerName(), seisDC[i].getCorbaObject(), copyToNameContext);
+        logger.info("Got " + seisDC.length + " seis datacenters");
+        for(int i = 0; i < seisDC.length; i++) {
+            String dns = seisDC[i].getServerDNS();
+            String name = seisDC[i].getServerName();
+            if(dns.equals(dnsToCopy)) {
+                if(skipServerPing
+                        || !seisDC[i].getCorbaObject()._non_existent()) {
+                    fisNS.rebind(dns,
+                                 name,
+                                 seisDC[i].getCorbaObject(),
+                                 copyToNameContext);
                 } else {
-                    System.out.println("Couldn't ping "+seisDC[i].getServerDNS()+" "+seisDC[i].getServerName()+", skipping");
+                    logger.info("Couldn't ping " + dns + " " + name
+                            + ", skipping");
                 }
             } else {
-                System.out.println("Doesn't match "+seisDC[i].getServerDNS()+", "+seisDC[i].getServerName());
+                logger.info("Doesn't match " + dns + ", " + name);
             }
         }
-
         NSNetworkDC[] networkDC = fisNS.getAllNetworkDC();
-        System.out.println("Got "+networkDC.length+" network datacenters");
-        for (int i = 0; i < networkDC.length; i++) {
-            if (networkDC[i].getServerDNS().equals(dnsToCopy)) {
-                if ( skipServerPing || ! networkDC[i].getCorbaObject()._non_existent()) {
-                    System.out.println("Rebind "+networkDC[i].getServerDNS()+", "+networkDC[i].getServerName());
-                    fisNS.rebind(networkDC[i].getServerDNS(), networkDC[i].getServerName(), networkDC[i].getNetworkDC(), copyToNameContext);
+        logger.info("Got " + networkDC.length + " network datacenters");
+        for(int i = 0; i < networkDC.length; i++) {
+            String dns = networkDC[i].getServerDNS();
+            String name = networkDC[i].getServerName();
+            if(dns.equals(dnsToCopy)) {
+                if(skipServerPing
+                        || !networkDC[i].getCorbaObject()._non_existent()) {
+                    logger.info("Rebind " + dns + ", " + name);
+                    fisNS.rebind(dns,
+                                 name,
+                                 networkDC[i].getNetworkDC(),
+                                 copyToNameContext);
                 } else {
-                    System.out.println("Couldn't ping "+networkDC[i].getServerDNS()+" "+networkDC[i].getServerName()+", skipping");
+                    logger.info("Couldn't ping " + dns + " " + name
+                            + ", skipping");
                 }
             } else {
-                System.out.println("Doesn't match "+networkDC[i].getServerDNS()+", "+networkDC[i].getServerName());
+                logger.info("Doesn't match " + dns + ", " + name);
             }
         }
-
         NSEventDC[] eventDC = fisNS.getAllEventDC();
-        System.out.println("Got "+eventDC.length+" event datacenters");
-        for (int i = 0; i < eventDC.length; i++) {
-            if (eventDC[i].getServerDNS().equals(dnsToCopy)) {
+        logger.info("Got " + eventDC.length + " event datacenters");
+        for(int i = 0; i < eventDC.length; i++) {
+            String dns = eventDC[i].getServerDNS();
+            String name = eventDC[i].getServerName();
+            if(dns.equals(dnsToCopy)) {
                 // only copy if can ping orginal
-                if ( skipServerPing || ! eventDC[i].getCorbaObject()._non_existent()) {
-                    System.out.println("Rebind "+eventDC[i].getServerDNS()+", "+eventDC[i].getServerName());
-                    fisNS.rebind(eventDC[i].getServerDNS(), eventDC[i].getServerName(), eventDC[i].getCorbaObject(), copyToNameContext);
+                if(skipServerPing
+                        || !eventDC[i].getCorbaObject()._non_existent()) {
+                    logger.info("Rebind " + dns + ", " + name);
+                    fisNS.rebind(dns,
+                                 name,
+                                 eventDC[i].getCorbaObject(),
+                                 copyToNameContext);
                 } else {
-                    System.out.println("Couldn't ping "+eventDC[i].getServerDNS()+" "+eventDC[i].getServerName()+", skipping");
+                    logger.info("Couldn't ping " + dns + " " + name
+                            + ", skipping");
                 }
             } else {
-                System.out.println("Doesn't match "+eventDC[i].getServerDNS()+", "+eventDC[i].getServerName());
+                logger.info("Doesn't match " + dns + ", " + name);
             }
         }
-
         NSPlottableDC[] plotDC = fisNS.getAllPlottableDC();
-        System.out.println("Got "+plotDC.length+" seis datacenters");
-        for (int i = 0; i < plotDC.length; i++) {
-            if (plotDC[i].getServerDNS().equals(dnsToCopy)) {
+        logger.info("Got " + plotDC.length + " seis datacenters");
+        for(int i = 0; i < plotDC.length; i++) {
+            String dns = plotDC[i].getServerDNS();
+            String name = plotDC[i].getServerName();
+            if(dns.equals(dnsToCopy)) {
                 // only copy if can ping orginal
-                if (skipServerPing || ! plotDC[i].getCorbaObject()._non_existent()) {
-                    System.out.println("Rebind "+plotDC[i].getServerDNS()+", "+plotDC[i].getServerName());
-                    fisNS.rebind(plotDC[i].getServerDNS(), plotDC[i].getServerName(), plotDC[i].getPlottableDC(), copyToNameContext);
+                if(skipServerPing
+                        || !plotDC[i].getCorbaObject()._non_existent()) {
+                    logger.info("Rebind " + dns + ", " + name);
+                    fisNS.rebind(dns,
+                                 name,
+                                 plotDC[i].getPlottableDC(),
+                                 copyToNameContext);
                 } else {
-                    System.out.println("Couldn't ping "+plotDC[i].getServerDNS()+" "+plotDC[i].getServerName()+", skipping");
+                    logger.info("Couldn't ping " + dns + " " + name
+                            + ", skipping");
                 }
             } else {
-                System.out.println("Doesn't match "+plotDC[i].getServerDNS()+", "+plotDC[i].getServerName());
+                logger.info("Doesn't match " + dns + ", " + name);
             }
         }
-
-        System.out.println("Done");
+        logger.info("Done");
     }
+
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(NameServiceCopy.class);
 }
-
-
