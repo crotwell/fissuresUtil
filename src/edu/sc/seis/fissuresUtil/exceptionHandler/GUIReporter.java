d60d2fec2aeda5f76bd10a49c73b4ca80f13d83f
groves <groves@localhost>
2005-01-03 14:41:28 -0500

    [svn r11550] added getGreeting and appendToGreeting methods

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/GUIReporter.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/GUIReporter.java
@@ -24,24 +24,19 @@ import javax.swing.WindowConstants;
 import org.apache.log4j.Logger;
 
 /**
- * Description: This class can be used to display the GUI showing the exception along with useful information.
- * It also shows the stackTrace. It also gives the option of saving the exception stack trace along with other
- * useful information added by the user.
- *
- *
- * Created: Thu Jan 31 16:39:57 2002
- *
- * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * Description: This class can be used to display the GUI showing the exception
+ * along with useful information. It also shows the stackTrace. It also gives
+ * the option of saving the exception stack trace along with other useful
+ * information added by the user. Created: Thu Jan 31 16:39:57 2002
+ * 
+ * @author <a href="mailto:">Srinivasa Telukutla </a>
  * @version
  */
-
-
-
-public class GUIReporter implements ExceptionReporter{
+public class GUIReporter implements ExceptionReporter {
 
     public void report(String message, Throwable e, List sections) {
-        if (displayFrame != null) {
-            if (atMostOneAtATime) {
+        if(displayFrame != null) {
+            if(atMostOneAtATime) {
                 // in this case, there is already a GUIReporter up, with a
                 // previous exception. So, we bounce this exception. The
                 // GlobalExceptionHandler will send it to the log files, so
@@ -58,33 +53,36 @@ public class GUIReporter implements ExceptionReporter{
         this.e = e;
         this.sections = sections;
         createFrame();
-        displayPanel.add(createGUI(e, message, sections),
-                         BorderLayout.CENTER);
+        displayPanel.add(createGUI(e, message, sections), BorderLayout.CENTER);
         displayPanel.revalidate();
         SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        displayFrame.pack();
-                        displayFrame.show();
-                    }
-                });
+
+            public void run() {
+                displayFrame.pack();
+                displayFrame.show();
+            }
+        });
     }
 
-    /** Determines if the display will show more than one exception at a time.
+    /**
+     * Determines if the display will show more than one exception at a time.
      * The default is TRUE.
-     **/
+     */
     public void setAtMostOneAtATime(boolean b) {
         atMostOneAtATime = b;
     }
 
     private JTabbedPane createGUI(Throwable e, String message, List sections) {
         JTabbedPane tabbedPane = new JTabbedPane();
-        if(greeting != null){
-            tabbedPane.addTab(greeting.getName(), createTextArea(greeting.getContents()));
+        if(greeting != null) {
+            tabbedPane.addTab(greeting.getName(),
+                              createTextArea(greeting.getContents()));
         }
         tabbedPane.addTab("Details", createTextArea(message));
-        tabbedPane.addTab("Stack Trace", createTextArea(ExceptionReporterUtils.getTrace(e)));
+        tabbedPane.addTab("Stack Trace",
+                          createTextArea(ExceptionReporterUtils.getTrace(e)));
         Iterator it = sections.iterator();
-        while(it.hasNext()){
+        while(it.hasNext()) {
             Section sec = (Section)it.next();
             tabbedPane.addTab(sec.getName(), createTextArea(sec.getContents()));
         }
@@ -94,13 +92,13 @@ public class GUIReporter implements ExceptionReporter{
         return tabbedPane;
     }
 
-    private static JScrollPane createTextArea(String message){
+    private static JScrollPane createTextArea(String message) {
         JTextArea messageArea = new JTextArea();
         messageArea.setLineWrap(true);
         messageArea.setFont(new Font("Serif", Font.PLAIN, 14));
         messageArea.setWrapStyleWord(true);
         messageArea.setEditable(false);
-        if(message != null){
+        if(message != null) {
             messageArea.setText(message);
         }
         return new JScrollPane(messageArea);
@@ -108,59 +106,62 @@ public class GUIReporter implements ExceptionReporter{
 
     private void createFrame() {
         displayFrame = new JFrame();
-        displayFrame.setDefaultCloseOperation (WindowConstants.DISPOSE_ON_CLOSE);
-        displayFrame.addWindowListener(new WindowAdapter(){
-                    public void windowClosing(WindowEvent e) {
-                        displayPanel = null;
-                        displayFrame = null;
-                    }
-                });
+        displayFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        displayFrame.addWindowListener(new WindowAdapter() {
+
+            public void windowClosing(WindowEvent e) {
+                displayPanel = null;
+                displayFrame = null;
+            }
+        });
         displayPanel = new JPanel(new BorderLayout());
-        displayPanel.add(createButtonPanel(),
-                         BorderLayout.SOUTH);
+        displayPanel.add(createButtonPanel(), BorderLayout.SOUTH);
         Dimension dimension = new Dimension(800, 400);
         displayPanel.setPreferredSize(dimension);
         displayFrame.setContentPane(displayPanel);
         displayFrame.setSize(dimension);
     }
 
-    private JPanel createButtonPanel(){
+    private JPanel createButtonPanel() {
         JButton closeButton = new JButton("Close");
         closeButton.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        displayFrame.dispose();
-                        displayFrame = null;
-                        displayPanel = null;
-                    }
-                });
+
+            public void actionPerformed(ActionEvent e) {
+                displayFrame.dispose();
+                displayFrame = null;
+                displayPanel = null;
+            }
+        });
         JButton saveToFile = new JButton("Save");
         saveToFile.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent ex) {
-                        try{
-                            writeFile();
-                        }catch(IOException e){
-                            int result = JOptionPane.showConfirmDialog(displayPanel,
-                                                                       "We were unable to write to that file. Try again?",
-                                                                       "Trouble writing exception file",
-                                                                       JOptionPane.OK_CANCEL_OPTION,
-                                                                       JOptionPane.WARNING_MESSAGE);
-                            if(result == JOptionPane.OK_OPTION){
-                                actionPerformed(null);
-                            }
-                        }
-                    }
 
-                    public void writeFile()throws IOException{
-                        JFileChooser fileChooser = new JFileChooser();
-                        fileChooser.setSelectedFile(new File(ExceptionReporterUtils.getClassName(e) + ".txt"));
-                        int rtnVal = fileChooser.showSaveDialog(displayPanel);
-                        if(rtnVal == JFileChooser.APPROVE_OPTION) {
-                            FileWriterReporter writer = new FileWriterReporter(fileChooser.getSelectedFile().getAbsoluteFile());
-                            writer.report(message, e, sections);
-                        }
+            public void actionPerformed(ActionEvent ex) {
+                try {
+                    writeFile();
+                } catch(IOException e) {
+                    int result = JOptionPane.showConfirmDialog(displayPanel,
+                                                               "We were unable to write to that file. Try again?",
+                                                               "Trouble writing exception file",
+                                                               JOptionPane.OK_CANCEL_OPTION,
+                                                               JOptionPane.WARNING_MESSAGE);
+                    if(result == JOptionPane.OK_OPTION) {
+                        actionPerformed(null);
                     }
-                });
+                }
+            }
 
+            public void writeFile() throws IOException {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setSelectedFile(new File(ExceptionReporterUtils.getClassName(e)
+                        + ".txt"));
+                int rtnVal = fileChooser.showSaveDialog(displayPanel);
+                if(rtnVal == JFileChooser.APPROVE_OPTION) {
+                    FileWriterReporter writer = new FileWriterReporter(fileChooser.getSelectedFile()
+                            .getAbsoluteFile());
+                    writer.report(message, e, sections);
+                }
+            }
+        });
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(closeButton);
         buttonPanel.add(saveToFile);
@@ -168,10 +169,23 @@ public class GUIReporter implements ExceptionReporter{
     }
 
     public static void setGreeting(String title, String contents) {
-        if(greeting != null){
-            contents = greeting.getContents() + contents;
+        setGreeting(new Section(title, contents));
+    }
+
+    public static void appendToGreeting(String title, String contents) {
+        if(greeting == null) {
+            setGreeting(title, contents);
+        } else {
+            greeting.setContents(greeting.getContents() + contents);
         }
-        greeting = new Section(title, contents);
+    }
+
+    public static void setGreeting(Section greeting) {
+        GUIReporter.greeting = greeting;
+    }
+
+    public static Section getGreeting() {
+        return greeting;
     }
 
     private boolean atMostOneAtATime = true;
