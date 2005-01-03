0c66a8688278501d443e18918a20c0bc1bd321da
groves <groves@localhost>
2005-01-03 14:41:02 -0500

    [svn r11549] cleaned up a bit

--- src/edu/sc/seis/fissuresUtil/database/AbstractDb.java
+++ src/edu/sc/seis/fissuresUtil/database/AbstractDb.java
@@ -5,20 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * This class acts an abstract class for database Operations.
- * AbstractDb.java
- *
- *
- * Created: Fri Feb  7 10:45:30 2003
- *
- * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * This class acts an abstract class for database Operations. AbstractDb.java
+ * Created: Fri Feb 7 10:45:30 2003
+ * 
+ * @author <a href="mailto:">Srinivasa Telukutla </a>
  * @version
  */
-
 public abstract class AbstractDb {
-    public AbstractDb () {}
 
-    public AbstractDb (String directoryName, String databaseName){
+    public AbstractDb(String directoryName, String databaseName) {
         this.directoryName = directoryName;
         this.databaseName = databaseName;
     }
@@ -28,14 +23,14 @@ public abstract class AbstractDb {
             if(connection == null) {
                 String driverName = new String("org.hsqldb.jdbcDriver");
                 Class.forName(driverName).newInstance();
-                connection = DriverManager.getConnection("jdbc:hsqldb:"+directoryName+"/"+databaseName, "sa", "");
+                connection = DriverManager.getConnection("jdbc:hsqldb:"
+                        + directoryName + "/" + databaseName, "sa", "");
             }
             return connection;
         } catch(Exception sqle) {
             sqle.printStackTrace();
             return null;
         }
-
     }
 
     public abstract void create() throws SQLException;
@@ -45,5 +40,4 @@ public abstract class AbstractDb {
     protected String databaseName;
 
     protected String directoryName;
-
 }// AbstractDb
