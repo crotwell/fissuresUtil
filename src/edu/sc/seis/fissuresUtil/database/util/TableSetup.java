d7de8e421a97631271a3f1ba8837bbcf4994a0cb
crotwell <crotwell@localhost>
2005-01-18 15:37:55 -0500

    [svn r11721] add utility setup methods, create sequences where needed

--- src/edu/sc/seis/fissuresUtil/database/util/TableSetup.java
+++ src/edu/sc/seis/fissuresUtil/database/util/TableSetup.java
@@ -3,7 +3,10 @@ package edu.sc.seis.fissuresUtil.database.util;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.context.Context;
 import edu.sc.seis.fissuresUtil.database.DBUtil;
+import edu.sc.seis.fissuresUtil.database.JDBCSequence;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 /**
@@ -11,10 +14,22 @@ import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
  */
 public class TableSetup {
 
-    public static void setup(String tablename,
+    public static void setup(String tableName, Connection conn, Object tableObj, String propFile) throws Exception {
+        setup(tableName, conn, tableObj, propFile, new VelocityContext());
+    }
+    
+    public static void setup(String tableName, Connection conn, Object tableObj, String propFile, Context ctx) throws Exception {
+        ctx.put("tablename", tableName);
+        SQLLoader sql = new SQLLoader(propFile, ctx);
+        TableSetup.customSetup(tableName, conn, tableObj, sql);
+    }
+    
+    public static void customSetup(String tablename,
                       Connection conn,
                       Object tableObj,
                       SQLLoader statements) throws SQLException {
+        // create seq before table in case of table dependency
+        createSequence(tablename, conn, tableObj, statements);
         createTable(tablename, conn, statements.get(tablename + ".create"));
         prepareStatements(tablename, conn, tableObj, statements);
     }
@@ -22,8 +37,48 @@ public class TableSetup {
     private static void createTable(String tablename,
                              Connection conn,
                              String creationStmt) throws SQLException {
-        if(!DBUtil.tableExists(tablename, conn)) {
-            conn.createStatement().executeUpdate(creationStmt);
+        if( ! DBUtil.tableExists(tablename, conn)) {
+            if (creationStmt == null ) {
+                throw new IllegalArgumentException("creation Statement, cannot be null.");
+            }
+            try {
+                conn.createStatement().executeUpdate(creationStmt);
+            } catch (SQLException e) {
+                logger.error("problem statement: "+creationStmt);
+                SQLException sqle = new SQLException(e.getMessage()+" "+creationStmt);
+                sqle.setStackTrace(e.getStackTrace());
+                throw sqle;
+            }
+        }
+    }
+    
+    /** creates JDBCSequences for fields named tableName+"Seq" using the
+     * property tableName+"Seq.create" and tableName+"Seq.nextVal"
+     */
+    public static void createSequence(String tableName,
+                                      Connection conn,
+                                      Object tableObj,
+                                      SQLLoader statements) throws SQLException {
+        try {
+        Field field = tableObj.getClass().getField(tableName+"Seq");
+        String key = tableName + "Seq.create";
+        if(field != null ) {
+            if (statements.has(key)) {
+                String sql = statements.get(key);
+                field.setAccessible(true);
+                field.set(tableObj, new JDBCSequence(conn, tableName+"Seq", statements.get(tableName+"Seq.create"), statements.get(tableName+"Seq.nextVal")));
+            } else {
+                throw new IllegalArgumentException(tableName+"Seq.create is not defined, unable to create sequence");
+            }
+        }
+        } catch(NoSuchFieldException e) {
+            // no field following the naming convention, so skip
+        } catch(IllegalArgumentException e) {
+            GlobalExceptionHandler.handle("Thought this couldn't happen since I checked the object type.",
+                                          e);
+        } catch(IllegalAccessException e) {
+            GlobalExceptionHandler.handle("Thought this couldn't happen since I called setAccessible.  Looks like I was wrong",
+                                          e);
         }
     }
 
@@ -56,4 +111,6 @@ public class TableSetup {
             }
         }
     }
+    
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(TableSetup.class);
 }
