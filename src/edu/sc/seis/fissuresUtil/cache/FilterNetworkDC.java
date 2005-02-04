6143edbeed96d6104f16c5c467de715a7f35cf54
groves <groves@localhost>
2005-02-04 16:01:57 -0500

    [svn r11972] added Patter reading static method from ChannelChooserTask

--- src/edu/sc/seis/fissuresUtil/cache/FilterNetworkDC.java
+++ src/edu/sc/seis/fissuresUtil/cache/FilterNetworkDC.java
@@ -1,5 +1,13 @@
 package edu.sc.seis.fissuresUtil.cache;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import org.omg.CORBA.NO_IMPLEMENT;
 import edu.iris.Fissures.IfNetwork.NetworkDCOperations;
@@ -21,7 +29,27 @@ public class FilterNetworkDC extends AbstractProxyNetworkDC {
     }
 
     public NetworkFinder a_finder() {
-        return new FilterNetworkFinder(getWrappedDC().a_finder(), this, patterns);
+        return new FilterNetworkFinder(getWrappedDC().a_finder(),
+                                       this,
+                                       patterns);
+    }
+
+    public static Pattern[] readPattern(String filterURL) throws IOException {
+        InputStream filterStream = new URL(filterURL.trim()).openStream();
+        Reader reader = new BufferedReader(new InputStreamReader(filterStream));
+        int curInt;
+        StringBuffer buff = new StringBuffer();
+        List gottenPatterns = new ArrayList();
+        while((curInt = reader.read()) != -1) {
+            char curChar = (char)curInt;
+            if(curChar != '\n') {
+                buff.append(curChar);
+            } else {
+                gottenPatterns.add(Pattern.compile(buff.toString()));
+                buff = new StringBuffer();
+            }
+        }
+        return (Pattern[])gottenPatterns.toArray(new Pattern[0]);
     }
 
     private Pattern[] patterns;
