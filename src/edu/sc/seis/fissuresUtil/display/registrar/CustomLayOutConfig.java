b02b1d5c5e1882468cd30c10239765bbffe6c651
danala <danala@localhost>
2005-02-07 16:36:32 -0500

    [svn r11990] adjust the axis around the lower and upper coordinates to avoid clipping of seismograms

--- src/edu/sc/seis/fissuresUtil/display/registrar/CustomLayOutConfig.java
+++ src/edu/sc/seis/fissuresUtil/display/registrar/CustomLayOutConfig.java
@@ -5,50 +5,55 @@ import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.model.UnitRangeImpl;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 
-
 /**
- * @author danala
- * Created on Jan 20, 2005
+ * @author danala Created on Jan 20, 2005 
+ * Creates a custom layout for use in REV
  */
-public class CustomLayOutConfig extends BasicLayoutConfig{
-    
-public CustomLayOutConfig(double minDistance, double maxDistance){
-    super();
-    this.minDistance = minDistance;
-    this.maxDistance = maxDistance;
-}
+public class CustomLayOutConfig extends BasicLayoutConfig {
 
-public LayoutEvent getLayout(){ return lastEvent; }
+    public CustomLayOutConfig(double minDistance, double maxDistance) {
+        super();
+        double axisCorrection = (maxDistance - minDistance) * PERCENT_OFFSET;
+        this.minDistance = minDistance - axisCorrection;
+        this.maxDistance = maxDistance + axisCorrection;
+    }
 
-public synchronized LayoutEvent generateLayoutEvent(){
-    
-    DataSetSeismogram[] seis = getSeismograms();
-    if(seis.length > 0){
-        double startDist =  minDistance;
-        double endDist = maxDistance;
-        double totalDistance = endDist - startDist;
-        double percentageOffset = 0.1;
-        LayoutData[] data = new LayoutData[seis.length];
-        for (int i = 0; i < data.length; i++){
-            DataSetSeismogram cur = seis[i];
-            double curDist = ((QuantityImpl)valueMap.get(cur)).getValue();
-            double centerPercentage = (curDist - startDist)/totalDistance;
-            data[i] = new LayoutData(cur,
-                                     centerPercentage - percentageOffset,
-                                     centerPercentage + percentageOffset);
-
-        }
-        UnitRangeImpl range = new UnitRangeImpl(startDist, endDist, UnitImpl.DEGREE);
-        lastEvent = new  LayoutEvent(data, range);
+    public LayoutEvent getLayout() {
         return lastEvent;
     }
-    lastEvent = LayoutEvent.EMPTY_EVENT;
-    return lastEvent;
-}
-private LayoutEvent lastEvent= LayoutEvent.EMPTY_EVENT;
-private double minDistance;
-private double maxDistance;
 
+    public synchronized LayoutEvent generateLayoutEvent() {
+        DataSetSeismogram[] seis = getSeismograms();
+        if(seis.length > 0) {
+            double startDist = minDistance;
+            double endDist = maxDistance;
+            double totalDistance = endDist - startDist;
+            LayoutData[] data = new LayoutData[seis.length];
+            for(int i = 0; i < data.length; i++) {
+                DataSetSeismogram cur = seis[i];
+                double curDist = ((QuantityImpl)valueMap.get(cur)).getValue();
+                double centerPercentage = (endDist - curDist) / totalDistance;
+                data[i] = new LayoutData(cur, centerPercentage
+                        - PERCENT_OFFSET, centerPercentage + PERCENT_OFFSET);
+            }
+            UnitRangeImpl range = null;
+            if(startDist > endDist) {
+                range = new UnitRangeImpl(endDist, startDist, UnitImpl.DEGREE);
+            } else {
+                range = new UnitRangeImpl(startDist, endDist, UnitImpl.DEGREE);
+            }
+            lastEvent = new LayoutEvent(data, range);
+            return lastEvent;
+        }
+        lastEvent = LayoutEvent.EMPTY_EVENT;
+        return lastEvent;
+    }
 
+    private LayoutEvent lastEvent = LayoutEvent.EMPTY_EVENT;
+    
+    public static double PERCENT_OFFSET = 0.1;
 
+    private double minDistance;
+
+    private double maxDistance;
 }
