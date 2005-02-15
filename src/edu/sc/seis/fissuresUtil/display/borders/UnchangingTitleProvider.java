8037b4c712868311407cd129ae6cc2a6b85e371a
Charles Groves <groves@seis.sc.edu>
2005-02-15 16:42:56 -0500

    [svn r12106] use DEFAULT_FONT for getFont

--- src/edu/sc/seis/fissuresUtil/display/borders/UnchangingTitleProvider.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/UnchangingTitleProvider.java
@@ -1,10 +1,27 @@
 package edu.sc.seis.fissuresUtil.display.borders;
 
-public class UnchangingTitleProvider implements TitleProvider{
-    public UnchangingTitleProvider(String title){ this.title = title; }
+import java.awt.Font;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 
-    public String getTitle() { return title; }
+public class UnchangingTitleProvider implements TitleProvider {
+
+    public UnchangingTitleProvider(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 
     private String title;
-}
 
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font f) {
+        this.font = f;
+    }
+
+    private Font font = DisplayUtils.DEFAULT_FONT;
+}
