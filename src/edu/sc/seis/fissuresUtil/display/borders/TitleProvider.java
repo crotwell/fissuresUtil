e7e268989d65be7b3a1f56d665b6af4dbce46764
groves <groves@localhost>
2005-02-15 16:42:44 -0500

    [svn r12105] add a getFont method

--- src/edu/sc/seis/fissuresUtil/display/borders/TitleProvider.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/TitleProvider.java
@@ -1,6 +1,10 @@
 package edu.sc.seis.fissuresUtil.display.borders;
 
-public interface TitleProvider{
-    public String getTitle();
-}
+import java.awt.Font;
 
+public interface TitleProvider {
+
+    public String getTitle();
+
+    public Font getFont();
+}
