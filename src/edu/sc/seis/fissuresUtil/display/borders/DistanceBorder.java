d5b4bf0aba7d45589f20dc60daca1869ce9b8ccd
groves <groves@localhost>
2005-02-24 12:19:42 -0500

    [svn r12223] only use a seismogram display internally

--- src/edu/sc/seis/fissuresUtil/display/borders/DistanceBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/DistanceBorder.java
@@ -1,31 +1,32 @@
 /**
  * DistanceBorder.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.display.borders;
+
 import edu.iris.Fissures.model.UnitRangeImpl;
-import edu.sc.seis.fissuresUtil.display.RecordSectionDisplay;
+import edu.sc.seis.fissuresUtil.display.SeismogramDisplay;
 
+public class DistanceBorder extends AbstractUnitRangeBorder {
 
-
-public class DistanceBorder extends AbstractUnitRangeBorder{
-    public DistanceBorder(RecordSectionDisplay rsd){
-        this(rsd,Border.LEFT,Border.DESCENDING);
+    public DistanceBorder(SeismogramDisplay rsd) {
+        this(rsd, Border.LEFT, Border.DESCENDING);
     }
-    public DistanceBorder(RecordSectionDisplay rsd, int side, int order){
-        super(side,order);
-        this.rsd=rsd;
+
+    public DistanceBorder(SeismogramDisplay rsd, int side, int order) {
+        super(side, order);
+        this.rsd = rsd;
         displayNegatives = false;
     }
 
-    public String getTitle() { return "Distance (Degrees)"; }
-
-    public UnitRangeImpl getRange() {
-        return rsd.getLayoutConfig().getLayout().getDistance();
+    public String getTitle() {
+        return "Distance (Degrees)";
     }
 
-    private RecordSectionDisplay rsd;
-}
+    public UnitRangeImpl getRange() {
+        return rsd.getDistance();
+    }
 
+    private SeismogramDisplay rsd;
+}
