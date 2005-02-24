27005583acff843e41530f4004038c99f15e00dc
groves <groves@localhost>
2005-02-24 12:18:37 -0500

    [svn r12221] new constructor that turns off the default addition of an "Amplitude" border

--- src/edu/sc/seis/fissuresUtil/display/borders/AmpBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/AmpBorder.java
@@ -5,30 +5,42 @@ import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.display.SeismogramDisplay;
 import edu.sc.seis.fissuresUtil.display.UnitDisplayUtil;
 
-public class AmpBorder extends AbstractUnitRangeBorder{
-    public AmpBorder(SeismogramDisplay disp){ this(disp, LEFT); }
+public class AmpBorder extends AbstractUnitRangeBorder {
 
-    public AmpBorder(SeismogramDisplay disp, int side){
+    public AmpBorder(SeismogramDisplay disp) {
+        this(disp, LEFT);
+    }
+
+    public AmpBorder(SeismogramDisplay disp, int side) {
+        this(disp, side, true);
+    }
+
+    public AmpBorder(SeismogramDisplay disp, int side,
+            boolean includeAmplitudeLabel) {
         super(side, ASCENDING);
         this.disp = disp;
-        add(new UnchangingTitleProvider("Amplitude"));
+        if(includeAmplitudeLabel) {
+            add(new UnchangingTitleProvider("Amplitude"));
+        }
     }
 
     public UnitRangeImpl getRange() {
         last = disp.getAmpConfig().getAmp();
-        if(SeismogramDisplay.PRINTING && last.equals(DisplayUtils.ONE_RANGE)){
-            //Since ONE_RANGE may indicate that the amp config doesn't know, have
+        if(SeismogramDisplay.PRINTING && last.equals(DisplayUtils.ONE_RANGE)) {
+            //Since ONE_RANGE may indicate that the amp config doesn't know,
+            // have
             //it calculate just in case new data has come in since our intitial
             //get amp
-            last=disp.getAmpConfig().calculate().getAmp();
+            last = disp.getAmpConfig().calculate().getAmp();
         }
         return last;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return UnitDisplayUtil.getNameForUnit(last.getUnit());
     }
-    
+
     private UnitRangeImpl last;
+
     private SeismogramDisplay disp;
 }
