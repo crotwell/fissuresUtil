ac14e0574f5e1aef51b7c0993a8ba9185741efde
Charles Groves <groves@seis.sc.edu>
2005-02-24 12:20:18 -0500

    [svn r12225] zero out tick sizes on side setting

--- src/edu/sc/seis/fissuresUtil/display/borders/TitleBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/TitleBorder.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.display.borders;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import edu.iris.Fissures.model.UnitRangeImpl;
 
@@ -9,17 +10,24 @@ import edu.iris.Fissures.model.UnitRangeImpl;
  */
 public class TitleBorder extends Border {
 
-    public TitleBorder(int side, int order, String title) {
+    public TitleBorder(int side, int order) {
         super(side, order);
-        labelTickHeight = 0;
-        tickHeight = 0;
-        labelTickWidth = 0;
-        tickWidth = 0;
+        labelTickLength = 0;
+        tickLength = 0;
+        setSide(side);
+    }
+
+    public TitleBorder(int side, int order, String title) {
+        this(side, order);
         add(new UnchangingTitleProvider(title));
     }
 
     protected void paintBorder(Graphics2D g2d) {
         bf.draw(getRange(), g2d);
+    }
+
+    public String getMaxLengthFormattedString() {
+        return "";
     }
 
     private class TitleBorderFormat extends BorderFormat {
