9b50fc5c2e174accce55eec5aadf6fd2111a1733
Philip Crotwell <crotwell@seis.sc.edu>
2005-02-25 09:26:25 -0500

    [svn r12241] add when to error message

--- src/edu/sc/seis/fissuresUtil/cache/InstrumentationLoader.java
+++ src/edu/sc/seis/fissuresUtil/cache/InstrumentationLoader.java
@@ -39,7 +39,8 @@ public class InstrumentationLoader extends Thread
                                  ChannelIdUtil.toStringNoDates(nextWork.seis.getRequestFilter().channel_id));
             } catch (ChannelNotFound e) {
                 GlobalExceptionHandler.handle("Could not load instrumentation for channel "+
-                                ChannelIdUtil.toString(nextWork.seis.getRequestFilter().channel_id),
+                                ChannelIdUtil.toString(nextWork.seis.getRequestFilter().channel_id)+
+                                " at "+nextWork.seis.getBeginTime().date_time,
                             e);
             } catch (org.omg.CORBA.SystemException e) {
                 // might be a transient??? Try again if not too many tries
