95fedd445465a39e74cef237fca0d0b3ef3692c8
Charles Groves <groves@seis.sc.edu>
2005-02-28 16:08:43 -0500

    [svn r12292] added grid sizes for multi splendored

--- src/edu/sc/seis/fissuresUtil/mockFissures/IfNetwork/MockStation.java
+++ src/edu/sc/seis/fissuresUtil/mockFissures/IfNetwork/MockStation.java
@@ -5,37 +5,55 @@ import edu.iris.Fissures.IfNetwork.Station;
 import edu.iris.Fissures.network.StationImpl;
 import edu.sc.seis.fissuresUtil.mockFissures.MockLocation;
 
-public class MockStation{
+public class MockStation {
 
-    public static Station createStation(){
-        return new StationImpl(MockStationId.createStationId(), "Test Station",
-                               MockLocation.SIMPLE, "Joe", "this is a test",
-                               "still, a test", MockNetworkAttr.createNetworkAttr());
-    }
-    
-    public static Station createRestartedStation(){
-        return new StationImpl(MockStationId.createRestartedStationId(), "Test Station",
-                               MockLocation.SIMPLE, "Joe", "this is a test",
-                               "still, a test", MockNetworkAttr.createNetworkAttr());
+    public static Station createStation() {
+        return new StationImpl(MockStationId.createStationId(),
+                               "Test Station",
+                               MockLocation.SIMPLE,
+                               "Joe",
+                               "this is a test",
+                               "still, a test",
+                               MockNetworkAttr.createNetworkAttr());
     }
 
-    public static Station createOtherStation(){
-        return new StationImpl(MockStationId.createOtherStationId(), "Noitats tset",
-                               MockLocation.BERLIN, "Frank", "tset a si siht",
-                               "tset a ,llits", MockNetworkAttr.createOtherNetworkAttr());
+    public static Station createRestartedStation() {
+        return new StationImpl(MockStationId.createRestartedStationId(),
+                               "Test Station",
+                               MockLocation.SIMPLE,
+                               "Joe",
+                               "this is a test",
+                               "still, a test",
+                               MockNetworkAttr.createNetworkAttr());
     }
-    
-    public static Station[] createMultiSplendoredStations(){
-        Station[] stations = new Station[20];
-        Location[] locations = MockLocation.create(4, 5);
+
+    public static Station createOtherStation() {
+        return new StationImpl(MockStationId.createOtherStationId(),
+                               "Noitats tset",
+                               MockLocation.BERLIN,
+                               "Frank",
+                               "tset a si siht",
+                               "tset a ,llits",
+                               MockNetworkAttr.createOtherNetworkAttr());
+    }
+
+    public static Station[] createMultiSplendoredStations() {
+        return createMultiSplendoredStations(4, 5);
+    }
+
+    public static Station[] createMultiSplendoredStations(int rows, int columns) {
+        Station[] stations = new Station[rows * columns];
+        Location[] locations = MockLocation.create(rows, columns);
         for(int i = 0; i < stations.length; i++) {
-            stations[i] = new StationImpl(MockStationId.createMultiSplendoredId("MS" + i), "Multi"+i,
-                                          locations[i], "Charlie", "Grid of Stations",
-                                          "Many Station Group", MockNetworkAttr.createMultiSplendoredAttr()); 
+            stations[i] = new StationImpl(MockStationId.createMultiSplendoredId("MS"
+                                                  + i),
+                                          "Multi" + i,
+                                          locations[i],
+                                          "Charlie",
+                                          "Grid of Stations",
+                                          "Many Station Group",
+                                          MockNetworkAttr.createMultiSplendoredAttr());
         }
         return stations;
     }
-    
-    
-
 }
