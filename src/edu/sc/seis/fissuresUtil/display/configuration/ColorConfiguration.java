92e56226cc16545f127b7b4b09749363b3b66bda
groves <groves@localhost>
2005-03-02 00:03:58 -0500

    [svn r12315] added id and base attributes to allow interconfig references

--- src/edu/sc/seis/fissuresUtil/display/configuration/ColorConfiguration.java
+++ src/edu/sc/seis/fissuresUtil/display/configuration/ColorConfiguration.java
@@ -2,32 +2,49 @@ package edu.sc.seis.fissuresUtil.display.configuration;
 
 import java.awt.Color;
 import org.w3c.dom.Element;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 /**
  * @author groves Created on Feb 23, 2005
  */
-public class ColorConfiguration {
+public class ColorConfiguration implements Cloneable {
 
-    public ColorConfiguration(Element el) {
-        r = Integer.parseInt(DOMHelper.extractText(el, "red", "255"));
-        g = Integer.parseInt(DOMHelper.extractText(el, "green", "255"));
-        b = Integer.parseInt(DOMHelper.extractText(el, "blue", "255"));
-        a = Integer.parseInt(DOMHelper.extractText(el, "alpha", "255"));
+    public void configure(Element el) {
+        r = DOMHelper.extractText(el, "red", r);
+        g = DOMHelper.extractText(el, "green", g);
+        b = DOMHelper.extractText(el, "blue", b);
+        a = DOMHelper.extractText(el, "alpha", a);
     }
 
-    /**
-     * Creates a color configuration from the element named name that is a child
-     * of el if one exists. Returns null if no such element is found
-     */
-    public static ColorConfiguration create(Element el, String name) {
-        if(DOMHelper.hasElement(el, name)) { return new ColorConfiguration(DOMHelper.getElement(el,
-                                                                                                name)); }
-        return null;
+    public static ColorConfiguration create(Element el) {
+        ColorConfiguration c = null;
+        if(defs.hasDefinition(el)) {
+            try {
+                ColorConfiguration base = (ColorConfiguration)defs.getDefinition(el);
+                c = (ColorConfiguration)base.clone();
+            } catch(CloneNotSupportedException e) {
+                GlobalExceptionHandler.handle("But I added clone to this object....");
+            }
+        } else {
+            c = new ColorConfiguration();
+        }
+        c.configure(el);
+        defs.updateDefinitions(el, c);
+        return c;
     }
 
     public Color createColor() {
-        return new Color(r, g, b, a);
+        return new Color(Integer.parseInt(r),
+                         Integer.parseInt(g),
+                         Integer.parseInt(b),
+                         Integer.parseInt(a));
     }
 
-    private int r, g, b, a;
+    public String toString() {
+        return "ColorConfiguration that produces " + createColor();
+    }
+
+    private static ConfigDefinitions defs = new ConfigDefinitions();
+
+    private String r = "255", g = "255", b = "255", a = "255";
 }
