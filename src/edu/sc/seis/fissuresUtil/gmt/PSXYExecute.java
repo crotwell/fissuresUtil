bfe4b6353f98a381b80a662195bb0cda836c9965
groves <groves@localhost>
2005-03-03 16:16:16 -0500

    [svn r12347] make sure to close the file output stream

--- src/edu/sc/seis/fissuresUtil/gmt/PSXYExecute.java
+++ src/edu/sc/seis/fissuresUtil/gmt/PSXYExecute.java
@@ -1,18 +1,20 @@
 package edu.sc.seis.fissuresUtil.gmt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-//import org.apache.log4j.Logger;
-import edu.sc.seis.fissuresUtil.bag.StreamPump;
+import java.io.StringReader;
 
 /**
  * @author oliverpa Created on Jan 14, 2005
  */
 public class PSXYExecute {
+
+    public static void close(String psFilename, String projection, String region)
+            throws InterruptedException, IOException {
+        String command = "psxy /dev/null -V -J" + projection + " -R" + region
+                + " -O";
+        GenericCommandExecute.execute(command);
+    }
 
     public static void addPoints(String psFilename,
                                  String projection,
@@ -22,7 +24,6 @@ public class PSXYExecute {
                                  String outlineColor,
                                  double[][] points) throws IOException,
             InterruptedException {
-        Runtime rt = Runtime.getRuntime();
         String command = "psxy -V -J" + projection + " -R" + region + " -S"
                 + symbol + " -O -K";
         if(fillColor != null) {
@@ -31,37 +32,29 @@ public class PSXYExecute {
         if(outlineColor != null) {
             command += " -W" + outlineColor;
         }
-        System.out.println("executing gmt command: " + command);
-        Process proc = rt.exec(command);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(psFilename,
-                                                                                               true)));
-        BufferedWriter procWriter = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
-        BufferedReader errReader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-        BufferedWriter errWriter = new BufferedWriter(new OutputStreamWriter(System.err));
-        StreamPump pump = new StreamPump(reader, writer);
-        StreamPump errPump = new StreamPump(errReader, errWriter);
-        pump.start();
-        errPump.start();
+        StringBuffer buff = new StringBuffer();
         for(int i = 0; i < points.length; i++) {
             for(int j = 0; j < points[i].length; j++) {
-                procWriter.write(points[i][j] + "");
+                buff.append(points[i][j] + "");
                 if(j < points[i].length - 1) {
-                    procWriter.write(" ");
+                    buff.append(" ");
                 } else {
-                    procWriter.write("\n");
+                    buff.append("\n");
                 }
             }
         }
-        procWriter.close();
-        int exitVal = proc.waitFor();
-        //waiting for finish of StreamPump runs
-        synchronized(pump) {}
-        synchronized(errPump) {}
-        System.out.println("command returned exit value " + exitVal);
+        FileOutputStream fos = new FileOutputStream(psFilename, true);
+        try {
+            GenericCommandExecute.execute(command,
+                                          new StringReader(buff.toString()),
+                                          fos,
+                                          System.err);
+        } finally {
+            fos.close();
+        }
     }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             double[][] points = { {-180, 90},
                                  {-135, 67.5},
@@ -76,9 +69,9 @@ public static void main(String[] args) {
                       "5/255",
                       points);
             double[][] morePoints = { {45, -22.5, 0.7},
-                                  {90, -45, 0.5},
-                                  {135, -67.5, 0.4},
-                                  {180, -90, 1.0}};
+                                     {90, -45, 0.5},
+                                     {135, -67.5, 0.4},
+                                     {180, -90, 1.0}};
             addPoints("/Volumes/heff/oliverpa/gmtTest/world.ps",
                       "Kf166/10i",
                       "-14/346/-90/90",
@@ -89,5 +82,5 @@ public static void main(String[] args) {
         } catch(Exception e) {
             e.printStackTrace();
         }
-    }    //private static Logger logger = Logger.getLogger(PSXYExecute.class);
+    } //private static Logger logger = Logger.getLogger(PSXYExecute.class);
 }
