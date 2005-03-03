ed42b9d5bb70a1bf695160601d8c926d544e0321
groves <groves@localhost>
2005-03-03 16:14:12 -0500

    [svn r12344] return command result

--- src/edu/sc/seis/fissuresUtil/gmt/ConvertExecute.java
+++ src/edu/sc/seis/fissuresUtil/gmt/ConvertExecute.java
@@ -2,17 +2,15 @@ package edu.sc.seis.fissuresUtil.gmt;
 
 import java.io.IOException;
 
-
 /**
- * @author oliverpa
- * Created on Jan 21, 2005
+ * @author oliverpa Created on Jan 21, 2005
  */
 public class ConvertExecute {
-    
-    public static void convert(String infile, String outfile, String options)
-            throws IOException, InterruptedException{
+
+    public static int convert(String infile, String outfile, String options)
+            throws IOException, InterruptedException {
         String command = "convert " + options + " " + infile + " " + outfile;
-        GenericCommandExecute.execute(command);
+        return GenericCommandExecute.execute(command);
     }
 
     public static void main(String[] args) {
