1c1a08a3fc53997054dfdc2c11aaede39214bbbb
Charles Groves <groves@seis.sc.edu>
2005-03-08 18:10:15 -0500

    [svn r12384] intern strings common to all networks with java String.intern(), keep internal listings of network specific strings for interning

--- src/edu/sc/seis/fissuresUtil/cache/CacheNetworkAccess.java
+++ src/edu/sc/seis/fissuresUtil/cache/CacheNetworkAccess.java
@@ -5,8 +5,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import org.apache.log4j.Logger;
 import edu.iris.Fissures.Time;
+import edu.iris.Fissures.TimeRange;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.iris.Fissures.IfNetwork.ChannelNotFound;
@@ -39,28 +41,44 @@ public class CacheNetworkAccess extends ProxyNetworkAccess {
         attr = null;
         stations = null;
         channelMap.clear();
+        knownStations.clear();
+        knownTimes.clear();
+        knownSites.clear();
         instrumentationMap.clear();
         super.reset();
     }
 
     public NetworkAttr get_attributes() {
-        if(attr == null) {
-            attr = net.get_attributes();
+        synchronized(this) {
+            if(attr == null) {
+                attr = net.get_attributes();
+                attr.description = attr.description.intern();
+                attr.name = attr.name.intern();
+                attr.owner = attr.owner.intern();
+                intern(attr.get_id());
+            }
         }
         return attr;
+    }
+
+    private void intern(NetworkId id) {
+        id.begin_time = intern(id.begin_time);
+        id.network_code = id.network_code.intern();
     }
 
     /**
      * retreives the stations for the network, but uses the cached copy if it
      * has been previously retrieved. The stations are also cleaned of duplicate
      * networkAttr objects to free memory.
-     * 
-     * @see clean(Station[])
      */
     public Station[] retrieve_stations() {
-        if(stations == null) {
-            stations = net.retrieve_stations();
-            clean(stations);
+        synchronized(knownStations) {
+            if(stations == null) {
+                stations = net.retrieve_stations();
+                for(int i = 0; i < stations.length; i++) {
+                    stations[i] = intern(stations[i]);
+                }
+            }
         }
         return stations;
     }
@@ -69,242 +87,147 @@ public class CacheNetworkAccess extends ProxyNetworkAccess {
      * retreives the channels for the stations, but uses the cached copy if it
      * has been previously retrieved. The channels are also cleaned of duplicate
      * site objects to free memory.
-     * 
-     * @see clean(Channel[])
      */
     public Channel[] retrieve_for_station(StationId id) {
         String idStr = StationIdUtil.toString(id);
-        if(!channelMap.containsKey(idStr)) {
-            Channel[] chans = net.retrieve_for_station(id);
-            if(chans.length > 0) {
-                clean(chans);
-            } else {
-                logger.debug("Got 0 channels for station "
-                        + StationIdUtil.toString(id) + " in network "
-                        + NetworkIdUtil.toString(get_attributes().get_id()));
+        synchronized(channelMap) {
+            if(!channelMap.containsKey(idStr)) {
+                Channel[] chans = net.retrieve_for_station(id);
+                for(int i = 0; i < chans.length; i++) {
+                    intern(chans[i]);
+                }
+                if(chans.length == 0) {
+                    logger.debug("Got 0 channels for station "
+                            + StationIdUtil.toString(id) + " in network "
+                            + NetworkIdUtil.toString(get_attributes().get_id()));
+                }
+                channelMap.put(idStr, chans);
+                return chans;
             }
-            channelMap.put(idStr, chans);
+            return (Channel[])channelMap.get(idStr);
         }
-        return (Channel[])channelMap.get(idStr);
     }
-    
+
     public Instrumentation retrieve_instrumentation(ChannelId id, Time the_time)
-        throws ChannelNotFound {
+            throws ChannelNotFound {
         MicroSecondDate date = new MicroSecondDate(the_time);
         Instrumentation inst = null;
         String idString = ChannelIdUtil.toString(id);
         List instForChannel = (List)instrumentationMap.get(idString);
-        if (instForChannel == null) {
+        if(instForChannel == null) {
             instForChannel = new ArrayList();
             instrumentationMap.put(idString, instForChannel);
         }
         for(Iterator iter = instForChannel.iterator(); iter.hasNext();) {
             InstHolder instHold = (InstHolder)iter.next();
-            if (instHold.range.intersects(date)) {
+            if(instHold.range.intersects(date)) {
                 inst = instHold.inst;
             }
         }
-        if (inst == null) {
-            inst =  net.retrieve_instrumentation(id, the_time);
+        if(inst == null) {
+            inst = net.retrieve_instrumentation(id, the_time);
             InstHolder instHold = new InstHolder(inst);
             instForChannel.add(instHold);
         }
         return inst;
     }
-    
+
     class InstHolder {
+
         InstHolder(Instrumentation inst) {
             this.inst = inst;
             this.range = new MicroSecondTimeRange(inst.effective_time);
         }
+
         Instrumentation inst;
+
         MicroSecondTimeRange range;
     }
 
-    /**
-     * Cleans the array of channels so that the sites are shared if they are
-     * identical. This frees memory that is otherwise wasted on identical copies
-     * of the same objects.
-     */
-    public static void clean(Channel[] chans) {
-        // check for zero length, in case site has no channels
-        if (chans.length == 0) { return; }
-        ArrayList knownSites = new ArrayList();
-        knownSites.add(chans[0].my_site);
-        for(int i = 0; i < chans.length; i++) {
-            clean(chans[i].get_id());
-            boolean foundSite = false;
-            Iterator it = knownSites.iterator();
-            while(!foundSite && it.hasNext()) {
-                Site site = (Site)it.next();
-                if(SiteIdUtil.areEqual(chans[i].my_site.get_id(), site.get_id())) {
-                    chans[i].my_site = site;
-                    chans[i].get_id().site_code = site.get_code();
-                    foundSite = true;
-                }
-            }
-            if(!foundSite) {
-                knownSites.add(0, chans[i].my_site);
-            }
-        }
-        clean((Site[])knownSites.toArray(new Site[0]));
+    private void intern(Channel channel) {
+        intern(channel.get_id());
+        channel.my_site = intern(channel.my_site);
+        intern(channel.effective_time);
+        channel.name = channel.name.intern();
     }
 
-    public static void clean(ChannelId id) {
-        id.channel_code = getKnownChannelCode(id.channel_code);
-        id.network_id = getKnown(id.network_id);
-        id.station_code = getKnownStationCode(id.station_code);
-        id.site_code = getKnownSiteCode(id.site_code);
-        id.begin_time = getKnown(id.begin_time);
+    private void intern(ChannelId id) {
+        id.channel_code = id.channel_code.intern();
+        id.network_id = get_attributes().get_id();
+        id.station_code = id.station_code.intern();
+        id.site_code = id.site_code.intern();
+        id.begin_time = intern(id.begin_time);
     }
 
-    public static void clean(SiteId id) {
-        id.network_id = getKnown(id.network_id);
-        id.station_code = getKnownStationCode(id.station_code);
-        id.site_code = getKnownSiteCode(id.site_code);
-        id.begin_time = getKnown(id.begin_time);
+    private void intern(SiteId id) {
+        id.network_id = get_attributes().get_id();
+        id.station_code = id.station_code.intern();
+        id.site_code = id.site_code.intern();
+        id.begin_time = intern(id.begin_time);
     }
 
-    public static void clean(StationId id) {
-        id.network_id = getKnown(id.network_id);
-        id.station_code = getKnownStationCode(id.station_code);
-        id.begin_time = getKnown(id.begin_time);
+    private void intern(StationId id) {
+        id.network_id = get_attributes().get_id();
+        id.station_code = id.station_code.intern();
+        id.begin_time = intern(id.begin_time);
     }
 
-    /**
-     * Cleans the array of sites so that the stations are shared if they are
-     * identical. This frees memory that is otherwise wasted on identical copies
-     * of the same objects.
-     */
-    public static void clean(Site[] sites) {
-        // check for zero length, can happen if a station has no sites
-        if (sites.length == 0) { return; }
-        ArrayList knownStations = new ArrayList();
-        knownStations.add(sites[0].my_station);
-        for(int i = 0; i < sites.length; i++) {
-            clean(sites[i].get_id());
-            boolean foundStation = false;
-            Iterator it = knownStations.iterator();
-            while(!foundStation && it.hasNext()) {
-                Station station = (Station)it.next();
-                if(StationIdUtil.areEqual(sites[i].my_station.get_id(),
-                                          station.get_id())) {
-                    sites[i].my_station = station;
-                    foundStation = true;
-                }
+    private Site intern(Site site) {
+        synchronized(knownSites) {
+            String id = SiteIdUtil.toString(site.get_id());
+            if(!knownSites.containsKey(id)) {
+                intern(site.get_id());
+                site.comment = site.comment.intern();
+                site.my_station = intern(site.my_station);
+                site.effective_time = intern(site.effective_time);
+                knownSites.put(id, site);
+                return site;
             }
-            if(!foundStation) {
-                knownStations.add(0, sites[i].my_station);
-            }
-        }
-        clean((Station[])knownStations.toArray(new Station[0]));
-    }
-
-    /**
-     * Cleans the array of stations so that the Network Attributes are shared if
-     * they are identical. This frees memory that is otherwise wasted on
-     * identical copies of the same objects.
-     */
-    public static void clean(Station[] stations) {
-        // check for zero length, can happen if a networkAccess has no stations
-        if (stations.length == 0) { return; }
-        ArrayList knownNets = new ArrayList();
-        knownNets.add(stations[0].my_network);
-        for(int i = 0; i < stations.length; i++) {
-            clean(stations[i].get_id());
-            boolean foundNet = false;
-            Iterator it = knownNets.iterator();
-            while(!foundNet && it.hasNext()) {
-                NetworkAttr network = (NetworkAttr)it.next();
-                if(NetworkIdUtil.areEqual(stations[i].my_network.get_id(),
-                                          network.get_id())) {
-                    stations[i].my_network = network;
-                    foundNet = true;
-                }
-            }
-            if(!foundNet) {
-                knownNets.add(0, stations[i].my_network);
-            }
+            return (Site)knownSites.get(id);
         }
     }
 
-    private static String getKnownChannelCode(String unkownCode) {
-        synchronized(knownChannelCodes) {
-            Iterator it = knownChannelCodes.iterator();
-            while(it.hasNext()) {
-                String cur = (String)it.next();
-                if(cur.equals(unkownCode)) return cur;
-            }
-            knownChannelCodes.add(unkownCode);
-        }
-        return unkownCode;
+    private TimeRange intern(TimeRange effective_time) {
+        effective_time.end_time = intern(effective_time.end_time);
+        effective_time.start_time = intern(effective_time.start_time);
+        return effective_time;
     }
 
-    private static List knownChannelCodes = Collections.synchronizedList(new ArrayList());
-
-    private static String getKnownStationCode(String unkownCode) {
-        synchronized(knownStationCodes) {
-            Iterator it = knownStationCodes.iterator();
-            while(it.hasNext()) {
-                String cur = (String)it.next();
-                if(cur.equals(unkownCode)) return cur;
+    public Station intern(Station station) {
+        synchronized(knownStations) {
+            String id = StationIdUtil.toString(station.get_id());
+            if(!knownStations.containsKey(id)) {
+                intern(station.get_id());
+                station.my_network = get_attributes();
+                knownStations.put(id, station);
+                return station;
             }
-            knownStationCodes.add(unkownCode);
+            return (Station)knownStations.get(id);
         }
-        return unkownCode;
     }
 
-    private static List knownStationCodes = Collections.synchronizedList(new ArrayList());
-
-    private static String getKnownSiteCode(String unkownCode) {
-        synchronized(knownSiteCodes) {
-            Iterator it = knownSiteCodes.iterator();
-            while(it.hasNext()) {
-                String cur = (String)it.next();
-                if(cur.equals(unkownCode)) return cur;
-            }
-            knownSiteCodes.add(unkownCode);
-        }
-        return unkownCode;
-    }
-
-    private static List knownSiteCodes = Collections.synchronizedList(new ArrayList());
-
-    private static NetworkId getKnown(NetworkId unknownId) {
-        synchronized(knownNetworkIds) {
-            Iterator it = knownNetworkIds.iterator();
-            while(it.hasNext()) {
-                NetworkId cur = (NetworkId)it.next();
-                if(NetworkIdUtil.areEqual(unknownId, cur)) return cur;
-            }
-            knownNetworkIds.add(unknownId);
-        }
-        return unknownId;
-    }
-
-    private static List knownNetworkIds = Collections.synchronizedList(new ArrayList());
-
-    private static Time getKnown(Time unknownTime) {
+    private Time intern(Time unknownTime) {
         synchronized(knownTimes) {
-            Iterator it = knownTimes.iterator();
-            while(it.hasNext()) {
-                Time cur = (Time)it.next();
-                if(cur.date_time.equals(unknownTime.date_time)
-                        && cur.leap_seconds_version == unknownTime.leap_seconds_version) { return cur; }
+            if(!knownTimes.containsKey(unknownTime.date_time)) {
+                knownTimes.put(unknownTime.date_time, unknownTime);
+                return unknownTime;
             }
-            knownTimes.add(unknownTime);
+            return (Time)knownTimes.get(unknownTime.date_time);
         }
-        return unknownTime;
     }
 
-    private static List knownTimes = Collections.synchronizedList(new ArrayList());
+    private Map knownTimes = Collections.synchronizedMap(new HashMap());
+
+    private Map knownStations = Collections.synchronizedMap(new HashMap());
+
+    private Map knownSites = Collections.synchronizedMap(new HashMap());
 
     private NetworkAttr attr;
 
     private Station[] stations;
 
     private HashMap channelMap = new HashMap();
-    
+
     private HashMap instrumentationMap = new HashMap();
 
     private static Logger logger = Logger.getLogger(CacheNetworkAccess.class);
