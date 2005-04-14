796c8ad709c71cab403d03818e051230cb77f807
groves <groves@localhost>
2005-04-13 21:35:09 -0400

    [svn r12880] log when preferred origin isnt found

--- src/edu/sc/seis/fissuresUtil/cache/EventUtil.java
+++ src/edu/sc/seis/fissuresUtil/cache/EventUtil.java
@@ -1,6 +1,5 @@
 /*
  * Created on Jul 20, 2004
- *
  */
 package edu.sc.seis.fissuresUtil.cache;
 
@@ -19,26 +18,26 @@ import edu.sc.seis.fissuresUtil.display.UnitDisplayUtil;
 
 /**
  * @author oliverpa
- *
  */
 public class EventUtil {
-	
+
     /**
      * This gets around the NoPreferredOrigin exception
      */
     public static Origin extractOrigin(EventAccessOperations ev) {
         try {
             return ev.get_preferred_origin();
-        } catch (NoPreferredOrigin e) {
+        } catch(NoPreferredOrigin e) {
+            logger.info("No preferred origin in event.  Trying get_origins instead");
             Origin[] oArray = ev.get_origins();
-            if (oArray.length > 0) {
+            if(oArray.length > 0) {
                 return oArray[0];
             } else {
                 throw new RuntimeException("No preferred origin", e);
             }
         }
     }
-	
+
     /**
      * @ returns a string for the form "Event: Location | Time | Magnitude |
      *   Depth"
@@ -48,28 +47,27 @@ public class EventUtil {
     }
 
     public static String getEventInfo(EventAccessOperations event, String format) {
-        return getEventInfo(event, format, new SimpleDateFormat(
-                "MM/dd/yyyy HH:mm:sss z"));
+        return getEventInfo(event,
+                            format,
+                            new SimpleDateFormat("MM/dd/yyyy HH:mm:sss z"));
     }
 
     /**
      * @ formats a string for the given event. To insert information about a
      *   certain item magic strings are used in the format string Magic Strings
      *   LOC adds the location of the event TIME adds the event time MAG adds
-     *   event magnitude DEPTH adds the depth
-     * 
-     * For example the string "Event: " + LOC + " | " + TIME + " | Mag: " + MAG + " |
-     * Depth: " + DEPTH + " " + DEPTH_UNIT produces the same thing as the no
-     * format call to getEventInfo
+     *   event magnitude DEPTH adds the depth For example the string "Event: " +
+     *   LOC + " | " + TIME + " | Mag: " + MAG + " | Depth: " + DEPTH + " " +
+     *   DEPTH_UNIT produces the same thing as the no format call to
+     *   getEventInfo
      */
     public static String getEventInfo(EventAccessOperations event,
-            String format, DateFormat sdf) {
-
+                                      String format,
+                                      DateFormat sdf) {
         Origin origin = extractOrigin(event);
-
         StringBuffer buf = new StringBuffer(format);
         int index = buf.indexOf(LOC);
-        if (index != -1) {
+        if(index != -1) {
             //Get geographic name of origin
             ParseRegions regions = ParseRegions.getInstance();
             String location = regions.getGeographicRegionName(event.get_attributes().region.number);
@@ -78,7 +76,6 @@ public class EventUtil {
         }
         return getOriginInfo(origin, buf.toString(), sdf);
     }
-    
 
     /**
      * @ returns a string for the form "Event: Location | Time | Magnitude |
@@ -89,52 +86,48 @@ public class EventUtil {
     }
 
     public static String getOriginInfo(Origin origin, String format) {
-        return getOriginInfo(origin, format, new SimpleDateFormat(
-                "MM/dd/yyyy HH:mm:sss z"));
+        return getOriginInfo(origin,
+                             format,
+                             new SimpleDateFormat("MM/dd/yyyy HH:mm:sss z"));
     }
 
-
     public static String getOriginInfo(Origin origin,
-            String format, DateFormat sdf) {
+                                       String format,
+                                       DateFormat sdf) {
         //Get Date and format it accordingly
         MicroSecondDate msd = new MicroSecondDate(origin.origin_time);
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         String originTimeString = sdf.format(msd);
-
         //Get Magnitude
         float mag = Float.NaN;
-        if (origin.magnitudes.length > 0) {
+        if(origin.magnitudes.length > 0) {
             mag = origin.magnitudes[0].value;
         }
-
         //get depth
         Quantity depth = origin.my_location.depth;
-
         float latitude = origin.my_location.latitude;
         float longitude = origin.my_location.longitude;
-
         StringBuffer buf = new StringBuffer(format);
-        for (int i = 0; i < magicStrings.length; i++) {
+        for(int i = 0; i < magicStrings.length; i++) {
             int index = buf.indexOf(magicStrings[i]);
-            if (index != -1) {
+            if(index != -1) {
                 buf.delete(index, index + magicStrings[i].length());
-                if (magicStrings[i].equals(TIME)) {
+                if(magicStrings[i].equals(TIME)) {
                     buf.insert(index, originTimeString);
-                } else if (magicStrings[i].equals(MAG)) {
-                    if (Float.isNaN(mag)) {
+                } else if(magicStrings[i].equals(MAG)) {
+                    if(Float.isNaN(mag)) {
                         buf.insert(index, "...");
                     } else {
                         buf.insert(index, mag);
                     }
-                } else if (magicStrings[i].equals(DEPTH)) {
+                } else if(magicStrings[i].equals(DEPTH)) {
                     buf.insert(index, depthFormatter.format(depth.value));
-                } else if (magicStrings[i].equals(DEPTH_UNIT)) {
-                    buf.insert(
-                            index,
-                            UnitDisplayUtil.getNameForUnit((UnitImpl) depth.the_units));
-                } else if (magicStrings[i].equals(LAT)) {
+                } else if(magicStrings[i].equals(DEPTH_UNIT)) {
+                    buf.insert(index,
+                               UnitDisplayUtil.getNameForUnit((UnitImpl)depth.the_units));
+                } else if(magicStrings[i].equals(LAT)) {
                     buf.insert(index, latitude);
-                } else if (magicStrings[i].equals(LON)) {
+                } else if(magicStrings[i].equals(LON)) {
                     buf.insert(index, longitude);
                 }
             }
@@ -148,10 +141,17 @@ public class EventUtil {
             DEPTH = "DEPTH", DEPTH_UNIT = "DEPTH_UNIT", LAT = "LAT",
             LON = "LON";
 
-    private static final String[] magicStrings = { LOC, TIME, MAG, DEPTH,
-            DEPTH_UNIT, LAT, LON };
+    private static final String[] magicStrings = {LOC,
+                                                  TIME,
+                                                  MAG,
+                                                  DEPTH,
+                                                  DEPTH_UNIT,
+                                                  LAT,
+                                                  LON};
 
     public static final String NO_ARG_STRING = "Event: " + LOC + " | " + TIME
-            + " | Mag: " + MAG + " | Depth " + DEPTH + " " + DEPTH_UNIT + "| (" +  LAT + ", " +  LON+")";
-    
+            + " | Mag: " + MAG + " | Depth " + DEPTH + " " + DEPTH_UNIT + "| ("
+            + LAT + ", " + LON + ")";
+
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(EventUtil.class);
 }
