9a37470da6e1d6bd412258471b121e25c9586489
Charles Groves <groves@seis.sc.edu>
2005-04-21 18:24:08 -0400

    [svn r13023] much boilerplate moved to NoTickBorder

--- src/edu/sc/seis/fissuresUtil/display/borders/TitleBorder.java
+++ src/edu/sc/seis/fissuresUtil/display/borders/TitleBorder.java
@@ -1,20 +1,13 @@
 package edu.sc.seis.fissuresUtil.display.borders;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.util.List;
-import edu.iris.Fissures.model.UnitRangeImpl;
 
 /**
  * @author groves Created on Feb 15, 2005
  */
-public class TitleBorder extends Border {
+public class TitleBorder extends NoTickBorder {
 
     public TitleBorder(int side, int order) {
         super(side, order);
-        labelTickLength = 0;
-        tickLength = 0;
-        setSide(side);
     }
 
     public TitleBorder(int side, int order, String title) {
@@ -22,12 +15,8 @@ public class TitleBorder extends Border {
         add(new UnchangingTitleProvider(title));
     }
 
-    protected void paintBorder(Graphics2D g2d) {
-        bf.draw(getRange(), g2d);
-    }
-
-    public String getMaxLengthFormattedString() {
-        return "";
+    protected BorderFormat createNoTickFormat() {
+        return new TitleBorderFormat();
     }
 
     private class TitleBorderFormat extends BorderFormat {
@@ -44,14 +33,4 @@ public class TitleBorder extends Border {
             return null;
         }
     }
-
-    protected List createFormats() {
-        return null;
-    }
-
-    protected UnitRangeImpl getRange() {
-        return null;
-    }
-
-    private BorderFormat bf = new TitleBorderFormat();
 }
