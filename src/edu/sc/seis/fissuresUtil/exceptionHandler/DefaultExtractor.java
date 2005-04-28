71d65821ee376d6aa34db94bf5368eb1f10adb5c
groves <groves@localhost>
2005-04-28 11:58:43 -0400

    [svn r13129] removed superfluous inclusion of throwable to string.  its in the stack trace

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/DefaultExtractor.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/DefaultExtractor.java
@@ -1,9 +1,8 @@
 /**
  * DefaultExtractor.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.exceptionHandler;
 
 import java.sql.SQLException;
@@ -17,21 +16,24 @@ public class DefaultExtractor implements Extractor {
 
     public String extract(Throwable throwable) {
         String traceString = "";
-        if (throwable instanceof FissuresException) {
-            traceString += "Description: "+((FissuresException)throwable).the_error.error_description+"\n";
-            traceString += "Error Code: "+((FissuresException)throwable).the_error.error_code+"\n";
+        if(throwable instanceof FissuresException) {
+            traceString += "Description: "
+                    + ((FissuresException)throwable).the_error.error_description
+                    + "\n";
+            traceString += "Error Code: "
+                    + ((FissuresException)throwable).the_error.error_code
+                    + "\n";
         }
-        if(throwable instanceof SQLException){
-            traceString += "SQLState: " + ((SQLException)throwable).getSQLState()+ '\n';
-            traceString += "Vendor code: " + ((SQLException)throwable).getErrorCode() + '\n';
+        if(throwable instanceof SQLException) {
+            traceString += "SQLState: "
+                    + ((SQLException)throwable).getSQLState() + '\n';
+            traceString += "Vendor code: "
+                    + ((SQLException)throwable).getErrorCode() + '\n';
         }
-        traceString += throwable.toString();
         return traceString;
     }
 
     public Throwable getSubThrowable(Throwable throwable) {
         return throwable.getCause();
     }
-
 }
-
