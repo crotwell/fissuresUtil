08ef5056635cb21bf03440e9c3ac12c8e6c1df44
groves <groves@localhost>
2005-05-04 13:43:19 -0400

    [svn r13233] moved createElement method to DOMHelper

--- tests/display/edu/sc/seis/fissuresUtil/display/configuration/SeismogramDisplayConfigurationTest.java
+++ tests/display/edu/sc/seis/fissuresUtil/display/configuration/SeismogramDisplayConfigurationTest.java
@@ -1,11 +1,6 @@
 package edu.sc.seis.fissuresUtil.display.configuration;
 
-import java.io.InputStream;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import junit.framework.TestCase;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  * @author groves Created on Feb 17, 2005
@@ -24,17 +19,7 @@ public class SeismogramDisplayConfigurationTest extends TestCase {
 
     public static SeismogramDisplayConfiguration create(String name)
             throws Exception {
-        return SeismogramDisplayConfiguration.create(createElement("edu/sc/seis/fissuresUtil/display/configuration/"
+        return SeismogramDisplayConfiguration.create(DOMHelper.createElement("edu/sc/seis/fissuresUtil/display/configuration/"
                 + name + ".xml"));
-    }
-
-    public static Element createElement(String loc) throws Exception {
-        ClassLoader cl = SeismogramDisplayConfigurationTest.class.getClassLoader();
-        InputStream source = cl.getResourceAsStream(loc);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setNamespaceAware(true);
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(source);
-        return doc.getDocumentElement();
     }
 }
