bb6ae8ce9af2901f0eb425f29637e4504b2d41e5
Philip Crotwell <crotwell@seis.sc.edu>
2005-05-18 14:46:29 -0400

    [svn r13467] test without the cache

--- tests/database/edu/sc/seis/fissuresUtil/database/JDBCUnitTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/JDBCUnitTest.java
@@ -18,11 +18,24 @@ public class JDBCUnitTest extends TestCase {
         assertEquals(UnitImpl.CUBIC_METER, table.get(id));
     }
     
+
+    public void testPutAndGetNoCache() throws SQLException, NotFound{
+        int id = table.put(UnitImpl.CUBIC_METER);
+        table.cache.clear();
+        assertEquals(UnitImpl.CUBIC_METER, table.get(id));
+        
+    }
     public void testGetDBId() throws NotFound, SQLException {
         int dbid = table.put(UnitImpl.CUBIC_METER);
         int gottenid = table.getDBId(UnitImpl.CUBIC_METER);
         assertEquals(dbid, gottenid);
     }
-    
+
+    public void testGetDBIdNoCache() throws NotFound, SQLException {
+        int dbid = table.put(UnitImpl.CUBIC_METER);
+        table.cache.clear();
+        int gottenid = table.getDBId(UnitImpl.CUBIC_METER);
+        assertEquals(dbid, gottenid);
+    }
     private JDBCUnit table;
 } // JDBCUnitTest
