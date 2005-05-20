36019c669f6ef1e7b62053f595990583132897b8
Philip Crotwell <crotwell@seis.sc.edu>
2005-05-20 10:42:43 -0400

    [svn r13505] first cut at a rotateGCP processor

--- src/edu/sc/seis/fissuresUtil/bag/Rotate.java
+++ src/edu/sc/seis/fissuresUtil/bag/Rotate.java
@@ -3,6 +3,10 @@ package edu.sc.seis.fissuresUtil.bag;
 import java.awt.geom.AffineTransform;
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.Location;
+import edu.iris.Fissures.Sampling;
+import edu.iris.Fissures.Time;
+import edu.iris.Fissures.Unit;
+import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.iris.Fissures.IfSeismogramDC.LocalMotionVector;
 import edu.iris.Fissures.IfSeismogramDC.VectorComponent;
 import edu.iris.Fissures.model.TimeInterval;
@@ -10,40 +14,73 @@ import edu.iris.Fissures.seismogramDC.LocalMotionVectorImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 /**
- * Rotate.java
- *
- *
- * Created: Sun Dec 15 13:43:21 2002
- *
+ * Rotate.java Created: Sun Dec 15 13:43:21 2002
+ * 
  * @author Philip Crotwell
- * @version $Id: Rotate.java 10618 2004-09-18 14:59:53Z crotwell $
+ * @version $Id: Rotate.java 13505 2005-05-20 14:42:43Z crotwell $
  */
 public class Rotate implements LocalMotionVectorFunction {
 
-    public Rotate() {
-
-    } // Rotate constructor
+    public Rotate() {} // Rotate constructor
 
     public LocalMotionVector apply(LocalMotionVector vec) {
         VectorComponent[] data = new VectorComponent[3];
+        return new LocalMotionVectorImpl(vec.get_id(),
+                                         vec.properties,
+                                         vec.begin_time,
+                                         vec.num_points,
+                                         vec.sampling_info,
+                                         vec.y_unit,
+                                         vec.channel_group,
+                                         vec.parm_ids,
+                                         (TimeInterval[])vec.time_corrections,
+                                         vec.sample_rate_history,
+                                         data);
+    }
 
-
-        return new  LocalMotionVectorImpl(vec.get_id(),
-                                          vec.properties,
-                                          vec.begin_time,
-                                          vec.num_points,
-                                          vec.sampling_info,
-                                          vec.y_unit,
-                                          vec.channel_group,
-                                          vec.parm_ids,
-                                              (TimeInterval[])vec.time_corrections,
-                                          vec.sample_rate_history,
-                                          data);
+    public static LocalSeismogramImpl[] rotateGCP(LocalSeismogramImpl x,
+                                                  LocalSeismogramImpl y,
+                                                  Location staLoc,
+                                                  Location evtLoc,
+                                                  String transverseCode,
+                                                  String radialCode)
+            throws FissuresException, IncompatibleSeismograms {
+        float[][] data = rotateGCP(x, y, staLoc, evtLoc);
+        LocalSeismogramImpl[] out = new LocalSeismogramImpl[2];
+        ChannelId xChanId = new ChannelId(x.channel_id.network_id,
+                                          x.channel_id.station_code,
+                                          x.channel_id.site_code,
+                                          x.channel_id.channel_code.substring(0,
+                                                                              2)
+                                                  + radialCode,
+                                          x.channel_id.begin_time);
+        out[0] = new LocalSeismogramImpl(x.get_id() + "Radial",
+                                         x.begin_time,
+                                         data[0].length,
+                                         x.sampling_info,
+                                         x.y_unit,
+                                         xChanId,
+                                         data[0]);
+        ChannelId yChanId = new ChannelId(y.channel_id.network_id,
+                                          y.channel_id.station_code,
+                                          y.channel_id.site_code,
+                                          y.channel_id.channel_code.substring(0,
+                                                                              2)
+                                                  + transverseCode,
+                                          y.channel_id.begin_time);
+        out[1] = new LocalSeismogramImpl(y.get_id() + "Radial",
+                                         y.begin_time,
+                                         data[1].length,
+                                         y.sampling_info,
+                                         y.y_unit,
+                                         yChanId,
+                                         data[1]);
+        return out;
     }
 
     /**
-     * rotates the two seismograms to the great circle path radial and
-     * transverse. It is assumed that the two seismograms orientation are
+     * rotates the two seismograms to the great circle path transverse (X) and
+     * radial (Y). It is assumed that the two seismograms orientation are
      * perpendicular to each other and are oriented in the east, x and north, y,
      * directions. They must also have the same number of data points.
      * 
@@ -55,20 +92,25 @@ public class Rotate implements LocalMotionVectorFunction {
     public static float[][] rotateGCP(LocalSeismogramImpl x,
                                       LocalSeismogramImpl y,
                                       Location staLoc,
-                                      Location evtLoc) throws FissuresException, IncompatibleSeismograms  {
+                                      Location evtLoc)
+            throws FissuresException, IncompatibleSeismograms {
         DistAz distAz = new DistAz(staLoc, evtLoc);
-        return Rotate.rotate(x, y, dtor(180+distAz.getBaz()));
+        return Rotate.rotate(x, y, dtor(180 + distAz.getBaz()));
     }
 
-    /** rotates the two seismograms by the given angle. It is assumed that
-     the two seismograms orientation are perpendicular to each other and
-     that the sense of the rotation is from x towards y.
+    /**
+     * rotates the two seismograms by the given angle. It is assumed that the
+     * two seismograms orientation are perpendicular to each other and that the
+     * sense of the rotation is from x towards y.
+     * 
      * @throws IncompatibleSeismograms
-     @returns the rotated data from the two seismograms, index 0 is
-     the new x and index 1 is the new y.*/
+     * @returns the rotated data from the two seismograms, index 0 is the new x
+     *          and index 1 is the new y.
+     */
     public static float[][] rotate(LocalSeismogramImpl x,
                                    LocalSeismogramImpl y,
-                                   double radians) throws FissuresException, IncompatibleSeismograms  {
+                                   double radians) throws FissuresException,
+            IncompatibleSeismograms {
         float[][] data = new float[2][];
         float[] temp = x.get_as_floats();
         data[0] = new float[temp.length];
@@ -81,36 +123,39 @@ public class Rotate implements LocalMotionVectorFunction {
     }
 
     /**
-     * Rotates the x and y arrays by the given angle in radians. The x and y axis are
-     * assumed to be perpendicular. Theta, in radians, is positive from
-     * x towards y, and so a rotation of PI/2 puts x into y and
-     * -y into x.
+     * Rotates the x and y arrays by the given angle in radians. The x and y
+     * axis are assumed to be perpendicular. Theta, in radians, is positive from
+     * x towards y, and so a rotation of PI/2 puts x into y and -y into x.
+     * 
      * @throws IncompatibleSeismograms
      */
-    public static void rotate(float[] x, float[] y, double radians) throws IncompatibleSeismograms {
+    public static void rotate(float[] x, float[] y, double radians)
+            throws IncompatibleSeismograms {
         rotate(x, y, AffineTransform.getRotateInstance(radians));
     }
 
-    /** Performs the rotation from the given matrix. It is assumed to be
-     a pure rotation matrix (no translation) and the translation components
-     of the affine transform are ignored if present. 
-     * @throws IncompatibleSeismograms*/
-    public static void rotate(float[] x, float[] y, AffineTransform affine) throws IncompatibleSeismograms {
-        if (x.length != y.length) {
-            throw new IncompatibleSeismograms("x and y must have the same length. "+x.length+" "+y.length);
-        }
+    /**
+     * Performs the rotation from the given matrix. It is assumed to be a pure
+     * rotation matrix (no translation) and the translation components of the
+     * affine transform are ignored if present.
+     * 
+     * @throws IncompatibleSeismograms
+     */
+    public static void rotate(float[] x, float[] y, AffineTransform affine)
+            throws IncompatibleSeismograms {
+        if(x.length != y.length) { throw new IncompatibleSeismograms("x and y must have the same length. "
+                + x.length + " " + y.length); }
         float tempx, tempy;
         // matrix is m00 m10 m01 m11 where the matrix is
         //           m00 m01
         //           m10 m11
-
         double[] matrix = new double[4];
         affine.getMatrix(matrix);
-        for (int i=0; i<x.length; i++) {
+        for(int i = 0; i < x.length; i++) {
             tempx = x[i];
             tempy = y[i];
-            x[i] = (float)(tempx*matrix[0] + tempy*matrix[2]);
-            y[i] = (float)(tempx*matrix[1] + tempy*matrix[3]);
+            x[i] = (float)(tempx * matrix[0] + tempy * matrix[2]);
+            y[i] = (float)(tempx * matrix[1] + tempy * matrix[3]);
         }
     }
 
@@ -123,11 +168,10 @@ public class Rotate implements LocalMotionVectorFunction {
     }
 
     public static double dtor(double degree) {
-        return Math.PI*degree/180.0;
+        return Math.PI * degree / 180.0;
     }
 
     public static double rtod(double radian) {
-        return radian*180.0/Math.PI;
+        return radian * 180.0 / Math.PI;
     }
-
 } // Rotate
