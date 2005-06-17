ca2ee5cd1817dd0d28b722ae2ba4df07eb0035bc
groves <groves@localhost>
2005-06-17 11:42:07 -0400

    [svn r13918] fixed for when getting a corba object throws an exception

--- src/edu/sc/seis/fissuresUtil/namingService/Deadhead.java
+++ src/edu/sc/seis/fissuresUtil/namingService/Deadhead.java
@@ -1,6 +1,8 @@
 package edu.sc.seis.fissuresUtil.namingService;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.omg.CORBA.SystemException;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
 import org.omg.CosNaming.NamingContextPackage.InvalidName;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
@@ -15,25 +17,41 @@ public class Deadhead {
         BasicConfigurator.configure();
         Initializer.init(args);
         unbindFrom = Initializer.getNS();
-        unbindDead(unbindFrom.getAllEventDC());
-        unbindDead(unbindFrom.getAllSeismogramDC());
-        unbindDead(unbindFrom.getAllNetworkDC());
-        unbindDead(unbindFrom.getAllPlottableDC());
+        unbindDead(unbindFrom.getAllEventDC(), FissuresNamingService.EVENTDC);
+        unbindDead(unbindFrom.getAllSeismogramDC(),
+                   FissuresNamingService.SEISDC);
+        unbindDead(unbindFrom.getAllNetworkDC(),
+                   FissuresNamingService.NETWORKDC);
+        unbindDead(unbindFrom.getAllPlottableDC(),
+                   FissuresNamingService.PLOTTABLEDC);
     }
 
-    private static void unbindDead(ServerNameDNS[] servers) throws NotFound,
-            CannotProceed, InvalidName {
+    private static void unbindDead(ServerNameDNS[] servers, String interfaceName)
+            throws NotFound, CannotProceed, InvalidName {
         for(int i = 0; i < servers.length; i++) {
-            CorbaChecker checker = new CorbaChecker(servers[i].getCorbaObject(),
+            org.omg.CORBA.Object object;
+            try {
+                object = servers[i].getCorbaObject();
+            } catch(SystemException e) {
+                logger.info(servers[i] + " threw " + e + " Unbinding");
+                unbindFrom.unbind(servers[i].getServerDNS(),
+                                  interfaceName,
+                                  servers[i].getServerName());
+                continue;
+            }
+            CorbaChecker checker = new CorbaChecker(object,
                                                     servers[i].toString());
             checker.run();
             if(checker.getStatus().getStatus() == ConnStatus.FAILED) {
+                logger.info("Unbinding " + servers[i]);
                 unbindFrom.unbind(servers[i].getServerDNS(),
-                                  servers[i].getServerName(),
-                                  servers[i].getCorbaObject());
+                                  interfaceName,
+                                  servers[i].getServerName());
             }
         }
     }
+
+    private static Logger logger = Logger.getLogger(Deadhead.class);
 
     private static FissuresNamingService unbindFrom;
 }
