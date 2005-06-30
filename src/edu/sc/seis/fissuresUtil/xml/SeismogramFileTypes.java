252085384ddf7badd80822ca94bd1fb2e30594b3
Charles Groves <groves@seis.sc.edu>
2005-06-30 11:28:19 -0400

    [svn r14098] added toString

--- src/edu/sc/seis/fissuresUtil/xml/SeismogramFileTypes.java
+++ src/edu/sc/seis/fissuresUtil/xml/SeismogramFileTypes.java
@@ -2,26 +2,31 @@ package edu.sc.seis.fissuresUtil.xml;
 
 /**
  * SeismogramFileTypes.java
- *
- *
+ * 
+ * 
  * Created: Tue Mar 18 15:38:13 2003
- *
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 public class SeismogramFileTypes {
-    private SeismogramFileTypes (String val, int intVal){
+
+    private SeismogramFileTypes(String val, int intVal) {
         this.val = val;
         this.intVal = intVal;
     }
 
+    public String toString() {
+        return getValue();
+    }
+
     public boolean equals(Object obj) {
-        if(! (obj instanceof SeismogramFileTypes) ) return false;
+        if(!(obj instanceof SeismogramFileTypes))
+            return false;
         return ((SeismogramFileTypes)obj).getValue().equals(this.val);
     }
 
@@ -35,45 +40,51 @@ public class SeismogramFileTypes {
 
     public URL getURLValue() {
         try {
-            return new URL(URL_PREFIX+getValue());
-        } catch (MalformedURLException e) {
+            return new URL(URL_PREFIX + getValue());
+        } catch(MalformedURLException e) {
             // shouldn't ever happen as these are static strings
-            GlobalExceptionHandler.handle("Trouble creating URL for file type "+getValue(), e);
+            GlobalExceptionHandler.handle("Trouble creating URL for file type "
+                    + getValue(), e);
         }
         return null;
     }
 
-    public static SeismogramFileTypes fromString(String typeURL) throws UnsupportedFileTypeException {
-        if (typeURL.equals(MSEED.getURLValue().toString())) {
+    public static SeismogramFileTypes fromString(String typeURL)
+            throws UnsupportedFileTypeException {
+        if(typeURL.equals(MSEED.getURLValue().toString())) {
             return MSEED;
-        } else if (typeURL.equals(SAC.getURLValue().toString())) {
+        } else if(typeURL.equals(SAC.getURLValue().toString())) {
             return SAC;
-        } else if (typeURL.equals(PSN.getURLValue().toString())) {
+        } else if(typeURL.equals(PSN.getURLValue().toString())) {
             return PSN;
         }
         throw new UnsupportedFileTypeException(typeURL);
     }
 
-    public static SeismogramFileTypes fromInt(int type) throws UnsupportedFileTypeException {
-        if (type == MSEED.getIntValue()) {
+    public static SeismogramFileTypes fromInt(int type)
+            throws UnsupportedFileTypeException {
+        if(type == MSEED.getIntValue()) {
             return MSEED;
-        } else if (type == SAC.getIntValue()) {
+        } else if(type == SAC.getIntValue()) {
             return SAC;
-        } else if (type == PSN.getIntValue()) {
+        } else if(type == PSN.getIntValue()) {
             return PSN;
         }
-        throw new UnsupportedFileTypeException(""+type);
+        throw new UnsupportedFileTypeException("" + type);
     }
 
-    public static final SeismogramFileTypes SAC = new SeismogramFileTypes("sac", 1);
+    public static final SeismogramFileTypes SAC = new SeismogramFileTypes("sac",
+                                                                          1);
 
-    public static final SeismogramFileTypes MSEED = new SeismogramFileTypes("mseed", 2);
+    public static final SeismogramFileTypes MSEED = new SeismogramFileTypes("mseed",
+                                                                            2);
 
-    public static final SeismogramFileTypes PSN = new SeismogramFileTypes("psn", 3);
+    public static final SeismogramFileTypes PSN = new SeismogramFileTypes("psn",
+                                                                          3);
 
     public static final String URL_PREFIX = "http://www.seis.sc.edu/xml/SeismogramFileTypes/";
 
     private String val;
-    private int intVal;
 
+    private int intVal;
 }// SeismogramFileTypes
