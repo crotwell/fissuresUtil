8e10caf340aa92e97b58a95d4132789dbf1c577f
fenner <fenner@localhost>
2005-08-04 17:29:52 -0400

    [svn r14453] works with new connection creator, and works with ne rt130 file reading

--- tests/edu/sc/seis/fissuresUtil/rt130ToLocalSeismogramImplTest/RT130ToLocalSeismogramImplTest.java
+++ tests/edu/sc/seis/fissuresUtil/rt130ToLocalSeismogramImplTest/RT130ToLocalSeismogramImplTest.java
@@ -1,28 +1,22 @@
 package edu.sc.seis.fissuresUtil.rt130ToLocalSeismogramImplTest;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
 import org.apache.log4j.BasicConfigurator;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
-import edu.sc.seis.fissuresUtil.database.ConnMgr;
+import edu.sc.seis.fissuresUtil.database.ConnectionCreator;
 import edu.sc.seis.fissuresUtil.database.JDBCTearDown;
 import edu.sc.seis.fissuresUtil.database.seismogram.JDBCSeismogramFiles;
+import edu.sc.seis.fissuresUtil.rt130.PacketType;
 import edu.sc.seis.fissuresUtil.rt130.RT130FormatException;
-import edu.sc.seis.fissuresUtil.rt130.RT130ToLocalSeismogramImpl;
-import edu.sc.seis.fissuresUtil.simple.Initializer;
+import edu.sc.seis.fissuresUtil.rt130.RT130FileReader;
+import edu.sc.seis.fissuresUtil.rt130.RT130ToLocalSeismogram;
 import edu.sc.seis.fissuresUtil.xml.SeismogramFileTypes;
 
 
 /**
- * @author fenner
- * Created on Jun 14, 2005
+ * @author fenner Created on Jun 14, 2005
  */
 public class RT130ToLocalSeismogramImplTest  extends JDBCTearDown {
 
@@ -31,47 +25,32 @@ public class RT130ToLocalSeismogramImplTest  extends JDBCTearDown {
     }
     
     public void setUp() throws SQLException{
-        //filePath = "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc Folder/2005130/9307/1/221325740_0036EE80";
-        //filePath = "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc Folder/2005130/9307/2/121254115_0036EE80";
-        //filePath = "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc Folder/2005129/9307/1/231354265_57D5D7C0";
+        // filePath =
+        // "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc
+        // Folder/2005130/9307/1/221325740_0036EE80";
+        // filePath =
+        // "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc
+        // Folder/2005130/9307/2/121254115_0036EE80";
+        // filePath =
+        // "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/tohpc
+        // Folder/2005129/9307/1/231354265_57D5D7C0";
         filePath = "/private/Network/Servers/roo.seis.sc.edu/Volumes/home/Users/fenner/20050617_huddle/2005168/91EB/1/200332765_0036EE80";
     }
 
-    public void testGeneral() throws SQLException{
-        File seismogramFile = new File(filePath);
-        FileInputStream fis = null;
-        try {
-            Connection conn = null;
-            try {
-                conn = ConnMgr.createConnection();
-            } catch(SQLException e) {
-                System.err.println("Error creating connection.");
-                e.printStackTrace();
-            }
-            
-            fis = new FileInputStream(seismogramFile);
-	        BufferedInputStream bis = new BufferedInputStream(fis);
-	        DataInputStream dis = new DataInputStream(bis);
-	        RT130ToLocalSeismogramImpl toSeismogram = new RT130ToLocalSeismogramImpl(dis, true);
-		    LocalSeismogramImpl[] seismogramArray = toSeismogram.readEntireDataFile();
-            
-            JDBCSeismogramFiles jdbcSeisFile = new JDBCSeismogramFiles(conn);
-            for(int i = 0; i < seismogramArray.length; i++){
-                jdbcSeisFile.saveSeismogramToDatabase(seismogramArray[i].channel_id,
-                                                      seismogramArray[i],
-                                                      filePath, 
-                                                      SeismogramFileTypes.RT_130);
-                System.err.println(i);
-            }
-	    } catch(FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();    
-        } catch(RT130FormatException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch(IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+    public void testGeneral() throws SQLException, RT130FormatException, IOException{
+        ConnectionCreator connCreator = new ConnectionCreator("jdbc:hsqldb:.", "HSQL", "SA", "");
+        Connection conn = connCreator.createConnection();
+        RT130FileReader toPackets = new RT130FileReader(filePath, true);
+	    PacketType[] packetArray = toPackets.processRT130Data();
+        RT130ToLocalSeismogram toSeismogram = new RT130ToLocalSeismogram(conn, null);
+        LocalSeismogramImpl[] seismogramArray = toSeismogram.ConvertRT130ToLocalSeismogram(packetArray);
+        JDBCSeismogramFiles jdbcSeisFile = new JDBCSeismogramFiles(conn);
+        for(int i = 0; i < seismogramArray.length; i++){
+            jdbcSeisFile.saveSeismogramToDatabase(seismogramArray[i].channel_id,
+                                                  seismogramArray[i],
+                                                  filePath, 
+                                                  SeismogramFileTypes.RT_130);
+            System.err.println(i);
         }
     }
     
