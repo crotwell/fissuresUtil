3d6a5e9275647cddd3b617c7dfb0c5a8c8fc0abc
Ben Fenner <fenner@seis.sc.edu>
2005-08-12 15:04:45 -0400

    [svn r14579] add NC file reading

--- src/edu/sc/seis/fissuresUtil/rt130/RT130ToLocalSeismogram.java
+++ src/edu/sc/seis/fissuresUtil/rt130/RT130ToLocalSeismogram.java
@@ -30,38 +30,41 @@ import edu.sc.seis.fissuresUtil.database.network.JDBCChannel;
 import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockNetworkId;
 
 public class RT130ToLocalSeismogram {
-    
-    public RT130ToLocalSeismogram(){
+
+    public RT130ToLocalSeismogram() {
         this.conn = null;
-        this.props = null;
+        this.ncFile = null;
     }
 
-    public RT130ToLocalSeismogram(Connection conn, Properties props) {
+    public RT130ToLocalSeismogram(Connection conn, NCFile ncFile) {
         this.conn = conn;
-        this.props = props;
+        this.ncFile = ncFile;
     }
 
-    public LocalSeismogramImpl[] ConvertRT130ToLocalSeismogram(PacketType[] seismogramDataPacket){
+    public LocalSeismogramImpl[] ConvertRT130ToLocalSeismogram(PacketType[] seismogramDataPacket) {
         LocalSeismogramImpl[] seismogramDataArray = new LocalSeismogramImpl[seismogramDataPacket.length];
         this.channel = new Channel[seismogramDataPacket.length];
-        for(int i = 0; i < seismogramDataPacket.length; i++){
-            seismogramDataArray[i] = ConvertRT130ToLocalSeismogram(seismogramDataPacket[i], i);
+        for(int i = 0; i < seismogramDataPacket.length; i++) {
+            seismogramDataArray[i] = ConvertRT130ToLocalSeismogram(seismogramDataPacket[i],
+                                                                   i);
         }
         return seismogramDataArray;
     }
-    
-    public LocalSeismogramImpl ConvertRT130ToLocalSeismogram(PacketType seismogramData, int i) {
+
+    public LocalSeismogramImpl ConvertRT130ToLocalSeismogram(PacketType seismogramData,
+                                                             int i) {
         Time mockBeginTimeOfChannel = seismogramData.begin_time_from_state_of_health_file.getFissuresTime();
         int numPoints = seismogramData.number_of_samples;
         SamplingImpl sampling = new SamplingImpl(seismogramData.sample_rate,
                                                  new TimeInterval(1,
                                                                   UnitImpl.SECOND));
         ChannelId channelId;
-        if(props == null || conn == null) {
+        if(ncFile == null || conn == null) {
             channelId = new ChannelId(MockNetworkId.createNetworkID(),
                                       seismogramData.unitIdNumber,
                                       "" + seismogramData.data_stream_number,
-                                      "BH" + seismogramData.channel_name[seismogramData.channel_number],
+                                      "BH"
+                                              + seismogramData.channel_name[seismogramData.channel_number],
                                       mockBeginTimeOfChannel);
         } else {
             this.channel[i] = createChannel(seismogramData, sampling);
@@ -80,32 +83,34 @@ public class RT130ToLocalSeismogram {
     }
 
     private Channel createChannel(PacketType seismogramData, Sampling sampling) {
-        String stationCode = props.getProperty(STATION_NAME + "."
-                + seismogramData.unitIdNumber);
-        if(stationCode == null){
+        String stationCode = ncFile.getUnitName(seismogramData.begin_time_from_state_of_health_file,
+                                                seismogramData.unitIdNumber);
+        if(stationCode == null) {
             stationCode = seismogramData.unitIdNumber;
+            System.err.println("/-------------------------");
+            System.err.println("| Unit name for DAS unit number "
+                    + seismogramData.unitIdNumber
+                    + " was not found in the NC file.");
+            System.err.println("| The name \""
+                    + seismogramData.unitIdNumber
+                    + "\" will be used instead.");
+            System.err.println("| To correct this entry in the database, please run UnitNameUpdater.");
+            System.err.println("\\-------------------------");
         }
-//        String networkIdString = props.getProperty(NETWORK_ID);
         String networkIdString = "XX";
-//        SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-//        MicroSecondDate msd = null;
-//        try {
-//            msd = new MicroSecondDate(simpleDate.parse(props.getProperty(NETWORK_BEGIN_TIME)));
-//        } catch(ParseException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        Time networkBeginTime = msd.getFissuresTime();
-        ISOTime isoTime = new ISOTime(1970, 0, 0, 0, 0);
-        Time networkBeginTime = isoTime.getDate().getFissuresTime();
+        Time networkBeginTime = ncFile.network_begin_time.getFissuresTime();
         Time channelBeginTime = seismogramData.begin_time_from_state_of_health_file.getFissuresTime();
         NetworkId networkId = new NetworkId(networkIdString, networkBeginTime);
-        ChannelId channelId = new ChannelId(networkId, stationCode, "00", "BH"
-                + seismogramData.channel_name[seismogramData.channel_number], channelBeginTime);
+        ChannelId channelId = new ChannelId(networkId,
+                                            stationCode,
+                                            "00",
+                                            "BH"
+                                                    + seismogramData.channel_name[seismogramData.channel_number],
+                                            channelBeginTime);
         TimeRange effectiveNetworkTime = new TimeRange(networkBeginTime,
-                                                TimeUtils.timeUnknown);
+                                                       TimeUtils.timeUnknown);
         TimeRange effectiveChannelTime = new TimeRange(channelBeginTime,
-                                                         TimeUtils.timeUnknown);
+                                                       TimeUtils.timeUnknown);
         SiteId siteId = new SiteId(networkId,
                                    stationCode,
                                    "00",
@@ -139,27 +144,23 @@ public class RT130ToLocalSeismogram {
                                      station,
                                      "");
         Channel newChannel = new ChannelImpl(channelId,
-                                          "",
-                                          new Orientation(0, -90),
-                                          sampling,
-                                          effectiveChannelTime,
-                                          site);
+                                             "",
+                                             new Orientation(0, -90),
+                                             sampling,
+                                             effectiveChannelTime,
+                                             site);
         return newChannel;
     }
-    
-    public Channel[] getChannels(){
+
+    public Channel[] getChannels() {
         return this.channel;
     }
-    
-    private Channel[] channel;
 
-    private final String NETWORK_ID = "network.networkId";
+    private Channel[] channel;
 
     private final String STATION_NAME = "station.stationName";
 
-    private final String NETWORK_BEGIN_TIME = "network.beginTime";
-
     private Connection conn;
 
-    private Properties props;
+    private NCFile ncFile;
 }
