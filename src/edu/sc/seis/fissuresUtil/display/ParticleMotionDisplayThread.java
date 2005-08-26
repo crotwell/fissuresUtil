ce93f782dbd1a6c9d9acf2d62371601689186dc2
groves <groves@localhost>
2005-08-26 16:50:11 -0400

    [svn r14641] use new data set seismogram methods in isHorizontalPlane

--- src/edu/sc/seis/fissuresUtil/display/ParticleMotionDisplayThread.java
+++ src/edu/sc/seis/fissuresUtil/display/ParticleMotionDisplayThread.java
@@ -1,22 +1,23 @@
 package edu.sc.seis.fissuresUtil.display;
+
 import java.awt.Color;
 import org.apache.log4j.Category;
+import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.sc.seis.fissuresUtil.display.registrar.TimeConfig;
-import edu.sc.seis.fissuresUtil.xml.DataSet;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 
 /**
  * ParticleMotionDisplayThread.java
- *
- *
+ * 
+ * 
  * Created: Fri Jul 26 12:42:56 2002
- *
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
+public class ParticleMotionDisplayThread {
 
-public class ParticleMotionDisplayThread{
     public ParticleMotionDisplayThread(DataSetSeismogram dataSetSeismogram,
                                        TimeConfig tc,
                                        ParticleMotionDisplay particleMotionDisplay,
@@ -26,48 +27,56 @@ public class ParticleMotionDisplayThread{
         this.particleMotionDisplay = particleMotionDisplay;
         this.displayColor = displayColor;
     }
-    
-    
+
     public void execute() {
         dssArray = DisplayUtils.getComponents(dataSetSeismogram);
-        if(dssArray.length < 2){// in case only one component was available
+        if(dssArray.length < 2) {// in case only one component was available
             completion = false;
             return;
         }
-        
         ChannelId[] channelGroup = new ChannelId[dssArray.length];
         for(int counter = 0; counter < dssArray.length; counter++) {
             channelGroup[counter] = dssArray[counter].getRequestFilter().channel_id;
         }
-        
         for(int counter = 0; counter < dssArray.length; counter++) {
             String counterOrientation = DisplayUtils.getOrientationName(channelGroup[counter].channel_code);
-            for(int subcounter = counter+1; subcounter < dssArray.length; subcounter++) {
-                boolean horizPlane = isHorizontalPlane(dssArray[counter].getRequestFilter().channel_id,
-                                                       dssArray[subcounter].getRequestFilter().channel_id,
-                                                       dssArray[counter].getDataSet());
+            for(int subcounter = counter + 1; subcounter < dssArray.length; subcounter++) {
+                boolean horizPlane = isHorizontalPlane(dssArray[counter].getChannel(),
+                                                       dssArray[subcounter].getChannel());
                 String subCounterOrientation = DisplayUtils.getOrientationName(channelGroup[subcounter].channel_code);
                 String orientationString;
-                DataSetSeismogram hSeis,vSeis;
-                //From display util determined orientation, lay the seismograms
-                //out in the particle motion view
-                if(counterOrientation.equals(DisplayUtils.UP)){//Up always goes vertical
+                DataSetSeismogram hSeis, vSeis;
+                // From display util determined orientation, lay the seismograms
+                // out in the particle motion view
+                if(counterOrientation.equals(DisplayUtils.UP)) {// Up always
+                    // goes vertical
                     vSeis = dssArray[counter];
                     hSeis = dssArray[subcounter];
-                    orientationString = counterOrientation + "-" + subCounterOrientation;
-                }else if(counterOrientation.equals(DisplayUtils.EAST)){//East always goes horizontal
+                    orientationString = counterOrientation + "-"
+                            + subCounterOrientation;
+                } else if(counterOrientation.equals(DisplayUtils.EAST)) {// East
+                    // always
+                    // goes
+                    // horizontal
                     vSeis = dssArray[subcounter];
                     hSeis = dssArray[counter];
-                    orientationString = subCounterOrientation + "-" + counterOrientation;
-                }else{//counterOrientation must be North
-                    if(subCounterOrientation.equals(DisplayUtils.EAST)){//North goes vertical with East
+                    orientationString = subCounterOrientation + "-"
+                            + counterOrientation;
+                } else {// counterOrientation must be North
+                    if(subCounterOrientation.equals(DisplayUtils.EAST)) {// North
+                        // goes
+                        // vertical
+                        // with
+                        // East
                         vSeis = dssArray[counter];
                         hSeis = dssArray[subcounter];
-                        orientationString = counterOrientation + "-" + subCounterOrientation;
-                    }else{//North goes horizontal with Up
+                        orientationString = counterOrientation + "-"
+                                + subCounterOrientation;
+                    } else {// North goes horizontal with Up
                         vSeis = dssArray[subcounter];
                         hSeis = dssArray[counter];
-                        orientationString = subCounterOrientation + "-" + counterOrientation;
+                        orientationString = subCounterOrientation + "-"
+                                + counterOrientation;
                     }
                 }
                 particleMotionDisplay.getView().add(hSeis,
@@ -80,39 +89,30 @@ public class ParticleMotionDisplayThread{
         }
         completion = true;
     }
-    
-    public boolean isHorizontalPlane(ChannelId channelIdone,
-                                     ChannelId channelIdtwo,
-                                     DataSet dataset) {
-        
-        if(dataset.getChannel(channelIdone).an_orientation.dip == 0 &&
-           dataset.getChannel(channelIdtwo).an_orientation.dip == 0)  {
-            return true;
-        }
-        return false;
+
+    public boolean isHorizontalPlane(Channel one, Channel two) {
+        return one.an_orientation.dip == 0 && two.an_orientation.dip == 0;
     }
-    
+
     /**
-     *@returns true if the execution step of the thread finishes correctly.
-     *
+     * @returns true if the execution step of the thread finishes correctly.
+     * 
      */
-    public boolean getCompletion(){ return completion; }
-    
+    public boolean getCompletion() {
+        return completion;
+    }
+
     private boolean completion = false;
-    
+
     private DataSetSeismogram dataSetSeismogram;
-    
+
     private DataSetSeismogram[] dssArray;
-    
+
     private TimeConfig tc;
-    
-    private boolean advancedOption = false;
-    
-    private ParticleMotionDisplay  particleMotionDisplay;
-    
+
+    private ParticleMotionDisplay particleMotionDisplay;
+
     private Color displayColor;
-    
-    static Category logger =
-        Category.getInstance(ParticleMotionDisplayThread.class.getName());
-    
+
+    static Category logger = Category.getInstance(ParticleMotionDisplayThread.class.getName());
 }// ParticleMotionDisplayThread
