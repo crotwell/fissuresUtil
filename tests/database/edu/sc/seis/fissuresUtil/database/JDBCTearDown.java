a7352ae9bd6a78ed7062d9069bf215c4f6fd18da
Charles Groves <groves@seis.sc.edu>
2005-09-07 11:20:52 -0400

    [svn r14707] set up logging as early as possible

--- tests/database/edu/sc/seis/fissuresUtil/database/JDBCTearDown.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/JDBCTearDown.java
@@ -2,11 +2,15 @@ package edu.sc.seis.fissuresUtil.database;
 
 import java.sql.SQLException;
 import junit.framework.TestCase;
+import org.apache.log4j.BasicConfigurator;
 
 /**
  * @author groves Created on Oct 28, 2004
  */
 public abstract class JDBCTearDown extends TestCase {
+    public JDBCTearDown(){
+    BasicConfigurator.configure();
+    }
 
     public void tearDown() throws SQLException {
         ConnMgr.createConnection().createStatement().execute("SHUTDOWN");
