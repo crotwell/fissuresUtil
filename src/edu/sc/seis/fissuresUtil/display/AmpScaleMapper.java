7423c9a7f36a9ad04bb752ac58623c800fab2e5d
Charles Groves <groves@seis.sc.edu>
2005-09-15 13:37:06 -0400

    [svn r14761] fixed formatting

--- src/edu/sc/seis/fissuresUtil/display/AmpScaleMapper.java
+++ src/edu/sc/seis/fissuresUtil/display/AmpScaleMapper.java
@@ -8,57 +8,56 @@ import edu.sc.seis.fissuresUtil.display.registrar.LazyAmpEvent;
 
 /**
  * AmpScaleMapper.java
- *
- *
+ * 
+ * 
  * Created: Fri Oct 22 14:47:39 1999
- *
+ * 
  * @author Philip Crotwell
  * @version
  */
-
 public class AmpScaleMapper extends UnitRangeMapper implements AmpListener {
-    
+
     /**
      * this constructor assumes that some outside entity is going to set the
      * range on this scalemapper
      */
-    public AmpScaleMapper(int totalPixels, int hintPixels){
+    public AmpScaleMapper(int totalPixels, int hintPixels) {
         this(totalPixels, hintPixels, null);
     }
-    
-    
+
     /**
-     * this constructor uses the passed in amp config to determine the value
-     * for the scale mapper
+     * this constructor uses the passed in amp config to determine the value for
+     * the scale mapper
      */
-    public AmpScaleMapper(int totalPixels,
-                          int hintPixels,
-                          AmpConfig ac){
+    public AmpScaleMapper(int totalPixels, int hintPixels, AmpConfig ac) {
         super(totalPixels, hintPixels, true);
         setUnitRange(DisplayUtils.ONE_RANGE);
-        if(ac != null) setAmpConfig(ac);
+        if(ac != null)
+            setAmpConfig(ac);
     }
-    
-    public void updateAmp(AmpEvent event){
-        if(event instanceof LazyAmpEvent){
+
+    public void updateAmp(AmpEvent event) {
+        if(event instanceof LazyAmpEvent) {
             ((LazyAmpEvent)event).addCalculateListener(this);
-        }else setUnitRange(event.getAmp());
+        } else
+            setUnitRange(event.getAmp());
     }
-    
-    public void setAmpConfig(AmpConfig ac){
-        if(ampConfig != null) ampConfig.removeListener(this);
+
+    public void setAmpConfig(AmpConfig ac) {
+        if(ampConfig != null) {
+            ampConfig.removeListener(this);
+        }
         ampConfig = ac;
         ac.addListener(this);
     }
-    
-    public String getAxisLabel(){
+
+    public String getAxisLabel() {
         return "Amplitude (" + UnitDisplayUtil.getNameForUnit(getUnit()) + ")";
     }
-    
+
     private UnitDisplayUtil unitDisplayUtil = new UnitDisplayUtil();
-    
+
     static Logger logger = Logger.getLogger(AmpScaleMapper.class);
-    
+
     private AmpConfig ampConfig;
-    
 } // AmpScaleMapper
