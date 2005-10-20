8234651d1f3e6f7ee247853f01af74f3cb4acce9
fenner <fenner@localhost>
2005-10-20 14:31:38 -0400

    [svn r14949] better logging messages

--- src/edu/sc/seis/fissuresUtil/database/seismogram/JDBCSeismogramFiles.java
+++ src/edu/sc/seis/fissuresUtil/database/seismogram/JDBCSeismogramFiles.java
@@ -285,7 +285,7 @@ public class JDBCSeismogramFiles extends JDBCTable {
                         }
                     }
                 } catch(Exception e) {
-                    GlobalExceptionHandler.handle("Problem occured while returning seismograms from the database.",
+                    GlobalExceptionHandler.handle("Problem occured while returning seismograms from the database." + "\n" + "The problem file is located at " + databaseResults.getString(4),
                                                   e);
                 }
             } else {
@@ -318,7 +318,7 @@ public class JDBCSeismogramFiles extends JDBCTable {
             RT130ToLocalSeismogram toSeismogram = new RT130ToLocalSeismogram();
             seismogramArray = toSeismogram.ConvertRT130ToLocalSeismogram(seismogramDataPacketArray);
         } catch(RT130FormatException e) {
-            logger.debug("Problem occured while returning rt130 seismograms from the file listed in the database.");
+            logger.debug("Problem occured while returning rt130 seismograms from the file listed in the database." + "\n" + "The problem file is located at " + seismogramFile);
             return new ArrayList(0);
         }
         List matchingSeismogramsResultList = new ArrayList(0);
