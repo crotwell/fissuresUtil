c06e9532cd4cd3d1abd3876c8955067c907253b2
Philip Crotwell <crotwell@seis.sc.edu>
2005-10-28 14:24:15 -0400

    [svn r15037] km to deg

--- src/edu/sc/seis/fissuresUtil/bag/DistAz.java
+++ src/edu/sc/seis/fissuresUtil/bag/DistAz.java
@@ -255,9 +255,12 @@ public class DistAz {
     private double stalat, stalon, evtlat, evtlon;
     private int hash;
     private boolean hashSet = false;
-    
+
     public static double degreesToKilometers(double degrees) {
         return degrees * 111.19;
+    }
+    public static double kilometersToDegrees(double kilometers) {
+        return kilometers / 111.19;
     }
 
     public static void main(String[] args) {
