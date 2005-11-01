e94491248760f42a6c53c86cf1a6132f28d3b146
Charles Groves <groves@seis.sc.edu>
2005-10-31 23:25:13 -0500

    [svn r15090] added SometimesNoData DC, AvailUnknown and made failures occur every 10 times and the first try

--- src/edu/sc/seis/fissuresUtil/mockFissures/IfSeismogramDC/NamedMockSeisDC.java
+++ src/edu/sc/seis/fissuresUtil/mockFissures/IfSeismogramDC/NamedMockSeisDC.java
@@ -1,32 +1,71 @@
 package edu.sc.seis.fissuresUtil.mockFissures.IfSeismogramDC;
 
+import org.omg.CORBA.UNKNOWN;
 import edu.iris.Fissures.IfSeismogramDC.DataCenter;
+import edu.iris.Fissures.IfSeismogramDC.LocalSeismogram;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 public class NamedMockSeisDC {
 
     public static DataCenter create(String name) {
-        if(name.equals("AvailAvailAgain")) {
+        if(name.equals("SometimesNoAvail")) {
             return new AvailAvailAgain();
+        } else if(name.equals("SometimesAvailUnknown")) {
+            return new AvailUnknown();
         } else if(name.equals("Simple")) {
             return new MockDC();
+        } else if(name.equals("SometimesNoData")) {
+            return new SometimesNoData();
         }
         throw new RuntimeException("No mock dc by the name of " + name
                 + " known");
     }
 
-    private static class AvailAvailAgain extends MockDC {
+    private static class FailCounter extends MockDC {
 
-        // Alternates between no available data then all data available per
-        // request
-        public RequestFilter[] available_data(RequestFilter[] a_filterseq) {
-            flipOnRequest = !flipOnRequest;
-            if(flipOnRequest) {
-                return a_filterseq;
-            }
-            return new RequestFilter[0];
+        public boolean failThisTime() {
+            return numRequests++ % failEvery == 0;
         }
 
-        boolean flipOnRequest = true;
+        private int numRequests;
+
+        private int failEvery = 10;
+    }
+
+    private static class AvailAvailAgain extends FailCounter {
+
+        // Alternates between no available data then all data available per 10
+        // requests
+        public RequestFilter[] available_data(RequestFilter[] a_filterseq) {
+            if(failThisTime()) {
+                return new RequestFilter[0];
+            }
+            return super.available_data(a_filterseq);
+        }
+    }
+
+    private static class AvailUnknown extends FailCounter {
+
+        // Alternates between an UNKNOWN then all data available per 10
+        // requests
+        public RequestFilter[] available_data(RequestFilter[] a_filterseq) {
+            if(failThisTime()) {
+                throw new UNKNOWN("The void consumes you");
+            }
+            return super.available_data(a_filterseq);
+        }
+    }
+
+    private static class SometimesNoData extends FailCounter {
+
+        // Alternates between an UNKNOWN then all data available per 10
+        // requests
+        public LocalSeismogram[] retrieve_seismograms(RequestFilter[] a_filterseq) {
+            if(failThisTime()) {
+                return new LocalSeismogramImpl[0];
+            }
+            return super.retrieve_seismograms(a_filterseq);
+        }
     }
 }
