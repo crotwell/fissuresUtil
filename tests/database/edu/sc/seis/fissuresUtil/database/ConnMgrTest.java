c02f8aba92e23fba2358464f920ecde2decdb063
Charles Groves <groves@seis.sc.edu>
2005-12-09 00:56:02 -0500

    [svn r15507] removed stupid db type set test

--- tests/database/edu/sc/seis/fissuresUtil/database/ConnMgrTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/ConnMgrTest.java
@@ -3,20 +3,9 @@ package edu.sc.seis.fissuresUtil.database;
 import java.io.IOException;
 import junit.framework.TestCase;
 
-public class ConnMgrTest extends TestCase{
-    public ConnMgrTest(String name){
-        super(name);
-    }
+public class ConnMgrTest extends TestCase {
 
-    public void testSetDB()throws IOException{
-//        ConnMgr.setDB();
-//        assertEquals("org.hsqldb.jdbcDriver", ConnMgr.getSQL("driver"));
-        
-        ConnMgr.setDB("POSTGRES");
-        assertEquals("org.postgresql.Driver", ConnMgr.getSQL("driver"));
-    }
-
-    public void testAddPropLocation() throws IOException{
+    public void testAddPropLocation() throws IOException {
         ConnMgr.addPropsLocation("edu/sc/seis/fissuresUtil/database/event/");
         ConnMgr.setDB();
         assertEquals("Extra event values", ConnMgr.getSQL("Extra.event.values"));
