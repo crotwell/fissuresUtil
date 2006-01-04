cc50e1cb7b5b484409a3f96b0ea2591810fa1412
fenner <fenner@localhost>
2006-01-04 16:59:44 -0500

    [svn r15665] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLLocation.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLLocation.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -10,61 +11,51 @@ import edu.iris.Fissures.Quantity;
 
 /**
  * XMLLocation.java
- *
- *
+ * 
+ * 
  * Created: Wed Jun 12 15:22:08 2002
- *
+ * 
  * @author <a href="mailto:">Philip Crotwell</a>
  * @version
  */
-
 public class XMLLocation {
 
     /**
      * StAX insert
      */
     public static void insert(XMLStreamWriter writer, Location loc)
-        throws XMLStreamException{
-
-        XMLUtil.writeTextElement(writer, "latitude", ""+loc.latitude);
-        XMLUtil.writeTextElement(writer, "longitude", ""+loc.longitude);
-
+            throws XMLStreamException {
+        XMLUtil.writeTextElement(writer, "latitude", "" + loc.latitude);
+        XMLUtil.writeTextElement(writer, "longitude", "" + loc.longitude);
         writer.writeStartElement("elevation");
         XMLQuantity.insert(writer, loc.elevation);
         XMLUtil.writeEndElementWithNewLine(writer);
-
         writer.writeStartElement("depth");
         XMLQuantity.insert(writer, loc.depth);
         XMLUtil.writeEndElementWithNewLine(writer);
-
-        if (loc.type.equals(LocationType.GEOGRAPHIC)){
+        if(loc.type.equals(LocationType.GEOGRAPHIC)) {
             XMLUtil.writeTextElement(writer, "type", "GEOGRAPHIC");
-        }
-        else {
+        } else {
             XMLUtil.writeTextElement(writer, "type", "GEOCENTRIC");
         }
     }
 
-
     /**
      * DOM insert
      */
-    public static void insert(Element element, Location loc){
+    public static void insert(Element element, Location loc) {
         Document doc = element.getOwnerDocument();
-        element.appendChild(XMLUtil.createTextElement(doc,
-                                                      "latitude",
-                                                      ""+loc.latitude));
-        element.appendChild(XMLUtil.createTextElement(doc,
-                                                      "longitude",
-                                                      ""+loc.longitude));
+        element.appendChild(XMLUtil.createTextElement(doc, "latitude", ""
+                + loc.latitude));
+        element.appendChild(XMLUtil.createTextElement(doc, "longitude", ""
+                + loc.longitude));
         Element elevation = doc.createElement("elevation");
         XMLQuantity.insert(elevation, loc.elevation);
         element.appendChild(elevation);
         Element depth = doc.createElement("depth");
         XMLQuantity.insert(depth, loc.depth);
         element.appendChild(depth);
-
-        if (loc.type.equals(LocationType.GEOGRAPHIC)) {
+        if(loc.type.equals(LocationType.GEOGRAPHIC)) {
             element.appendChild(XMLUtil.createTextElement(doc,
                                                           "type",
                                                           "GEOGRAPHIC"));
@@ -75,21 +66,15 @@ public class XMLLocation {
         } // end of else
     }
 
-
     public static Location getLocation(Element base) {
-
-        float latitude = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base, "latitude")));
-        float longitude = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base, "longitude")));
+        float latitude = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base,
+                                                                             "latitude")));
+        float longitude = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base,
+                                                                              "longitude")));
         Element elevationNode = XMLUtil.getElement(base, "elevation");
-        Quantity elevation = null;
-        if(elevationNode != null) {
-            elevation = XMLQuantity.getQuantity(elevationNode);
-        }
+        Quantity elevation = XMLQuantity.getQuantity(elevationNode);
         Element depthNode = XMLUtil.getElement(base, "depth");
-        Quantity depth = null;
-        if(depthNode != null) {
-            depth = XMLQuantity.getQuantity(depthNode);
-        }
+        Quantity depth = XMLQuantity.getQuantity(depthNode);
         LocationType locationType;
         String type = XMLUtil.getText(XMLUtil.getElement(base, "type"));
         if(type.equals("GEOGRAPHIC")) {
@@ -97,39 +82,27 @@ public class XMLLocation {
         } else {
             locationType = LocationType.GEOCENTRIC;
         }
-        return new Location(latitude,
-                            longitude,
-                            elevation,
-                            depth,
-                            locationType);
+        return new Location(latitude, longitude, elevation, depth, locationType);
     }
 
-    // public static Location getLocation(Element base) {
-
-    //  float latitude = Float.parseFloat(XMLUtil.evalString(base, "latitude"));
-    //  float longitude = Float.parseFloat(XMLUtil.evalString(base, "longitude"));
-    //  NodeList elevationNode = XMLUtil.evalNodeList(base, "elevation");
-    //  Quantity elevation = null;
-    //  if(elevationNode != null && elevationNode.getLength() !=  0) {
-    //      elevation = XMLQuantity.getQuantity((Element)elevationNode.item(0));
-    //  }
-    //  NodeList depthNode = XMLUtil.evalNodeList(base, "depth");
-    //  Quantity depth = null;
-    //  if(depthNode != null && depthNode.getLength() != 0) {
-    //      depth = XMLQuantity.getQuantity((Element)depthNode.item(0));
-    //  }
-    //  LocationType locationType;
-    //  String type = XMLUtil.evalString(base, "type");
-    //  if(type.equals("GEOGRAPHIC")) {
-    //      locationType = LocationType.GEOGRAPHIC;
-    //  } else {
-    //      locationType = LocationType.GEOCENTRIC;
-    //  }
-    //  return new Location(latitude,
-    //              longitude,
-    //              elevation,
-    //              depth,
-    //              locationType);
-    // }
-
-}//XMLLocation
+    public static Location getLocation(XMLStreamReader parser)
+            throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "latitude");
+        float latitude = Float.parseFloat(parser.getElementText());
+        XMLUtil.gotoNextStartElement(parser, "longitude");
+        float longitude = Float.parseFloat(parser.getElementText());
+        XMLUtil.gotoNextStartElement(parser, "elevation");
+        Quantity elevation = XMLQuantity.getQuantity(parser);
+        XMLUtil.gotoNextStartElement(parser, "depth");
+        Quantity depth = XMLQuantity.getQuantity(parser);
+        LocationType locationType;
+        XMLUtil.gotoNextStartElement(parser, "type");
+        String type = parser.getElementText();
+        if(type.equals("GEOGRAPHIC")) {
+            locationType = LocationType.GEOGRAPHIC;
+        } else {
+            locationType = LocationType.GEOCENTRIC;
+        }
+        return new Location(latitude, longitude, elevation, depth, locationType);
+    }
+}// XMLLocation
