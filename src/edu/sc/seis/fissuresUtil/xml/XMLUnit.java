37441a94bbf65796067dad3b780ba57968a3552d
Ben Fenner <fenner@seis.sc.edu>
2006-01-04 17:08:13 -0500

    [svn r15672] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLUnit.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLUnit.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -80,83 +81,26 @@ public class XMLUnit {
     }
 
     public static Unit getUnit(Element base) {
-
-        //Get the UnitBase
-        UnitBase the_unit_base = null;
         Element the_unit_base_node = XMLUtil.getElement(base, "the_unit_base");
-        if(the_unit_base_node != null) {
-            the_unit_base = XMLUnitBase.getUnitBase(the_unit_base_node);
-        }
-
-        //Get the subUnit Elements.
-        Unit[] elements = new Unit[0];
-        Element[] elements_list = XMLUtil.getElementArray(base, "elements");
-        if(elements_list != null && elements_list.length != 0) {
-            for(int counter = 0; counter < elements_list.length; counter++) {
-                elements[counter] = XMLUnit.getUnit(elements_list[counter]);
-            }
-        }
-
-        //Get the power
+        UnitBase the_unit_base = XMLUnitBase.getUnitBase(the_unit_base_node);
         int power = Integer.parseInt(XMLUtil.getText(XMLUtil.getElement(base, "power")));
-
-        //Get the name
-        String name = XMLUtil.getText(XMLUtil.getElement(base, "name"));
-
-        //Get the multi_factor
-        double multi_factor = (double)Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base, "multi_factor")));
-
-        //Get the exponent
         int exponent = Integer.parseInt(XMLUtil.getText(XMLUtil.getElement(base, "exponent")));
 
-        /*return new UnitImpl(
-         power,
-         name,
-         multi_factor,
-         exponent);*/
         return new UnitImpl(the_unit_base,
                             exponent,
                             power);
     }
-
-    //  public static Unit getUnit(Element base) {
-
-    //  //Get the UnitBase
-    //  UnitBase the_unit_base = null;
-    //  NodeList the_unit_base_node = XMLUtil.evalNodeList(base, "the_unit_base");
-    //  if(the_unit_base_node != null && the_unit_base_node.getLength() != 0) {
-    //      the_unit_base = XMLUnitBase.getUnitBase((Element)the_unit_base_node.item(0));
-    //  }
-
-    //  //Get the subUnit Elements.
-    //  Unit[] elements = new Unit[0];
-    //  NodeList elements_list = XMLUtil.evalNodeList(base, "elements");
-    //  if(elements_list != null && elements_list.getLength() != 0) {
-    //      for(int counter = 0; counter < elements_list.getLength(); counter++) {
-    //      elements[counter] = XMLUnit.getUnit((Element)elements_list.item(0));
-    //      }
-    //  }
-
-    //  //Get the power
-    //  int power = Integer.parseInt(XMLUtil.evalString(base, "power"));
-
-    //  //Get the name
-    //  String name = XMLUtil.evalString(base, "name");
-
-    //  //Get the multi_factor
-    //  double multi_factor = (double)Float.parseFloat(XMLUtil.evalString(base, "multi_factor"));
-
-    //  //Get the exponent
-    //  int exponent = Integer.parseInt(XMLUtil.evalString(base, "exponent"));
-
-    //  /*return new UnitImpl(
-    //              power,
-    //              name,
-    //              multi_factor,
-    //              exponent);*/
-    //  return new UnitImpl(the_unit_base,
-    //              exponent,
-    //              power);
-    //     }
+    
+    public static Unit getUnit(XMLStreamReader parser) throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "the_unit_base");
+        UnitBase the_unit_base = XMLUnitBase.getUnitBase(parser);
+        XMLUtil.gotoNextStartElement(parser, "power");
+        int power = Integer.parseInt(parser.getElementText());
+        XMLUtil.gotoNextStartElement(parser, "exponent");
+        int exponent = Integer.parseInt(parser.getElementText());
+        return new UnitImpl(the_unit_base,
+                            exponent,
+                            power);
+    }
 
 }// XMLUnit
