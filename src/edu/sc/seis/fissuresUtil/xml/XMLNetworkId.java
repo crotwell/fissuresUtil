12f1a16e02b9da2f71335dad0f468faaf9a71b35
Ben Fenner <fenner@seis.sc.edu>
2006-01-04 17:14:35 -0500

    [svn r15680] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLNetworkId.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLNetworkId.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -8,22 +9,21 @@ import edu.iris.Fissures.IfNetwork.NetworkId;
 
 /**
  * XMLNetworkId.java
- *
- *
- * Created: Mon Jul  1 14:52:45 2002
- *
+ * 
+ * 
+ * Created: Mon Jul 1 14:52:45 2002
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
-
 public class XMLNetworkId {
 
     /**
      * StAX insert method
      */
-    public static void insert(XMLStreamWriter writer, NetworkId networkId) throws XMLStreamException{
+    public static void insert(XMLStreamWriter writer, NetworkId networkId)
+            throws XMLStreamException {
         XMLUtil.writeTextElement(writer, "network_code", networkId.network_code);
-
         writer.writeStartElement("begin_time");
         XMLTime.insert(writer, networkId.begin_time);
         XMLUtil.writeEndElementWithNewLine(writer);
@@ -32,42 +32,30 @@ public class XMLNetworkId {
     /**
      * DOM insert method
      */
-    public static void insert(Element element, NetworkId networkId){
-     Document doc = element.getOwnerDocument();
-     element.appendChild(XMLUtil.createTextElement(doc,
-                               "network_code",
-                               networkId.network_code));
-     Element begin_time = doc.createElement("begin_time");
-     XMLTime.insert(begin_time, networkId.begin_time);
-     element.appendChild(begin_time);
+    public static void insert(Element element, NetworkId networkId) {
+        Document doc = element.getOwnerDocument();
+        element.appendChild(XMLUtil.createTextElement(doc,
+                                                      "network_code",
+                                                      networkId.network_code));
+        Element begin_time = doc.createElement("begin_time");
+        XMLTime.insert(begin_time, networkId.begin_time);
+        element.appendChild(begin_time);
     }
 
     public static NetworkId getNetworkId(Element base) {
-    //get NetworkCode
-    String network_code = XMLUtil.getText(XMLUtil.getElement(base, "network_code"));
-    //System.out.println("The network_code is "+network_code);
-
-    //get begin_time
-    edu.iris.Fissures.Time begin_time = new edu.iris.Fissures.Time();
-    Element begin_time_node = XMLUtil.getElement(base, "begin_time");
-    if(begin_time_node != null) {
-        begin_time = XMLTime.getFissuresTime(begin_time_node);
+        String network_code = XMLUtil.getText(XMLUtil.getElement(base,
+                                                                 "network_code"));
+        Element begin_time_node = XMLUtil.getElement(base, "begin_time");
+        edu.iris.Fissures.Time begin_time = XMLTime.getFissuresTime(begin_time_node);
+        return new NetworkId(network_code, begin_time);
     }
-    return new NetworkId(network_code, begin_time);
-
+    
+    public static NetworkId getNetworkId(XMLStreamReader parser) throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "network_code");
+        String network_code = parser.getElementText();
+        XMLUtil.gotoNextStartElement(parser, "begin_time");
+        edu.iris.Fissures.Time begin_time = XMLTime.getFissuresTime(parser);
+        return new NetworkId(network_code, begin_time);
     }
-
-//  public static NetworkId getNetworkId(Element base) {
-//  //get NetworkCode
-//  String network_code = XMLUtil.evalString(base, "network_code");
-//  //System.out.println("The network_code is "+network_code);
-
-//  //get begin_time
-//  edu.iris.Fissures.Time begin_time = new edu.iris.Fissures.Time();
-//  NodeList begin_time_node = XMLUtil.evalNodeList(base, "begin_time");
-//  if(begin_time_node != null && begin_time_node.getLength() != 0) {
-//      begin_time = XMLTime.getFissuresTime((Element)begin_time_node.item(0));
-//  }
-//  return new NetworkId(network_code, begin_time);
-//     }
+    
 }// XMLNetworkId
