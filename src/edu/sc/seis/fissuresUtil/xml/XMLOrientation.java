527b98914bef62126b152b28b02cf8015f75c32a
fenner <fenner@localhost>
2006-01-04 17:12:56 -0500

    [svn r15678] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLOrientation.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLOrientation.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -8,24 +9,22 @@ import edu.iris.Fissures.Orientation;
 
 /**
  * XMLOrientation.java
- *
- *
- * Created: Tue Jul  9 14:26:54 2002
- *
+ * 
+ * 
+ * Created: Tue Jul 9 14:26:54 2002
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
-
 public class XMLOrientation {
 
     /**
      * StAX insert method
      */
     public static void insert(XMLStreamWriter writer, Orientation orientation)
-        throws XMLStreamException{
-
-        XMLUtil.writeTextElement(writer, "azimuth", ""+orientation.azimuth);
-        XMLUtil.writeTextElement(writer, "dip", ""+orientation.dip);
+            throws XMLStreamException {
+        XMLUtil.writeTextElement(writer, "azimuth", "" + orientation.azimuth);
+        XMLUtil.writeTextElement(writer, "dip", "" + orientation.dip);
     }
 
     /**
@@ -33,54 +32,46 @@ public class XMLOrientation {
      */
     public static void insert(Element element, Orientation orientation) {
         Document doc = element.getOwnerDocument();
-        element.appendChild(XMLUtil.createTextElement(doc,
-                                                      "azimuth",
-                                                      ""+orientation.azimuth));
-
-        element.appendChild(XMLUtil.createTextElement(doc,
-                                                      "dip",
-                                                      ""+orientation.dip));
+        element.appendChild(XMLUtil.createTextElement(doc, "azimuth", ""
+                + orientation.azimuth));
+        element.appendChild(XMLUtil.createTextElement(doc, "dip", ""
+                + orientation.dip));
     }
 
     public static Orientation getOrientation(Element base) {
-        //get the azimuth
         float azimuth;
         try {
-            azimuth = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base, "azimuth")));
+            azimuth = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base,
+                                                                          "azimuth")));
         } catch(NumberFormatException nfe) {
             azimuth = 0.0f;
         }
-
-        //get the dip
         float dip;
         try {
-            dip = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base, "dip")));
+            dip = Float.parseFloat(XMLUtil.getText(XMLUtil.getElement(base,
+                                                                      "dip")));
         } catch(NumberFormatException nfe) {
             dip = 0.0f;
         }
-        return new Orientation(azimuth,
-                               dip);
-
+        return new Orientation(azimuth, dip);
     }
-
-    //     public static Orientation getOrientation(Element base) {
-    //  //get the azimuth
-    //  float azimuth;
-    //  try {
-    //      azimuth = Float.parseFloat(XMLUtil.evalString(base, "azimuth"));
-    //  } catch(NumberFormatException nfe) {
-    //      azimuth = 0.0f;
-    //  }
-
-    //  //get the dip
-    //  float dip;
-    //  try {
-    //      dip = Float.parseFloat(XMLUtil.evalString(base, "dip"));
-    //  } catch(NumberFormatException nfe) {
-    //      dip = 0.0f;
-    //  }
-    //  return new Orientation(azimuth,
-    //                 dip);
-
-    //     }
+    
+    public static Orientation getOrientation(XMLStreamReader parser) throws XMLStreamException {
+        float azimuth;
+        try {
+            XMLUtil.gotoNextStartElement(parser, "azimuth");
+            azimuth = Float.parseFloat(parser.getElementText());
+        } catch(NumberFormatException nfe) {
+            azimuth = 0.0f;
+        }
+        float dip;
+        try {
+            XMLUtil.gotoNextStartElement(parser, "dip");
+            dip = Float.parseFloat(parser.getElementText());
+        } catch(NumberFormatException nfe) {
+            dip = 0.0f;
+        }
+        return new Orientation(azimuth, dip);
+    }
+    
 }// XMLOrientation
