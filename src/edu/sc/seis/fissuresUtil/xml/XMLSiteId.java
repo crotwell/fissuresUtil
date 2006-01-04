953a20d77c479ec7639d8e50bab5d2bb613b86a8
fenner <fenner@localhost>
2006-01-04 16:40:57 -0500

    [svn r15656] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLSiteId.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLSiteId.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -9,29 +10,25 @@ import edu.iris.Fissures.IfNetwork.SiteId;
 
 /**
  * XMLSiteId.java
- *
- *
- * Created: Tue Jul  9 13:33:00 2002
- *
+ * 
+ * 
+ * Created: Tue Jul 9 13:33:00 2002
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
-
 public class XMLSiteId {
 
     /**
      * StAX insert
      */
     public static void insert(XMLStreamWriter writer, SiteId siteId)
-        throws XMLStreamException{
-
+            throws XMLStreamException {
         writer.writeStartElement("network_id");
         XMLNetworkId.insert(writer, siteId.network_id);
         XMLUtil.writeEndElementWithNewLine(writer);
-
         XMLUtil.writeTextElement(writer, "station_code", siteId.station_code);
         XMLUtil.writeTextElement(writer, "site_code", siteId.site_code);
-
         writer.writeStartElement("begin_time");
         XMLTime.insert(writer, siteId.begin_time);
         XMLUtil.writeEndElementWithNewLine(writer);
@@ -40,12 +37,11 @@ public class XMLSiteId {
     /**
      * DOM insert
      */
-    public static void insert(Element element, SiteId siteId){
+    public static void insert(Element element, SiteId siteId) {
         Document doc = element.getOwnerDocument();
         Element network_id = doc.createElement("network_id");
         XMLNetworkId.insert(network_id, siteId.network_id);
         element.appendChild(network_id);
-
         element.appendChild(XMLUtil.createTextElement(doc,
                                                       "station_code",
                                                       siteId.station_code));
@@ -55,69 +51,29 @@ public class XMLSiteId {
         Element begin_time = doc.createElement("begin_time");
         XMLTime.insert(begin_time, siteId.begin_time);
         element.appendChild(begin_time);
-
     }
 
     public static SiteId getSiteId(Element base) {
-
-        //get the network_id
-        NetworkId network_id = null;
         Element network_id_node = XMLUtil.getElement(base, "network_id");
-        if(network_id_node != null) {
-
-            network_id = XMLNetworkId.getNetworkId(network_id_node);
-        }
-        //if(network_id == null) System.out.println("Network ID is null");
-        //else System.out.println("The Network Id is NOT NULL");
-
-        //get the station_code
-        String station_code = XMLUtil.getText(XMLUtil.getElement(base, "station_code"));
-
-        //get the site_code
+        NetworkId network_id = XMLNetworkId.getNetworkId(network_id_node);
+        String station_code = XMLUtil.getText(XMLUtil.getElement(base,
+                                                                 "station_code"));
         String site_code = XMLUtil.getText(XMLUtil.getElement(base, "site_code"));
-
-        //get the begin_time
-        edu.iris.Fissures.Time begin_time = new edu.iris.Fissures.Time();
         Element begin_time_node = XMLUtil.getElement(base, "begin_time");
-        if(begin_time_node != null) {
-            begin_time = XMLTime.getFissuresTime(begin_time_node);
-        }
-        return new SiteId(network_id,
-                          station_code,
-                          site_code,
-                          begin_time);
-
+        edu.iris.Fissures.Time begin_time = XMLTime.getFissuresTime(begin_time_node);
+        return new SiteId(network_id, station_code, site_code, begin_time);
     }
 
-    //    public static SiteId getSiteId(Element base) {
-
-    //  //get the network_id
-    //  NetworkId network_id = null;
-    //  NodeList network_id_node = XMLUtil.evalNodeList(base, "network_id");
-    //  if(network_id_node != null && network_id_node.getLength() != 0) {
-
-    //      network_id = XMLNetworkId.getNetworkId((Element)network_id_node.item(0));
-    //  }
-    //  //if(network_id == null) System.out.println("Network ID is null");
-    //  //else System.out.println("The Network Id is NOT NULL");
-
-    //  //get the station_code
-    //  String station_code = XMLUtil.evalString(base, "station_code");
-
-    //  //get the site_code
-    //  String site_code = XMLUtil.evalString(base, "site_code");
-
-    //  //get the begin_time
-    //  edu.iris.Fissures.Time begin_time = new edu.iris.Fissures.Time();
-    //  NodeList begin_time_node = XMLUtil.evalNodeList(base, "begin_time");
-    //  if(begin_time_node != null && begin_time_node.getLength() != 0) {
-    //      begin_time = XMLTime.getFissuresTime((Element)begin_time_node.item(0));
-    //  }
-    //  return new SiteId(network_id,
-    //            station_code,
-    //            site_code,
-    //            begin_time);
-
-    //     }
-
+    public static SiteId getSiteId(XMLStreamReader parser)
+            throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "network_id");
+        NetworkId network_id = XMLNetworkId.getNetworkId(parser);
+        XMLUtil.gotoNextStartElement(parser, "station_code");
+        String station_code = parser.getElementText();
+        XMLUtil.gotoNextStartElement(parser, "site_code");
+        String site_code = parser.getElementText();
+        XMLUtil.gotoNextStartElement(parser, "begin_time");
+        edu.iris.Fissures.Time begin_time = XMLTime.getFissuresTime(parser);
+        return new SiteId(network_id, station_code, site_code, begin_time);
+    }
 }// XMLSiteId
