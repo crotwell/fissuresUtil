f78e03f208bf9523893ff47be944e328ef778597
Ben Fenner <fenner@seis.sc.edu>
2006-01-04 16:52:22 -0500

    [svn r15662] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLRequestFilter.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLRequestFilter.java
@@ -1,12 +1,12 @@
 /**
  * XMLRequestFilter.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -20,16 +20,13 @@ public class XMLRequestFilter {
      * StAX insert
      */
     public static void insert(XMLStreamWriter writer, RequestFilter filter)
-        throws XMLStreamException{
-
+            throws XMLStreamException {
         writer.writeStartElement("channelId");
         XMLChannelId.insert(writer, filter.channel_id);
         XMLUtil.writeEndElementWithNewLine(writer);
-
         writer.writeStartElement("start_time");
         XMLTime.insert(writer, filter.start_time);
         XMLUtil.writeEndElementWithNewLine(writer);
-
         writer.writeStartElement("end_time");
         XMLTime.insert(writer, filter.end_time);
         XMLUtil.writeEndElementWithNewLine(writer);
@@ -39,13 +36,10 @@ public class XMLRequestFilter {
      * DOM insert
      */
     public static void insert(Element element, RequestFilter filter) {
-
         Document doc = element.getOwnerDocument();
-
         Element id = doc.createElement("channelId");
         XMLChannelId.insert(id, filter.channel_id);
         element.appendChild(id);
-
         Element start_time = doc.createElement("start_time");
         XMLTime.insert(start_time, filter.start_time);
         element.appendChild(start_time);
@@ -55,33 +49,23 @@ public class XMLRequestFilter {
     }
 
     public static RequestFilter getRequestFilter(Element base) {
-
-        //get the channel id
         Element id_node = XMLUtil.getElement(base, "channelId");
-        ChannelId id = null;
-        if(id_node != null) {
-            id = XMLChannelId.getChannelId(id_node);
-        }
-
-
-        //get effective_time range
+        ChannelId id = XMLChannelId.getChannelId(id_node);
         Element start_time_node = XMLUtil.getElement(base, "start_time");
-        Time start_time = new Time();
-        if(start_time_node != null) {
-            start_time = XMLTime.getFissuresTime(start_time_node);
-        }
-
-        //get effective_time range
+        Time start_time = XMLTime.getFissuresTime(start_time_node);
         Element end_time_node = XMLUtil.getElement(base, "end_time");
-        Time end_time = new Time();
-        if(end_time_node != null) {
-            end_time = XMLTime.getFissuresTime(end_time_node);
-        }
+        Time end_time = XMLTime.getFissuresTime(end_time_node);
+        return new RequestFilter(id, start_time, end_time);
+    }
 
-        return new RequestFilter(id,
-                                 start_time,
-                                 end_time);
-
+    public static RequestFilter getRequestFilter(XMLStreamReader parser)
+            throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "channelId");
+        ChannelId id = XMLChannelId.getChannelId(parser);
+        XMLUtil.gotoNextStartElement(parser, "start_time");
+        Time start_time = XMLTime.getFissuresTime(parser);
+        XMLUtil.gotoNextStartElement(parser, "end_time");
+        Time end_time = XMLTime.getFissuresTime(parser);
+        return new RequestFilter(id, start_time, end_time);
     }
 }
-
