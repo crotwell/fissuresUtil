e2eddd69a8fad560ae43eb1a4177f83aec4b1aee
fenner <fenner@localhost>
2006-01-04 17:08:39 -0500

    [svn r15674] add StAX reading

--- src/edu/sc/seis/fissuresUtil/xml/XMLEvent.java
+++ src/edu/sc/seis/fissuresUtil/xml/XMLEvent.java
@@ -1,6 +1,7 @@
 package edu.sc.seis.fissuresUtil.xml;
 
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -10,45 +11,39 @@ import edu.iris.Fissures.IfEvent.EventAttr;
 import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
 import edu.iris.Fissures.IfEvent.Origin;
 import edu.iris.Fissures.event.EventAttrImpl;
+import edu.sc.seis.fissuresUtil.cache.EventUtil;
 
 /**
  * XMLEvent.java
- *
- *
+ * 
+ * 
  * Created: Wed Jun 12 14:55:38 2002
- *
+ * 
  * @author <a href="mailto:">Philip Crotwell</a>
  * @version
  */
-
 public class XMLEvent {
 
     /**
      * StAX insert
      */
-    public static void insert(XMLStreamWriter writer, EventAccessOperations event)
-        throws XMLStreamException{
-
+    public static void insert(XMLStreamWriter writer,
+                              EventAccessOperations event)
+            throws XMLStreamException {
         XMLUtil.writeTextElement(writer, "name", event.get_attributes().name);
-
         writer.writeStartElement("region");
         XMLFlinnEngdahlRegion.insert(writer, event.get_attributes().region);
         XMLUtil.writeEndElementWithNewLine(writer);
-
-        try{
-            Origin prefOrigin = event.get_preferred_origin();
-            writer.writeStartElement("preferred_origin");
-            XMLOrigin.insert(writer, prefOrigin);
-            XMLUtil.writeEndElementWithNewLine(writer);
-        }
-        catch (NoPreferredOrigin e){}
+        Origin prefOrigin = EventUtil.extractOrigin(event);
+        writer.writeStartElement("preferred_origin");
+        XMLOrigin.insert(writer, prefOrigin);
+        XMLUtil.writeEndElementWithNewLine(writer);
     }
 
     /**
      * DOM insert
      */
-    public static void insert(Element element,
-                              EventAccessOperations event)  {
+    public static void insert(Element element, EventAccessOperations event) {
         Document doc = element.getOwnerDocument();
         element.appendChild(XMLUtil.createTextElement(doc,
                                                       "name",
@@ -56,40 +51,43 @@ public class XMLEvent {
         Element feRegion = doc.createElement("region");
         XMLFlinnEngdahlRegion.insert(feRegion, event.get_attributes().region);
         element.appendChild(feRegion);
-
-        try {
-            Element prefOrigin = doc.createElement("preferred_origin");
-            XMLOrigin.insert(prefOrigin, event.get_preferred_origin());
-            element.appendChild(prefOrigin);
-        } catch (NoPreferredOrigin e) {
-
-        } // end of try-catch
-
+        Element prefOrigin = doc.createElement("preferred_origin");
+        XMLOrigin.insert(prefOrigin, EventUtil.extractOrigin(event));
+        element.appendChild(prefOrigin);
     }
 
     public static EventAttr getEvent(Element base) {
         String name = XMLUtil.getText(XMLUtil.getElement(base, "name"));
-        Element regionNode = XMLUtil.getElement(base, "region");
-        Element regionElement = null;
-        if(regionNode != null) {
-            regionElement = regionNode;
-        }
-        FlinnEngdahlRegion flinnEngdahlRegion =
-            XMLFlinnEngdahlRegion.getRegion(regionElement);
-        return new EventAttrImpl(name,
-                                 flinnEngdahlRegion);
+        Element region = XMLUtil.getElement(base, "region");
+        FlinnEngdahlRegion flinnEngdahlRegion = XMLFlinnEngdahlRegion.getRegion(region);
+        return new EventAttrImpl(name, flinnEngdahlRegion);
+    }
+
+    public static EventAttr getEvent(XMLStreamReader parser)
+            throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "name");
+        String name = parser.getElementText();
+        FlinnEngdahlRegion flinnEngdahlRegion = XMLFlinnEngdahlRegion.getRegion(parser);
+        return new EventAttrImpl(name, flinnEngdahlRegion);
     }
 
     public static Origin getPreferredOrigin(Element base) {
-        Element preferred_originNode = XMLUtil.getElement(base,"preferred_origin");
-        Element preferred_originElement = null;
+        Element preferred_originElement = XMLUtil.getElement(base,
+                                                             "preferred_origin");
         Origin preferred_origin;
-        if(preferred_originNode != null) {
-            preferred_originElement = preferred_originNode;
-            preferred_origin =
-                XMLOrigin.getOrigin(preferred_originElement);
-        }  else preferred_origin = null;
+        if(preferred_originElement != null) {
+            preferred_origin = XMLOrigin.getOrigin(preferred_originElement);
+        } else
+            preferred_origin = null;
         return preferred_origin;
     }
 
+    public static Origin getPreferredOrigin(XMLStreamReader parser) throws XMLStreamException {
+        XMLUtil.gotoNextStartElement(parser, "preferred_origin");
+        if(parser.hasNext()){
+            return XMLOrigin.getOrigin(parser);
+        } else {
+            return null;
+        }
+    }
 }// XMLEvent
