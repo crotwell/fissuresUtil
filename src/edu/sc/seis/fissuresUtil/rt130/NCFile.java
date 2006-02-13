1920ebd9fc56e32eb9b4b2ab871b1f01b1986d7c
fenner <fenner@localhost>
2006-02-13 18:29:36 -0500

    [svn r16056] formatting and better error messages

--- src/edu/sc/seis/fissuresUtil/rt130/NCFile.java
+++ src/edu/sc/seis/fissuresUtil/rt130/NCFile.java
@@ -18,11 +18,10 @@ import edu.iris.Fissures.model.ISOTime;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.fissuresUtil.time.SortTool;
 
-
 public class NCFile {
 
     public NCFile(String ncFileLocation) throws IOException {
-        File file = new File(ncFileLocation);
+        file = new File(ncFileLocation);
         FileInputStream fis = new FileInputStream(file);
         BufferedInputStream bis = new BufferedInputStream(fis);
         DataInputStream dis = new DataInputStream(bis);
@@ -34,13 +33,13 @@ public class NCFile {
         boolean done = false;
         String data = "";
         String line = "";
-        while(!done){
-            try{
+        while(!done) {
+            try {
                 line = in.readLine();
-                if(line == null){
+                if(line == null) {
                     done = true;
                 } else {
-                    if(line.startsWith("#")){
+                    if(line.startsWith("#")) {
                         // Do nothing
                     } else {
                         data = data.concat(line);
@@ -53,19 +52,19 @@ public class NCFile {
         }
         StringTokenizer st = new StringTokenizer(data);
         String token = "";
-        while(st.hasMoreTokens()){
+        while(st.hasMoreTokens()) {
             token = st.nextToken();
-            if(token.equals("START")){
+            if(token.equals("START")) {
                 MicroSecondDate time = stringToMicroSecondDate(st.nextToken());
-                if(network_begin_time == null){
+                if(network_begin_time == null) {
                     network_begin_time = time;
                 }
                 Map dataHashMap = new HashMap();
-                while(!st.nextToken().startsWith("chan/dip/azi")){
+                while(!st.nextToken().startsWith("chan/dip/azi")) {
                     // Skip to relevant data
                 }
                 String temp = st.nextToken();
-                while(!temp.startsWith("END")){
+                while(!temp.startsWith("END")) {
                     String unitName = temp;
                     String unitId = st.nextToken().substring(0, 4);
                     dataHashMap.put(unitId, unitName);
@@ -79,40 +78,51 @@ public class NCFile {
         Set keySet = timeAndDataHashMap.keySet();
         MicroSecondDate[] keyArray = (MicroSecondDate[])keySet.toArray(new MicroSecondDate[0]);
         keyList = new ArrayList(keyArray.length);
-        for(int i = 0; i < keyArray.length; i++){
+        for(int i = 0; i < keyArray.length; i++) {
             keyList.add(keyArray[i]);
         }
         Collections.sort(keyList, new SortTool.AscendingTimeSorter());
     }
-    
+
     private MicroSecondDate stringToMicroSecondDate(String timeString) {
         StringTokenizer st = new StringTokenizer(timeString, ":");
         int yearInt = Integer.parseInt(st.nextToken());
         int daysOfYearInt = Integer.parseInt(st.nextToken());
         int hoursInt = Integer.parseInt(st.nextToken());
-        ISOTime isoTime = new ISOTime(yearInt,
-                                      daysOfYearInt,
-                                      hoursInt,
-                                      0,
-                                      0);
+        ISOTime isoTime = new ISOTime(yearInt, daysOfYearInt, hoursInt, 0, 0);
         return isoTime.getDate();
     }
-    
-    public String getUnitName(MicroSecondDate time, String unitId){
+
+    public String getUnitName(MicroSecondDate time, String unitId) {
         MicroSecondDate[] keyArray = (MicroSecondDate[])keyList.toArray(new MicroSecondDate[0]);
-        for(int i = 0; i < keyArray.length; i++){
-            if(time.before(keyArray[i])){
+        for(int i = 0; i < keyArray.length; i++) {
+            if(time.before(keyArray[i])) {
                 Map dataHashMap = (Map)timeAndDataHashMap.get(keyArray[i - 1]);
                 String unitName = (String)dataHashMap.get(unitId);
                 return unitName;
             }
         }
-        return null;
+        System.err.println("/-------------------------");
+        System.err.println("| Unit name for DAS unit number " + unitId
+                + " was not found in the NC file.");
+        System.err.println("| The name \"" + unitId
+                + "\" will be used instead.");
+        System.err.println("| To correct this entry in the database, please run UnitNameUpdater.");
+        System.err.println("| ");
+        System.err.println("| The time requested was: " + time.toString());
+        System.err.println("\\-------------------------");
+        return unitId;
+    }
+
+    public String getCanonicalPath() throws IOException {
+        return file.getCanonicalPath();
     }
 
     public MicroSecondDate network_begin_time;
-    
+
     private List keyList;
-    
+
     private Map timeAndDataHashMap;
+
+    private File file;
 }
