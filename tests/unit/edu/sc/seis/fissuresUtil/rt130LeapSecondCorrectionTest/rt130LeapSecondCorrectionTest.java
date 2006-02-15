802478e0d1ed1b09166c353efce4c92163c00b18
Charles Groves <groves@seis.sc.edu>
2006-02-15 10:58:40 -0500

    [svn r16080] locate the corrections and leap seconds file on the classpath

--- tests/unit/edu/sc/seis/fissuresUtil/rt130LeapSecondCorrectionTest/rt130LeapSecondCorrectionTest.java
+++ tests/unit/edu/sc/seis/fissuresUtil/rt130LeapSecondCorrectionTest/rt130LeapSecondCorrectionTest.java
@@ -7,7 +7,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 import junit.framework.TestCase;
 import edu.iris.Fissures.model.MicroSecondDate;
@@ -16,22 +15,6 @@ import edu.sc.seis.fissuresUtil.rt130.leapSecondCorrection.LeapSecondApplier;
 public class rt130LeapSecondCorrectionTest extends TestCase {
 
     public void setUp() throws IOException, ParseException {
-        leapSecondsFileLoc = "C:\\Documents and Settings\\fenner.POKEY\\Desktop\\src\\seis\\fissuresUtil\\tests\\unit\\edu\\sc\\seis\\fissuresUtil\\rt130LeapSecondCorrectionTest\\LeapSecondsFile.txt";
-        correctionsFileLoc = "C:\\Documents and Settings\\fenner.POKEY\\Desktop\\src\\seis\\fissuresUtil\\tests\\unit\\edu\\sc\\seis\\fissuresUtil\\rt130LeapSecondCorrectionTest\\Corrections.txt";
-        /*--------------
-         * 
-         * LeapSecondsFile.txt:
-         * 05:365:23:59:59:999
-         * 06:181:23:59:59:999
-         * 
-         * Corrections.txt:
-         * 939D;05:365:10:01:47:000
-         * 939D;06:015:16:50:45:000
-         * 91FC;06:020:19:07:39:000
-         * 940B;05:281:19:29:44:000
-         * 6991;06:195:12:30:30:000
-         * 
-         --------------*/
         LeapSecondApplier.addLeapSeconds(leapSecondsFileLoc);
         LeapSecondApplier.addCorrections(correctionsFileLoc);
         format = new SimpleDateFormat("yy:DDD:HH:mm:ss:SSS");
@@ -181,7 +164,9 @@ public class rt130LeapSecondCorrectionTest extends TestCase {
         assertEquals(cal.get(Calendar.SECOND), 32);
     }
 
-    private String leapSecondsFileLoc, correctionsFileLoc;
+    private String leapSecondsFileLoc = "edu/sc/seis/fissuresUtil/rt130LeapSecondCorrectionTest/LeapSecondsFile.txt";
+
+    private String correctionsFileLoc = "edu/sc/seis/fissuresUtil/rt130LeapSecondCorrectionTest/Corrections.txt";
 
     private SimpleDateFormat format;
 
