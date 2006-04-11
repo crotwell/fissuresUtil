8430ce640b9bdf5df2d0c15129b9a35a249b2e26
crotwell <crotwell@localhost>
2006-04-11 17:04:48 -0400

    [svn r16826] add test for Phil's crazy double

--- tests/database/edu/sc/seis/fissuresUtil/database/JDBCQuantityTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/JDBCQuantityTest.java
@@ -20,5 +20,22 @@ public class JDBCQuantityTest extends TestCase{
         assertEquals(id, id2);
         assertEquals(twelveCubicMeters, table.get(id));
     }
+
+    public void testCrazyDoublePut() throws SQLException, NotFound{
+        JDBCQuantity table = new JDBCQuantity();
+        // this value caused problems in postgres, the value
+        // extracted from the database was slightly different
+        // from the value put in
+        long l = Long.parseLong("4619454727891976192");
+        double d = Double.longBitsToDouble(l);
+        QuantityImpl crazyMeters = new QuantityImpl(d, UnitImpl.METER);
+        int id = table.put(crazyMeters);
+        int id2 = table.put(crazyMeters);
+        assertEquals(id, id2);
+        QuantityImpl dbCrazyMeters = table.get(id);
+        assertEquals(crazyMeters, dbCrazyMeters);
+        int id3 = table.put(dbCrazyMeters);
+        assertEquals("put value extracted from db", id, id3);
+    }
 }
 
