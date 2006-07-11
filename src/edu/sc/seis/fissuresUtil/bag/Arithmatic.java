4016efa8c0968fea9c2dd3bda98b368c16193f28
groves <groves@localhost>
2006-07-11 14:17:31 -0400

    [svn r17824] handle arrays of seismograms and dont require a sensitivity object since its not needed

--- src/edu/sc/seis/fissuresUtil/bag/Arithmatic.java
+++ src/edu/sc/seis/fissuresUtil/bag/Arithmatic.java
@@ -6,6 +6,14 @@ import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 public class Arithmatic {
     
+    public static LocalSeismogramImpl[] mul(LocalSeismogramImpl[] seis, float val) throws FissuresException {
+        LocalSeismogramImpl[] results = new LocalSeismogramImpl[seis.length];
+        for(int i = 0; i < seis.length; i++) {
+            results[i] = mul(seis[i], val);
+        }
+        return results;
+    }
+    
     public static LocalSeismogramImpl mul(LocalSeismogramImpl seis, float val) throws FissuresException {
         float[] data = seis.get_as_floats();
         float[] out = new float[data.length];
@@ -14,6 +22,15 @@ public class Arithmatic {
         }
         return new LocalSeismogramImpl(seis, out);
     }
+    
+    public static LocalSeismogramImpl[] div(LocalSeismogramImpl[] seis, float val) throws FissuresException {
+        LocalSeismogramImpl[] results = new LocalSeismogramImpl[seis.length];
+        for(int i = 0; i < seis.length; i++) {
+            results[i] = div(seis[i], val);
+        }
+        return results;
+    }
+    
     
     public static LocalSeismogramImpl div(LocalSeismogramImpl seis, float val) throws FissuresException {
         return mul(seis, 1/val);
