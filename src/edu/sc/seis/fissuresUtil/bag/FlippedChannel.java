8046cce621f2f7a8a19e0c0cb71c24801b14e7b5
Charles Groves <groves@seis.sc.edu>
2006-07-11 14:17:31 -0400

    [svn r17824] handle arrays of seismograms and dont require a sensitivity object since its not needed

--- src/edu/sc/seis/fissuresUtil/bag/FlippedChannel.java
+++ src/edu/sc/seis/fissuresUtil/bag/FlippedChannel.java
@@ -3,34 +3,33 @@ package edu.sc.seis.fissuresUtil.bag;
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.Orientation;
 import edu.iris.Fissures.IfNetwork.Channel;
-import edu.iris.Fissures.IfNetwork.Sensitivity;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 public class FlippedChannel {
 
-	public static ChannelSeismogram correct(Channel chan,
-			LocalSeismogramImpl seis, Sensitivity sensitivity)
-			throws FissuresException {
-		if (check(chan)) {
-			return new ChannelSeismogram(OrientationUtil.flip(chan), Arithmatic
-					.mul(seis, -1), sensitivity);
-		}
-		return new ChannelSeismogram(chan, seis, sensitivity);
-	}
+    public static ChannelSeismogram correct(Channel chan,
+                                            LocalSeismogramImpl seis)
+            throws FissuresException {
+        if(check(chan)) {
+            return new ChannelSeismogram(OrientationUtil.flip(chan),
+                                         Arithmatic.mul(seis, -1),
+                                         null);
+        }
+        return new ChannelSeismogram(chan, seis, null);
+    }
 
-	public static boolean check(Channel chan) {
-		return (chan.get_code().charAt(2) == 'Z' && check(OrientationUtil
-				.getUp(), chan))
-				|| (chan.get_code().charAt(2) == 'N' && check(OrientationUtil
-						.getNorth(), chan))
-				|| (chan.get_code().charAt(2) == 'E' && check(OrientationUtil
-						.getEast(), chan));
+    public static boolean check(Channel chan) {
+        return (chan.get_code().charAt(2) == 'Z' && check(OrientationUtil.getUp(),
+                                                          chan))
+                || (chan.get_code().charAt(2) == 'N' && check(OrientationUtil.getNorth(),
+                                                              chan))
+                || (chan.get_code().charAt(2) == 'E' && check(OrientationUtil.getEast(),
+                                                              chan));
+    }
 
-	}
+    public static boolean check(Orientation correct, Channel chan) {
+        return OrientationUtil.angleBetween(correct, chan.an_orientation) >= 180 - tol;
+    }
 
-	public static boolean check(Orientation correct, Channel chan) {
-		return OrientationUtil.angleBetween(correct, chan.an_orientation) >= 180 - tol;
-	}
-
-	private static double tol = 0.01;
+    private static double tol = 0.01;
 }
