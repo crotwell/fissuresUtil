e01f884db9ad91dbf8a6bb2cce2a337ede9558b6
oliverpa <oliverpa@localhost>
2006-09-15 18:08:36 -0400

    [svn r18200] initial

---
+++ src/edu/sc/seis/fissuresUtil/display/borders/TimeElapsedBorder.java
@@ -0,0 +1,37 @@
+package edu.sc.seis.fissuresUtil.display.borders;
+
+import edu.iris.Fissures.model.UnitImpl;
+import edu.iris.Fissures.model.UnitRangeImpl;
+import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
+import edu.sc.seis.fissuresUtil.display.SeismogramDisplay;
+import edu.sc.seis.fissuresUtil.display.UnitDisplayUtil;
+
+public class TimeElapsedBorder extends UnitRangeBorder {
+
+    public TimeElapsedBorder(SeismogramDisplay disp) {
+        this(disp, UnitImpl.SECOND);
+    }
+
+    public TimeElapsedBorder(SeismogramDisplay disp, UnitImpl units) {
+        this(disp, units, TOP);
+    }
+
+    public TimeElapsedBorder(SeismogramDisplay disp, UnitImpl units, int side) {
+        super(side, ASCENDING, "Time (" + UnitDisplayUtil.getNameForUnit(units)
+                + ")");
+        this.disp = disp;
+        this.units = units;
+    }
+
+    public UnitRangeImpl getRange() {
+        MicroSecondTimeRange timeRange = disp.getTimeConfig().getTime();
+        UnitRangeImpl ur = new UnitRangeImpl(0,
+                                             timeRange.getInterval().value,
+                                             timeRange.getInterval().the_units);
+        return ur.convertTo(units);
+    }
+
+    private SeismogramDisplay disp;
+
+    private UnitImpl units;
+}
