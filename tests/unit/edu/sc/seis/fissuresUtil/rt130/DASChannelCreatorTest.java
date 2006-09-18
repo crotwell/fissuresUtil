8d469024f73c7d06bd7ea8544af6b6e647ea73ff
groves <groves@localhost>
2006-09-18 14:03:59 -0400

    [svn r18213] use rt130filereader directly instead of contracting out to RT130SamplingFinder.  Only lookup sampling for channels that need it

--- tests/unit/edu/sc/seis/fissuresUtil/rt130/DASChannelCreatorTest.java
+++ tests/unit/edu/sc/seis/fissuresUtil/rt130/DASChannelCreatorTest.java
@@ -1,6 +1,5 @@
 package edu.sc.seis.fissuresUtil.rt130;
 
-import java.io.IOException;
 import junit.framework.TestCase;
 import edu.iris.Fissures.Orientation;
 import edu.iris.Fissures.IfNetwork.Channel;
@@ -8,34 +7,22 @@ import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.bag.OrientationUtil;
-import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
 
 public class DASChannelCreatorTest extends TestCase {
 
     public void testUnknownDasChannelCreation() {
-        DASChannelCreator creator = new DASChannelCreator(NCReaderTest.net,
-                                                          new SamplingFinder() {
-
-                                                              public int find(String file,
-                                                                              MicroSecondTimeRange fileTimeWindow)
-                                                                      throws RT130FormatException,
-                                                                      IOException {
-                                                                  return 40;
-                                                              }
-                                                          });
+        DASChannelCreator creator = new DASChannelCreator(NCReaderTest.net);
         MicroSecondDate now = new MicroSecondDate();
-        Channel[] chans = creator.create("1337",
-                                         "1/00331133",
-                                         new MicroSecondTimeRange(now, now));
+        Channel[] chans = creator.create("1337", now, "1", 40);
         assertEquals(3, chans.length);
         assertEquals("1337", chans[0].my_site.my_station.get_code());
         // We assume that the channels are BH[ZNE]
         assertEquals("BHZ", chans[0].get_code());
         Channel[] newChans = creator.create("1337",
-                                            "1/00331133",
-                                            new MicroSecondTimeRange(now.subtract(new TimeInterval(1,
-                                                                                                   UnitImpl.MINUTE)),
-                                                                     now));
+                                            now.subtract(new TimeInterval(1,
+                                                                          UnitImpl.MINUTE)),
+                                            "1",
+                                            40);
         // Dummy DAS channels just expand to fill whatever time is requsted of
         // that das
         for(int i = 0; i < newChans.length; i++) {
