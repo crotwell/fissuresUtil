c42f5f78029255df3e6eea64763bd3de52b3b2a7
Charles Groves <groves@seis.sc.edu>
2006-12-29 16:46:54 -0500

    [svn r18649] expose title text for use in dynamic titlers in sod

--- src/edu/sc/seis/fissuresUtil/display/configuration/BorderTitleConfiguration.java
+++ src/edu/sc/seis/fissuresUtil/display/configuration/BorderTitleConfiguration.java
@@ -30,6 +30,10 @@ public class BorderTitleConfiguration {
         }
         return tp;
     }
+    
+    public String getTitle(){
+        return title;
+    }
 
     public void setText(String text) {
         this.title = text;
