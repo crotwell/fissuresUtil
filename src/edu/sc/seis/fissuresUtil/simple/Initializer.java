d1089d9c1058ed2945fb7c6f4a79e15091343b78
Charles Groves <groves@seis.sc.edu>
2007-01-31 12:02:06 -0500

    [svn r18770] Get help messages for find scripts to below a line per option Allow a props file to be added to the find scripts Add put a sock in it functionality to prop loading if we don't want messages going to std out

--- src/edu/sc/seis/fissuresUtil/simple/Initializer.java
+++ src/edu/sc/seis/fissuresUtil/simple/Initializer.java
@@ -89,6 +89,14 @@ public abstract class Initializer {
 
     public static Properties loadProperties(String[] args, Properties baseProps)
             throws IOException {
+        return loadProperties(args, baseProps, true);
+    }
+
+    /**
+     * If chatty is true, notify std out for every prop file loaded
+     */
+    public static Properties loadProperties(String[] args, Properties baseProps, boolean chatty)
+            throws IOException {
         for(int i = 0; i < args.length - 1; i++) {
             for(int j = 0; j < POSSIBLE_PROP_OPTION_NAMES.length; j++) {
                 String propFilename = args[i + 1];
@@ -96,7 +104,9 @@ public abstract class Initializer {
                     // override with values in local directory,
                     // but still load defaults with original name
                     loadProps(new FileInputStream(propFilename), baseProps);
-                    System.out.println("loaded props file from " + args[i + 1]);
+                    if(chatty){
+                        System.out.println("loaded props file from " + args[i + 1]);
+                    }
                 }
             }
         }
