c7549e8319f562084d3680d375d3ddad0af6d908
Philip Crotwell <crotwell@seis.sc.edu>
2007-07-31 15:31:17 -0400

    [svn r19150] add and sub methods

--- src/edu/sc/seis/fissuresUtil/bag/Arithmatic.java
+++ src/edu/sc/seis/fissuresUtil/bag/Arithmatic.java
@@ -6,6 +6,35 @@ import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 public class Arithmatic {
     
+    public static LocalSeismogramImpl[] add(LocalSeismogramImpl[] seis, float val) throws FissuresException {
+        LocalSeismogramImpl[] results = new LocalSeismogramImpl[seis.length];
+        for(int i = 0; i < seis.length; i++) {
+            results[i] = add(seis[i], val);
+        }
+        return results;
+    }
+    
+    public static LocalSeismogramImpl add(LocalSeismogramImpl seis, float val) throws FissuresException {
+        float[] data = seis.get_as_floats();
+        float[] out = new float[data.length];
+        for(int i = 0; i < data.length; i++) {
+            out[i] = data[i] + val;
+        }
+        return new LocalSeismogramImpl(seis, out);
+    }
+    
+    public static LocalSeismogramImpl[] sub(LocalSeismogramImpl[] seis, float val) throws FissuresException {
+        LocalSeismogramImpl[] results = new LocalSeismogramImpl[seis.length];
+        for(int i = 0; i < seis.length; i++) {
+            results[i] = sub(seis[i], val);
+        }
+        return results;
+    }
+    
+    public static LocalSeismogramImpl sub(LocalSeismogramImpl seis, float val) throws FissuresException {
+        return add(seis, -val);
+    }
+    
     public static LocalSeismogramImpl[] mul(LocalSeismogramImpl[] seis, float val) throws FissuresException {
         LocalSeismogramImpl[] results = new LocalSeismogramImpl[seis.length];
         for(int i = 0; i < seis.length; i++) {
@@ -31,8 +60,16 @@ public class Arithmatic {
         return results;
     }
     
-    
     public static LocalSeismogramImpl div(LocalSeismogramImpl seis, float val) throws FissuresException {
         return mul(seis, 1/val);
+    }
+    
+    public static LocalSeismogramImpl invert(LocalSeismogramImpl seis) throws FissuresException {
+        float[] data = seis.get_as_floats();
+        float[] out = new float[data.length];
+        for(int i = 0; i < data.length; i++) {
+            out[i] = 1/data[i];
+        }
+        return new LocalSeismogramImpl(seis, out);
     }
 }
