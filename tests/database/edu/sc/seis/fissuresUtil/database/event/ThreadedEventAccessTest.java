6affe30597e2e6e92db03e4d092bc0ec61f2f25c
Philip Crotwell <crotwell@seis.sc.edu>
2007-08-21 10:52:56 -0400

    [svn r19218] sleep until all threads are done, reindent

--- tests/database/edu/sc/seis/fissuresUtil/database/event/ThreadedEventAccessTest.java
+++ tests/database/edu/sc/seis/fissuresUtil/database/event/ThreadedEventAccessTest.java
@@ -5,33 +5,43 @@ import java.sql.SQLException;
 import junit.framework.TestCase;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.fissuresUtil.database.ConnMgr;
+import edu.sc.seis.fissuresUtil.database.JDBCTearDown;
 import edu.sc.seis.fissuresUtil.mockFissures.IfEvent.MockEventAccessOperations;
 
-public class ThreadedEventAccessTest extends TestCase {
+public class ThreadedEventAccessTest extends JDBCTearDown {
 
-	public void testThreadedPut() {
-		Runnable r = new Runnable() {
-			public void run() {
-				
-				try {
-					JDBCEventAccess eventTable = new JDBCEventAccess(ConnMgr.createConnection());
-					EventAccessOperations[] events = MockEventAccessOperations
-							.createEvents();
-					for (int i = 0; i < events.length; i++) {
-						eventTable.put(events[i], null, null, null);
-					}
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		};
-		Thread[] t = new Thread[10];
-		for (int i = 0; i < t.length; i++) {
-			t[i] = new Thread(r);
-		}
-		for (int i = 0; i < t.length; i++) {
-			t[i].start();
-		}
-	}
+    public void testThreadedPut() {
+        Runnable r = new Runnable() {
+
+            public void run() {
+                try {
+                    JDBCEventAccess eventTable = new JDBCEventAccess(ConnMgr.createConnection());
+                    EventAccessOperations[] events = MockEventAccessOperations.createEvents();
+                    for(int i = 0; i < events.length; i++) {
+                        eventTable.put(events[i], null, null, null);
+                    }
+                } catch(SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        };
+        ThreadGroup tgroup = new ThreadGroup("ThreadedEventAccessTest");
+        Thread[] t = new Thread[2];
+        for(int i = 0; i < t.length; i++) {
+            t[i] = new Thread(tgroup, r);
+        }
+        for(int i = 0; i < t.length; i++) {
+            t[i].start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+        r.run();
+        while (tgroup.activeCount() > 0) {
+            try {
+                Thread.sleep(100);
+            } catch(InterruptedException e) {}
+        }
+    }
 }
