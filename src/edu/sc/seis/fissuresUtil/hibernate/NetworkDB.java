743701e10b00c191775bd251e588bcc4dd05b3f0
Philip Crotwell <crotwell@seis.sc.edu>
2007-09-20 13:23:13 -0400

    [svn r19259] more work on net hibernation

--- src/edu/sc/seis/fissuresUtil/hibernate/NetworkDB.java
+++ src/edu/sc/seis/fissuresUtil/hibernate/NetworkDB.java
@@ -6,8 +6,13 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.exception.ConstraintViolationException;
 
+import edu.iris.Fissures.IfNetwork.NetworkAttr;
+import edu.iris.Fissures.IfNetwork.NetworkId;
 import edu.iris.Fissures.IfNetwork.Station;
+import edu.iris.Fissures.network.NetworkAttrImpl;
+import edu.iris.Fissures.network.NetworkIdUtil;
 import edu.iris.Fissures.network.StationImpl;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 
@@ -21,11 +26,52 @@ public class NetworkDB extends AbstractHibernateDB {
         super(factory);
     }
 
-    public long put(Station sta) {
+    public int put(NetworkAttr net) {
+        Session session = getSession();
+        Integer dbid = (Integer)session.save(net);
+        return dbid.intValue();
+    }
+
+    public int put(Station sta) {
         Session session = getSession();
         internUnit(sta.my_location);
+        try {
+            int netDbid = put(sta.my_network);
+            System.out.println("Put net: "+netDbid+"  "+((NetworkAttrImpl)sta.my_network).getDbid());
+        } catch(ConstraintViolationException e) {
+            // assume network is already put, attach net
+            try {
+            sta.my_network = getNetworkByCode(sta.my_network.get_id());
+            } catch (NotFound ee) {
+                // something bad happening to the database...
+                throw new RuntimeException(e);
+            }
+        }
         Integer dbid = (Integer)session.save(sta);
-        return dbid.longValue();
+        return dbid.intValue();
+    }
+
+    private NetworkAttr getNetworkByCode(NetworkId netId) throws NotFound {
+        Session session = getSession();
+        Query query = session.createQuery(getNetworkByCodeString);
+        query.setString("netCode", netId.network_code);
+        List result = query.list();
+        if(NetworkIdUtil.isTemporary(netId)) {
+            Iterator it = result.iterator();
+            while(it.hasNext()) {
+                NetworkAttr n = (NetworkAttr)it.next();
+                if (NetworkIdUtil.areEqual(netId, n.get_id())) {
+                    return n;
+                }
+            }
+            throw new NotFound();
+        } else {
+            if(result.size() > 0) {
+                NetworkAttr out = (NetworkAttr)result.get(0);
+                return out;
+            }
+            throw new NotFound();
+        }
     }
 
     public Station getStation(int dbid) throws NotFound {
@@ -35,7 +81,6 @@ public class NetworkDB extends AbstractHibernateDB {
         List result = query.list();
         if(result.size() > 0) {
             StationImpl out = (StationImpl)result.get(0);
-            session.close();
             return out;
         }
         throw new NotFound();
@@ -51,4 +96,6 @@ public class NetworkDB extends AbstractHibernateDB {
     static String getStationByDbIdString = "From edu.iris.Fissures.network.StationImpl s WHERE dbid = :dbid";
 
     static String getAllStationsString = "From edu.iris.Fissures.network.StationImpl s";
+
+    static String getNetworkByCodeString = "From edu.iris.Fissures.network.NetworkAttrImpl n WHERE network_code = :netCode";
 }
