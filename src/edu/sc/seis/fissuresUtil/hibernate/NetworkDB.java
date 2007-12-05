836d871e895ca7a49681c4e51f717b202c72ab9a
Philip Crotwell <crotwell@seis.sc.edu>
2007-12-05 16:48:27 -0500

    [svn r19454] reindent, add get channel by id

--- src/edu/sc/seis/fissuresUtil/hibernate/NetworkDB.java
+++ src/edu/sc/seis/fissuresUtil/hibernate/NetworkDB.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.exception.ConstraintViolationException;
 
+import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.iris.Fissures.IfNetwork.NetworkAttr;
 import edu.iris.Fissures.IfNetwork.NetworkId;
 import edu.iris.Fissures.IfNetwork.StationId;
@@ -32,47 +34,104 @@ public class NetworkDB extends AbstractHibernateDB {
     }
 
     public void rollback() {
-        System.out.println("Rollback: "+this);
+        System.out.println("Rollback: " + this);
         super.rollback();
     }
 
     public void commit() {
-        System.out.println("commit: "+this);
+        System.out.println("commit: " + this);
         super.commit();
     }
-    
-    public int put(NetworkAttr net) {
+
+    public int put(NetworkAttrImpl net) {
         Session session = getSession();
-        Integer dbid = (Integer)session.save(net);
-        return dbid.intValue();
+        try {
+            return ((Integer)session.save(net)).intValue();
+        } catch(ConstraintViolationException e) {
+            // likely networkAttr is already in db, so update
+            Iterator fromDB = getNetworkByCode(net.get_code()).iterator();
+            if(fromDB.hasNext()) {
+                if(NetworkIdUtil.isTemporary(net.get_code())) {
+                    while(fromDB.hasNext()) {
+                        NetworkAttrImpl indb = (NetworkAttrImpl)fromDB.next();
+                        if(net.get_code() == indb.get_code()
+                                && NetworkIdUtil.getTwoCharYear(net.get_id()) == NetworkIdUtil.getTwoCharYear(indb.get_id())) {
+                            net.associateInDB(indb);
+                            getSession().evict(indb);
+                            getSession().saveOrUpdate(net);
+                            return net.getDbid();
+                        }
+                        // shouldn't happen...
+                        throw new RuntimeException("can't find by code/date after ConstraintViolationException",
+                                                   e);
+                    }
+                } else {
+                    // use first and only net
+                    NetworkAttrImpl inDb = (NetworkAttrImpl)fromDB.next();
+                    net.associateInDB(inDb);
+                    getSession().saveOrUpdate(net);
+                    return net.getDbid();
+                }
+            }
+            // shouldn't happen...
+            throw new RuntimeException("can't find by code after ConstraintViolationException",
+                                       e);
+        }
     }
 
     public int put(StationImpl sta) {
         Integer dbid;
-        if (((NetworkAttrImpl)sta.my_network).getDbid() == 0) {
+        if(((NetworkAttrImpl)sta.my_network).getDbid() == 0) {
             // assume network info is already put, attach net
             try {
                 sta.my_network = getNetworkById(sta.my_network.get_id());
             } catch(NotFound ee) {
                 // must not have been added yet
-                put(sta.my_network);
+                put((NetworkAttrImpl)sta.my_network);
             }
         }
         internUnit(sta);
-        dbid = (Integer)getSession().save(sta);
-        return dbid.intValue();
+        if(sta.getDbid() != 0) {
+            getSession().saveOrUpdate(sta);
+            return sta.getDbid();
+        }
+        try {
+            // maybe station is already in db, so update
+            StationImpl indb = getStationById(sta.get_id());
+            sta.associateInDB(indb);
+            getSession().evict(indb);
+            getSession().saveOrUpdate(sta);
+            return sta.getDbid();
+        } catch(NotFound e) {
+            dbid = (Integer)getSession().save(sta);
+            return dbid.intValue();
+        }
     }
 
     public int put(ChannelImpl chan) {
         Integer dbid;
         internUnit(chan);
-        try {
-            chan.my_site.my_station = getStationById(chan.my_site.my_station.get_id());
-        } catch(NotFound e) {
-            int staDbid = put((StationImpl)chan.my_site.my_station);
+        if(((StationImpl)chan.my_site.my_station).getDbid() == 0) {
+            try {
+                chan.my_site.my_station = getStationById(chan.my_site.my_station.get_id());
+            } catch(NotFound e) {
+                int staDbid = put((StationImpl)chan.my_site.my_station);
+            }
         }
-        dbid = (Integer)getSession().save(chan);
-        return dbid.intValue();
+        try {
+            dbid = (Integer)getSession().save(chan);
+            return dbid.intValue();
+        } catch(ConstraintViolationException e) {
+            // likely station is already in db, so update
+            try {
+                ChannelImpl indb = getChannel(chan.get_id());
+                chan.associateInDB(indb);
+                getSession().saveOrUpdate(chan);
+                return chan.getDbid();
+            } catch(NotFound nf) {
+                throw new RuntimeException("Shouldn't happen", e);
+            }
+        }
     }
 
     public StationImpl[] getStationByCodes(String netCode, String staCode) {
@@ -82,27 +141,26 @@ public class NetworkDB extends AbstractHibernateDB {
         List result = query.list();
         return (StationImpl[])result.toArray(new StationImpl[0]);
     }
-    
-    
+
     public List getAllStationsByCode(String staCode) {
         Query query = getSession().createQuery(getAllStationsByCode);
         query.setEntity("staCode", staCode);
         return query.list();
     }
-    
+
     private StationImpl getStationById(StationId staId) throws NotFound {
         Query query = getSession().createQuery(getStationByIdString);
         query.setString("netCode", staId.network_id.network_code);
         query.setString("staCode", staId.station_code);
-        query.setTimestamp("staBegin", new MicroSecondDate(staId.begin_time).getTimestamp());
-        List result = query.list();
-        if(result.size() > 0) {
-            StationImpl out = (StationImpl)result.get(0);
+        query.setTimestamp("staBegin",
+                           new MicroSecondDate(staId.begin_time).getTimestamp());
+        StationImpl out = (StationImpl)query.uniqueResult();
+        if(out != null) {
             return out;
         }
         throw new NotFound();
     }
-    
+
     public List getNetworkByCode(String netCode) {
         Query query = getSession().createQuery(getNetworkByCodeString);
         query.setString("netCode", netCode);
@@ -130,8 +188,9 @@ public class NetworkDB extends AbstractHibernateDB {
     }
 
     public StationImpl getStation(int dbid) throws NotFound {
-        StationImpl out = (StationImpl)getSession().get(StationImpl.class, new Integer(dbid));
-        if (out == null) {
+        StationImpl out = (StationImpl)getSession().get(StationImpl.class,
+                                                        new Integer(dbid));
+        if(out == null) {
             throw new NotFound();
         }
         return out;
@@ -149,10 +208,11 @@ public class NetworkDB extends AbstractHibernateDB {
         List result = query.list();
         return (StationImpl[])result.toArray(new StationImpl[0]);
     }
-    
+
     public ChannelImpl getChannel(int dbid) throws NotFound {
-        ChannelImpl out = (ChannelImpl)getSession().get(ChannelImpl.class, new Integer(dbid));
-        if (out == null) {
+        ChannelImpl out = (ChannelImpl)getSession().get(ChannelImpl.class,
+                                                        new Integer(dbid));
+        if(out == null) {
             throw new NotFound();
         }
         return out;
@@ -163,36 +223,54 @@ public class NetworkDB extends AbstractHibernateDB {
         List result = query.list();
         List out = new ArrayList();
         Iterator it = result.iterator();
-        while (it.hasNext()) {
+        while(it.hasNext()) {
             NetworkAttrImpl attr = (NetworkAttrImpl)it.next();
             CacheNetworkAccess cnet = new LazyNetworkAccess(attr, networkDC);
             out.add(cnet);
         }
         return (CacheNetworkAccess[])out.toArray(new CacheNetworkAccess[0]);
     }
-    
+
     public ChannelImpl[] getChannelsForStation(StationImpl station) {
         Query query = getSession().createQuery(getChannelForStation);
         query.setEntity("station", station);
         List result = query.list();
         return (ChannelImpl[])result.toArray(new ChannelImpl[0]);
     }
-    
-    public ChannelImpl[] getChannelsForStation(StationImpl station, MicroSecondDate when) {
+
+    public ChannelImpl[] getChannelsForStation(StationImpl station,
+                                               MicroSecondDate when) {
         Query query = getSession().createQuery(getChannelForStationAtTime);
         query.setEntity("station", station);
         query.setTimestamp("when", when.getTimestamp());
         List result = query.list();
         return (ChannelImpl[])result.toArray(new ChannelImpl[0]);
     }
-    
+
     public ChannelImpl getChannel(String net,
                                   String sta,
                                   String site,
                                   String chan,
-                                  MicroSecondDate when)
-            throws NotFound {
-        Query query = getSession().createQuery(getChannelByCode);
+                                  MicroSecondDate when) throws NotFound {
+        return getChannel(net, sta, site, chan, when, getChannelByCode);
+    }
+
+    public ChannelImpl getChannel(ChannelId id) throws NotFound {
+        return getChannel(id.network_id.network_code,
+                          id.station_code,
+                          id.site_code,
+                          id.channel_code,
+                          new MicroSecondDate(id.begin_time),
+                          getChannelById);
+    }
+
+    protected ChannelImpl getChannel(String net,
+                                     String sta,
+                                     String site,
+                                     String chan,
+                                     MicroSecondDate when,
+                                     String queryString) throws NotFound {
+        Query query = getSession().createQuery(queryString);
         query.setString("netCode", net);
         query.setString("stationCode", sta);
         query.setString("siteCode", site);
@@ -200,7 +278,7 @@ public class NetworkDB extends AbstractHibernateDB {
         query.setTimestamp("when", when.getTimestamp());
         query.setMaxResults(1);
         List result = query.list();
-        if (result.size() == 0) {
+        if(result.size() == 0) {
             throw new NotFound();
         }
         return (ChannelImpl)result.get(0);
@@ -209,43 +287,61 @@ public class NetworkDB extends AbstractHibernateDB {
     public void internUnit(StationImpl sta) {
         internUnit(sta.getLocation());
     }
-    
-    /** assumes station has aready been interned as this needs to happen to avoid dup stations. */
+
+    /**
+     * assumes station has aready been interned as this needs to happen to avoid
+     * dup stations.
+     */
     public void internUnit(ChannelImpl chan) {
         internUnit(chan.getSite().getLocation());
         internUnit(chan.getSite().getStation());
         internUnit(chan.getSamplingInfo().interval);
     }
-    
+
     private static NetworkDB singleton;
-    
+
     public static NetworkDB getSingleton() {
-        if (singleton == null) {
+        if(singleton == null) {
             singleton = new NetworkDB();
         }
         return singleton;
     }
-    
-    static String getStationByCodes = "SELECT s From "+StationImpl.class.getName()+" s WHERE s.networkAttr.id.network_code = :netCode AND s.id.station_code = :staCode";
-    
-    static String getAllStationsByCode = "SELECT s From "+StationImpl.class.getName()+" s WHERE s.id.station_code = :staCode";
-    
-    static String getStationByIdString = getStationByCodes + " AND sta_begin_time = :staBegin";
 
-    static String getStationForNetwork = "From "+StationImpl.class.getName()+" s WHERE s.networkAttr = :netAttr";
-    
-    static String getStationForNetworkStation = getStationForNetwork+" and s.code = :staCode";
-    
-    static String getChannelForStation = "From "+ChannelImpl.class.getName()+" c WHERE c.site.station = :station";
-    
-    static String getChannelForStationAtTime = getChannelForStation+" and :when between c.beginTime and c.endTime";
-    
-    static String getChannelByCode = "From "+ChannelImpl.class.getName()+" c WHERE c.code = :chanCode AND c.site.code = :siteCode AND c.site.station.code = :stationCode AND c.site.station..networkAttr.id.network_code = :netCode AND :when between c.begin and c.end";
-    
+    static String getStationByCodes = "SELECT s From "
+            + StationImpl.class.getName()
+            + " s WHERE s.networkAttr.id.network_code = :netCode AND s.id.station_code = :staCode";
+
+    static String getAllStationsByCode = "SELECT s From "
+            + StationImpl.class.getName()
+            + " s WHERE s.id.station_code = :staCode";
+
+    static String getStationByIdString = getStationByCodes
+            + " AND sta_begin_time = :staBegin";
+
+    static String getStationForNetwork = "From " + StationImpl.class.getName()
+            + " s WHERE s.networkAttr = :netAttr";
+
+    static String getStationForNetworkStation = getStationForNetwork
+            + " and s.code = :staCode";
+
+    static String getChannelForStation = "From " + ChannelImpl.class.getName()
+            + " c WHERE c.site.station = :station";
+
+    static String getChannelForStationAtTime = getChannelForStation
+            + " and :when between c.beginTime and c.endTime";
+
+    static String getChannelByCode = "From "
+            + ChannelImpl.class.getName()
+            + " c WHERE c.code = :chanCode AND c.site.code = :siteCode AND c.site.station.code = :stationCode AND c.site.station.networkAttr.id.network_code = :netCode AND :when between c.begin and c.end";
+
+    static String getChannelById = "From "
+            + ChannelImpl.class.getName()
+            + " c WHERE c.code = :chanCode AND c.site.code = :siteCode AND c.site.station.code = :stationCode AND c.site.station.networkAttr.id.network_code = :netCode AND :when = c.begin";
+
     static String getAllStationsString = "From edu.iris.Fissures.network.StationImpl s";
 
     static String getAllNetsString = "From edu.iris.Fissures.network.NetworkAttrImpl n";
-    
-    static String getNetworkByCodeString = getAllNetsString+" WHERE network_code = :netCode";
 
+    static String getNetworkByCodeString = getAllNetsString
+            + " WHERE network_code = :netCode";
 }
