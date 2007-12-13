b628e118da5b8f04ee17bb95d075eb515e9220cd
crotwell <crotwell@localhost>
2007-12-13 14:57:29 -0500

    [svn r19483] location type in hibernate

---
+++ src/edu/sc/seis/fissuresUtil/hibernate/LocTypeAccess.java
@@ -0,0 +1,68 @@
+package edu.sc.seis.fissuresUtil.hibernate;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.hibernate.HibernateException;
+import org.hibernate.PropertyNotFoundException;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.property.Getter;
+import org.hibernate.property.PropertyAccessor;
+import org.hibernate.property.Setter;
+
+import edu.iris.Fissures.Location;
+import edu.iris.Fissures.LocationType;
+
+public class LocTypeAccess implements PropertyAccessor {
+
+    public Getter getGetter(Class arg0, String arg1)
+            throws PropertyNotFoundException {
+        return new Getter() {
+
+            public Object get(Object owner) throws HibernateException {
+                return new Integer(((Location)owner).type.value());
+            }
+
+            public Object getForInsert(Object target,
+                                       Map mergeMap,
+                                       SessionImplementor session)
+                    throws HibernateException {
+                return get(target);
+            }
+
+            public Method getMethod() {
+                return null;
+            }
+
+            public String getMethodName() {
+                return null;
+            }
+
+            public Class getReturnType() {
+                return Integer.class;
+            }
+        };
+    }
+
+    public Setter getSetter(Class arg0, String arg1)
+            throws PropertyNotFoundException {
+        return new Setter() {
+
+            public Method getMethod() {
+                return null;
+            }
+
+            public String getMethodName() {
+                return null;
+            }
+
+            public void set(Object target,
+                            Object value,
+                            SessionFactoryImplementor factory)
+                    throws HibernateException {
+                ((Location)target).type = LocationType.from_int(((Integer)value).intValue());
+            }
+        };
+    }
+}
