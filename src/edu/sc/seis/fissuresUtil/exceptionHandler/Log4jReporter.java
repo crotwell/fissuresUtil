18c45f8f36f4af4a689b47347352d82713a12ec9
Philip Crotwell <crotwell@seis.sc.edu>
2008-03-06 10:33:53 -0500

    [svn r19653] use extractors to get subThrowables

--- src/edu/sc/seis/fissuresUtil/exceptionHandler/Log4jReporter.java
+++ src/edu/sc/seis/fissuresUtil/exceptionHandler/Log4jReporter.java
@@ -6,6 +6,7 @@
 
 package edu.sc.seis.fissuresUtil.exceptionHandler;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import org.apache.log4j.Logger;
@@ -15,10 +16,18 @@ import org.apache.log4j.Logger;
 public class Log4jReporter implements ExceptionReporter{
 
     public void report(String message, Throwable e, List sections) throws IOException{
-        if (e.getCause() != null) {
-            logger.error(message, e.getCause());
-        }
         logger.error(message, e);
+        if (e.getCause() != null) {
+            logger.error("...caused by:", e.getCause());
+        } else {
+            Iterator it = GlobalExceptionHandler.getExtractors().iterator();
+            while(it.hasNext()) {
+                Extractor ext = (Extractor)it.next();
+                if (ext.canExtract(e)) {
+                    report("...caused by subthrowable:", ext.getSubThrowable(e), new ArrayList());
+                }
+            }
+        }
         Iterator it = sections.iterator();
         while(it.hasNext()) {
             Section section = (Section)it.next();
