7f3d4bcd96b973c053be6475cfc153a1909f8c73
crotwell <crotwell@localhost>
2008-04-17 16:03:08 -0400

    [svn r19756] work with postgres as well as hsql

--- src/edu/sc/seis/fissuresUtil/hibernate/HibernateUtil.java
+++ src/edu/sc/seis/fissuresUtil/hibernate/HibernateUtil.java
@@ -25,30 +25,40 @@ public class HibernateUtil {
 
     public synchronized static Configuration getConfiguration() {
         if(configuration == null) {
-            logger.debug("Hibernate configuration is null, loading config from "+configFile);
+            logger.debug("Hibernate configuration is null, loading config from "
+                    + configFile);
             configuration = new Configuration().configure(configFile);
         }
         return configuration;
     }
-    
+
     public synchronized static void setConfigFile(String s) {
-        logger.warn("Reseting hibernate configuration: "+s);
+        logger.warn("Reseting hibernate configuration: " + s);
         configFile = s;
         sessionFactory = null;
         configuration = null;
     }
-    
+
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(HibernateUtil.class);
 
     public static void setUpFromConnMgr(Properties props) {
         synchronized(HibernateUtil.class) {
-            getConfiguration()
+            if(ConnMgr.getDB_TYPE().equals(ConnMgr.HSQL)) {
+                logger.info("using hsql dialect");
+                getConfiguration().setProperty("hibernate.dialect", org.hibernate.dialect.HSQLDialect.class.getName());
+            } else if(ConnMgr.getDB_TYPE().equals(ConnMgr.POSTGRES)) {
+                logger.info("using postgres dialect");
+                getConfiguration().setProperty("hibernate.dialect", org.hibernate.dialect.PostgreSQLDialect.class.getName());
+            }
+            getConfiguration().setProperty("hibernate.connection.driver_class",
+                                           ConnMgr.getDriver())
                     .setProperty("hibernate.connection.url", ConnMgr.getURL())
                     .setProperty("hibernate.connection.username",
                                  ConnMgr.getUser())
                     .setProperty("hibernate.connection.password",
                                  ConnMgr.getPass())
-                    .addProperties(props);
+                    .addProperties(props)
+                    .addProperties(ConnMgr.getDBProps());
         }
     }
 }
