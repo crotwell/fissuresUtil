f81e9ab77e13a2867c99a5a535eec3ecba099740
Philip Crotwell <crotwell@seis.sc.edu>
2008-06-11 17:53:40 -0400

    [svn r19812] hibernate the plottable and seisfil db stuff

--- src/edu/sc/seis/fissuresUtil/hibernate/PlottableDB.java
+++ src/edu/sc/seis/fissuresUtil/hibernate/PlottableDB.java
@@ -1,8 +1,15 @@
 package edu.sc.seis.fissuresUtil.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.iris.Fissures.IfNetwork.ChannelId;
+import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.fissuresUtil.database.plottable.PlottableChunk;
 import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
+import edu.sc.seis.fissuresUtil.display.SimplePlotUtil;
+import edu.sc.seis.fissuresUtil.time.RangeTool;
+import edu.sc.seis.fissuresUtil.time.ReduceTool;
 
 
 public class PlottableDB extends AbstractHibernateDB {
@@ -18,15 +25,69 @@ public class PlottableDB extends AbstractHibernateDB {
 
     public PlottableChunk[] get(MicroSecondTimeRange requestRange,
                                 ChannelId chanId,
-                                int i) {
+                                int pixelsPerDay) {
         // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException("Not yet implemented");
     }
 
-    public void put(PlottableChunk[] plottableChunks) {
-        for(int i = 0; i < plottableChunks.length; i++) {
-            getSession().save(plottableChunks[i]);
+    public void put(PlottableChunk[] chunks) {
+        MicroSecondTimeRange stuffInDB = getDroppingRange(chunks);
+        PlottableChunk[] dbChunks = get(stuffInDB,
+                                        chunks[0].getChannel(),
+                                        chunks[0].getPixelsPerDay());
+        PlottableChunk[] everything = new PlottableChunk[chunks.length
+                + dbChunks.length];
+        System.arraycopy(dbChunks, 0, everything, 0, dbChunks.length);
+        System.arraycopy(chunks, 0, everything, dbChunks.length, chunks.length);
+        // scrutinizeEverything(everything, "unmerged");
+        everything = ReduceTool.merge(everything);
+        // scrutinizeEverything(everything, "merged");
+        everything = breakIntoDays(everything);
+        // scrutinizeEverything(everything, "split into days");
+        int rowsDropped = drop(stuffInDB,
+                               chunks[0].getChannel(),
+                               chunks[0].getPixelsPerDay());
+        for(int i = 0; i < everything.length; i++) {
+            getSession().save(everything[i]);
         }
     }
+    
 
+    public int drop(MicroSecondTimeRange requestRange,
+                    ChannelId id,
+                    int samplesPerDay) {
+        // TODO Auto-generated method stub
+        throw new RuntimeException("Not yet implemented");
+        
+    }
+    
+    protected PlottableChunk[] getSmallChunks(MicroSecondTimeRange requestRange,
+                                ChannelId chanId,
+                                int pixelsPerDay) {
+        // TODO Auto-generated method stub
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    private PlottableChunk[] breakIntoDays(PlottableChunk[] everything) {
+        List results = new ArrayList();
+        for(int i = 0; i < everything.length; i++) {
+            PlottableChunk[] days = everything[i].breakIntoDays();
+            for(int j = 0; j < days.length; j++) {
+                results.add(days[j]);
+            }
+        }
+        return (PlottableChunk[])results.toArray(new PlottableChunk[0]);
+    }
+
+    private static MicroSecondTimeRange getDroppingRange(PlottableChunk[] chunks) {
+        MicroSecondTimeRange stuffInDB = RangeTool.getFullTime(chunks);
+        MicroSecondDate startTime = PlottableChunk.stripToDay(stuffInDB.getBeginTime());
+        MicroSecondDate strippedEnd = PlottableChunk.stripToDay(stuffInDB.getEndTime());
+        if(!strippedEnd.equals(stuffInDB.getEndTime())) {
+            strippedEnd = strippedEnd.add(PlottableChunk.ONE_DAY);
+        }
+        return new MicroSecondTimeRange(startTime, strippedEnd);
+    }
+
+    protected static int MIN_CHUNK_SIZE = 100;
 }
