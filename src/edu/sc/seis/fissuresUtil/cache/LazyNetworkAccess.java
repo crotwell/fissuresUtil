9a968fe8423314e7e40d96f2ec463c54ffc6ffc5
crotwell <crotwell@localhost>
2008-09-15 10:22:16 -0400

    [svn r19922] take finder or dc in constructor

--- src/edu/sc/seis/fissuresUtil/cache/LazyNetworkAccess.java
+++ src/edu/sc/seis/fissuresUtil/cache/LazyNetworkAccess.java
@@ -12,15 +12,19 @@ import edu.iris.Fissures.network.NetworkIdUtil;
 public class LazyNetworkAccess extends CacheNetworkAccess {
 
     public LazyNetworkAccess(NetworkAttrImpl attr, ProxyNetworkDC netDC) {
+        this(attr, (ProxyNetworkFinder)netDC.a_finder());
+    }
+
+    public LazyNetworkAccess(NetworkAttrImpl attr, ProxyNetworkFinder netFinder) {
         super(null);
         this.attr = attr;
-        this.netDC = netDC;
+        this.netFinder = netFinder;
     }
 
     public NetworkAccess getNetworkAccess() {
         if(super.getNetworkAccess() == null) {
             try {
-                setNetworkAccess(netDC.a_finder().retrieve_by_id(attr.get_id()));
+                setNetworkAccess(netFinder.retrieve_by_id(attr.get_id()));
             } catch(NetworkNotFound e) {
                 throw new RuntimeException("unable to reconnect to networkaccess: "
                                                    + NetworkIdUtil.toString(attr),
@@ -30,5 +34,5 @@ public class LazyNetworkAccess extends CacheNetworkAccess {
         return super.getNetworkAccess();
     }
 
-    ProxyNetworkDC netDC;
+    ProxyNetworkFinder netFinder;
 }
