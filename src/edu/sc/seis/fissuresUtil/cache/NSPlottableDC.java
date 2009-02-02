0d2b75f22652dfa7b07989a636e8fc5989ed4a94
Philip Crotwell <crotwell@seis.sc.edu>
2009-02-02 14:00:45 -0500

    [svn r20244] implement ProxyPlottableDC instead of PlottableDCOperations, reformat

--- src/edu/sc/seis/fissuresUtil/cache/NSPlottableDC.java
+++ src/edu/sc/seis/fissuresUtil/cache/NSPlottableDC.java
@@ -15,13 +15,14 @@ import edu.iris.Fissures.IfPlottable.PlottableDC;
 import edu.iris.Fissures.IfPlottable.PlottableDCOperations;
 import edu.iris.Fissures.IfPlottable.PlottableNotAvailable;
 import edu.iris.Fissures.IfPlottable.UnsupportedDimension;
+import edu.iris.Fissures.IfSeismogramDC.DataCenter;
+import edu.iris.Fissures.IfSeismogramDC.DataCenterOperations;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
 
-public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
+public class NSPlottableDC implements ServerNameDNS, ProxyPlottableDC {
 
-    public NSPlottableDC(String serverDNS, String serverName,
-            FissuresNamingService fissuresNamingService) {
+    public NSPlottableDC(String serverDNS, String serverName, FissuresNamingService fissuresNamingService) {
         this.serverDNS = serverDNS;
         this.serverName = serverName;
         this.namingService = fissuresNamingService;
@@ -34,18 +35,17 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
     public String getServerName() {
         return serverName;
     }
-    
-    public String getFullName(){
+
+    public String getFullName() {
         return getServerDNS() + "/" + getServerName();
     }
-
 
     public String getServerType() {
         return PLOTTABLEDC_TYPE;
     }
 
     public synchronized void reset() {
-        if(plottableDC.get() != null){
+        if (plottableDC.get() != null) {
             ((PlottableDC)plottableDC.get())._release();
         }
         plottableDC.set(null);
@@ -55,42 +55,28 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
         return getCorbaObject();
     }
 
-    public synchronized org.omg.CORBA.Object getCorbaObject() {
+    public synchronized PlottableDC getCorbaObject() {
         return getPlottableDC();
     }
 
     public synchronized PlottableDC getPlottableDC() {
-        if(plottableDC.get() == null) {
+        if (plottableDC.get() == null) {
             try {
                 try {
-                    plottableDC.set(namingService.getPlottableDC(serverDNS,
-                                                                 serverName));
+                    plottableDC.set(namingService.getPlottableDC(serverDNS, serverName));
                 } catch(Throwable t) {
                     namingService.reset();
-                    plottableDC.set(namingService.getPlottableDC(serverDNS,
-                                                                 serverName));
+                    plottableDC.set(namingService.getPlottableDC(serverDNS, serverName));
                 }
             } catch(org.omg.CosNaming.NamingContextPackage.NotFound e) {
-                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "
-                                                          + serverName + " "
-                                                          + serverDNS + " "
-                                                          + e.toString(),
-                                                  0,
-                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve " + serverName + " " + serverDNS + " "
+                        + e.toString(), 0, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
             } catch(org.omg.CosNaming.NamingContextPackage.CannotProceed e) {
-                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "
-                                                          + serverName + " "
-                                                          + serverDNS + " "
-                                                          + e.toString(),
-                                                  0,
-                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve " + serverName + " " + serverDNS + " "
+                        + e.toString(), 0, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
             } catch(org.omg.CosNaming.NamingContextPackage.InvalidName e) {
-                throw new org.omg.CORBA.TRANSIENT("Unable to resolve "
-                                                          + serverName + " "
-                                                          + serverDNS + " "
-                                                          + e.toString(),
-                                                  0,
-                                                  org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+                throw new org.omg.CORBA.TRANSIENT("Unable to resolve " + serverName + " " + serverDNS + " "
+                        + e.toString(), 0, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
             } // end of try-catch
         } // end of if ()
         return (PlottableDC)plottableDC.get();
@@ -101,8 +87,7 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
             return getPlottableDC().get_whole_day_sizes();
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().get_whole_day_sizes();
         } // end of try-catch
@@ -113,24 +98,19 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
             return getPlottableDC().get_event_sizes();
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().get_event_sizes();
         } // end of try-catch
     }
 
-    public Plottable[] get_for_day(ChannelId chan,
-                                   int year,
-                                   int jday,
-                                   Dimension dim) throws PlottableNotAvailable,
+    public Plottable[] get_for_day(ChannelId chan, int year, int jday, Dimension dim) throws PlottableNotAvailable,
             UnsupportedDimension {
         try {
             return getPlottableDC().get_for_day(chan, year, jday, dim);
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().get_for_day(chan, year, jday, dim);
         } // end of try-catch
@@ -141,42 +121,37 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
             return getPlottableDC().custom_sizes();
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().custom_sizes();
         } // end of try-catch
     }
 
-    public Plottable[] get_for_event(EventAccess event,
-                                     ChannelId chan,
-                                     Dimension dim)
-            throws PlottableNotAvailable, UnsupportedDimension {
+    public Plottable[] get_for_event(EventAccess event, ChannelId chan, Dimension dim) throws PlottableNotAvailable,
+            UnsupportedDimension {
         try {
             return getPlottableDC().get_for_event(event, chan, dim);
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().get_for_event(event, chan, dim);
         } // end of try-catch
     }
 
-    public Plottable[] get_plottable(RequestFilter rf, Dimension dim)
-            throws PlottableNotAvailable, UnsupportedDimension, NotImplemented {
+    public Plottable[] get_plottable(RequestFilter rf, Dimension dim) throws PlottableNotAvailable,
+            UnsupportedDimension, NotImplemented {
         try {
             return getPlottableDC().get_plottable(rf, dim);
         } catch(Throwable e) {
             // retry in case regetting from name service helps
-            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.",
-                        e);
+            logger.warn("Exception in a_channel_finder(), regetting from nameservice to try again.", e);
             reset();
             return getPlottableDC().get_plottable(rf, dim);
         } // end of try-catch
     }
 
-    protected ThreadLocal plottableDC = new ThreadLocal();
+    protected ThreadLocal<PlottableDC> plottableDC = new ThreadLocal<PlottableDC>();
 
     protected String serverDNS;
 
@@ -185,4 +160,17 @@ public class NSPlottableDC implements ServerNameDNS, PlottableDCOperations {
     protected FissuresNamingService namingService;
 
     private static Logger logger = Logger.getLogger(NSPlottableDC.class);
+
+    public PlottableDCOperations getWrappedDC() {
+        return getPlottableDC();
+    }
+
+    public PlottableDCOperations getWrappedDC(Class wrappedClass) {
+        if (wrappedClass.equals(PlottableDC.class)) {
+            return getCorbaObject();
+        } else {
+            throw new IllegalArgumentException("NSPlottableDCs only contain PlottableDCs, so it can't contain a ProxyDC of class "
+                    + wrappedClass);
+        }
+    }
 }
