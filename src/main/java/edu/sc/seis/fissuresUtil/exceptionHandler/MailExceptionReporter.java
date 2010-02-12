a46ac41a68a9d72f1a246ded798f7f824d56947e
crotwell <crotwell@localhost>
2010-02-12 16:30:37 -0500

    [svn r21063] helper method to add mail reporter

--- src/main/java/edu/sc/seis/fissuresUtil/exceptionHandler/MailExceptionReporter.java
+++ src/main/java/edu/sc/seis/fissuresUtil/exceptionHandler/MailExceptionReporter.java
@@ -29,6 +29,19 @@ public class MailExceptionReporter extends ResultMailer implements
                     + " is the max number to send");
         }
     }
+    
+    public static void addMailExceptionReporter(Properties mailProps) {
+        if(mailProps.containsKey("mail.smtp.host")) {
+            try {
+                GlobalExceptionHandler.add(new MailExceptionReporter(mailProps));
+            } catch(MissingPropertyException e) {
+                logger.debug("Not able to add a mail reporter.  This is only a problem if you specified one",
+                             e);
+            }
+        } else {
+            logger.debug("Not trying to add a mail reporter since mail.smtp.host isn't set");
+        }
+    }
 
     /**
      * mail.limit specifies the number of emails to send
