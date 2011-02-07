d41f4f244d483e95b99e51ce179faffecb09efd5
Philip Crotwell <crotwell@seis.sc.edu>
2011-02-07 18:26:35 -0500

    [svn r21997] initial

---
+++ src/main/java/edu/sc/seis/fissuresUtil/stationxml/StationChannelBundle.java
@@ -0,0 +1,37 @@
+package edu.sc.seis.fissuresUtil.stationxml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.iris.Fissures.IfNetwork.Sensitivity;
+import edu.iris.Fissures.network.ChannelImpl;
+import edu.iris.Fissures.network.StationImpl;
+
+
+public class StationChannelBundle {
+
+    public StationChannelBundle(StationImpl station, List<ChannelSensitivityBundle> chanList) {
+        this.station = station;
+        this.chanList = chanList;
+    }
+    
+    public StationChannelBundle(StationImpl station) {
+        this( station, new ArrayList<ChannelSensitivityBundle>());
+    }
+    
+    public void setChanList(List<ChannelSensitivityBundle> chanList) {
+        this.chanList = chanList;
+    }
+
+
+    public StationImpl getStation() {
+        return station;
+    }
+    
+    public List<ChannelSensitivityBundle> getChanList() {
+        return chanList;
+    }
+
+    StationImpl station;
+    List<ChannelSensitivityBundle> chanList;
+}
