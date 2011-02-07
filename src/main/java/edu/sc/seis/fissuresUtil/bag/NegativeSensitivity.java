162eb66c576be4bcff88622a61760ccf63a76a2f
crotwell <crotwell@localhost>
2011-02-07 18:31:48 -0500

    [svn r22003] response as quantity

--- src/main/java/edu/sc/seis/fissuresUtil/bag/NegativeSensitivity.java
+++ src/main/java/edu/sc/seis/fissuresUtil/bag/NegativeSensitivity.java
@@ -2,14 +2,19 @@ package edu.sc.seis.fissuresUtil.bag;
 
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.IfNetwork.Sensitivity;
+import edu.iris.Fissures.model.QuantityImpl;
 import edu.iris.Fissures.network.ChannelImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 
 public class NegativeSensitivity {
 
-	public static boolean check(Sensitivity sensitivity) {
-		return sensitivity.sensitivity_factor < 0;
-	}
+    public static boolean check(Sensitivity sensitivity) {
+        return sensitivity.sensitivity_factor < 0;
+    }
+    
+    public static boolean check(QuantityImpl sensitivity) {
+        return sensitivity.value < 0;
+    }
 
 	public static ChannelSeismogram correct(ChannelImpl chan,
 			LocalSeismogramImpl seis, Sensitivity sensitivity)
