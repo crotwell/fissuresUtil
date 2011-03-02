fbb23e9695e81f1c7a2cf23ecafa39c9f8ad0c5d
Philip Crotwell <crotwell@seis.sc.edu>
2011-03-02 07:41:36 -0500

    [svn r22161] faster Btime to MicroSecondDate conversion, test it. Work on plottable to/from DataRecord, but this does not work yet

--- src/test/java/edu/sc/seis/fissuresUtil/mseed/TestFissuresConvert.java
+++ src/test/java/edu/sc/seis/fissuresUtil/mseed/TestFissuresConvert.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import junit.framework.TestCase;
 import edu.iris.Fissures.FissuresException;
+import edu.iris.Fissures.model.ISOTime;
+import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.SamplingImpl;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
@@ -13,6 +15,7 @@ import edu.iris.dmc.seedcodec.CodecException;
 import edu.sc.seis.fissuresUtil.display.SimplePlotUtil;
 import edu.sc.seis.fissuresUtil.hibernate.PlottableChunk;
 import edu.sc.seis.fissuresUtil.mockFissures.IfSeismogramDC.MockSeismogram;
+import edu.sc.seis.seisFile.mseed.Btime;
 import edu.sc.seis.seisFile.mseed.DataRecord;
 import edu.sc.seis.seisFile.mseed.SeedFormatException;
 
@@ -62,7 +65,7 @@ public class TestFissuresConvert extends TestCase {
     public void testPlottableToMSeed() throws CodecException, IOException, SeedFormatException, FissuresException {
         LocalSeismogramImpl[] seis = new LocalSeismogramImpl[] {MockSeismogram.createDelta()};
         List<PlottableChunk> chunk = SimplePlotUtil.makePlottables(seis, 6000);
-        List<DataRecord> drList = FissuresConvert.toMSeed(chunk, seis[0].getChannelID());
+        List<DataRecord> drList = FissuresConvert.toMSeed(chunk);
         List<PlottableChunk> outChunk = FissuresConvert.toPlottable(drList);
         int numOut = 0;
         int numIn = 0;
@@ -88,5 +91,17 @@ public class TestFissuresConvert extends TestCase {
                 assertEquals(""+i, inY[i], outY[i]);
             }
         }
+    }
+    
+    public void testCovertBtime() {
+        Btime startStruct = new Btime(2011, 60, 1, 2, 3, 4567);
+        MicroSecondDate d1 = FissuresConvert.getMicroSecondTime(startStruct);
+        ISOTime iso = new ISOTime(startStruct.year,
+                                  startStruct.jday,
+                                  startStruct.hour,
+                                  startStruct.min,
+                                  startStruct.sec);
+        MicroSecondDate d = iso.getDate().add(new TimeInterval(startStruct.tenthMilli, UnitImpl.TENTHMILLISECOND));
+        assertEquals(d, d1);
     }
 }
