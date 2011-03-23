612d65c36f5f0640f8dd3c1f4a7492c100ed7eb8
Philip Crotwell <crotwell@seis.sc.edu>
2011-03-23 14:14:02 -0400

    [svn r22223] force TimeZone in constructor

--- src/main/java/edu/sc/seis/fissuresUtil/chooser/ThreadSafeSimpleDateFormat.java
+++ src/main/java/edu/sc/seis/fissuresUtil/chooser/ThreadSafeSimpleDateFormat.java
@@ -3,22 +3,30 @@ package edu.sc.seis.fissuresUtil.chooser;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 
 public class ThreadSafeSimpleDateFormat {
 
     public ThreadSafeSimpleDateFormat(String format) {
+        this(format, TimeZone.getTimeZone("GMT"));
+    }
+    
+    public ThreadSafeSimpleDateFormat(String format, TimeZone zone) {
         this.format = format;
+        this.zone = zone;
     }
     
     protected ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
         protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat(format);
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            sdf.setTimeZone(zone);
+            return sdf;
         }
     };
 
-    public final SimpleDateFormat get() {
-    return threadLocal.get();
+    protected final SimpleDateFormat get() {
+        return threadLocal.get();
     }
     
     public String format(Date e) {
@@ -29,5 +37,11 @@ public class ThreadSafeSimpleDateFormat {
         return get().parse(s);
     }
     
-    String format;
+    public String toPattern() {
+        return format;
+    }
+    
+    protected String format;
+    
+    protected TimeZone zone;
 }
