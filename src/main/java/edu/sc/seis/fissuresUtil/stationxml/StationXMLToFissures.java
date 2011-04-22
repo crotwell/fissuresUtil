7aa6b21fac7311fdd166f8584801221c6262949a
crotwell <crotwell@localhost>
2011-04-22 14:59:43 -0400

    [svn r22309] remove main, only testing code not needed

--- src/main/java/edu/sc/seis/fissuresUtil/stationxml/StationXMLToFissures.java
+++ src/main/java/edu/sc/seis/fissuresUtil/stationxml/StationXMLToFissures.java
@@ -204,39 +204,6 @@ public class StationXMLToFissures {
         return new MicroSecondDate(xml).getFissuresTime();
     }
     
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException, StationXMLException {
-        if (args.length == 0) {
-            System.out.println("Usage: StationXMLToFissures filename.xml");
-            return;
-        }
-        String filename = args[0];
-        XMLInputFactory factory = XMLInputFactory.newInstance();
-        XMLEventReader r = factory.createXMLEventReader(filename,
-                                                        new FileInputStream(filename));
-        XMLEvent e = r.peek();
-        while(! e.isStartElement()) {
-            System.out.println(e);
-            e = r.nextEvent(); // eat this one
-            e = r.peek();  // peek at the next
-        }
-        System.out.println("StaMessage");
-        StaMessage staMessage = new StaMessage(r);
-        System.out.println("Source: "+staMessage.getSource());
-        System.out.println("Sender: "+staMessage.getSender());
-        System.out.println("Module: "+staMessage.getModule());
-        System.out.println("SentDate: "+staMessage.getSentDate());
-        StationIterator it = staMessage.getStations();
-        while(it.hasNext()) {
-            Station s = it.next();
-            System.out.println("XML Station: "+s.getNetCode()+"."+s.getStaCode()+" "+s.getStationEpochs().size());
-            List<StationImpl> sta = convert(s);
-            for (StationImpl stationImpl : sta) {
-                System.out.println("Station: "+stationImpl);
-            }
-        }
-        System.out.println("Done station iterate");
-    }
-    
     public static final String UNKNOWN = "";
     
     public static final TimeInterval ONE_SECOND = new TimeInterval(1, UnitImpl.SECOND);
