9734caf9a07a0fe1ff65ebf023a37d3df2b393c6
crotwell <crotwell@localhost>
2011-07-22 15:53:08 -0400

    [svn r22501] sleep already exists below

--- src/main/java/edu/sc/seis/fissuresUtil/cache/BaseRetryStrategy.java
+++ src/main/java/edu/sc/seis/fissuresUtil/cache/BaseRetryStrategy.java
@@ -18,10 +18,6 @@ public abstract class BaseRetryStrategy implements RetryStrategy {
         if (numRetries == -1 || tryCount <= numRetries) {
             // do a reset every other time
             if (tryCount % 2 == 0) {
-                try {
-                    Thread.sleep(tryCount * 10 *1000); // sleep 10 sec time retries in case server is overwhelmed
-                } catch(InterruptedException e) {
-                }
                 server.reset();
             }
             BulletproofVestFactory.retrySleep(tryCount);
