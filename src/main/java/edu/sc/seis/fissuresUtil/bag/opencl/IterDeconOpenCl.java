696a91b7192017c78eb00c09112d0204f30b3c22
Philip Crotwell <crotwell@seis.sc.edu>
2012-07-27 15:23:38 -0400

    zap println

--- src/main/java/edu/sc/seis/fissuresUtil/bag/opencl/IterDeconOpenCl.java
+++ src/main/java/edu/sc/seis/fissuresUtil/bag/opencl/IterDeconOpenCl.java
@@ -242,7 +242,6 @@ public class IterDeconOpenCl {
         int workGroupSize = (int)Math.min(128, indexReduceAbsMax.getWorkGroupSize().get(queue.getDevice()).intValue());
         int globalWorkSize = (int)Math.min(queue.getDevice().getMaxComputeUnits()*4, workGroupSize);
         if (workGroupSize > globalWorkSize) {workGroupSize = globalWorkSize;}
-        System.out.println("calcMaxSpike workGroup: "+workGroupSize+"  globalSize:"+globalWorkSize+"  device wg: "+indexReduceAbsMax.getWorkGroupSize().get(queue.getDevice()).intValue());
         
         LocalSize sharedMemSize = LocalSize.ofFloatArray(globalWorkSize/workGroupSize);
         LocalSize sharedMemIndexSize = LocalSize.ofIntArray(globalWorkSize/workGroupSize);
