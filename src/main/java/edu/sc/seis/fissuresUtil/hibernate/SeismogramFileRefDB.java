340410e346e8c3b5ff5fcb990a0db0c1daf15a5c
Philip Crotwell <crotwell@seis.sc.edu>
2015-11-02 11:09:38 -0500

    add getSeismogramsForEventForChannel, reformat

--- src/main/java/edu/sc/seis/fissuresUtil/hibernate/SeismogramFileRefDB.java
+++ src/main/java/edu/sc/seis/fissuresUtil/hibernate/SeismogramFileRefDB.java
@@ -41,7 +41,7 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
                                          SeismogramFileTypes filetype) {
         saveSeismogramToDatabase(new SeismogramFileReference(channel, seis, fileLocation, filetype));
     }
-    
+
     public void saveSeismogramToDatabase(SeismogramFileReference seisRef) {
         getSession().save(seisRef);
     }
@@ -53,35 +53,43 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
                                          SeismogramFileTypes filetype) {
         saveSeismogramToDatabase(new EventSeismogramFileReference(event, channel, seis, fileLocation, filetype));
     }
+
     public void saveSeismogramToDatabase(EventSeismogramFileReference seisRef) {
         getSession().save(seisRef);
     }
-    
+
     public List<EventSeismogramFileReference> getSeismogramsForEvent(CacheEvent event) {
-        String query = "from "
-            + EventSeismogramFileReference.class.getName()
-            + " where event = :event";
+        String query = "from " + EventSeismogramFileReference.class.getName() + " where event = :event";
         Query q = getSession().createQuery(query);
         q.setEntity("event", event);
         return q.list();
     }
-    
-    public URLDataSetSeismogram getDataSetSeismogram(ChannelId chan, CacheEvent event, RequestFilter rf) {
-        logger.debug("getDataSetSeismogram: "+getTXID()+"  "+ChannelIdUtil.toStringNoDates(chan)+"  "+event+"  "+rf.start_time.date_time+"  "+rf.end_time.date_time);
-        PrintIfNotCalledOff delayMessage = new PrintIfNotCalledOff(" long running getDataSetSeismogram: "+getTXID()+"  "+ChannelIdUtil.toStringNoDates(chan)+"  "+event+"  "+rf.start_time.date_time+"  "+rf.end_time.date_time);
+
+    public List<EventSeismogramFileReference> getSeismogramsForEventForChannel(CacheEvent event, ChannelId chan) {
         String query = "from "
-            + EventSeismogramFileReference.class.getName()
-            + " where event = :event and "
-            + " networkCode = :netCode and stationCode = :staCode and siteCode = :siteCode and channelCode = :chanCode ";
+                + EventSeismogramFileReference.class.getName()
+                + " where event = :event and "
+                + " networkCode = :netCode and stationCode = :staCode and siteCode = :siteCode and channelCode = :chanCode ";
         Query q = getSession().createQuery(query);
         q.setEntity("event", event);
         q.setString("netCode", chan.network_id.network_code);
         q.setString("staCode", chan.station_code);
         q.setString("siteCode", chan.site_code);
         q.setString("chanCode", chan.channel_code);
-        logger.debug("Before query for event: "+event.getDbid()+"  "+ChannelIdUtil.toStringNoDates(chan));
+        logger.debug("Before query for event: " + event.getDbid() + "  " + ChannelIdUtil.toStringNoDates(chan));
         List<EventSeismogramFileReference> esRefList = q.list();
-        logger.debug("After query for event: "+event.getDbid()+"  "+ChannelIdUtil.toStringNoDates(chan)+"  found:"+esRefList.size());
+        logger.debug("After query for event: " + event.getDbid() + "  " + ChannelIdUtil.toStringNoDates(chan)
+                + "  found:" + esRefList.size());
+        return esRefList;
+    }
+
+    public URLDataSetSeismogram getDataSetSeismogram(ChannelId chan, CacheEvent event, RequestFilter rf) {
+        logger.debug("getDataSetSeismogram: " + getTXID() + "  " + ChannelIdUtil.toStringNoDates(chan) + "  " + event
+                + "  " + rf.start_time.date_time + "  " + rf.end_time.date_time);
+        PrintIfNotCalledOff delayMessage = new PrintIfNotCalledOff(" long running getDataSetSeismogram: " + getTXID()
+                + "  " + ChannelIdUtil.toStringNoDates(chan) + "  " + event + "  " + rf.start_time.date_time + "  "
+                + rf.end_time.date_time);
+        List<EventSeismogramFileReference> esRefList = getSeismogramsForEventForChannel(event, chan);
         List<URL> urlList = new ArrayList<URL>();
         List<SeismogramFileTypes> ftList = new ArrayList<SeismogramFileTypes>();
         for (EventSeismogramFileReference esRef : esRefList) {
@@ -89,7 +97,8 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
             try {
                 ftList.add(SeismogramFileTypes.fromInt(esRef.getFileType()));
             } catch(UnsupportedFileTypeException e) {
-                throw new RuntimeException("Should not happen as only valid file types should get into the db via hibernate.", e);
+                throw new RuntimeException("Should not happen as only valid file types should get into the db via hibernate.",
+                                           e);
             }
         }
         delayMessage.callOff();
@@ -100,21 +109,15 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
                                         rf);
     }
 
-    public RequestFilter[] findMatchingSeismograms(RequestFilter[] requestArray,
-                                                   boolean ignoreNetworkTimes) {
-        List results = queryDatabaseForSeismograms(requestArray,
-                                                   false,
-                                                   ignoreNetworkTimes);
+    public RequestFilter[] findMatchingSeismograms(RequestFilter[] requestArray, boolean ignoreNetworkTimes) {
+        List results = queryDatabaseForSeismograms(requestArray, false, ignoreNetworkTimes);
         RequestFilter[] request = (RequestFilter[])results.toArray(new RequestFilter[results.size()]);
         RequestFilter[] reduced = ReduceTool.merge(request);
         return reduced;
     }
 
-    public LocalSeismogramImpl[] getMatchingSeismograms(RequestFilter[] requestArray,
-                                                    boolean ignoreNetworkTimes) {
-        List results = queryDatabaseForSeismograms(requestArray,
-                                                   true,
-                                                   ignoreNetworkTimes);
+    public LocalSeismogramImpl[] getMatchingSeismograms(RequestFilter[] requestArray, boolean ignoreNetworkTimes) {
+        List results = queryDatabaseForSeismograms(requestArray, true, ignoreNetworkTimes);
         LocalSeismogramImpl[] seis = (LocalSeismogramImpl[])results.toArray(new LocalSeismogramImpl[0]);
         LocalSeismogramImpl[] reduced = ReduceTool.merge(seis);
         return reduced;
@@ -125,11 +128,8 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
                                             boolean ignoreNetworkTimes) {
         RequestFilter[] minimalRequest = ReduceTool.merge(request);
         List resultCollector = new ArrayList();
-        for(int i = 0; i < minimalRequest.length; i++) {
-            queryDatabaseForSeismogram(resultCollector,
-                                       minimalRequest[i],
-                                       returnSeismograms,
-                                       ignoreNetworkTimes);
+        for (int i = 0; i < minimalRequest.length; i++) {
+            queryDatabaseForSeismogram(resultCollector, minimalRequest[i], returnSeismograms, ignoreNetworkTimes);
         }
         return resultCollector;
     }
@@ -144,7 +144,7 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
         ChannelImpl chanId;
         Cut cutter = new Cut(request);
         try {
-            if(ignoreNetworkTimes) {
+            if (ignoreNetworkTimes) {
                 chanId = chanTable.getChannel(request.channel_id.network_id.network_code,
                                               request.channel_id.station_code,
                                               request.channel_id.site_code,
@@ -174,44 +174,40 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
         q.setTimestamp("end", adjustedEndTime.getTimestamp());
         q.setTimestamp("begin", adjustedBeginTime.getTimestamp());
         List<SeismogramFileReference> databaseResults = q.list();
-        if(returnSeismograms) {
-            for(SeismogramFileReference seisRef : databaseResults) {
+        if (returnSeismograms) {
+            for (SeismogramFileReference seisRef : databaseResults) {
                 try {
                     File seismogramFile = new File(seisRef.getFilePath());
                     SeismogramFileTypes filetype = SeismogramFileTypes.fromInt(seisRef.getFileType());
                     LocalSeismogramImpl[] curSeis;
-                    if(filetype.equals(SeismogramFileTypes.RT_130)) {
+                    if (filetype.equals(SeismogramFileTypes.RT_130)) {
                         List refTekSeis = getMatchingSeismogramsFromRefTek(seismogramFile.getCanonicalPath(),
                                                                            request.channel_id,
                                                                            adjustedBeginTime,
                                                                            adjustedEndTime);
                         curSeis = (LocalSeismogramImpl[])refTekSeis.toArray(new LocalSeismogramImpl[refTekSeis.size()]);
                     } else {
-                        URLDataSetSeismogram urlSeis = new URLDataSetSeismogram(seismogramFile.toURL(),
-                                                                                filetype);
+                        URLDataSetSeismogram urlSeis = new URLDataSetSeismogram(seismogramFile.toURL(), filetype);
                         curSeis = urlSeis.getSeismograms();
                     }
-                    for(int j = 0; j < curSeis.length; j++) {
+                    for (int j = 0; j < curSeis.length; j++) {
                         LocalSeismogramImpl seis = cutter.applyEncoded(curSeis[j]);
-                        if(seis != null) {
+                        if (seis != null) {
                             resultCollector.add(seis);
                         }
                     }
                 } catch(Exception e) {
                     GlobalExceptionHandler.handle("Problem occured while returning seismograms from the database."
-                                                          + "\n"
-                                                          + "The problem file is located at "
-                                                          + seisRef.getFilePath(),
-                                                  e);
+                            + "\n" + "The problem file is located at " + seisRef.getFilePath(), e);
                 }
             }
         } else {
-            for(SeismogramFileReference seisRef : databaseResults) {
+            for (SeismogramFileReference seisRef : databaseResults) {
                 RequestFilter req = new RequestFilter(chanIdxxx,
                                                       new MicroSecondDate(seisRef.getBeginTime()).getFissuresTime(),
                                                       new MicroSecondDate(seisRef.getEndTime()).getFissuresTime());
                 req = cutter.apply(req);
-                if(req != null) {
+                if (req != null) {
                     resultCollector.add(req);
                 }
             }
@@ -221,20 +217,14 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
     private List getMatchingSeismogramsFromRefTek(String seismogramFile,
                                                   ChannelId chanId,
                                                   MicroSecondDate beginTime,
-                                                  MicroSecondDate endTime)
-            throws IOException, RT130FormatException {
+                                                  MicroSecondDate endTime) throws IOException, RT130FormatException {
         RT130FileReader toSeismogramDataPacket = new RT130FileReader();
         File file = new File(seismogramFile);
         String fileName = file.getName();
         String unitIdNumber = file.getParentFile().getParentFile().getName();
-        String yearAndDay = file.getParentFile()
-                .getParentFile()
-                .getParentFile()
-                .getName();
-        MicroSecondDate fileBeginTime = FileNameParser.getBeginTime(yearAndDay,
-                                                                    fileName);
-        fileBeginTime = LeapSecondApplier.applyLeapSecondCorrection(unitIdNumber,
-                                                                    fileBeginTime);
+        String yearAndDay = file.getParentFile().getParentFile().getParentFile().getName();
+        MicroSecondDate fileBeginTime = FileNameParser.getBeginTime(yearAndDay, fileName);
+        fileBeginTime = LeapSecondApplier.applyLeapSecondCorrection(unitIdNumber, fileBeginTime);
         // fileTimeWindow is calculated using the begin time from the file name
         // and end time from the request. This is not the typical, or an ideal
         // method. The typical method involves obtaining the begin time of the
@@ -249,15 +239,14 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
         RT130ToLocalSeismogram toSeismogram = new RT130ToLocalSeismogram();
         LocalSeismogramImpl[] seis = toSeismogram.convert(seismogramDataPacketArray);
         List matchingSeis = new ArrayList();
-        for(int i = 0; i < seis.length; i++) {
+        for (int i = 0; i < seis.length; i++) {
             // Check to make sure the seismograms returned fall within the
             // requested time window, and compare the channel code of the
             // channel requested with the channel code of the dummy channel
             // created during the above file reading. If the codes are equal,
             // set the dummy channel equal to the channel requested (real
             // channel).
-            if(seis[i].channel_id.channel_code.equals(chanId.channel_code)
-                    && seis[i].getBeginTime().before(endTime)
+            if (seis[i].channel_id.channel_code.equals(chanId.channel_code) && seis[i].getBeginTime().before(endTime)
                     && seis[i].getEndTime().after(beginTime)) {
                 seis[i].channel_id = chanId;
                 matchingSeis.add(seis[i]);
@@ -266,19 +255,19 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
         return matchingSeis;
     }
 
-
-    /** removes the seismogram reference from the database for the given file name. */
+    /**
+     * removes the seismogram reference from the database for the given file
+     * name.
+     */
     public int removeSeismogramFromDatabase(String seisFile) {
-        String query = "delete  "
-            + SeismogramFileReference.class.getName()
-            + " where filePath = "+seisFile;
+        String query = "delete  " + SeismogramFileReference.class.getName() + " where filePath = " + seisFile;
         return getSession().createQuery(query).executeUpdate();
     }
-    
+
     private static SeismogramFileRefDB singleton;
 
     public static SeismogramFileRefDB getSingleton() {
-        if(singleton == null) {
+        if (singleton == null) {
             singleton = new SeismogramFileRefDB();
         }
         return singleton;
@@ -286,8 +275,7 @@ public class SeismogramFileRefDB extends AbstractHibernateDB {
 
     protected NetworkDB chanTable = NetworkDB.getSingleton();
 
-    private static final TimeInterval ONE_SECOND = new TimeInterval(1,
-                                                                    UnitImpl.SECOND);
+    private static final TimeInterval ONE_SECOND = new TimeInterval(1, UnitImpl.SECOND);
 
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SeismogramFileRefDB.class);
 }
