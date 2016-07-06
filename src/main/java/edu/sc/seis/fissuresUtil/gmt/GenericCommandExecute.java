f6dfa206f1f45b8f04087df4153a8f7aa27aa377
Philip Crotwell <crotwell@seis.sc.edu>
2016-07-06 11:41:28 -0400

    new way to doing pipe, but only in comment until I can test

--- src/main/java/edu/sc/seis/fissuresUtil/gmt/GenericCommandExecute.java
+++ src/main/java/edu/sc/seis/fissuresUtil/gmt/GenericCommandExecute.java
@@ -39,6 +39,23 @@ public class GenericCommandExecute {
                               Writer stdout,
                               Writer stderr) throws InterruptedException,
             IOException {
+        /* ... new way...
+        ProcessBuilder pb =
+                new ProcessBuilder("myCommand", "myArg1", "myArg2");
+              Map<String, String> env = pb.environment();
+              env.put("VAR1", "myValue");
+              env.remove("OTHERVAR");
+              env.put("VAR2", env.get("VAR1") + "suffix");
+              pb.directory(new File("myDir"));
+              File log = new File("log");
+              pb.redirectErrorStream(true);
+              pb.redirectOutput(Redirect.appendTo(log));
+              Process p = pb.start();
+              assert pb.redirectInput() == Redirect.PIPE;
+              assert pb.redirectOutput().file() == log;
+              assert p.getInputStream().read() == -1;
+        */
+        
         Runtime rt = Runtime.getRuntime();
         // System.out.println("executing command: " + command);
         Process proc = rt.exec(command);
